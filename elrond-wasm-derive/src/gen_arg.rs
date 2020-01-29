09ad982ddffe94c81d52b5325ee6a42483a0db04
andrei-marinica <andrei_m_marinica@yahoo.com>
2020-01-29 10:35:45 +0200

    deduplicated arg type snippet generation

--- elrond-wasm-derive/src/gen_arg.rs
+++ elrond-wasm-derive/src/gen_arg.rs
@@ -20,6 +20,39 @@ pub fn generate_arg_call_name(arg: &syn::FnArg, arg_index: isize) -> Option<proc
     }
 }
 
+fn generate_snippet_for_arg_type_path(type_ident: &syn::Ident, pat: &syn::Pat, arg_index_i32: i32) -> proc_macro2::TokenStream {
+    let type_str = type_ident.to_string();
+    match type_str.as_str() {
+        "Address" =>
+            quote!{
+                let #pat: Address = self.api.get_argument_address(#arg_index_i32);
+            },
+        "Vec" =>
+            quote!{
+                let #pat: Vec<u8> = self.api.get_argument_vec(#arg_index_i32);
+            },
+        "BigInt" =>
+            quote!{
+                let #pat = self.api.get_argument_big_int_signed(#arg_index_i32);
+            },
+        "BI" =>
+            quote!{
+                let #pat = self.api.get_argument_big_int_signed(#arg_index_i32);
+            },
+        "BU" =>
+            quote!{
+                let #pat = self.api.get_argument_big_int_unsigned(#arg_index_i32);
+            },
+        "i64" =>
+            quote!{
+                let #pat: i64 = self.api.get_argument_i64(#arg_index_i32);
+            },
+        other_stype_str => {
+            panic!("Unsupported argument type {:?} for arg init snippet", other_stype_str)
+        }
+    }
+}
+
 pub fn generate_arg_init_snippet(arg: &syn::FnArg, arg_index: isize) -> proc_macro2::TokenStream {
     match arg {
         syn::FnArg::SelfRef(ref selfref) => {
@@ -34,36 +67,8 @@ pub fn generate_arg_init_snippet(arg: &syn::FnArg, arg_index: isize) -> proc_mac
             let arg_index_i32 = arg_index as i32;
             match ty {                
                 syn::Type::Path(type_path) => {
-                    let type_str = type_path.path.segments.last().unwrap().value().ident.to_string();
-                    match type_str.as_str() {
-                        "Address" =>
-                            quote!{
-                                let #pat: Address = self.api.get_argument_address(#arg_index_i32);
-                            },
-                        "Vec" =>
-                            quote!{
-                                let #pat: Vec<u8> = self.api.get_argument_vec(#arg_index_i32);
-                            },
-                        "BigInt" =>
-                            quote!{
-                                let #pat = self.api.get_argument_big_int_signed(#arg_index_i32);
-                            },
-                        "BI" =>
-                            quote!{
-                                let #pat = self.api.get_argument_big_int_signed(#arg_index_i32);
-                            },
-                        "BU" =>
-                            quote!{
-                                let #pat = self.api.get_argument_big_int_unsigned(#arg_index_i32);
-                            },
-                        "i64" =>
-                            quote!{
-                                let #pat: i64 = self.api.get_argument_i64(#arg_index_i32);
-                            },
-                        other_stype_str => {
-                            panic!("Unsupported argument type {:?} for arg init snippet", other_stype_str)
-                        }
-                    }
+                    let type_ident = type_path.path.segments.last().unwrap().value().ident.clone();
+                    generate_snippet_for_arg_type_path(&type_ident, pat, arg_index_i32)
                 },             
                 syn::Type::Reference(type_reference) => {
                     if type_reference.mutability != None {
@@ -71,38 +76,13 @@ pub fn generate_arg_init_snippet(arg: &syn::FnArg, arg_index: isize) -> proc_mac
                     }
                     match &*type_reference.elem {
                         syn::Type::Path(type_path) => {
-                            let type_str = type_path.path.segments.last().unwrap().value().ident.to_string();
-                            match type_str.as_str() {
-                                "Address" =>
-                                    quote!{
-                                        let #pat: Address = self.api.get_argument_address(#arg_index_i32);
-                                    },
-                                "Vec" =>
-                                    quote!{
-                                        let #pat: Vec<u8> = self.api.get_argument_vec(#arg_index_i32);
-                                    },
-                                "BigInt" =>
-                                    quote!{
-                                        let #pat: BigInt = self.api.get_argument_big_int_signed(#arg_index_i32);
-                                    },
-                                "BI" =>
-                                    quote!{
-                                        let #pat = self.api.get_argument_big_int_signed(#arg_index_i32);
-                                    },
-                                "BU" =>
-                                    quote!{
-                                        let #pat = self.api.get_argument_big_int_unsigned(#arg_index_i32);
-                                    },
-                                other_stype_str => {
-                                    panic!("Unsupported reference argument type: {:?}", other_stype_str)
-                                }
-                            }
+                            let type_ident = type_path.path.segments.last().unwrap().value().ident.clone();
+                            generate_snippet_for_arg_type_path(&type_ident, pat, arg_index_i32)
                         },
                         _ => {
                             panic!("Unsupported reference argument type, reference does not contain type path: {:?}", type_reference)
                         }
                     }
-                    
                 },
                 other_arg => panic!("Unsupported argument type: {:?}, neither path nor reference", other_arg)
 			}
