d2f9d877b69c233a70ec33186ea8c43d34972367
andrei-marinica <andrei.marinica@elrond.com>
2020-05-06 09:47:02 +0300

    snippets cleanup

--- elrond-wasm-derive/src/snippets.rs
+++ elrond-wasm-derive/src/snippets.rs
@@ -5,11 +5,11 @@ pub fn contract_imports() -> proc_macro2::TokenStream {
         use elrond_wasm::{H256, Address, StorageKey, ErrorMessage};
         use elrond_wasm::{ContractHookApi, ContractIOApi, BigIntApi, BigUintApi};
         use elrond_wasm::err_msg;
+        use elrond_wasm::serializer::{to_bytes, from_bytes};
         use core::ops::{Add, Sub, Mul, Div, Rem};
         use core::ops::{AddAssign, SubAssign, MulAssign, DivAssign, RemAssign};
         use core::ops::{BitAnd, BitOr, BitXor, Shr, Shl};
         use core::ops::{BitAndAssign, BitOrAssign, BitXorAssign, ShrAssign, ShlAssign};
-        use serde;
     }
 }
 
