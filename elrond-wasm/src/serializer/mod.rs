bfdaca07c46a1bbc269b3a8267458eba14f17dca
andrei-marinica <andrei.marinica@elrond.com>
2020-05-18 22:10:42 +0300

    fixed get argument small unsigned

--- elrond-wasm/src/serializer/mod.rs
+++ elrond-wasm/src/serializer/mod.rs
@@ -4,7 +4,7 @@ mod bytes_de;
 mod bytes_err;
 
 pub use bytes_ser::to_bytes;
-pub use bytes_de::from_bytes;
+pub use bytes_de::{from_bytes, bytes_to_number};
 pub use bytes_err::SDError;
 
 #[cfg(test)]
