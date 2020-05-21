45822e5d5762a1817aa01de6d9245cbc372c43bb
andrei-marinica <andrei.marinica@elrond.com>
2020-05-21 19:09:44 +0300

    contract modules

--- elrond-wasm-derive/src/contract_impl.rs
+++ elrond-wasm-derive/src/contract_impl.rs
@@ -1,19 +1,17 @@
 use super::*;
 use super::contract_gen::*;
 
-pub fn process_contract(
-    args: proc_macro::TokenStream,
-    input: proc_macro::TokenStream,
-) -> proc_macro::TokenStream {
-
-    let args_input = parse_macro_input!(args as syn::AttributeArgs);
-    let proc_input = &parse_macro_input!(input as syn::ItemTrait);
-
-    let contract = Contract::new(args_input, proc_input);
+pub fn contract_implementation(
+    contract: &Contract,
+) -> proc_macro2::TokenStream {
 
     let contract_impl_ident = contract.contract_impl_name.clone();
     let trait_name_ident = contract.trait_name.clone();
     let method_impls = contract.extract_method_impls();
+
+    if contract.supertrait_paths.len() > 0 {
+      panic!("contract inheritance currently not supported");
+    }
 
     let call_methods = contract.generate_call_methods();
     let auto_impl_defs = contract.generate_auto_impl_defs();
@@ -25,16 +23,18 @@ pub fn process_contract(
     let contract_imports = snippets::contract_imports();
     let api_where = snippets::api_where();
 
+    let supertrait_impls = contract.generate_supertrait_impls();
     let contract_trait_api_impl = snippets::contract_trait_api_impl(&contract_impl_ident);
 
     // this definition is common to release and debug mode
     let main_definition = quote! {
-      #[macro_use]
-      extern crate elrond_wasm;
 
       #contract_imports
 
-      pub trait #trait_name_ident<T, BigInt, BigUint>: ContractHookApi<BigInt, BigUint> + Sized 
+      pub trait #trait_name_ident<T, BigInt, BigUint>: 
+      ContractHookApi<BigInt, BigUint>
+      // #( + #supertrait_paths <T, BigInt, BigUint>)* // currently not supported
+      + Sized 
       #api_where
       {
         #(#method_impls)*
@@ -77,6 +77,8 @@ pub fn process_contract(
 
       #contract_trait_api_impl
 
+      #(#supertrait_impls)*
+
       impl <T, BigInt, BigUint> #trait_name_ident<T, BigInt, BigUint> for #contract_impl_ident<T, BigInt, BigUint> 
       #api_where
       {
@@ -107,7 +109,7 @@ pub fn process_contract(
 
     if wasm32_mode() {
       // release mode adds endpoints for wasmer 
-      proc_macro::TokenStream::from(quote! {
+      quote! {
         #main_definition
 
         use elrond_wasm_node::{ArwenBigInt, ArwenBigUint};
@@ -119,17 +121,11 @@ pub fn process_contract(
         }
 
         #(#endpoints)*
-
-        #[no_mangle]
-        pub fn callBack () {
-          let inst = new_arwen_instance();
-          inst.callback();
-        }
-      })
+      }
     } else {
       // debug mode adds the contract interface, that we use for the mocks
       // this interface also relies on "call" methods with no parameter and a function selector
-      proc_macro::TokenStream::from(quote! {
+      quote! {
         #main_definition
   
         use elrond_wasm::CallableContract;
@@ -144,6 +140,6 @@ pub fn process_contract(
             Box::new(#contract_impl_ident::new(self.api.clone()))
           }
         }
-      })
+      }
     }
 }
