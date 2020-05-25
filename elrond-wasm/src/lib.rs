2f892d15383987687912408d601d013e211838d1
andrei-marinica <andrei.marinica@elrond.com>
2020-05-25 08:27:26 +0300

    imports macro

--- elrond-wasm/src/lib.rs
+++ elrond-wasm/src/lib.rs
@@ -348,3 +348,19 @@ macro_rules! contract_proxy {
       $s.contract_proxy($address) as Box<dyn $proxy_trait<BigInt, BigUint>>
   };
 }
+
+/// Getting all imports needed for a smart contract.
+#[macro_export]
+macro_rules! imports {
+    () => {
+        use elrond_wasm::{Box, Vec, String};
+        use elrond_wasm::{H256, Address, StorageKey, ErrorMessage};
+        use elrond_wasm::{ContractHookApi, ContractIOApi, BigIntApi, BigUintApi, OtherContractHandle, AsyncCallResult, AsyncCallError};
+        use elrond_wasm::err_msg;
+        use elrond_wasm::serde as serde;
+        use core::ops::{Add, Sub, Mul, Div, Rem};
+        use core::ops::{AddAssign, SubAssign, MulAssign, DivAssign, RemAssign};
+        use core::ops::{BitAnd, BitOr, BitXor, Shr, Shl};
+        use core::ops::{BitAndAssign, BitOrAssign, BitXorAssign, ShrAssign, ShlAssign};
+  };
+}
