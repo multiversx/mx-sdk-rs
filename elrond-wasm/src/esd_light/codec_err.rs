354657539a1eddc18cc1ad345b51a25737d81370
andrei-marinica <andrei.marinica@elrond.com>
2020-05-26 23:33:43 +0300

    fixed typo

--- elrond-wasm/src/esd_light/codec_err.rs
+++ elrond-wasm/src/esd_light/codec_err.rs
@@ -12,7 +12,7 @@ pub enum DeError {
 impl DeError {
     pub fn message_bytes(&self) -> &'static [u8] {
         match self {
-            DeError::InputTooShort => &b"input too stort"[..],
+            DeError::InputTooShort => &b"input too short"[..],
             DeError::InputTooLong => &b"input too long"[..],
             DeError::InvalidValue => &b"invalid value"[..],
             DeError::Custom(msg) => msg,
