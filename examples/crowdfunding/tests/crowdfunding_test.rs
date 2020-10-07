93a21af926fedaa44f2addada48e55091c98e479
dorin.iancu <dorin.iancu@elrond.com>
2020-10-07 16:11:06 +0300

    removed commented code

--- examples/crowdfunding/tests/crowdfunding_test.rs
+++ examples/crowdfunding/tests/crowdfunding_test.rs
@@ -13,22 +13,3 @@ fn test_status()
     assert_eq!(5, cf.get_deadline());
     assert_eq!(Status::FundingPeriod.to_u8(), cf.status().to_u8());
 }
-
-/*extern crate adder;
-use adder::*;
-use elrond_wasm_debug::*;
-
-#[test]
-fn test_add() {
-    let adder = AdderImpl::new(TxContext::dummy());
-
-    adder.init(&RustBigInt::from(5));
-    assert_eq!(RustBigInt::from(5), *adder.get_mut_sum());
-
-    let _ = adder.add(&RustBigInt::from(7));
-    assert_eq!(RustBigInt::from(12), *adder.get_mut_sum());
-
-    let _ = adder.add(&RustBigInt::from(1));
-    assert_eq!(RustBigInt::from(13), *adder.get_mut_sum());
-}
-*/
