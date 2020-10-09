cb67e2e1ffbdee4b7215678a9d15f360687435d3
Camil Băncioiu <ioan.camil.bancioiu@gmail.com>
2020-10-09 16:11:31 +0300

    Add API for ESDT

--- elrond-wasm/src/lib.rs
+++ elrond-wasm/src/lib.rs
@@ -81,6 +81,10 @@ where
     
     fn get_call_value_big_uint(&self) -> BigUint;
 
+    fn get_esdt_value_big_uint(&self) -> BigUint;
+
+    fn get_esdt_token_name(&self) -> Option<Vec<u8>>;
+
     fn send_tx(&self, to: &Address, amount: &BigUint, message: &str);
 
     fn async_call(&self, to: &Address, amount: &BigUint, data: &[u8]);
