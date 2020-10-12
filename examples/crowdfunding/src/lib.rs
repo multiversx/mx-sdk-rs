262adf171c6bed395f7993d7992b8c91e67856aa
dorin.iancu <dorin.iancu@elrond.com>
2020-10-12 15:42:47 +0300

    crowdfunding fix + additional tests and added optional owner field for accounts

--- examples/crowdfunding/src/lib.rs
+++ examples/crowdfunding/src/lib.rs
@@ -84,7 +84,7 @@ pub trait Crowdfunding {
             Status::Successful => {
                 let caller = self.get_caller();
                 if &caller != &self.get_owner() {
-                    return sc_error!("only owner can claim succesful funding");
+                    return sc_error!("only owner can claim successful funding");
                 }
                 self.send_tx(&caller, &self.get_sc_balance(), "funding success");
                 Ok(())
@@ -94,6 +94,7 @@ pub trait Crowdfunding {
                 let deposit = self.get_deposit(&caller);
                 if &deposit > &0 {
                     self.send_tx(&caller, &deposit, "reclaim failed funding");
+                    self.set_deposit(&caller, &BigUint::zero());
                 }
                 Ok(())
             },
