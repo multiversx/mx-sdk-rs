bb049ee5041e79e474c276590383de619a85ab01
dorin.iancu <dorin.iancu@elrond.com>
2020-10-14 14:20:06 +0300

    implementation, not tested yet

--- examples/crowdfunding-erc20/src/lib.rs
+++ examples/crowdfunding-erc20/src/lib.rs
@@ -1,40 +1,51 @@
 
 #![no_std]
 #![allow(unused_attributes)]
+#![allow(unused_variables)]
 
 imports!();
 
-#[derive(PartialEq, Clone, Copy)]
-pub enum Status {
-    FundingPeriod,
-    Successful,
-    Failed
+#[elrond_wasm_derive::callable(Erc20Proxy)]
+pub trait Erc20 {
+    #[callback(transfer_from_callback)]
+    fn transferFrom(&self,
+        #[callback_arg] sender: Address,
+        recipient: Address,
+        #[callback_arg] amount: BigUint);
+
+    fn transfer(&self, to: Address, amount: BigUint);
 }
 
 #[elrond_wasm_derive::contract(CrowdfundingImpl)]
 pub trait Crowdfunding {
 
     #[init]
-    fn init(&self, target: &BigUint, deadline: u64) {
+    fn init(&self, target: &BigUint, deadline: u64, erc20_contract_address: Address) {
         let my_address : Address = self.get_caller();
+
         self.set_owner(&my_address);
+        self.set_erc20_contract_address(&erc20_contract_address);
         self.set_target(target);
         self.set_deadline(deadline);
     }
 
-    #[payable]
     #[endpoint]
-    fn fund(&self, #[payment] payment: &BigUint) -> SCResult<()> {
+    fn fund(&self, amount: BigUint) -> SCResult<()> {
         if self.get_block_nonce() > self.get_deadline() {
             return sc_error!("cannot fund after deadline");
         }
 
         let caller = self.get_caller();
-        let mut deposit = self.get_deposit(&caller);
-        deposit += payment;
-        self.set_deposit(&caller, &deposit);
+        let erc20_address = self.get_erc20_contract_address();
+        let cf_contract_address = self.get_sc_address();
 
-        return Ok(());
+        let erc20_proxy = contract_proxy!(self, &erc20_address, Erc20);
+        erc20_proxy.transferFrom(
+            caller,
+            cf_contract_address,
+            amount);
+
+        Ok(())
     }
 
     #[view]    
@@ -59,18 +70,60 @@ pub trait Crowdfunding {
                 if &caller != &self.get_owner() {
                     return sc_error!("only owner can claim successful funding");
                 }
-                self.send_tx(&caller, &self.get_sc_balance(), "funding success");
+
+                let erc20_address = self.get_erc20_contract_address();
+                let erc20_proxy = contract_proxy!(self, &erc20_address, Erc20);
+                let balance = self.get_mut_total_balance();
+
+                erc20_proxy.transfer( caller, balance.clone());
+
                 Ok(())
             },
             Status::Failed => {
                 let caller = self.get_caller();
                 let deposit = self.get_deposit(&caller);
+
                 if &deposit > &0 {
-                    self.send_tx(&caller, &deposit, "reclaim failed funding");
+                    let erc20_address = self.get_erc20_contract_address();
+                    let erc20_proxy = contract_proxy!(self, &erc20_address, Erc20);
+                    
+                    erc20_proxy.transfer( caller.clone(), deposit);
                     self.set_deposit(&caller, &BigUint::zero());
                 }
+
                 Ok(())
             },
+        }
+    }
+
+    #[callback]
+    fn transfer_from_callback(&self, 
+        result: AsyncCallResult<()>,
+        #[callback_arg] sender: Address,
+        #[callback_arg] amount: BigUint) {
+
+        // transaction started before deadline, ended after -> refund
+        if self.get_block_nonce() > self.get_deadline() {
+            let erc20_address = self.get_erc20_contract_address();
+            let erc20_proxy = contract_proxy!(self, &erc20_address, Erc20);
+            
+            erc20_proxy.transfer( sender, amount);
+
+            return;
+        }
+
+        match result {
+            AsyncCallResult::Ok(()) => {
+                let mut deposit = self.get_deposit(&sender);
+                let mut balance = self.get_mut_total_balance();
+                deposit += amount.clone();
+                *balance += amount.clone();
+
+                self.set_deposit(&sender, &deposit);
+            },
+            AsyncCallResult::Err(_) => {
+                
+            }
         }
     }
 
@@ -101,9 +154,27 @@ pub trait Crowdfunding {
     #[view]
     #[storage_get("deposit")]
     fn get_deposit(&self, donor: &Address) -> BigUint;
+
+    #[storage_set("erc20_contract_address")]
+    fn set_erc20_contract_address(&self, address: &Address);
+
+    #[view]
+    #[storage_get("erc20_contract_address")]
+    fn get_erc20_contract_address(&self) -> Address;
+
+    #[view]
+    #[storage_get_mut("total_balance")]
+    fn get_mut_total_balance(&self) -> mut_storage!(BigUint);
 }
 
 use elrond_wasm::elrond_codec::*;
+
+#[derive(PartialEq, Clone, Copy)]
+pub enum Status {
+    FundingPeriod,
+    Successful,
+    Failed
+}
 
 impl Status {
     pub fn to_u8(&self) -> u8 {
---
+++ examples/crowdfunding-erc20/tests/crowdfunding_test.rs
@@ -0,0 +1,15 @@
+
+/*extern crate crowdfunding_erc20;
+use crowdfunding_erc20::*;
+use elrond_wasm_debug::*;
+
+#[test]
+fn test_status() {
+    let cf = CrowdfundingImpl::new(TxContext::dummy());
+
+    cf.init(&RustBigUint::from(100u32), 5);
+    assert_eq!(RustBigUint::from(100u32), cf.get_target());
+    assert_eq!(5, cf.get_deadline());
+    assert_eq!(Status::FundingPeriod.to_u8(), cf.status().to_u8());
+}
+*/
