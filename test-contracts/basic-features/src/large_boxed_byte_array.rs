ce0226f638cfcbae740e8ca0fefb321090095181
andrei-marinica <andrei.marinica@elrond.com>
2020-10-20 02:04:57 +0300

    basic features structures test

---
+++ test-contracts/basic-features/src/large_boxed_byte_array.rs
@@ -0,0 +1,34 @@
+use elrond_wasm::Box;
+use elrond_wasm::elrond_codec::*;
+
+const ARRAY_SIZE: usize = 512;
+
+pub struct LargeBoxedByteArray(Box<[u8; ARRAY_SIZE]>);
+
+impl NestedEncode for LargeBoxedByteArray {
+    #[inline]
+    fn dep_encode_to<O: OutputBuffer>(&self, dest: &mut O) -> Result<(), EncodeError> {
+        self.0.dep_encode_to(dest)
+    }
+}
+
+impl TopEncode for LargeBoxedByteArray {
+    #[inline]
+    fn top_encode<'o, B: OutputBuffer, O: TopEncodeOutput<'o, B>>(&self, output: O) -> Result<(), EncodeError> {
+        self.0.top_encode(output)
+    }
+}
+
+impl NestedDecode for LargeBoxedByteArray {
+    #[inline]
+    fn dep_decode<I: Input>(input: &mut I) -> Result<Self, DecodeError> {
+        Ok(LargeBoxedByteArray(Box::<[u8; ARRAY_SIZE]>::dep_decode(input)?))
+    }
+}
+
+impl TopDecode for LargeBoxedByteArray {
+    #[inline]
+    fn top_decode<I: TopDecodeInput>(input: I) -> Result<Self, DecodeError> {
+        Ok(LargeBoxedByteArray(Box::<[u8; ARRAY_SIZE]>::top_decode(input)?))
+    }
+}
