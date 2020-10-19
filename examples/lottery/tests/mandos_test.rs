8a8e176eb49be8a2d4235d8142e1fc1eb47dc173
dorin.iancu <dorin.iancu@elrond.com>
2020-10-19 12:26:09 +0300

    lottery implementation - untested

---
+++ examples/lottery/tests/mandos_test.rs
@@ -0,0 +1,19 @@
+
+/*extern crate lottery;
+use lottery::*;
+use elrond_wasm::*;
+use elrond_wasm_debug::*;
+
+fn contract_map() -> ContractMap<TxContext> {
+    let mut contract_map = ContractMap::new();
+    contract_map.register_contract(
+        "file:../output/lottery.wasm",
+        Box::new(|context| Box::new(LotteryImpl::new(context))));
+    contract_map
+}
+
+#[test]
+fn test_mandos() {
+    //parse_execute_mandos("mandos/adder.scen.json", &contract_map());
+}
+*/
