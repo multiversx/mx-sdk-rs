0a1a1891511bdc465c6f68d3e04c4e0d987536c8
dorin.iancu <dorin.iancu@elrond.com>
2020-10-30 09:08:46 +0200

    removed trash files

--- examples/lottery-esdt/wasm/target/release/build/proc-macro2-fdd02956611eb695/build_script_build-fdd02956611eb695.d
+++
@@ -1,5 +0,0 @@
-/home/elrond/elrond-wasm-rs/examples/lottery-esdt/wasm/target/release/build/proc-macro2-fdd02956611eb695/build_script_build-fdd02956611eb695: /home/elrond/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.24/build.rs
-
-/home/elrond/elrond-wasm-rs/examples/lottery-esdt/wasm/target/release/build/proc-macro2-fdd02956611eb695/build_script_build-fdd02956611eb695.d: /home/elrond/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.24/build.rs
-
-/home/elrond/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/proc-macro2-1.0.24/build.rs:
--- examples/lottery-esdt/wasm/target/wasm32-unknown-unknown/release/deps/cfg_if-31734eb0c4049b32.d
+++
@@ -1,7 +0,0 @@
-/home/elrond/elrond-wasm-rs/examples/lottery-esdt/wasm/target/wasm32-unknown-unknown/release/deps/cfg_if-31734eb0c4049b32.rmeta: /home/elrond/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs
-
-/home/elrond/elrond-wasm-rs/examples/lottery-esdt/wasm/target/wasm32-unknown-unknown/release/deps/libcfg_if-31734eb0c4049b32.rlib: /home/elrond/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs
-
-/home/elrond/elrond-wasm-rs/examples/lottery-esdt/wasm/target/wasm32-unknown-unknown/release/deps/cfg_if-31734eb0c4049b32.d: /home/elrond/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs
-
-/home/elrond/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/cfg-if-0.1.10/src/lib.rs:
