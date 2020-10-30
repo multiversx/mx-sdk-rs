c76cf6f8f214fd074417db17284b2e20617ff595
dorin.iancu <dorin.iancu@elrond.com>
2020-10-30 11:45:29 +0200

    partial impl part2

--- mandos/src/account.rs
+++ mandos/src/account.rs
@@ -7,7 +7,7 @@ pub struct Account {
     pub nonce: U64Value,
     pub balance: BigUintValue,
     pub storage: BTreeMap<BytesKey, BytesValue>,
-    pub esdt: Option<BTreeMap<BytesKey, BytesValue>>,
+    pub esdt: Option<BTreeMap<BytesKey, BigUintValue>>,
     pub code: Option<BytesValue>,
 }
 
@@ -22,7 +22,7 @@ impl InterpretableFrom<AccountRaw> for Account {
                 BytesValue::interpret_from(v, context))).collect(),
             esdt: from.esdt.map(|tree| tree.into_iter().map(|(k, v)| (
                 BytesKey::interpret_from(k, context), 
-                BytesValue::interpret_from(v, context))).collect()),
+                BigUintValue::interpret_from(v, context))).collect()),
             code: from.code.map(|c| BytesValue::interpret_from(c, context)),
         }
     }
