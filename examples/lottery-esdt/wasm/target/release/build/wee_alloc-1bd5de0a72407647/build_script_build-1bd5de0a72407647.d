80a2a8d97ab4ae3c65d88975001c12ae9d3b285d
dorin.iancu <dorin.iancu@elrond.com>
2020-10-30 09:08:46 +0200

    removed trash files

--- examples/lottery-esdt/wasm/target/release/build/wee_alloc-1bd5de0a72407647/build_script_build-1bd5de0a72407647.d
+++
@@ -1,5 +0,0 @@
-/home/elrond/elrond-wasm-rs/examples/lottery-esdt/wasm/target/release/build/wee_alloc-1bd5de0a72407647/build_script_build-1bd5de0a72407647: /home/elrond/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/wee_alloc-0.4.5/build.rs
-
-/home/elrond/elrond-wasm-rs/examples/lottery-esdt/wasm/target/release/build/wee_alloc-1bd5de0a72407647/build_script_build-1bd5de0a72407647.d: /home/elrond/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/wee_alloc-0.4.5/build.rs
-
-/home/elrond/elrondsdk/vendor-rust/registry/src/github.com-1ecc6299db9ec823/wee_alloc-0.4.5/build.rs:
--- examples/lottery-esdt/wasm/target/wasm32-unknown-unknown/release/deps/lottery_esdt_wasm.d
+++
@@ -1,5 +0,0 @@
-/home/elrond/elrond-wasm-rs/examples/lottery-esdt/wasm/target/wasm32-unknown-unknown/release/deps/lottery_esdt_wasm.wasm: src/lib.rs
-
-/home/elrond/elrond-wasm-rs/examples/lottery-esdt/wasm/target/wasm32-unknown-unknown/release/deps/lottery_esdt_wasm.d: src/lib.rs
-
-src/lib.rs:
