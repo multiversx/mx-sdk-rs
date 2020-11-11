188b4366c7bdf732349ab32c5b1655c73951a8db
andrei-marinica <andrei.marinica@elrond.com>
2020-11-11 16:23:27 +0200

    removed accidentally committed target

--- contracts/modules/elrond-wasm-module-features/target/rls/debug/deps/unicode_xid-460f9c27fbb1b178.d
+++
@@ -1,8 +0,0 @@
-/home/andreim/elrond/newsc/sc-delegation-rs/features-module/target/rls/debug/deps/unicode_xid-460f9c27fbb1b178.rmeta: /home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/lib.rs /home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/tables.rs
-
-/home/andreim/elrond/newsc/sc-delegation-rs/features-module/target/rls/debug/deps/libunicode_xid-460f9c27fbb1b178.rlib: /home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/lib.rs /home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/tables.rs
-
-/home/andreim/elrond/newsc/sc-delegation-rs/features-module/target/rls/debug/deps/unicode_xid-460f9c27fbb1b178.d: /home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/lib.rs /home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/tables.rs
-
-/home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/lib.rs:
-/home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-xid-0.2.1/src/tables.rs:
