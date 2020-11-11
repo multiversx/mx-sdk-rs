6433bd53d16b1f1d812d01f9565d5dc58b6a5589
andrei-marinica <andrei.marinica@elrond.com>
2020-11-11 10:02:59 +0200

    removed target folder

--- contracts/modules/elrond-wasm-module-features/target/rls/debug/deps/features_module-635771e7e0668034.d
+++
@@ -1,5 +0,0 @@
-/home/andreim/elrond/newsc/sc-delegation-rs/features-module/target/rls/debug/deps/features_module-635771e7e0668034.rmeta: src/lib.rs
-
-/home/andreim/elrond/newsc/sc-delegation-rs/features-module/target/rls/debug/deps/features_module-635771e7e0668034.d: src/lib.rs
-
-src/lib.rs:
--- contracts/modules/elrond-wasm-module-features/target/rls/debug/deps/features_module-b230d4b267f40610.d
+++
@@ -1,5 +0,0 @@
-/home/andreim/elrond/newsc/sc-delegation-rs/features-module/target/rls/debug/deps/features_module-b230d4b267f40610.rmeta: src/lib.rs
-
-/home/andreim/elrond/newsc/sc-delegation-rs/features-module/target/rls/debug/deps/features_module-b230d4b267f40610.d: src/lib.rs
-
-src/lib.rs:
