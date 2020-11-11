758df20c8db5f49af4e4bb4b9f5f1c2d267a088f
andrei-marinica <andrei.marinica@elrond.com>
2020-11-11 10:02:59 +0200

    removed target folder

--- contracts/modules/elrond-wasm-module-features/target/rls/debug/build/libc-b06d10b2cc4b7e02/build_script_build-b06d10b2cc4b7e02.d
+++
@@ -1,5 +0,0 @@
-/home/andreim/elrond/newsc/sc-delegation-rs/features-module/target/rls/debug/build/libc-b06d10b2cc4b7e02/build_script_build-b06d10b2cc4b7e02: /home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.73/build.rs
-
-/home/andreim/elrond/newsc/sc-delegation-rs/features-module/target/rls/debug/build/libc-b06d10b2cc4b7e02/build_script_build-b06d10b2cc4b7e02.d: /home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.73/build.rs
-
-/home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.73/build.rs:
--- contracts/modules/elrond-wasm-module-features/target/rls/debug/build/wee_alloc-aadaa8e269158506/build_script_build-aadaa8e269158506.d
+++
@@ -1,5 +0,0 @@
-/home/andreim/elrond/newsc/sc-delegation-rs/features-module/target/rls/debug/build/wee_alloc-aadaa8e269158506/build_script_build-aadaa8e269158506: /home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/wee_alloc-0.4.5/build.rs
-
-/home/andreim/elrond/newsc/sc-delegation-rs/features-module/target/rls/debug/build/wee_alloc-aadaa8e269158506/build_script_build-aadaa8e269158506.d: /home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/wee_alloc-0.4.5/build.rs
-
-/home/andreim/.cargo/registry/src/github.com-1ecc6299db9ec823/wee_alloc-0.4.5/build.rs:
