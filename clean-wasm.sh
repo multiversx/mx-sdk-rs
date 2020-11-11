ae585011bdfaad60b977a0f6cff679493b8c6070
andrei-marinica <andrei.marinica@elrond.com>
2020-11-11 09:47:57 +0200

    script improvements

--- clean-wasm.sh
+++ clean-wasm.sh
@@ -2,30 +2,17 @@
 
 # cleans all wasm targets
 
-cd contracts/examples/adder/wasm
-cargo clean
-cd ../../../..
-
-cd contracts/examples/crypto-bubbles/wasm
-cargo clean
-cd ../../../..
-
-cd contracts/examples/factorial/wasm
-cargo clean
-cd ../../../..
-
-cd contracts/examples/simple-erc20/wasm
-cargo clean
-cd ../../../..
-
-cd contracts/feature-tests/basic-features/wasm
-cargo clean
-cd ../../../..
-
-cd contracts/feature-tests/async/async-alice/wasm
-cargo clean
-cd ../../../../..
-
-cd contracts/feature-tests/async/async-bob/wasm
-cargo clean
-cd ../../../../..
+erdpy --verbose contract clean "contracts/benchmarks/str-repeat"
+erdpy --verbose contract clean "contracts/examples/adder"
+erdpy --verbose contract clean "contracts/examples/crowdfunding-egld"
+erdpy --verbose contract clean "contracts/examples/crowdfunding-erc20"
+erdpy --verbose contract clean "contracts/examples/crowdfunding-esdt"
+erdpy --verbose contract clean "contracts/examples/crypto-bubbles"
+erdpy --verbose contract clean "contracts/examples/factorial"
+erdpy --verbose contract clean "contracts/examples/lottery-egld"
+erdpy --verbose contract clean "contracts/examples/lottery-erc20"
+erdpy --verbose contract clean "contracts/examples/simple-erc20"
+erdpy --verbose contract clean "contracts/feature-tests/basic-features"
+erdpy --verbose contract clean "contracts/feature-tests/async/async-alice"
+erdpy --verbose contract clean "contracts/feature-tests/async/async-bob"
+erdpy --verbose contract clean "contracts/feature-tests/use-module"
