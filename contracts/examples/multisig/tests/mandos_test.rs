0abbd3adca06fe237a1c80fb50eca026270aa7d3
andrei-marinica <andrei.marinica@elrond.com>
2020-11-17 09:34:44 +0200

    multisig - more mandos tests

--- contracts/examples/multisig/tests/mandos_test.rs
+++ contracts/examples/multisig/tests/mandos_test.rs
@@ -6,13 +6,23 @@ use multisig::*;
 fn contract_map() -> ContractMap<TxContext> {
 	let mut contract_map = ContractMap::new();
 	contract_map.register_contract(
-		"file:../output/multisig.wasm",
+		"file:../../output/multisig.wasm",
 		Box::new(|context| Box::new(MultisigImpl::new(context))),
 	);
 	contract_map
 }
 
 #[test]
-fn test_mandos() {
-	parse_execute_mandos("mandos/deploy.scen.json", &contract_map());
+fn test_change_board() {
+	parse_execute_mandos("mandos/changeBoard.scen.json", &contract_map());
+}
+
+#[test]
+fn test_change_quorum() {
+	parse_execute_mandos("mandos/changeQuorum.scen.json", &contract_map());
+}
+
+#[test]
+fn test_change_quorum_too_big() {
+	parse_execute_mandos("mandos/changeQuorum_tooBig.scen.json", &contract_map());
 }
