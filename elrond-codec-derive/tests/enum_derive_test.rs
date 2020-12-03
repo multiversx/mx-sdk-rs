359ba507a3c41d70c16085b5daae1e21582cc550
dorin.iancu <dorin.iancu@elrond.com>
2020-12-03 11:51:35 +0200

    added simple enum support

---
+++ elrond-codec-derive/tests/enum_derive_test.rs
@@ -0,0 +1,18 @@
+extern crate elrond_codec_derive;
+use elrond_codec_derive::*;
+
+use elrond_codec::*;
+
+// to test, run the following command in elrond-codec-derive folder:
+// cargo expand --test enum_derive_test > expanded.rs
+
+#[derive(TopEncode, TopDecode)]
+enum DayOfWeek {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
