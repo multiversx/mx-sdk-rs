1e67e48ea38faf5bded95f50e6c50b2a3866a656
dorin.iancu <dorin.iancu@elrond.com>
2020-12-17 17:16:10 +0200

    bidding

--- contracts/examples/crypto-kitties/kitty-auction/src/lib.rs
+++ contracts/examples/crypto-kitties/kitty-auction/src/lib.rs
@@ -39,6 +39,7 @@ pub trait KittyAuction {
 		starting_price: BigUint, ending_price: BigUint, deadline: u64) -> SCResult<()> {
 
 		require!(!self.is_up_for_auction(kitty_id), "kitty already auctioned!");
+		require!(starting_price > 0, "starting price must be higher than 0!");
 		require!(starting_price < ending_price, 
 			"starting price must be less than ending price!");
 		require!(deadline < self.get_block_timestamp(), 
@@ -53,6 +54,37 @@ pub trait KittyAuction {
 
 		Ok(())
 	}
+
+	#[payable]
+	#[endpoint]
+	fn bid(&self, kitty_id: u32, #[payment] payment: BigUint) -> SCResult<()> {
+		require!(self.is_up_for_auction(kitty_id), "Kitty is not up for auction!");
+
+		let caller = self.get_caller();
+		let mut auction = self.get_auction(kitty_id);
+
+		require!(caller != auction.kitty_owner, "can't bid on your own kitty!");
+		require!(self.get_block_timestamp() < auction.deadline,
+			"auction ended already!");
+		require!(payment >= auction.starting_price, 
+			"bid amount must be higher than or equal to starting price!");
+		require!(payment > auction.current_bid, 
+			"bid amount must be higher than current winning bid!");
+		require!(payment <= auction.ending_price,
+			"bid amount must be less than or equal to ending price!");
+
+		// refund losing bid
+		if auction.current_winner != Address::zero() {
+			self.send_tx(&auction.current_winner, &auction.current_bid, "bid refund");
+		}
+
+		// update auction bid and winner
+		auction.current_bid = payment;
+		auction.current_winner = caller;
+		self.set_auction(kitty_id, &auction);
+
+		Ok(())
+	} // TO DO: bidding completion logic
 
 	// callbacks
 
