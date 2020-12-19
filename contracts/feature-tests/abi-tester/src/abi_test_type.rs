ad8f925001046de6a5a6e1fe0175b409a285e5a6
andrei-marinica <andrei.marinica@elrond.com>
2020-12-19 23:52:44 +0200

    abi generation - multi args, multi results, missing provide_type_descriptions fixes

--- contracts/feature-tests/abi-tester/src/abi_test_type.rs
+++ contracts/feature-tests/abi-tester/src/abi_test_type.rs
@@ -32,3 +32,27 @@ pub struct OnlyShowsUpAsNested2 {
 pub struct OnlyShowsUpAsNested3 {
 	pub something: (),
 }
+
+/// Tests that the ABI generator also fetches types that only appear as fields.
+#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, TypeAbi)]
+pub struct OnlyShowsUpAsNested4 {
+	pub something: (),
+}
+
+/// Tests that the ABI generator also fetches types that only appear as fields.
+#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, TypeAbi)]
+pub struct OnlyShowsUpAsNested5 {
+	pub something: (),
+}
+
+/// Tests that the ABI generator also fetches types that only appear as fields.
+#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, TypeAbi)]
+pub struct OnlyShowsUpAsNested6 {
+	pub something: (),
+}
+
+/// Tests that the ABI generator also fetches types that only appear as fields.
+#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, TypeAbi)]
+pub struct OnlyShowsUpAsNested7 {
+	pub something: (),
+}
