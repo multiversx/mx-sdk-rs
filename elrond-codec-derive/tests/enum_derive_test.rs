c546a4ab990ba6077e4d02f6c16d1a68942b488d
andrei-marinica <andrei.marinica@elrond.com>
2020-12-21 02:32:13 +0200

    codec derive - enum test

--- elrond-codec-derive/tests/enum_derive_test.rs
+++ elrond-codec-derive/tests/enum_derive_test.rs
@@ -7,7 +7,7 @@ use elrond_codec::*;
 // to test, run the following command in elrond-codec-derive folder:
 // cargo expand --test enum_derive_test > enum_expanded.rs
 
-#[derive(PartialEq, Debug, TopEncode, TopDecode, NestedEncode, NestedDecode)]
+#[derive(TopEncode, TopDecode, NestedEncode, NestedDecode, PartialEq, Clone, Debug)]
 enum DayOfWeek {
 	Monday,
 	Tuesday,
@@ -19,7 +19,7 @@ enum DayOfWeek {
 }
 
 #[test]
-fn fieldless_enum_test() {
+fn fieldless_enum() {
 	check_top_encode_decode(DayOfWeek::Monday, &[]);
 	check_top_encode_decode(DayOfWeek::Tuesday, &[1]);
 	check_top_encode_decode(DayOfWeek::Wednesday, &[2]);
@@ -41,32 +41,79 @@ fn fieldless_enum_test() {
 	assert_eq!(DayOfWeek::Monday, check_top_decode(&[0, 0]));
 }
 
-#[derive(PartialEq, Debug, NestedEncode, NestedDecode, TopEncode)]
-enum Message {
+#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, PartialEq, Clone, Debug)]
+enum EnumWithEverything {
 	Quit,
 	Today(DayOfWeek),
-	Write(Vec<u8>),
+	Write(Vec<u8>, u16),
+	Struct {
+		int: u16,
+		seq: Vec<u8>,
+		another_byte: u8,
+		uint_32: u32,
+		uint_64: u64,
+	},
 }
 
 #[test]
-fn field_enum_test() {
-	check_dep_encode_decode(Message::Quit, &[0]);
-	check_dep_encode_decode(Message::Today(DayOfWeek::Friday), &[1, 4]);
-	check_dep_encode_decode(Message::Write(Vec::new()), &[2, /*vec length */ 0, 0, 0, 0]);
-	#[rustfmt::skip]
-	check_dep_encode_decode(
-		Message::Write([1, 2, 3].to_vec()),
-		&[
-			/* discriminant */ 2, 
-			/* vec length */ 0, 0, 0, 3,
-			/* vec contents */ 1, 2, 3,
-		],
-	);
+fn field_enum_zero_value() {
+	check_top_encode_decode(EnumWithEverything::Quit, &[]);
+	check_dep_encode_decode(EnumWithEverything::Quit, &[0]);
+	assert_eq!(EnumWithEverything::Quit, check_top_decode(&[0])); // also allowed
 }
 
-/* not supported yet - complex enums
-#[derive(TopEncode, TopDecode)]
-enum ComplexEnum {
-	ComplexField(u8)
+#[test]
+fn field_enum_variant_with_value() {
+	check_top_encode_decode(EnumWithEverything::Today(DayOfWeek::Friday), &[1, 4]);
+	check_dep_encode_decode(EnumWithEverything::Today(DayOfWeek::Friday), &[1, 4]);
+
+	let enum_tuple_0 = EnumWithEverything::Write(Vec::new(), 0);
+	#[rustfmt::skip]
+	let enum_tuple_0_bytes = &[
+		/* discriminant */ 2,
+		/* vec length */ 0, 0, 0, 0,
+		/* u16 */ 0, 0,
+	];
+	check_top_encode_decode(enum_tuple_0.clone(), enum_tuple_0_bytes);
+	check_dep_encode_decode(enum_tuple_0, enum_tuple_0_bytes);
 }
-*/
+
+#[test]
+fn field_enum_variant_with_tuple() {
+	let enum_tuple_1 = EnumWithEverything::Write([1, 2, 3].to_vec(), 4);
+	#[rustfmt::skip]
+	let enum_tuple_1_bytes = &[
+		/* discriminant */ 2, 
+		/* vec length */ 0, 0, 0, 3,
+		/* vec contents */ 1, 2, 3,
+		/* an extra 16 */ 0, 4,
+	];
+
+	check_top_encode_decode(enum_tuple_1.clone(), enum_tuple_1_bytes);
+	check_dep_encode_decode(enum_tuple_1, enum_tuple_1_bytes);
+}
+
+#[test]
+fn field_enum_struct_variant() {
+	let enum_struct = EnumWithEverything::Struct {
+		int: 0x42,
+		seq: vec![0x1, 0x2, 0x3, 0x4, 0x5],
+		another_byte: 0x6,
+		uint_32: 0x12345,
+		uint_64: 0x123456789,
+	};
+
+	#[rustfmt::skip]
+	let enum_struct_bytes = &[
+		/* discriminant */ 3,
+		/* int */ 0, 0x42,
+		/* seq length */ 0, 0, 0, 5,
+		/* seq contents */ 1, 2, 3, 4, 5,
+		/* another_byte */ 6,
+		/* uint_32 */ 0x00, 0x01, 0x23, 0x45,
+		/* uint_64 */ 0x00, 0x00, 0x00, 0x01, 0x23, 0x45, 0x67, 0x89,
+	];
+
+	check_top_encode_decode(enum_struct.clone(), enum_struct_bytes);
+	check_dep_encode_decode(enum_struct, enum_struct_bytes);
+}
