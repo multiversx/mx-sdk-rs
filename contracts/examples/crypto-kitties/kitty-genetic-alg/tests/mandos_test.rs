ff9fd6bc5787bb33e947fe5d5944bdb512878230
dorin.iancu <dorin.iancu@elrond.com>
2020-12-22 11:34:43 +0200

    kitty genetic alg tests

--- contracts/examples/crypto-kitties/kitty-genetic-alg/tests/mandos_test.rs
+++ contracts/examples/crypto-kitties/kitty-genetic-alg/tests/mandos_test.rs
@@ -1,5 +1,5 @@
-/*extern crate kitty_ownership;
-use kitty_ownership::*;
+extern crate kitty_genetic_alg;
+use kitty_genetic_alg::*;
 
 use elrond_wasm::*;
 use elrond_wasm_debug::*;
@@ -7,13 +7,18 @@ use elrond_wasm_debug::*;
 fn contract_map() -> ContractMap<TxContext> {
 	let mut contract_map = ContractMap::new();
 	contract_map.register_contract(
-		"file:../output/non-fungible-tokens.wasm",
-		Box::new(|context| Box::new(NonFungibleTokensImpl::new(context))),
+		"file:../output/kitty-genetic-alg.wasm",
+		Box::new(|context| Box::new(KittyGeneticAlgImpl::new(context))),
 	);
 	contract_map
 }
 
 #[test]
-fn nft_init() {
-	parse_execute_mandos("mandos/nft-init.scen.json", &contract_map());
-}*/
+fn init() {
+	parse_execute_mandos("mandos/init.scen.json", &contract_map());
+}
+
+#[test]
+fn generate_kitty_genes_test() {
+	parse_execute_mandos("mandos/generate-kitty-genes.scen.json", &contract_map());
+}
