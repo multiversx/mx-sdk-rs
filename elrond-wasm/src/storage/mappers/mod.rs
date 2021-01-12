6a6a34cf1c60ee1eb3954093db3d2d31e49c96fc
andrei-marinica <andrei.marinica@elrond.com>
2021-01-12 08:30:17 +0200

    storage mappers - user mapper; usage in ping-pong-egld

--- elrond-wasm/src/storage/mappers/mod.rs
+++ elrond-wasm/src/storage/mappers/mod.rs
@@ -1,7 +1,9 @@
 mod mapper;
 mod single_value_mapper;
+mod user_mapper;
 mod vec_mapper;
 
 pub use mapper::StorageMapper;
 pub use single_value_mapper::SingleValueMapper;
+pub use user_mapper::UserMapper;
 pub use vec_mapper::VecMapper;
