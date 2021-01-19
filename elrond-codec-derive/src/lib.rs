c0cf71e7ae20aad168bb32c5ec9968c63563fe19
andrei-marinica <andrei.marinica@elrond.com>
2021-01-19 19:08:41 +0200

    codec top encode/decode with defaults

--- elrond-codec-derive/src/lib.rs
+++ elrond-codec-derive/src/lib.rs
@@ -8,8 +8,8 @@ mod util;
 
 use nested_de_derive::nested_decode_impl;
 use nested_en_derive::nested_encode_impl;
-use top_de_derive::top_decode_impl;
-use top_en_derive::top_encode_impl;
+use top_de_derive::{top_decode_impl, top_decode_or_default_impl};
+use top_en_derive::{top_encode_impl, top_encode_or_default_impl};
 
 use proc_macro::TokenStream;
 
@@ -27,6 +27,13 @@ pub fn top_encode_derive(input: TokenStream) -> TokenStream {
 	top_encode_impl(&ast)
 }
 
+#[proc_macro_derive(TopEncodeOrDefault)]
+pub fn top_encode_or_default_derive(input: TokenStream) -> TokenStream {
+	let ast = syn::parse(input).unwrap();
+
+	top_encode_or_default_impl(&ast)
+}
+
 #[proc_macro_derive(NestedDecode)]
 pub fn nested_decode_derive(input: TokenStream) -> TokenStream {
 	let ast = syn::parse(input).unwrap();
@@ -39,4 +46,11 @@ pub fn top_decode_derive(input: TokenStream) -> TokenStream {
 	let ast = syn::parse(input).unwrap();
 
 	top_decode_impl(&ast)
+}
+
+#[proc_macro_derive(TopDecodeOrDefault)]
+pub fn top_decode_or_default_derive(input: TokenStream) -> TokenStream {
+	let ast = syn::parse(input).unwrap();
+
+	top_decode_or_default_impl(&ast)
 }
--- elrond-codec-derive/src/top_de_derive.rs
+++ elrond-codec-derive/src/top_de_derive.rs
@@ -75,10 +75,12 @@ fn zero_value_if(name: &syn::Ident, data_enum: &syn::DataEnum) -> proc_macro2::T
 	}
 }
 
-pub fn top_decode_impl(ast: &syn::DeriveInput) -> TokenStream {
+/// Only returns the trait implementation method bodies, without the impl or method definitions.
+fn top_decode_method_bodies(
+	ast: &syn::DeriveInput,
+) -> (proc_macro2::TokenStream, proc_macro2::TokenStream) {
 	let name = &ast.ident;
-	let (impl_generics, ty_generics, where_clause) = &ast.generics.split_for_impl();
-	let gen = match &ast.data {
+	match &ast.data {
 		syn::Data::Struct(data_struct) => {
 			let field_dep_decode_snippets =
 				fields_decl_syntax(&data_struct.fields, |index, field| {
@@ -88,33 +90,26 @@ pub fn top_decode_impl(ast: &syn::DeriveInput) -> TokenStream {
 				fields_decl_syntax(&data_struct.fields, |index, field| {
 					dep_decode_or_exit_snippet(index, field)
 				});
-			quote! {
-				impl #impl_generics elrond_codec::TopDecode for #name #ty_generics #where_clause {
-					fn top_decode<I: elrond_codec::TopDecodeInput>(top_input: I) -> core::result::Result<Self, elrond_codec::DecodeError> {
-						let bytes = top_input.into_boxed_slice_u8();
-						let input = &mut &*bytes;
-						let result = #name #field_dep_decode_snippets ;
-						if !input.is_empty() {
-							return core::result::Result::Err(elrond_codec::DecodeError::INPUT_TOO_LONG);
-						}
-						core::result::Result::Ok(result)
-					}
 
-					fn top_decode_or_exit<I: elrond_codec::TopDecodeInput, ExitCtx: Clone>(
-						top_input: I,
-						c: ExitCtx,
-						exit: fn(ExitCtx, elrond_codec::DecodeError) -> !,
-					) -> Self {
-						let bytes = top_input.into_boxed_slice_u8();
-						let input = &mut &*bytes;
-						let result = #name #field_dep_encode_or_exit_snippets ;
-						if !input.is_empty() {
-							exit(c, elrond_codec::DecodeError::INPUT_TOO_LONG);
-						}
-						result
-					}
+			let top_decode_body = quote! {
+				let bytes = top_input.into_boxed_slice_u8();
+				let input = &mut &*bytes;
+				let result = #name #field_dep_decode_snippets ;
+				if !input.is_empty() {
+					return core::result::Result::Err(elrond_codec::DecodeError::INPUT_TOO_LONG);
 				}
-			}
+				core::result::Result::Ok(result)
+			};
+			let top_decode_or_exit_body = quote! {
+				let bytes = top_input.into_boxed_slice_u8();
+				let input = &mut &*bytes;
+				let result = #name #field_dep_encode_or_exit_snippets ;
+				if !input.is_empty() {
+					exit(c, elrond_codec::DecodeError::INPUT_TOO_LONG);
+				}
+				result
+			};
+			(top_decode_body, top_decode_or_exit_body)
 		},
 		syn::Data::Enum(data_enum) => {
 			assert!(
@@ -125,27 +120,20 @@ pub fn top_decode_impl(ast: &syn::DeriveInput) -> TokenStream {
 				// fieldless enums are special, they can be top-decoded as u8 directly
 				let top_decode_arms = fieldless_enum_match_arm_result_ok(&name, &data_enum);
 				let top_decode_or_exit_arms = fieldless_enum_match_arm(&name, &data_enum);
-				quote! {
-					impl #impl_generics elrond_codec::TopDecode for #name #ty_generics #where_clause {
-						fn top_decode<I: elrond_codec::TopDecodeInput>(input: I) -> core::result::Result<Self, elrond_codec::DecodeError> {
-							match <u8 as elrond_codec::TopDecode>::top_decode(input)? {
-								#(#top_decode_arms)*
-								_ => core::result::Result::Err(elrond_codec::DecodeError::INVALID_VALUE),
-							}
-						}
 
-						fn top_decode_or_exit<I: elrond_codec::TopDecodeInput, ExitCtx: Clone>(
-							input: I,
-							c: ExitCtx,
-							exit: fn(ExitCtx, elrond_codec::DecodeError) -> !,
-						) -> Self {
-							match <u8 as elrond_codec::TopDecode>::top_decode_or_exit(input, c.clone(), exit) {
-								#(#top_decode_or_exit_arms)*
-								_ => exit(c, elrond_codec::DecodeError::INVALID_VALUE),
-							}
-						}
+				let top_decode_body = quote! {
+					match <u8 as elrond_codec::TopDecode>::top_decode(top_input)? {
+						#(#top_decode_arms)*
+						_ => core::result::Result::Err(elrond_codec::DecodeError::INVALID_VALUE),
 					}
-				}
+				};
+				let top_decode_or_exit_body = quote! {
+					match <u8 as elrond_codec::TopDecode>::top_decode_or_exit(top_input, c.clone(), exit) {
+						#(#top_decode_or_exit_arms)*
+						_ => exit(c, elrond_codec::DecodeError::INVALID_VALUE),
+					}
+				};
+				(top_decode_body, top_decode_or_exit_body)
 			} else {
 				let variant_dep_decode_snippets = variant_dep_decode_snippets(&name, &data_enum);
 				let variant_dep_decode_or_exit_snippets =
@@ -153,44 +141,90 @@ pub fn top_decode_impl(ast: &syn::DeriveInput) -> TokenStream {
 				let zero_value_if_result_ok = zero_value_if_result_ok(&name, &data_enum);
 				let zero_value_if = zero_value_if(&name, &data_enum);
 
-				quote! {
-					impl #impl_generics elrond_codec::TopDecode for #name #ty_generics #where_clause {
-						fn top_decode<I: elrond_codec::TopDecodeInput>(top_input: I) -> core::result::Result<Self, elrond_codec::DecodeError> {
-							#zero_value_if_result_ok
-							let bytes = top_input.into_boxed_slice_u8();
-							let input = &mut &*bytes;
-							let result = match <u8 as elrond_codec::NestedDecode>::dep_decode(input)? {
-								#(#variant_dep_decode_snippets)*
-								_ => core::result::Result::Err(elrond_codec::DecodeError::INVALID_VALUE),
-							};
-							if !input.is_empty() {
-								return core::result::Result::Err(elrond_codec::DecodeError::INPUT_TOO_LONG);
-							}
-							result
-						}
-
-						fn top_decode_or_exit<I: elrond_codec::TopDecodeInput, ExitCtx: Clone>(
-							top_input: I,
-							c: ExitCtx,
-							exit: fn(ExitCtx, elrond_codec::DecodeError) -> !,
-						) -> Self {
-							#zero_value_if
-							let bytes = top_input.into_boxed_slice_u8();
-							let input = &mut &*bytes;
-							let result = match <u8 as elrond_codec::NestedDecode>::dep_decode_or_exit(input, c.clone(), exit) {
-								#(#variant_dep_decode_or_exit_snippets)*
-								_ => exit(c, elrond_codec::DecodeError::INVALID_VALUE),
-							};
-							if !input.is_empty() {
-								exit(c, elrond_codec::DecodeError::INPUT_TOO_LONG);
-							}
-							result
-						}
+				let top_decode_body = quote! {
+					#zero_value_if_result_ok
+					let bytes = top_input.into_boxed_slice_u8();
+					let input = &mut &*bytes;
+					let result = match <u8 as elrond_codec::NestedDecode>::dep_decode(input)? {
+						#(#variant_dep_decode_snippets)*
+						_ => core::result::Result::Err(elrond_codec::DecodeError::INVALID_VALUE),
+					};
+					if !input.is_empty() {
+						return core::result::Result::Err(elrond_codec::DecodeError::INPUT_TOO_LONG);
 					}
-				}
+					result
+				};
+				let top_decode_or_exit_body = quote! {
+					#zero_value_if
+					let bytes = top_input.into_boxed_slice_u8();
+					let input = &mut &*bytes;
+					let result = match <u8 as elrond_codec::NestedDecode>::dep_decode_or_exit(input, c.clone(), exit) {
+						#(#variant_dep_decode_or_exit_snippets)*
+						_ => exit(c, elrond_codec::DecodeError::INVALID_VALUE),
+					};
+					if !input.is_empty() {
+						exit(c, elrond_codec::DecodeError::INPUT_TOO_LONG);
+					}
+					result
+				};
+				(top_decode_body, top_decode_or_exit_body)
 			}
 		},
 		syn::Data::Union(_) => panic!("Union not supported"),
+	}
+}
+
+pub fn top_decode_impl(ast: &syn::DeriveInput) -> TokenStream {
+	let name = &ast.ident;
+	let (impl_generics, ty_generics, where_clause) = &ast.generics.split_for_impl();
+	let (top_decode_body, top_decode_or_exit_body) = top_decode_method_bodies(ast);
+
+	let gen = quote! {
+		impl #impl_generics elrond_codec::TopDecode for #name #ty_generics #where_clause {
+			fn top_decode<I: elrond_codec::TopDecodeInput>(top_input: I) -> core::result::Result<Self, elrond_codec::DecodeError> {
+				#top_decode_body
+			}
+
+			fn top_decode_or_exit<I: elrond_codec::TopDecodeInput, ExitCtx: Clone>(
+				top_input: I,
+				c: ExitCtx,
+				exit: fn(ExitCtx, elrond_codec::DecodeError) -> !,
+			) -> Self {
+				#top_decode_or_exit_body
+			}
+		}
+	};
+
+	gen.into()
+}
+
+pub fn top_decode_or_default_impl(ast: &syn::DeriveInput) -> TokenStream {
+	let name = &ast.ident;
+	let (impl_generics, ty_generics, where_clause) = &ast.generics.split_for_impl();
+	let (top_decode_body, top_decode_or_exit_body) = top_decode_method_bodies(ast);
+
+	let gen = quote! {
+		impl #impl_generics elrond_codec::TopDecode for #name #ty_generics #where_clause {
+			fn top_decode<I: elrond_codec::TopDecodeInput>(top_input: I) -> core::result::Result<Self, elrond_codec::DecodeError> {
+				if top_input.byte_len() == 0 {
+					Ok(<#name as elrond_codec::DecodeDefault>::default())
+				} else {
+					#top_decode_body
+				}
+			}
+
+			fn top_decode_or_exit<I: elrond_codec::TopDecodeInput, ExitCtx: Clone>(
+				top_input: I,
+				c: ExitCtx,
+				exit: fn(ExitCtx, elrond_codec::DecodeError) -> !,
+			) -> Self {
+				if top_input.byte_len() == 0 {
+					<#name as elrond_codec::DecodeDefault>::default()
+				} else {
+					#top_decode_or_exit_body
+				}
+			}
+		}
 	};
 
 	gen.into()
