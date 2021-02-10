f67a6ac5507fdd60a1e9ab693df6207f400357aa
andrei-marinica <andrei.marinica@elrond.com>
2021-02-10 18:32:08 +0200

    moved io types to io module

---
+++ elrond-wasm/src/types/io/mod.rs
@@ -0,0 +1,21 @@
+mod arg_buffer;
+mod async_call_result;
+mod multi_args;
+mod multi_result;
+mod multi_result_vec;
+mod optional_arg;
+mod optional_result;
+mod sc_error;
+mod sc_result;
+mod var_args;
+
+pub use arg_buffer::ArgBuffer;
+pub use async_call_result::{AsyncCallError, AsyncCallResult};
+pub use multi_args::*;
+pub use multi_result::*;
+pub use multi_result_vec::MultiResultVec;
+pub use optional_arg::OptionalArg;
+pub use optional_result::OptionalResult;
+pub use sc_error::SCError;
+pub use sc_result::SCResult;
+pub use var_args::VarArgs;
