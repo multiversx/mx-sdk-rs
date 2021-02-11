f6651162a802f84e4b21ac09a5488dd93efda4c1
andrei-marinica <andrei.marinica@elrond.com>
2021-02-11 02:08:40 +0200

    send types - send_egld, async_call, choice

---
+++ elrond-wasm/src/types/io/async_call.rs
@@ -0,0 +1,82 @@
+use crate::abi::{OutputAbi, TypeAbi, TypeDescriptionContainer};
+use crate::api::{BigUintApi, ErrorApi, SendApi, ESDT_TRANSFER_STRING};
+use crate::hex_call_data::HexCallDataSerializer;
+use crate::io::AsyncCallArg;
+use crate::io::EndpointResult;
+use crate::types::{Address, SCError};
+use alloc::string::String;
+use alloc::vec::Vec;
+
+pub struct AsyncCall<BigUint: BigUintApi> {
+	to: Address,
+	egld_payment: BigUint,
+	hex_data: HexCallDataSerializer,
+}
+
+impl<BigUint: BigUintApi> AsyncCall<BigUint> {
+	pub fn new(to: Address, egld_payment: BigUint, endpoint_name: &[u8]) -> Self {
+		AsyncCall {
+			to,
+			egld_payment,
+			hex_data: HexCallDataSerializer::new(endpoint_name),
+		}
+	}
+
+	pub fn with_esdt(
+		to: Address,
+		esdt_payment: &BigUint,
+		esdt_token_name: &[u8],
+		endpoint_name: &[u8],
+	) -> Self {
+		let mut hex_data = HexCallDataSerializer::new(ESDT_TRANSFER_STRING);
+		hex_data.push_argument_bytes(esdt_token_name);
+		hex_data.push_argument_bytes(esdt_payment.to_bytes_be().as_slice());
+		hex_data.push_argument_bytes(endpoint_name);
+		AsyncCall {
+			to,
+			egld_payment: BigUint::zero(),
+			hex_data,
+		}
+	}
+
+	pub fn push_argument_raw_bytes(&mut self, bytes: &[u8]) {
+		self.hex_data.push_argument_bytes(bytes);
+	}
+
+	pub fn push_argument_or_exit<A: AsyncCallArg, ExitCtx: Clone>(
+		&mut self,
+		arg: A,
+		c: ExitCtx,
+		exit: fn(ExitCtx, SCError) -> !,
+	) {
+		// TODO: propagate fast exit down the serializer
+		// TODO: also expose an EncodingError instead of the SCError
+		if let Err(serialization_err) = arg.push_async_arg(&mut self.hex_data) {
+			exit(c, serialization_err);
+		}
+	}
+}
+
+impl<FA, BigUint> EndpointResult<FA> for AsyncCall<BigUint>
+where
+	BigUint: BigUintApi + 'static,
+	FA: SendApi<BigUint> + ErrorApi + Clone + 'static,
+{
+	#[inline]
+	fn finish(&self, api: FA) {
+		api.async_call_raw(&self.to, &self.egld_payment, self.hex_data.as_slice());
+	}
+}
+
+impl<BigUint: BigUintApi> TypeAbi for AsyncCall<BigUint> {
+	fn type_name() -> String {
+		"AsyncCall".into()
+	}
+
+	/// No ABI output.
+	fn output_abis(_: &[&'static str]) -> Vec<OutputAbi> {
+		Vec::new()
+	}
+
+	fn provide_type_descriptions<TDC: TypeDescriptionContainer>(_: &mut TDC) {}
+}
