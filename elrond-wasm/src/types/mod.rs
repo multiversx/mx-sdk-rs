e99c3b0d7a4854761483915fb91677d97988f148
andrei-marinica <andrei.marinica@elrond.com>
2021-02-18 18:26:52 +0200

    reorganized types module

--- elrond-wasm/src/types/mod.rs
+++ elrond-wasm/src/types/mod.rs
@@ -1,18 +1,9 @@
 mod borrowed_mut_storage;
-mod boxed_bytes;
-mod code_metadata;
-mod h256;
-mod h256_address;
+mod general;
+mod interaction;
 mod io;
-mod queue;
-mod token_identifier;
-
-pub use io::*;
 
 pub use borrowed_mut_storage::BorrowedMutStorage;
-pub use boxed_bytes::BoxedBytes;
-pub use code_metadata::CodeMetadata;
-pub use h256::H256;
-pub use h256_address::Address;
-pub use queue::Queue;
-pub use token_identifier::TokenIdentifier;
+pub use general::*;
+pub use interaction::*;
+pub use io::*;
