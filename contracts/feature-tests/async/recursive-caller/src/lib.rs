f5be60382d7f1c1104d166f483c2821915efa89e
andrei-marinica <andrei.marinica@elrond.com>
2021-03-07 11:10:40 +0200

    callback async call bugfix

--- contracts/feature-tests/async/recursive-caller/src/lib.rs
+++ contracts/feature-tests/async/recursive-caller/src/lib.rs
@@ -20,6 +20,7 @@ pub trait AlsoRecursiveCaller {
 	fn recursive_send_funds(
 		&self,
 		to: &Address,
+		token_identifier: &TokenIdentifier,
 		amount: &BigUint,
 		counter: u32,
 	) -> ContractCall<BigUint>;
@@ -35,51 +36,59 @@ pub trait RecursiveCaller {
 	fn recursive_send_funds(
 		&self,
 		to: &Address,
+		token_identifier: &TokenIdentifier,
 		amount: &BigUint,
 		counter: u32,
-	) -> OptionalResult<AsyncCall<BigUint>> {
-		self.recursive_send_funds_event(to, amount, counter);
-		if counter > 0 {
-			OptionalResult::Some(
-				contract_call!(self, to.clone(), VaultProxy)
-					.accept_funds()
-					.async_call()
-					.with_callback(
-						self.callbacks()
-							.recursive_send_funds_callback(to, amount, counter),
-					),
-			)
-		} else {
-			OptionalResult::None
-		}
+	) -> AsyncCall<BigUint> {
+		self.recursive_send_funds_event(to, token_identifier, amount, counter);
+
+		contract_call!(self, to.clone(), VaultProxy)
+			.with_token_transfer(token_identifier.clone(), amount.clone())
+			.accept_funds()
+			.async_call()
+			.with_callback(self.callbacks().recursive_send_funds_callback(
+				to,
+				token_identifier,
+				amount,
+				counter,
+			))
 	}
 
 	#[callback]
 	fn recursive_send_funds_callback(
 		&self,
 		to: &Address,
+		token_identifier: &TokenIdentifier,
 		amount: &BigUint,
 		counter: u32,
-	) -> AsyncCall<BigUint> {
-		self.recursive_send_funds_callback_event(to, amount, counter);
+	) -> OptionalResult<AsyncCall<BigUint>> {
+		self.recursive_send_funds_callback_event(to, token_identifier, amount, counter);
 
-		contract_call!(self, to.clone(), AlsoRecursiveCallerProxy)
-			.recursive_send_funds(&to, amount, counter - 1)
-			.async_call()
+		if counter > 1 {
+			OptionalResult::Some(
+				contract_call!(self, self.get_sc_address(), AlsoRecursiveCallerProxy)
+					.recursive_send_funds(&to, token_identifier, amount, counter - 1)
+					.async_call(),
+			)
+		} else {
+			OptionalResult::None
+		}
 	}
 
 	#[event("recursive_send_funds")]
 	fn recursive_send_funds_event(
 		&self,
 		#[indexed] to: &Address,
+		#[indexed] token_identifier: &TokenIdentifier,
 		#[indexed] amount: &BigUint,
 		counter: u32,
 	);
 
-	#[event("recursive_send_funds")]
+	#[event("recursive_send_funds_callback")]
 	fn recursive_send_funds_callback_event(
 		&self,
 		#[indexed] to: &Address,
+		#[indexed] token_identifier: &TokenIdentifier,
 		#[indexed] amount: &BigUint,
 		counter: u32,
 	);
