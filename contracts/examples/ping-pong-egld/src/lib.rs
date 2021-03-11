a49e7846d4c2bc4fa93631d4985bf07ac0d05202
andrei-marinica <andrei.marinica@elrond.com>
2021-03-11 09:54:31 +0200

    ping-pong-egld - docs, ping optional data

--- contracts/examples/ping-pong-egld/src/lib.rs
+++ contracts/examples/ping-pong-egld/src/lib.rs
@@ -10,8 +10,27 @@ use user_status::UserStatus;
 /// Derived empirically.
 const PONG_ALL_LOW_GAS_LIMIT: u64 = 3_000_000;
 
+/// A contract that allows anyone to send a fixed sum, locks it for a while and then allows users to take it back.
+/// Sending funds to the contract is called "ping".
+/// Taking the same funds back is called "pong".
+///
+/// Restrictions:
+/// - `ping` can be called only after the contract is activated. By default the contract is activated on deploy.
+/// - Users can only `ping` once, ever.
+/// - Only the set amount can be `ping`-ed, no more, no less.
+/// - The contract can optionally have a maximum cap. No more users can `ping` after the cap has been reached.
+/// - The `ping` endpoint optionally accepts
+/// - `pong` can only be called after the contract expired (a certain duration has passed since activation).
+/// - `pongAll` can be used to send to all users to `ping`-ed. If it runs low on gas, it will interrupt itself.
+/// It can be continued anytime.
 #[elrond_wasm_derive::contract(PingPongImpl)]
 pub trait PingPong {
+
+	/// Necessary configuration when deploying:
+	/// `ping_amount` - the exact EGLD amounf that needs to be sent when `ping`-ing.
+	/// `duration_in_seconds` - how much time (in seconds) until contract expires.
+	/// `opt_activation_timestamp` - optionally specify the contract to only actvivate at a later date.
+	/// `max_funds` - optional funding cap, no more funds than this can be added to the contract.
 	#[init]
 	fn init(
 		&self,
@@ -29,9 +48,11 @@ pub trait PingPong {
 		self.max_funds().set(&max_funds.into_option());
 	}
 
+	/// User sends some EGLD to be locked in the contract for a period of time.
+	/// Optional `_data` argument is ignored.
 	#[payable("EGLD")]
 	#[endpoint]
-	fn ping(&self, #[payment] payment: &BigUint, _data: BoxedBytes) -> SCResult<()> {
+	fn ping(&self, #[payment] payment: &BigUint, #[var_args] _data: OptionalArg<BoxedBytes>) -> SCResult<()> {
 		require!(
 			payment == &self.ping_amount().get(),
 			"the payment must match the fixed sum"
@@ -94,6 +115,8 @@ pub trait PingPong {
 		}
 	}
 
+	/// User can take back funds from the contract.
+	/// Can only be called after expiration.
 	#[endpoint]
 	fn pong(&self) -> SCResult<()> {
 		require!(
@@ -106,6 +129,11 @@ pub trait PingPong {
 		self.pong_by_user_id(user_id)
 	}
 
+	/// Send back funds to all users who pinged.
+	/// Returns
+	/// - `completed` if everything finished
+	/// - `interrupted` if run out of gas midway.
+	/// Can only be called after expiration.
 	#[endpoint(pongAll)]
 	fn pong_all(&self) -> SCResult<OperationCompletionStatus> {
 		require!(
@@ -133,14 +161,11 @@ pub trait PingPong {
 		}
 	}
 
+	/// Lists the addresses of all users that have `ping`-ed,
+	/// in the order they have `ping`-ed
 	#[view(getUserAddresses)]
 	fn get_user_addresses(&self) -> MultiResultVec<Address> {
 		self.user_mapper().get_all_addresses().into()
-	}
-
-	#[view]
-	fn get_sc_balance_view(&self) -> BigUint {
-		self.get_sc_balance()
 	}
 
 	// storage
@@ -153,10 +178,13 @@ pub trait PingPong {
 	#[storage_mapper("deadline")]
 	fn deadline(&self) -> SingleValueMapper<Self::Storage, u64>;
 
+	/// Block timestamp of the block where the contract got activated.
+	/// If not specified in the constructor it is the the deploy block timestamp.
 	#[view(getActivationTimestamp)]
 	#[storage_mapper("activation_timestamp")]
 	fn activation_timestamp(&self) -> SingleValueMapper<Self::Storage, u64>;
 
+	/// Optional funding cap.
 	#[view(getMaxFunds)]
 	#[storage_mapper("max_funds")]
 	fn max_funds(&self) -> SingleValueMapper<Self::Storage, Option<BigUint>>;
@@ -164,10 +192,16 @@ pub trait PingPong {
 	#[storage_mapper("user")]
 	fn user_mapper(&self) -> UserMapper<Self::Storage>;
 
+	/// State of user funds.
+	/// 0 - user unknown, never `ping`-ed
+	/// 1 - `ping`-ed
+	/// 2 - `pong`-ed
 	#[view(getUserStatus)]
 	#[storage_mapper("user_status")]
 	fn user_status(&self, user_id: usize) -> SingleValueMapper<Self::Storage, UserStatus>;
 
+	/// Part of the `pongAll` status, the last user to be processed.
+	/// 0 if never called `pongAll` or `pongAll` completed..
 	#[view(pongAllLastUser)]
 	#[storage_mapper("pong_all_last_user")]
 	fn pong_all_last_user(&self) -> SingleValueMapper<Self::Storage, usize>;
