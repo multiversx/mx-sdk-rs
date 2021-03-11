6ab8f8619df911f44397471b73fe0a6b87980ce6
dorin.iancu <dorin.iancu@elrond.com>
2021-03-11 16:35:22 +0200

    set and unset special roles

--- elrond-wasm/src/types/general/esdt_token_type.rs
+++ elrond-wasm/src/types/general/esdt_token_type.rs
@@ -9,41 +9,41 @@ const ESDT_TYPE_INVALID: &'static [u8] = &[];
 
 #[derive(Clone, PartialEq, Debug)]
 pub enum EsdtTokenType {
-    Fungible,
-    NonFungible,
-    SemiFungible,
-    Invalid,
+	Fungible,
+	NonFungible,
+	SemiFungible,
+	Invalid,
 }
 
 impl EsdtTokenType {
-    pub fn as_u8(&self) -> u8 {
-        match self {
-            Self::Fungible => 0,
-            Self::NonFungible => 1,
-            Self::SemiFungible => 2,
-            Self::Invalid => 3,
-        }
-    }
+	pub fn as_u8(&self) -> u8 {
+		match self {
+			Self::Fungible => 0,
+			Self::NonFungible => 1,
+			Self::SemiFungible => 2,
+			Self::Invalid => 3,
+		}
+	}
 
-    pub fn as_type_name(&self) -> &'static [u8] {
-        match self {
-            Self::Fungible => ESDT_TYPE_FUNGIBLE,
-            Self::NonFungible => ESDT_TYPE_NON_FUNGIBLE,
-            Self::SemiFungible => ESDT_TYPE_SEMI_FUNGIBLE,
-            Self::Invalid => ESDT_TYPE_INVALID,
-        }
-    }
+	pub fn as_type_name(&self) -> &'static [u8] {
+		match self {
+			Self::Fungible => ESDT_TYPE_FUNGIBLE,
+			Self::NonFungible => ESDT_TYPE_NON_FUNGIBLE,
+			Self::SemiFungible => ESDT_TYPE_SEMI_FUNGIBLE,
+			Self::Invalid => ESDT_TYPE_INVALID,
+		}
+	}
 }
 
 impl From<u8> for EsdtTokenType {
 	#[inline]
 	fn from(value: u8) -> Self {
 		match value {
-            0 => Self::Fungible,
-            1 => Self::NonFungible,
-            2 => Self::SemiFungible,
-            _ => Self::Invalid
-        }
+			0 => Self::Fungible,
+			1 => Self::NonFungible,
+			2 => Self::SemiFungible,
+			_ => Self::Invalid,
+		}
 	}
 }
 
@@ -51,17 +51,14 @@ impl<'a> From<&'a [u8]> for EsdtTokenType {
 	#[inline]
 	fn from(byte_slice: &'a [u8]) -> Self {
 		if byte_slice == ESDT_TYPE_FUNGIBLE {
-            Self::Fungible
-        }
-        else if byte_slice == ESDT_TYPE_NON_FUNGIBLE {
-            Self::NonFungible
-        }
-        else if byte_slice == ESDT_TYPE_SEMI_FUNGIBLE {
-            Self::SemiFungible
-        }
-        else {
-            Self::Invalid
-        }
+			Self::Fungible
+		} else if byte_slice == ESDT_TYPE_NON_FUNGIBLE {
+			Self::NonFungible
+		} else if byte_slice == ESDT_TYPE_SEMI_FUNGIBLE {
+			Self::SemiFungible
+		} else {
+			Self::Invalid
+		}
 	}
 }
 
