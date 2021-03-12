c588b714395660a6cfc45c11a6a9350bc3945a7c
dorin.iancu <dorin.iancu@elrond.com>
2021-03-12 15:49:41 +0200

    getESDTTokenData proof of concept

---
+++ elrond-wasm/src/types/general/esdt_token_data.rs
@@ -0,0 +1,28 @@
+use crate::{abi::TypeAbi, api::BigUintApi};
+use alloc::string::String;
+use elrond_codec::*;
+
+use super::{Address, BoxedBytes, EsdtTokenType, H256};
+
+// TODO
+pub struct EsdtTokenData<BigUint: BigUintApi> {
+	pub token_type: EsdtTokenType,
+	pub amount: BigUint,
+	pub frozen: bool,
+	pub hash: H256,
+	pub name: BoxedBytes,
+	pub attributes: BoxedBytes,
+	pub creator: Address,
+	pub royalties: u32,
+	pub uris: Vec<BoxedBytes>,
+}
+
+impl<BigUint: BigUintApi> EsdtTokenData<BigUint> {
+	
+}
+
+impl<BigUint: BigUintApi> TypeAbi for EsdtTokenData<BigUint> {
+	fn type_name() -> String {
+		"EsdtTokenData".into()
+	}
+}
