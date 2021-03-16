948759ef9c6633593c58bdfb38a3936d18dba28c
dorin.iancu <dorin.iancu@elrond.com>
2021-03-16 18:55:58 +0200

    snippet improvements

--- contracts/feature-tests/local-esdt-and-nft/interaction/basic.snippets.sh
+++ contracts/feature-tests/local-esdt-and-nft/interaction/basic.snippets.sh
@@ -1,6 +1,6 @@
 ALICE="/home/elrond/elrond-sdk/erdpy/testnet/wallets/users/alice.pem"
-ALICE_ADDRESS=0x0139472eff6886771a982f3083da5d421f24c29181e63888228dc81ca60d69e1
 ADDRESS=$(erdpy data load --key=address-testnet)
+ADDRESS_DECODED=$(erdpy wallet bech32 --decode ${ADDRESS})
 DEPLOY_TRANSACTION=$(erdpy data load --key=deployTransaction-testnet)
 PROXY=http://localhost:7950
 CHAIN_ID=local-testnet
@@ -9,7 +9,7 @@ TOKEN_DISPLAY_NAME=0x46756e6769626c65546f6b656e # "FungibleToken"
 TOKEN_TICKER=0x46554e47544f4b # "FUNGTOK"
 
 # Manually update after issue
-TOKEN_IDENTIFIER=0x
+TOKEN_IDENTIFIER=0x46554e47544f4b2d623639646635
 
 deploy() {
     erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} --gas-limit=75000000 --send --outfile="deploy-testnet.interaction.json" --proxy=${PROXY} --chain=${CHAIN_ID} || return
@@ -44,14 +44,18 @@ localBurn() {
 
 # 0x01 = localmint roles, 0x02 = localburn role
 setLocalRolesFungible() {
-    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=60000000 --function="setLocalRoles" --arguments ${ALICE_ADDRESS} ${TOKEN_IDENTIFIER} 0x01 0x02 --send --proxy=${PROXY} --chain=${CHAIN_ID}
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=500000000 --function="setLocalRoles" --arguments 0x${ADDRESS_DECODED} ${TOKEN_IDENTIFIER} 0x01 0x02 --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
 removeLocalRolesFungible() {
-    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=60000000 --function="unsetLocalRoles" --arguments ${ALICE_ADDRESS} ${TOKEN_IDENTIFIER} 0x01 0x02 --send --proxy=${PROXY} --chain=${CHAIN_ID}
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=500000000 --function="unsetLocalRoles" --arguments 0x${ADDRESS_DECODED} ${TOKEN_IDENTIFIER} 0x01 0x02 --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
 # Queries
+
+getFungibleEsdtBalance() {
+    erdpy --verbose contract query ${ADDRESS} --function="getFungibleEsdtBalance" --arguments ${TOKEN_IDENTIFIER} --proxy=${PROXY}
+}
 
 getLastIssuedToken() {
     erdpy --verbose contract query ${ADDRESS} --function="lastIssuedToken" --proxy=${PROXY}
