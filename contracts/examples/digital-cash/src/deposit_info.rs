a9842ebbdfbb67b43a9e076956057021bea8a140
Valentin C <valentin.craciun99@gmail.com>
2021-03-17 12:08:42 +0200

    fixes after review

--- contracts/examples/digital-cash/src/deposit_info.rs
+++ contracts/examples/digital-cash/src/deposit_info.rs
@@ -1,13 +1,12 @@
-use elrond_wasm::{api::BigUintApi, types::TokenIdentifier};
 use elrond_wasm::types::Address;
+use elrond_wasm::{api::BigUintApi, types::TokenIdentifier};
 
 elrond_wasm::derive_imports!();
 
 #[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, TypeAbi)]
-pub struct DepositInfo<BigUint: BigUintApi>
-{
-	pub amount : BigUint,
-	pub depositor_address : Address,
-	pub expiration: u64,
-	pub token_name : TokenIdentifier
-}		
+pub struct DepositInfo<BigUint: BigUintApi> {
+    pub amount: BigUint,
+    pub depositor_address: Address,
+    pub expiration_round: u64,
+    pub token_name: TokenIdentifier,
+}
