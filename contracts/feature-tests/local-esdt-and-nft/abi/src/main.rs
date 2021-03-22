88eadace3fab8db0081f24ee58f4799b570feacc
andrei-marinica <andrei.marinica@elrond.com>
2021-03-22 11:23:38 +0200

    removed feature test abi crates

--- contracts/feature-tests/local-esdt-and-nft/abi/src/main.rs
+++
@@ -1,7 +0,0 @@
-use local_esdt_and_nft::*;
-use elrond_wasm_debug::*;
-
-fn main() {
-	let contract = LocalEsdtAndEsdtNftImpl::new(TxContext::dummy());
-	print!("{}", abi_json::contract_abi(&contract));
-}
