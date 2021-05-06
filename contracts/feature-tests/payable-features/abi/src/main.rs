2bb3ae80047e9f3b780f2d2d5ffea5bfab3de8e1
andrei-marinica <andrei.marinica@elrond.com>
2021-05-06 23:00:01 +0300

    contract modules redesign - contract upgrades

--- contracts/feature-tests/payable-features/abi/src/main.rs
+++ contracts/feature-tests/payable-features/abi/src/main.rs
@@ -1,6 +1,3 @@
-use elrond_wasm_debug::*;
-
 fn main() {
-	let contract = payable_features::contract_obj(TxContext::dummy());
-	print!("{}", abi_json::contract_abi(&contract));
+	elrond_wasm_debug::abi_json::print_abi::<payable_features::AbiProvider>();
 }
