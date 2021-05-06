49c0e7f3b34903c76df73ac5bdefa85a3cb702b0
andrei-marinica <andrei.marinica@elrond.com>
2021-05-06 22:47:29 +0300

    contract modules redesign - use-module fix + ABI test

--- contracts/feature-tests/use-module/tests/use_module_abi_test.rs
+++ contracts/feature-tests/use-module/tests/use_module_abi_test.rs
@@ -5,15 +5,15 @@ use std::fs::File;
 use std::io::Write;
 
 #[test]
-fn test_abi_generated_ok() {
+fn use_module_abi_generated_ok() {
 	// load expected from disk
-	let expected_abi_json = fs::read_to_string("./abi_test_expected.abi.json").unwrap();
+	let expected_abi_json = fs::read_to_string("./use_module_expected.abi.json").unwrap();
 
 	// generate ABI
-	let contract_abi_json = abi_json::contract_abi::<abi_tester::AbiProvider>();
+	let contract_abi_json = abi_json::contract_abi::<use_module::AbiProvider>();
 
 	// save generated ABI to disk for easier comparison in case something is off
-	let mut file = File::create("abi_test_generated.abi.json").unwrap();
+	let mut file = File::create("use_module_generated.abi.json").unwrap();
 	file.write_all(contract_abi_json.as_bytes()).unwrap();
 
 	// check!
