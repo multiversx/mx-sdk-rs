5ee2e01355f50e5853ade677a3435b8d493f1c15
andrei-diaconescu <andreidiaconescu_1999@yahoo.com>
2021-05-10 15:23:13 +0300

    added elliptic curves functionalities in apis

--- elrond-wasm-debug/src/api/crypto_api_mock.rs
+++ elrond-wasm-debug/src/api/crypto_api_mock.rs
@@ -1,10 +1,12 @@
+use super::big_int_api_mock::*;
+use super::big_uint_api_mock::*;
 use crate::TxContext;
 use elrond_wasm::api::CryptoApi;
-use elrond_wasm::types::H256;
+use elrond_wasm::types::{BoxedBytes, EllipticCurve, H256};
 use sha2::Sha256;
 use sha3::{Digest, Keccak256};
 
-impl CryptoApi for TxContext {
+impl CryptoApi<RustBigInt, RustBigUint> for TxContext {
 	fn sha256(&self, data: &[u8]) -> H256 {
 		let mut hasher = Sha256::new();
 		hasher.update(data);
@@ -29,5 +31,93 @@ impl CryptoApi for TxContext {
 
 	fn verify_secp256k1(&self, _key: &[u8], _message: &[u8], _signature: &[u8]) -> bool {
 		panic!("verify_secp256k1 not implemented yet!")
+	}
+
+	fn add_ec(
+		&self,
+		curve: &EllipticCurve<RustBigUint>,
+		x_first_point: RustBigUint,
+		y_first_point: RustBigUint,
+		x_second_point: RustBigUint,
+		y_second_point: RustBigUint,
+	) -> (RustBigUint, RustBigUint) {
+		panic!("add_ec not implemented yet!")
+	}
+
+	fn double_ec(
+		&self,
+		curve: &EllipticCurve<RustBigUint>,
+		x_point: RustBigUint,
+		y_point: RustBigUint,
+	) -> (RustBigUint, RustBigUint) {
+		panic!("double_ec not implemented yet!")
+	}
+
+	fn is_on_curve_ec(
+		&self,
+		curve: &EllipticCurve<RustBigUint>,
+		x_point: RustBigUint,
+		y_point: RustBigUint,
+	) -> bool {
+		panic!("is_on_curve_ec not implemented yet!")
+	}
+
+	fn scalar_mult(
+		&self,
+		curve: &EllipticCurve<RustBigUint>,
+		x_point: RustBigUint,
+		y_point: RustBigUint,
+		data: BoxedBytes,
+	) -> (RustBigUint, RustBigUint) {
+		panic!("scalar_mult not implemented yet!")
+	}
+
+	fn scalar_base_mult(
+		&self,
+		curve: &EllipticCurve<RustBigUint>,
+		data: BoxedBytes,
+	) -> (RustBigUint, RustBigUint) {
+		panic!("scalar_base_mult not implemented yet!")
+	}
+
+	fn marshal_ec(
+		&self,
+		curve: &EllipticCurve<RustBigUint>,
+		x_pair: RustBigUint,
+		y_pair: RustBigUint,
+	) -> BoxedBytes {
+		panic!("marshal_ec not implemented yet!")
+	}
+
+	fn marshal_compressed_ec(
+		&self,
+		curve: &EllipticCurve<RustBigUint>,
+		x_pair: RustBigUint,
+		y_pair: RustBigUint,
+	) -> BoxedBytes {
+		panic!("marshal_compressed_ec not implemented yet!")
+	}
+
+	fn unmarshal_ec(
+		&self,
+		curve: &EllipticCurve<RustBigUint>,
+		data: BoxedBytes,
+	) -> (RustBigUint, RustBigUint) {
+		panic!("unmarshal_ec not implemented yet!")
+	}
+
+	fn unmarshal_compressed_ec(
+		&self,
+		curve: &EllipticCurve<RustBigUint>,
+		data: BoxedBytes,
+	) -> (RustBigUint, RustBigUint) {
+		panic!("unmarshal_compressed_ec not implemented yet!")
+	}
+
+	fn generate_key_ec(
+		&self,
+		curve: &EllipticCurve<RustBigUint>,
+	) -> (RustBigUint, RustBigUint, BoxedBytes) {
+		panic!("generate_key_ec not implemented yet!")
 	}
 }
--- elrond-wasm/src/api/crypto_api.rs
+++ elrond-wasm/src/api/crypto_api.rs
@@ -1,6 +1,11 @@
-use crate::types::H256;
+use super::{BigIntApi, BigUintApi};
+use crate::types::{BoxedBytes, EllipticCurve, H256};
 
-pub trait CryptoApi {
+pub trait CryptoApi<BigInt, BigUint>
+where
+	BigInt: BigIntApi<BigUint> + 'static,
+	BigUint: BigUintApi + 'static,
+{
 	fn sha256(&self, data: &[u8]) -> H256;
 
 	fn keccak256(&self, data: &[u8]) -> H256;
@@ -12,4 +17,67 @@ pub trait CryptoApi {
 	/// Note: the signature is minimum 2 bytes in length,
 	/// the second byte encodes the length of the remaining signature bytes.
 	fn verify_secp256k1(&self, key: &[u8], message: &[u8], signature: &[u8]) -> bool;
+
+	//Elliptic Curves functionalities
+
+	fn add_ec(
+		&self,
+		curve: &EllipticCurve<BigUint>,
+		x_first_point: BigUint,
+		y_first_point: BigUint,
+		x_second_point: BigUint,
+		y_second_point: BigUint,
+	) -> (BigUint, BigUint);
+
+	fn double_ec(
+		&self,
+		curve: &EllipticCurve<BigUint>,
+		x_point: BigUint,
+		y_point: BigUint,
+	) -> (BigUint, BigUint);
+
+	fn is_on_curve_ec(
+		&self,
+		curve: &EllipticCurve<BigUint>,
+		x_point: BigUint,
+		y_point: BigUint,
+	) -> bool;
+
+	fn scalar_mult(
+		&self,
+		curve: &EllipticCurve<BigUint>,
+		x_point: BigUint,
+		y_point: BigUint,
+		data: BoxedBytes,
+	) -> (BigUint, BigUint);
+
+	fn scalar_base_mult(
+		&self,
+		curve: &EllipticCurve<BigUint>,
+		data: BoxedBytes,
+	) -> (BigUint, BigUint);
+
+	fn marshal_ec(
+		&self,
+		curve: &EllipticCurve<BigUint>,
+		x_pair: BigUint,
+		y_pair: BigUint,
+	) -> BoxedBytes;
+
+	fn marshal_compressed_ec(
+		&self,
+		curve: &EllipticCurve<BigUint>,
+		x_pair: BigUint,
+		y_pair: BigUint,
+	) -> BoxedBytes;
+
+	fn unmarshal_ec(&self, curve: &EllipticCurve<BigUint>, data: BoxedBytes) -> (BigUint, BigUint);
+
+	fn unmarshal_compressed_ec(
+		&self,
+		curve: &EllipticCurve<BigUint>,
+		data: BoxedBytes,
+	) -> (BigUint, BigUint);
+
+	fn generate_key_ec(&self, curve: &EllipticCurve<BigUint>) -> (BigUint, BigUint, BoxedBytes);
 }
