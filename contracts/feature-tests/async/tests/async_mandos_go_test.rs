378fc2b314c88cc2bb74ab8afb749845951a708a
andrei-marinica <andrei.marinica@elrond.com>
2021-05-26 00:06:59 +0300

    cargo fmt

--- contracts/feature-tests/async/tests/async_mandos_go_test.rs
+++ contracts/feature-tests/async/tests/async_mandos_go_test.rs
@@ -1,206 +1,206 @@
 #[test]
 fn forw_raw_async_accept_egld_go() {
-    elrond_wasm_debug::mandos_go("mandos/forw_raw_async_accept_egld.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forw_raw_async_accept_egld.scen.json");
 }
 
 #[test]
 fn forw_raw_async_accept_esdt_go() {
-    elrond_wasm_debug::mandos_go("mandos/forw_raw_async_accept_esdt.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forw_raw_async_accept_esdt.scen.json");
 }
 
 #[test]
 fn forw_raw_async_echo_go() {
-    elrond_wasm_debug::mandos_go("mandos/forw_raw_async_echo.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forw_raw_async_echo.scen.json");
 }
 
 #[test]
 fn forw_raw_direct_egld_go() {
-    elrond_wasm_debug::mandos_go("mandos/forw_raw_direct_egld.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forw_raw_direct_egld.scen.json");
 }
 
 #[test]
 fn forw_raw_direct_esdt_go() {
-    elrond_wasm_debug::mandos_go("mandos/forw_raw_direct_esdt.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forw_raw_direct_esdt.scen.json");
 }
 
 #[test]
 fn forw_raw_sync_echo_go() {
-    elrond_wasm_debug::mandos_go("mandos/forw_raw_sync_echo.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forw_raw_sync_echo.scen.json");
 }
 
 #[test]
 fn forw_raw_sync_egld_go() {
-    elrond_wasm_debug::mandos_go("mandos/forw_raw_sync_egld.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forw_raw_sync_egld.scen.json");
 }
 
 #[test]
 fn forwarder_call_async_accept_egld_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_async_accept_egld.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_async_accept_egld.scen.json");
 }
 
 #[test]
 fn forwarder_call_async_accept_esdt_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_async_accept_esdt.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_async_accept_esdt.scen.json");
 }
 
 #[test]
 fn forwarder_call_async_accept_nft_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_async_accept_nft.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_async_accept_nft.scen.json");
 }
 
 #[test]
 fn forwarder_call_sync_accept_egld_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_sync_accept_egld.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_sync_accept_egld.scen.json");
 }
 
 #[test]
 fn forwarder_call_sync_accept_esdt_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_sync_accept_esdt.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_sync_accept_esdt.scen.json");
 }
 
 #[test]
 fn forwarder_call_sync_accept_nft_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_sync_accept_nft.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_sync_accept_nft.scen.json");
 }
 
 #[test]
 fn forwarder_call_sync_accept_then_read_egld_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_sync_accept_then_read_egld.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_sync_accept_then_read_egld.scen.json");
 }
 
 #[test]
 fn forwarder_call_sync_accept_then_read_esdt_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_sync_accept_then_read_esdt.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_sync_accept_then_read_esdt.scen.json");
 }
 
 #[test]
 fn forwarder_call_sync_accept_then_read_nft_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_sync_accept_then_read_nft.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_sync_accept_then_read_nft.scen.json");
 }
 
 #[test]
 fn forwarder_call_transf_exec_accept_egld_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_transf_exec_accept_egld.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_transf_exec_accept_egld.scen.json");
 }
 
 #[test]
 fn forwarder_call_transf_exec_accept_egld_twice_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_transf_exec_accept_egld_twice.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_transf_exec_accept_egld_twice.scen.json");
 }
 
 #[test]
 fn forwarder_call_transf_exec_accept_esdt_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_transf_exec_accept_esdt.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_transf_exec_accept_esdt.scen.json");
 }
 
 #[test]
 fn forwarder_call_transf_exec_accept_esdt_twice_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_transf_exec_accept_esdt_twice.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_transf_exec_accept_esdt_twice.scen.json");
 }
 
 #[test]
 fn forwarder_call_transf_exec_accept_nft_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_transf_exec_accept_nft.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_transf_exec_accept_nft.scen.json");
 }
 
 #[test]
 fn forwarder_call_transf_exec_accept_sft_twice_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_call_transf_exec_accept_sft_twice.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_call_transf_exec_accept_sft_twice.scen.json");
 }
 
 #[test]
 fn forwarder_nft_create_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_nft_create.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_nft_create.scen.json");
 }
 
 #[test]
 fn forwarder_nft_transfer_async_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_nft_transfer_async.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_nft_transfer_async.scen.json");
 }
 
 #[test]
 fn forwarder_nft_transfer_exec_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_nft_transfer_exec.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_nft_transfer_exec.scen.json");
 }
 
 #[test]
 fn forwarder_send_twice_egld_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_send_twice_egld.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_send_twice_egld.scen.json");
 }
 
 #[test]
 fn forwarder_send_twice_esdt_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_send_twice_esdt.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_send_twice_esdt.scen.json");
 }
 
 #[test]
 fn forwarder_sync_echo_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_sync_echo.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_sync_echo.scen.json");
 }
 
 #[test]
 fn forwarder_sync_echo_range_go() {
-    elrond_wasm_debug::mandos_go("mandos/forwarder_sync_echo_range.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/forwarder_sync_echo_range.scen.json");
 }
 
 #[test]
 fn message_othershard_go() {
-    elrond_wasm_debug::mandos_go("mandos/message_otherShard.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/message_otherShard.scen.json");
 }
 
 #[test]
 fn message_othershard_callback_go() {
-    elrond_wasm_debug::mandos_go("mandos/message_otherShard_callback.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/message_otherShard_callback.scen.json");
 }
 
 #[test]
 fn message_sameshard_go() {
-    elrond_wasm_debug::mandos_go("mandos/message_sameShard.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/message_sameShard.scen.json");
 }
 
 #[test]
 fn message_sameshard_callback_go() {
-    elrond_wasm_debug::mandos_go("mandos/message_sameShard_callback.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/message_sameShard_callback.scen.json");
 }
 
 #[test]
 fn payment_othershard_go() {
-    elrond_wasm_debug::mandos_go("mandos/payment_otherShard.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/payment_otherShard.scen.json");
 }
 
 #[test]
 fn payment_othershard_callback_go() {
-    elrond_wasm_debug::mandos_go("mandos/payment_otherShard_callback.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/payment_otherShard_callback.scen.json");
 }
 
 #[test]
 fn payment_sameshard_go() {
-    elrond_wasm_debug::mandos_go("mandos/payment_sameShard.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/payment_sameShard.scen.json");
 }
 
 #[test]
 fn payment_sameshard_callback_go() {
-    elrond_wasm_debug::mandos_go("mandos/payment_sameShard_callback.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/payment_sameShard_callback.scen.json");
 }
 
 #[test]
 fn recursive_caller_egld_1_go() {
-    elrond_wasm_debug::mandos_go("mandos/recursive_caller_egld_1.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/recursive_caller_egld_1.scen.json");
 }
 
 #[test]
 fn recursive_caller_egld_2_go() {
-    elrond_wasm_debug::mandos_go("mandos/recursive_caller_egld_2.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/recursive_caller_egld_2.scen.json");
 }
 
 #[test]
 fn recursive_caller_egld_x_go() {
-    elrond_wasm_debug::mandos_go("mandos/recursive_caller_egld_x.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/recursive_caller_egld_x.scen.json");
 }
 
 #[test]
 fn recursive_caller_esdt_1_go() {
-    elrond_wasm_debug::mandos_go("mandos/recursive_caller_esdt_1.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/recursive_caller_esdt_1.scen.json");
 }
 
 // #[test]
@@ -215,11 +215,10 @@ fn recursive_caller_esdt_1_go() {
 
 #[test]
 fn send_egld_go() {
-    elrond_wasm_debug::mandos_go("mandos/send_egld.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/send_egld.scen.json");
 }
 
 #[test]
 fn send_esdt_go() {
-    elrond_wasm_debug::mandos_go("mandos/send_esdt.scen.json");
+	elrond_wasm_debug::mandos_go("mandos/send_esdt.scen.json");
 }
-
