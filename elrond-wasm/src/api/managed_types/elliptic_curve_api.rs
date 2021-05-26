e10ea643009cb1aa310d05291e922e26ed57c4b2
andrei-diaconescu <andreidiaconescu_1999@yahoo.com>
2021-05-26 14:39:32 +0300

    moved bigint,biguint,ec into one managedTypes folder

---
+++ elrond-wasm/src/api/managed_types/elliptic_curve_api.rs
@@ -0,0 +1,100 @@
+use crate::abi;
+use crate::types::BoxedBytes;
+
+/// Definition of the EllipticCurve type required by the API
+pub trait EllipticCurveApi:
+Sized
++ elrond_codec::NestedEncode
++ elrond_codec::TopEncode
++ elrond_codec::NestedDecode
++ elrond_codec::TopDecode
++ abi::TypeAbi
+{
+    type EllipticCurve;
+    type BigUint;
+
+    fn new_elliptic_curve(
+        field_order: Self::BigUint,
+		base_point_order: Self::BigUint,
+		eq_constant: Self::BigUint,
+		x_base_point: Self::BigUint,
+		y_base_point: Self::BigUint,
+		size_of_field: i32,
+    ) -> Self;
+
+    fn p224_ec() -> Self;
+
+    fn p256_ec() -> Self;
+
+    fn p384_ec() -> Self;
+
+    fn p521_ec() -> Self;
+
+    fn add_ec(
+        &self,
+        curve: &Self::EllipticCurve,
+        x_first_point: Self::BigUint,
+		y_first_point: Self::BigUint,
+		x_second_point: Self::BigUint,
+		y_second_point: Self::BigUint,
+    ) ->(Self::BigUint,Self::BigUint);
+
+    fn double_ec(
+		&self,
+		curve: &Self::EllipticCurve,
+		x_point: Self::BigUint,
+		y_point: Self::BigUint,
+	) -> (Self::BigUint, Self::BigUint);
+
+	fn is_on_curve_ec(
+		&self,
+		curve: &Self::EllipticCurve,
+		x_point: Self::BigUint,
+		y_point: Self::BigUint,
+	) -> bool;
+
+	fn scalar_mult(
+		&self,
+		curve: &Self::EllipticCurve,
+		x_point: Self::BigUint,
+		y_point: Self::BigUint,
+		data: BoxedBytes,
+	) -> (Self::BigUint, Self::BigUint);
+
+	fn scalar_base_mult(
+		&self,
+		curve: &Self::EllipticCurve,
+		data: BoxedBytes,
+	) -> (Self::BigUint, Self::BigUint);
+
+	fn marshal_ec(
+		&self,
+		curve: &Self::EllipticCurve,
+		x_pair: Self::BigUint,
+		y_pair: Self::BigUint,
+	) -> BoxedBytes;
+
+	fn marshal_compressed_ec(
+		&self,
+		curve: &Self::EllipticCurve,
+		x_pair: Self::BigUint,
+		y_pair: Self::BigUint,
+	) -> BoxedBytes;
+
+	fn unmarshal_ec(
+		&self,
+		curve: &Self::EllipticCurve,
+		data: BoxedBytes,
+	) -> (Self::BigUint, Self::BigUint);
+
+	fn unmarshal_compressed_ec(
+		&self,
+		curve: &Self::EllipticCurve,
+		data: BoxedBytes,
+	) -> (Self::BigUint, Self::BigUint);
+
+	fn generate_key_ec(
+		&self,
+		curve: &Self::EllipticCurve,
+	) -> (Self::BigUint, Self::BigUint, BoxedBytes);
+}
