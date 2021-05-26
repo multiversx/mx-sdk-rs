ced39894530c8b1b0b0730ac336d4fbacd50969d
andrei-diaconescu <andreidiaconescu_1999@yahoo.com>
2021-05-26 14:46:16 +0300

    added the uncallable api

--- elrond-wasm/src/api/uncallable/mod.rs
+++ elrond-wasm/src/api/uncallable/mod.rs
@@ -3,12 +3,14 @@ mod big_uint_api_uncallable;
 mod error_api_uncallable;
 mod send_api_uncallable;
 mod storage_api_uncallable;
+mod elliptic_curve_api_uncallable;
 
 pub use big_int_api_uncallable::*;
 pub use big_uint_api_uncallable::*;
 pub use error_api_uncallable::*;
 pub use send_api_uncallable::*;
 pub use storage_api_uncallable::*;
+pub use elliptic_curve_api_uncallable::*;
 
 /// Dummy type with no implementation.
 /// Provides context in ABI generators.
