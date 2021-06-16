5f2ceafa6beb2f72a6946535eada8f98c9529a90
andrei-marinica <andrei.marinica@elrond.com>
2021-06-16 08:26:48 +0300

    forwarder fuzz - call types, max depth

--- contracts/feature-tests/async/forwarder/src/call_programmed.rs
+++ contracts/feature-tests/async/forwarder/src/call_programmed.rs
@@ -1,18 +1,23 @@
 elrond_wasm::imports!();
 elrond_wasm::derive_imports!();
 
-#[derive(TopEncode, TopDecode, TypeAbi)]
+#[derive(TopEncode, TopDecode, NestedEncode, NestedDecode, TypeAbi)]
 pub enum ProgrammedCallType {
 	SyncCall,
+	AsyncCall,
+	TransferExecute,
 }
 
 #[derive(TopEncode, TopDecode, TypeAbi)]
 pub struct ProgrammedCall<BigUint: BigUintApi> {
+	call_type: ProgrammedCallType,
 	to: Address,
 	payment_token: TokenIdentifier,
 	payment_nonce: u64,
 	payment_amount: BigUint,
 }
+
+const MAX_SYNC_CALL_DEPTH: usize = 10;
 
 #[elrond_wasm_derive::module]
 pub trait ForwarderProgrammedCallModule {
@@ -26,12 +31,14 @@ pub trait ForwarderProgrammedCallModule {
 	#[endpoint]
 	fn add_programmed_call(
 		&self,
+		call_type: ProgrammedCallType,
 		to: Address,
 		payment_token: TokenIdentifier,
 		payment_nonce: u64,
 		payment_amount: Self::BigUint,
 	) {
 		self.programmed_calls().push_back(ProgrammedCall {
+			call_type,
 			to,
 			payment_token,
 			payment_nonce,
@@ -40,23 +47,66 @@ pub trait ForwarderProgrammedCallModule {
 	}
 
 	#[endpoint]
-	fn forward_programmed_calls(&self) {
-		while let Some(call) = self.programmed_calls().pop_front() {
-			let () = self
-				.self_proxy(call.to)
-				.forward_programmed_calls()
-				.execute_on_dest_context();
+	#[payable("*")]
+	fn forward_programmed_calls(
+		&self,
+		#[payment_token] token_identifier: TokenIdentifier,
+		#[payment_nonce] token_nonce: u64,
+		#[payment_amount] token_payment: Self::BigUint,
+		sync_call_depth: usize,
+	) -> OptionalResult<AsyncCall<Self::SendApi>> {
+		self.forward_programmed_calls_event(&token_identifier, token_nonce, &token_payment);
 
-			// self.execute_on_dest_context_result_event(result.as_slice());
+		while let Some(call) = self.programmed_calls().pop_front() {
+			match call.call_type {
+				ProgrammedCallType::SyncCall => {
+					if sync_call_depth >= MAX_SYNC_CALL_DEPTH - 1 {
+						return OptionalResult::None;
+					}
+					let _ = self
+						.self_proxy(call.to)
+						.forward_programmed_calls(
+							call.payment_token,
+							call.payment_nonce,
+							call.payment_amount,
+							sync_call_depth + 1,
+						)
+						.execute_on_dest_context();
+				},
+				ProgrammedCallType::AsyncCall => {
+					return OptionalResult::Some(
+						self.self_proxy(call.to)
+							.forward_programmed_calls(
+								call.payment_token,
+								call.payment_nonce,
+								call.payment_amount,
+								0,
+							)
+							.async_call(),
+					)
+				},
+				ProgrammedCallType::TransferExecute => {
+					let () = self
+						.self_proxy(call.to)
+						.forward_programmed_calls(
+							call.payment_token,
+							call.payment_nonce,
+							call.payment_amount,
+							0,
+						)
+						.transfer_execute();
+				},
+			}
 		}
+
+		OptionalResult::None
 	}
 
 	#[event("forward_programmed_calls")]
 	fn forward_programmed_calls_event(
 		&self,
 		#[indexed] token_identifier: &TokenIdentifier,
-		#[indexed] token_type: &[u8],
-		#[indexed] token_payment: &Self::BigUint,
 		#[indexed] token_nonce: u64,
+		#[indexed] token_payment: &Self::BigUint,
 	);
 }
