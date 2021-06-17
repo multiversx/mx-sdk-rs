b5c650d0611c3874a1984e6f5bfb44c438914672
Alin Cruceat <alyn_509@yahoo.com>
2021-06-17 13:37:14 +0300

    separated NFT & SFT common methods

--- contracts/examples/bonding-curve/src/tokens/non_fungible_token.rs
+++ contracts/examples/bonding-curve/src/tokens/non_fungible_token.rs
@@ -1,17 +1,13 @@
 elrond_wasm::imports!();
 elrond_wasm::derive_imports!();
 
-use crate::{events, storage};
+use crate::{common_methods, events, storage};
 
-#[derive(TopEncode, TopDecode, TypeAbi)]
-pub struct Color {
-	r: u8,
-	g: u8,
-	b: u8,
-}
 #[elrond_wasm_derive::module]
 
-pub trait NFTModule<Color>: storage::StorageModule + events::EventsModule {
+pub trait NFTModule<Color>:
+	storage::StorageModule + events::EventsModule + common_methods::CommonMethods
+{
 	#[payable("EGLD")]
 	#[endpoint(nftIssue)]
 	fn nft_issue(
@@ -38,51 +34,5 @@ pub trait NFTModule<Color>: storage::StorageModule + events::EventsModule {
 			)
 			.async_call()
 			.with_callback(self.callbacks().nft_issue_callback(&caller))
-	}
-
-	#[callback]
-	fn nft_issue_callback(
-		&self,
-		caller: &Address,
-		#[call_result] result: AsyncCallResult<TokenIdentifier>,
-	) {
-		match result {
-			AsyncCallResult::Ok(token_identifier) => {
-				self.issued_token().set(&token_identifier);
-			},
-			AsyncCallResult::Err(_) => {
-				let (returned_tokens, token_identifier) = self.call_value().payment_token_pair();
-				if token_identifier.is_egld() && returned_tokens > 0 {
-					self.send().direct_egld(caller, &returned_tokens, &[]);
-				}
-			},
-		}
-	}
-
-	#[endpoint(nftCreate)]
-	fn nft_create(
-		&self,
-		token_identifier: TokenIdentifier,
-		amount: Self::BigUint,
-		name: BoxedBytes,
-		royalties: Self::BigUint,
-		hash: BoxedBytes,
-		color: Color,
-		uri: BoxedBytes,
-	) {
-		self.send().esdt_nft_create::<Color>(
-			&token_identifier,
-			&amount,
-			&name,
-			&royalties,
-			&hash,
-			&color,
-			&[uri],
-		);
-	}
-
-	#[endpoint(nftBurn)]
-	fn nft_burn(&self, token_identifier: TokenIdentifier, nonce: u64, amount: Self::BigUint) {
-		self.send().esdt_nft_burn(&token_identifier, nonce, &amount);
 	}
 }
--- contracts/examples/bonding-curve/src/tokens/semi_fungible_token.rs
+++ contracts/examples/bonding-curve/src/tokens/semi_fungible_token.rs
@@ -1,10 +1,12 @@
 elrond_wasm::imports!();
 elrond_wasm::derive_imports!();
 
-use crate::{events, storage};
+use crate::{common_methods, events, storage};
 
 #[elrond_wasm_derive::module]
-pub trait SFTModule: storage::StorageModule + events::EventsModule {
+pub trait SFTModule<Color>:
+	storage::StorageModule + events::EventsModule + common_methods::CommonMethods
+{
 	#[payable("EGLD")]
 	#[endpoint(sftIssue)]
 	fn sft_issue(
@@ -30,29 +32,10 @@ pub trait SFTModule: storage::StorageModule + events::EventsModule {
 				},
 			)
 			.async_call()
-			.with_callback(self.callbacks().sft_issue_callback(&caller))
+			.with_callback(self.callbacks().nft_issue_callback(caller))
 	}
 
-	#[callback]
-	fn sft_issue_callback(
-		&self,
-		caller: &Address,
-		#[call_result] result: AsyncCallResult<TokenIdentifier>,
-	) {
-		match result {
-			AsyncCallResult::Ok(token_identifier) => {
-				self.issued_token().set(&token_identifier);
-			},
-			AsyncCallResult::Err(_) => {
-				let (returned_tokens, token_identifier) = self.call_value().payment_token_pair();
-				if token_identifier.is_egld() && returned_tokens > 0 {
-					self.send().direct_egld(caller, &returned_tokens, &[]);
-				}
-			},
-		}
-	}
-
-	#[endpoint(nftAddQuantity)]
+	#[endpoint(sftAddQuantity)]
 	fn sft_add_quantity(
 		&self,
 		token_identifier: TokenIdentifier,
