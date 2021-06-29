159117131ae5dd7ef55cf58915252d4a5f24a230
Alin Cruceat <alyn_509@yahoo.com>
2021-06-29 11:59:37 +0300

    added readme

--- contracts/examples/bonding-curve/src/lib.rs
+++ contracts/examples/bonding-curve/src/lib.rs
@@ -28,32 +28,6 @@ mod non_fungible_token;
 #[path = "tokens/semi_fungible_token.rs"]
 mod semi_fungible_token;
 
-// This contract enables using a bonding curve for defining the behaviour of the price of the token as its balance changes.
-//
-// The contract allows issuing of any ESDT token and together with its issue elements such as details about the supply
-// will be stored together with the Balance in an entity called CurveArguments. Because of working with different types of ESDT,
-// the entity under which we will make the mapping with the curve function will be called Token, containing the TokenIdentifier and the nonce.
-// The behaviour however is differend depending
-// on the issued token:
-// - FT:
-//		* defines one bonding curve
-//		* the nonce from Token should be set 0
-//		* the supply and balance are indicated by the amount minted
-// - SFT:
-//		* defines multiple bonding curves (one per each nonce)
-//		* the supply and balance are indicated by the amount of each nonce
-// - NFT:
-//		* defines one bonding curve
-//		* the nonce from Token should be set 0
-//		* the supply and balance are indicated by the number of nonces
-//
-// The bonding curve functions are set up in function_selector.rs
-//
-// When using this contract one should do the following process for each issued token:
-//	- issue the token
-//  - mint the token
-//	- set the curve function
-
 #[elrond_wasm_derive::contract]
 pub trait BondingCurve:
 	fungible_token::FTModule
