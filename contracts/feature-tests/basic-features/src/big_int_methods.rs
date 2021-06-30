c0811a8273a8d5e2236b35b07294c7d7cdc53828
Andrei Marinica <andrei.marinica@elrond.com>
2021-06-30 16:48:28 +0300

    big int to small int API

--- contracts/feature-tests/basic-features/src/big_int_methods.rs
+++ contracts/feature-tests/basic-features/src/big_int_methods.rs
@@ -2,4 +2,14 @@ elrond_wasm::imports!();
 
 /// TODO: add neg, abs, sqrt etc. here
 #[elrond_wasm_derive::module]
-pub trait BigIntMethods {}
+pub trait BigIntMethods {
+	#[endpoint]
+	fn big_uint_to_u64(&self, bu: &Self::BigUint) -> OptionalResult<u64> {
+		bu.to_u64().into()
+	}
+
+	#[endpoint]
+	fn big_int_to_i64(&self, bi: &Self::BigInt) -> OptionalResult<i64> {
+		bi.to_i64().into()
+	}
+}
