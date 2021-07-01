4ae4df6063ff93acb835d2d700a2e8ae0658fcd0
dorin.iancu <dorin.iancu@elrond.com>
2021-07-01 16:11:24 +0300

    propose logic

--- contracts/modules/elrond-wasm-module-governance/src/governance_proposal.rs
+++ contracts/modules/elrond-wasm-module-governance/src/governance_proposal.rs
@@ -1,10 +1,13 @@
 use elrond_wasm::{
 	api::BigUintApi,
-	types::{Address, BoxedBytes},
+	types::{Address, BoxedBytes, MultiArg5, TokenIdentifier},
 	Vec,
 };
 
 elrond_wasm::derive_imports!();
+
+pub type GovernanceActionAsMultiArg<BigUint> =
+	MultiArg5<Address, TokenIdentifier, BigUint, BoxedBytes, Vec<BoxedBytes>>;
 
 #[derive(TypeAbi, TopEncode, TopDecode, PartialEq)]
 pub enum GovernanceProposalStatus {
@@ -22,7 +25,8 @@ pub enum GovernanceProposalStatus {
 #[derive(TypeAbi, TopEncode, TopDecode, NestedEncode, NestedDecode)]
 pub struct GovernanceAction<BigUint: BigUintApi> {
 	pub dest_address: Address,
-	pub call_value: BigUint,
+	pub token_id: TokenIdentifier,
+	pub amount: BigUint,
 	pub function_name: BoxedBytes,
 	pub arguments: Vec<BoxedBytes>,
 }
