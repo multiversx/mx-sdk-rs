f9b99cc266502d308ccd26066ea9f784689fe9df
dorin.iancu <dorin.iancu@elrond.com>
2021-07-08 11:04:35 +0300

    add test functions

--- contracts/feature-tests/use-module/tests/use_module_mandos_rs_test.rs
+++ contracts/feature-tests/use-module/tests/use_module_mandos_rs_test.rs
@@ -67,3 +67,30 @@ fn use_module_internal_rs() {
 fn use_module_pause_rs() {
 	elrond_wasm_debug::mandos_rs("mandos/use_module_pause.scen.json", &contract_map());
 }
+
+// Governance module tests
+
+#[test]
+fn cancel_defeated_proposal_rs() {
+    elrond_wasm_debug::mandos_rs("mandos/use_module_governance/cancel_defeated_proposal.scen.json", &contract_map());
+}
+
+#[test]
+fn change_configuration_rs() {
+    elrond_wasm_debug::mandos_rs("mandos/use_module_governance/change_configuration.scen.json", &contract_map());
+}
+
+#[test]
+fn init_rs() {
+    elrond_wasm_debug::mandos_rs("mandos/use_module_governance/init.scen.json", &contract_map());
+}
+
+#[test]
+fn invalid_proposals_rs() {
+    elrond_wasm_debug::mandos_rs("mandos/use_module_governance/invalid_proposals.scen.json", &contract_map());
+}
+
+#[test]
+fn withdraw_governance_tokens_rs() {
+    elrond_wasm_debug::mandos_rs("mandos/use_module_governance/withdraw_governance_tokens.scen.json", &contract_map());
+}
