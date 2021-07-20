49a08e55fc221166d3779d079fcf531edafe3764
dorin.iancu <dorin.iancu@elrond.com>
2021-07-20 16:55:27 +0300

    manual payment arg handling + tests

--- contracts/feature-tests/composability/forwarder/src/nft.rs
+++ contracts/feature-tests/composability/forwarder/src/nft.rs
@@ -27,13 +27,16 @@ pub trait ForwarderNftModule: storage::ForwarderStorageModule {
 	#[endpoint]
 	fn buy_nft(
 		&self,
-		#[payment_token] payment_token: TokenIdentifier,
-		#[payment_nonce] payment_nonce: u64,
-		#[payment_amount] payment_amount: Self::BigUint,
+		//#[payment_token] payment_token: TokenIdentifier,
+		//#[payment_nonce] payment_nonce: u64,
+		//#[payment_amount] payment_amount: Self::BigUint,
 		nft_id: TokenIdentifier,
 		nft_nonce: u64,
 		nft_amount: Self::BigUint,
 	) -> Self::BigUint {
+		let (payment_amount, payment_token) = self.call_value().payment_token_pair();
+		let payment_nonce = self.call_value().esdt_token_nonce();
+
 		self.send().sell_nft(
 			&nft_id,
 			nft_nonce,
