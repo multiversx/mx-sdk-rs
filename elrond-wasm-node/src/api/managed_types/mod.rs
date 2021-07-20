8c08916724941846e81ce6287509bfe83758b793
Andrei Marinica <andrei.marinica@elrond.com>
2021-07-20 12:54:33 +0300

    managed buffers - initial impl

--- elrond-wasm-node/src/api/managed_types/mod.rs
+++ elrond-wasm-node/src/api/managed_types/mod.rs
@@ -1,7 +1,9 @@
 mod big_int_api_node;
 mod big_uint_api_node;
 mod elliptic_curve_api_node;
+mod managed_buffer_node;
 
 pub use big_int_api_node::*;
 pub use big_uint_api_node::*;
 pub use elliptic_curve_api_node::*;
+pub use managed_buffer_node::*;
