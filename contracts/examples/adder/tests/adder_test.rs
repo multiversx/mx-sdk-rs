72a28df79dec42363d86916dd21ed8a71992481d
dorin.iancu <dorin.iancu@elrond.com>
2021-07-27 12:47:52 +0300

    update crypto-bubbles

--- contracts/examples/adder/tests/adder_test.rs
+++ contracts/examples/adder/tests/adder_test.rs
@@ -6,12 +6,12 @@ use elrond_wasm_debug::TxContext;
 fn test_add() {
 	let adder = adder::contract_obj(TxContext::dummy());
 
-	adder.init(&RustBigInt::from(5));
-	assert_eq!(RustBigInt::from(5), adder.get_sum());
+	adder.init(RustBigInt::from(5));
+	assert_eq!(RustBigInt::from(5), adder.sum().get());
 
-	let _ = adder.add(&RustBigInt::from(7));
-	assert_eq!(RustBigInt::from(12), adder.get_sum());
+	let _ = adder.add(RustBigInt::from(7));
+	assert_eq!(RustBigInt::from(12), adder.sum().get());
 
-	let _ = adder.add(&RustBigInt::from(1));
-	assert_eq!(RustBigInt::from(13), adder.get_sum());
+	let _ = adder.add(RustBigInt::from(1));
+	assert_eq!(RustBigInt::from(13), adder.sum().get());
 }
