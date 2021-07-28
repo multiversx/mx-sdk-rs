47697ea3760f0936bcac09695926daa3e723dc5b
Andrei Marinica <andrei.marinica@elrond.com>
2021-07-28 18:50:28 +0300

    order book - folder and crate rename

--- contracts/examples/order-book/pair/wasm/src/lib.rs
+++ contracts/examples/order-book/pair/wasm/src/lib.rs
@@ -1,4 +1,4 @@
 #![no_std]
 
-pub use pair::*;
+pub use order_book_pair::*;
 pub use elrond_wasm_output::*;
--- contracts/examples/sc-order-book-rs/factory/abi/src/main.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    elrond_wasm_debug::abi_json::print_abi::<factory::AbiProvider>();
-}
