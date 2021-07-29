9336aad53aa090fb99df64fa2ac44e3fd7fb047a
Andrei Marinica <andrei.marinica@elrond.com>
2021-07-29 09:54:00 +0300

    cargo fmt

--- elrond-wasm/src/api/managed_types/managed_buffer_api.rs
+++ elrond-wasm/src/api/managed_types/managed_buffer_api.rs
@@ -3,15 +3,15 @@ use crate::types::BoxedBytes;
 
 /// A raw bytes buffer managed by Arwen.
 pub trait ManagedBufferApi: Sized + TypeAbi {
-	fn new_empty() -> Self;
+    fn new_empty() -> Self;
 
-	fn new_from_bytes(bytes: &[u8]) -> Self;
+    fn new_from_bytes(bytes: &[u8]) -> Self;
 
-	fn len(&self) -> usize;
+    fn len(&self) -> usize;
 
-	fn overwrite(&mut self, value: &[u8]);
+    fn overwrite(&mut self, value: &[u8]);
 
-	fn extend_from_slice(&mut self, slice: &[u8]);
+    fn extend_from_slice(&mut self, slice: &[u8]);
 
-	fn to_boxed_bytes(&self) -> BoxedBytes;
+    fn to_boxed_bytes(&self) -> BoxedBytes;
 }
