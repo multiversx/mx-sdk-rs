133ea3a620f071549cf883fe551094e2df018e54
Alin Cruceat <alyn_509@yahoo.com>
2021-08-02 17:47:56 +0300

    replace ifs with map

--- elrond-wasm-debug/src/mandos_step/sc_call.rs
+++ elrond-wasm-debug/src/mandos_step/sc_call.rs
@@ -4,7 +4,6 @@ use num_bigint::BigUint;
 use num_traits::Zero;
 
 use crate::{BlockchainMock, ContractMap, TxContext, TxInput};
-
 pub fn execute(
     state: &mut BlockchainMock,
     contract_map: &ContractMap<TxContext>,
@@ -12,22 +11,18 @@ pub fn execute(
     tx: &Box<TxCall>,
     expect: &Option<TxExpect>,
 ) {
+    let mut esdt_value = BigUint::zero();
+    let mut esdt_token_identifier = Vec::new();
+    tx.esdt_value.as_ref().map(|value| {
+        esdt_value = value.esdt_value.value.clone();
+        esdt_token_identifier = value.esdt_token_name.value.clone();
+    });
     let tx_input = TxInput {
         from: tx.from.value.into(),
         to: tx.to.value.into(),
         call_value: tx.call_value.value.clone(),
-        esdt_value: if let Some(esdt) = &tx.esdt_value {
-            // TODO: clean this up
-            esdt.esdt_value.value.clone()
-        } else {
-            BigUint::zero()
-        },
-        esdt_token_identifier: if let Some(esdt) = &tx.esdt_value {
-            // TODO: clean this up
-            esdt.esdt_token_name.value.clone()
-        } else {
-            Vec::new()
-        },
+        esdt_value,
+        esdt_token_identifier,
         func_name: tx.function.as_bytes().to_vec(),
         args: tx
             .arguments
@@ -39,7 +34,7 @@ pub fn execute(
         tx_hash: execute_helper_functions::generate_tx_hash_dummy(tx_id.as_str()),
     };
     state.increase_nonce(&tx_input.from);
-    let tx_result = execute_sc_call_with_async_and_callback(tx_input, state, contract_map).unwrap();
+    let tx_result = sc_call_with_async_and_callback(tx_input, state, contract_map).unwrap();
     if let Some(tx_expect) = expect {
         check_tx_output(tx_id.as_str(), tx_expect, &tx_result);
     }
