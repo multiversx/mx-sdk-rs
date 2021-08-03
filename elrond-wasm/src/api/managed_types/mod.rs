d588b399c3dae4ae0b4a7d283bde5b309135e05b
Andrei Marinica <andrei.marinica@elrond.com>
2021-08-03 08:16:11 +0300

    managed types - moved to types

--- elrond-wasm/src/api/managed_types/mod.rs
+++ elrond-wasm/src/api/managed_types/mod.rs
@@ -1,14 +1,10 @@
-mod big_int;
 mod big_int_api;
-mod big_uint;
 mod big_uint_api;
 mod elliptic_curve_api;
 mod managed_buffer_api;
 mod managed_type_api;
 
-pub use big_int::*;
 pub use big_int_api::*;
-pub use big_uint::*;
 pub use big_uint_api::*;
 pub use elliptic_curve_api::*;
 pub use managed_buffer_api::*;
