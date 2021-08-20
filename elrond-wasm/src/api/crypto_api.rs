6113d90b2979850f8ca125445d1f37c4a90d3f79
dorin.iancu <dorin.iancu@elrond.com>
2021-08-20 16:00:06 +0300

    custom secp256k1 sig verification + sig generation

--- elrond-wasm/src/api/crypto_api.rs
+++ elrond-wasm/src/api/crypto_api.rs
@@ -1,5 +1,5 @@
 use super::BigUintApi;
-use crate::types::H256;
+use crate::types::{BoxedBytes, H256, MessageHashType};
 use alloc::boxed::Box;
 
 pub trait CryptoApi {
@@ -19,4 +19,14 @@ pub trait CryptoApi {
     /// Note: the signature is minimum 2 bytes in length,
     /// the second byte encodes the length of the remaining signature bytes.
     fn verify_secp256k1(&self, key: &[u8], message: &[u8], signature: &[u8]) -> bool;
+
+    fn verify_custom_secp256k1(
+        &self,
+        key: &[u8],
+        message: &[u8],
+        signature: &[u8],
+        hash_type: MessageHashType,
+    ) -> bool;
+
+    fn encode_secp256k1_der_signature(&self, r: &[u8], s: &[u8]) -> BoxedBytes;
 }
