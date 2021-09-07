24943015dd82dc5551975861acbf9d56466a73ed
dorin.iancu <dorin.iancu@elrond.com>
2021-09-07 14:21:08 +0300

    add test running function

--- contracts/examples/nft-minter/tests/mandos_go_test.rs
+++ contracts/examples/nft-minter/tests/mandos_go_test.rs
@@ -1,4 +1,14 @@
-/*#[test]
-fn adder_go() {
-    elrond_wasm_debug::mandos_go("mandos/adder.scen.json");
-}*/
+#[test]
+fn init_go() {
+    elrond_wasm_debug::mandos_go("mandos/init.scen.json");
+}
+
+#[test]
+fn create_nft_go() {
+    elrond_wasm_debug::mandos_go("mandos/create_nft.scen.json");
+}
+
+#[test]
+fn buy_nft_go() {
+    elrond_wasm_debug::mandos_go("mandos/buy_nft.scen.json");
+}
