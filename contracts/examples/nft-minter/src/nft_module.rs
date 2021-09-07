701790bf81f9284d4625ffa4ed43159cc0c581fc
dorin.iancu <dorin.iancu@elrond.com>
2021-09-07 13:12:53 +0300

    make attributes more generic

--- contracts/examples/nft-minter/src/nft_module.rs
+++ contracts/examples/nft-minter/src/nft_module.rs
@@ -7,11 +7,6 @@ const NFT_AMOUNT: u32 = 1;
 const ROYALTIES_MAX: u32 = 10_000;
 
 #[derive(TypeAbi, TopEncode, TopDecode)]
-pub struct ExampleAttributes {
-    pub creation_timestamp: u64,
-}
-
-#[derive(TypeAbi, TopEncode, TopDecode)]
 pub struct PriceTag<M: ManagedTypeApi> {
     pub token: TokenIdentifier<M>,
     pub nonce: u64,
@@ -63,66 +58,6 @@ pub trait NftModule {
                 &[EsdtLocalRole::NftCreate],
             )
             .async_call())
-    }
-
-    #[only_owner]
-    #[endpoint(createNft)]
-    fn create_nft(
-        &self,
-        name: BoxedBytes,
-        royalties: BigUint,
-        uri: BoxedBytes,
-        selling_price: BigUint,
-        #[var_args] opt_token_used_as_payment: OptionalArg<TokenIdentifier>,
-        #[var_args] opt_token_used_as_payment_nonce: OptionalArg<u64>,
-    ) -> SCResult<u64> {
-        self.require_token_issued()?;
-        self.require_local_roles_set()?;
-        require!(royalties <= ROYALTIES_MAX, "Royalties cannot exceed 100%");
-
-        let token_used_as_payment = opt_token_used_as_payment
-            .into_option()
-            .unwrap_or_else(|| self.types().token_identifier_egld());
-        require!(
-            token_used_as_payment.is_egld() || token_used_as_payment.is_valid_esdt_identifier(),
-            "Invalid token used as payment"
-        );
-
-        let token_used_as_payment_nonce = if token_used_as_payment.is_egld() {
-            0
-        } else {
-            opt_token_used_as_payment_nonce
-                .into_option()
-                .unwrap_or_default()
-        };
-
-        let nft_token_id = self.nft_token_id().get();
-        let attributes = ExampleAttributes {
-            creation_timestamp: self.blockchain().get_block_timestamp(),
-        };
-
-        let mut serialized_attributes = Vec::new();
-        attributes.top_encode(&mut serialized_attributes)?;
-
-        let attributes_hash = self.crypto().sha256(&serialized_attributes);
-
-        let nft_nonce = self.send().esdt_nft_create(
-            &nft_token_id,
-            &self.types().big_uint_from(NFT_AMOUNT),
-            &name,
-            &royalties,
-            &attributes_hash.as_bytes().into(),
-            &attributes,
-            &[uri],
-        );
-
-        self.price_tag(nft_nonce).set(&PriceTag {
-            token: token_used_as_payment,
-            nonce: token_used_as_payment_nonce,
-            amount: selling_price,
-        });
-
-        Ok(nft_nonce)
     }
 
     // endpoints
@@ -208,6 +143,46 @@ pub trait NftModule {
     }
 
     // private
+
+    fn create_nft_with_attributes<T: TopEncode>(
+        &self,
+        name: BoxedBytes,
+        royalties: BigUint,
+        attributes: T,
+        uri: BoxedBytes,
+        selling_price: BigUint,
+        token_used_as_payment: TokenIdentifier,
+        token_used_as_payment_nonce: u64,
+    ) -> SCResult<u64> {
+        self.require_token_issued()?;
+        self.require_local_roles_set()?;
+        require!(royalties <= ROYALTIES_MAX, "Royalties cannot exceed 100%");
+
+        let nft_token_id = self.nft_token_id().get();
+
+        let mut serialized_attributes = Vec::new();
+        attributes.top_encode(&mut serialized_attributes)?;
+
+        let attributes_hash = self.crypto().sha256(&serialized_attributes);
+
+        let nft_nonce = self.send().esdt_nft_create(
+            &nft_token_id,
+            &self.types().big_uint_from(NFT_AMOUNT),
+            &name,
+            &royalties,
+            &attributes_hash.as_bytes().into(),
+            &attributes,
+            &[uri],
+        );
+
+        self.price_tag(nft_nonce).set(&PriceTag {
+            token: token_used_as_payment,
+            nonce: token_used_as_payment_nonce,
+            amount: selling_price,
+        });
+
+        Ok(nft_nonce)
+    }
 
     fn require_token_issued(&self) -> SCResult<()> {
         require!(!self.nft_token_id().is_empty(), "Token not issued");
