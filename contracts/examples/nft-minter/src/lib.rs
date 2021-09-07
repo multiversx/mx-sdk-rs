659e5f01bec6d42dc21635ebef97ff23f29ed3b5
dorin.iancu <dorin.iancu@elrond.com>
2021-09-07 13:12:53 +0300

    make attributes more generic

--- contracts/examples/nft-minter/src/lib.rs
+++ contracts/examples/nft-minter/src/lib.rs
@@ -1,11 +1,52 @@
 #![no_std]
 
 elrond_wasm::imports!();
+elrond_wasm::derive_imports!();
 
 mod nft_module;
+
+#[derive(TypeAbi, TopEncode, TopDecode)]
+pub struct ExampleAttributes {
+    pub creation_timestamp: u64,
+}
 
 #[elrond_wasm::derive::contract]
 pub trait NftMinter: nft_module::NftModule {
     #[init]
     fn init(&self) {}
+
+    #[only_owner]
+    #[endpoint]
+    fn create_nft(
+        &self,
+        name: BoxedBytes,
+        royalties: BigUint,
+        attributes: ExampleAttributes,
+        uri: BoxedBytes,
+        selling_price: BigUint,
+        #[var_args] opt_token_used_as_payment: OptionalArg<TokenIdentifier>,
+        #[var_args] opt_token_used_as_payment_nonce: OptionalArg<u64>,
+    ) -> SCResult<u64> {
+        let token_used_as_payment = opt_token_used_as_payment
+            .into_option()
+            .unwrap_or_else(|| self.types().token_identifier_egld());
+
+        let token_used_as_payment_nonce = if token_used_as_payment.is_egld() {
+            0
+        } else {
+            opt_token_used_as_payment_nonce
+                .into_option()
+                .unwrap_or_default()
+        };
+
+        self.create_nft_with_attributes(
+            name,
+            royalties,
+            attributes,
+            uri,
+            selling_price,
+            token_used_as_payment,
+            token_used_as_payment_nonce,
+        )
+    }
 }
