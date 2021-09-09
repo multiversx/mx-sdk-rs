e955289289205123ccfb6d8bc498f6dbad4a6112
Claudiu-Marcel Bruda <claudiu725@yahoo.com>
2021-09-09 19:32:34 +0300

    Fixes: handle first token as well, and only once

--- elrond-wasm-derive/src/preprocessing.rs
+++ elrond-wasm-derive/src/preprocessing.rs
@@ -116,24 +116,30 @@ fn perform_substitutions(input: TokenStream, substitutions: &SubstitutionsMap) -
     let mut result = Vec::<TokenTree>::new();
     let mut tt_iter = input.into_iter();
     let mut to_skip: usize = 0;
-    while let Some(tt) = tt_iter.next() {
+    loop {
         if to_skip > 0 {
             to_skip -= 1;
+            tt_iter.next();
             continue;
-        }
-        match tt {
-            TokenTree::Group(g) => {
-                result.push(TokenTree::Group(Group::new(
-                    g.delimiter(),
-                    perform_substitutions(g.stream(), substitutions),
-                )));
-                continue;
-            },
-            _ => result.push(tt),
         }
         if let Some((sub_length, sub)) = check_subsequence(substitutions, tt_iter.clone()) {
             result.extend(sub.clone().into_iter());
             to_skip = sub_length;
+            continue;
+        }
+        if let Some(tt) = tt_iter.next() {
+            match tt {
+                TokenTree::Group(g) => {
+                    result.push(TokenTree::Group(Group::new(
+                        g.delimiter(),
+                        perform_substitutions(g.stream(), substitutions),
+                    )));
+                    continue;
+                },
+                _ => result.push(tt),
+            }
+        } else {
+            break;
         }
     }
     proc_macro::TokenStream::from_iter(result.into_iter())
