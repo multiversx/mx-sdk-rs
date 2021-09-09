debac1bda5d746edfd670542190577351df47017
Andrei Marinica <andrei.marinica@elrond.com>
2021-09-09 17:14:36 +0300

    contract api refactor - cleanup

--- elrond-wasm/src/contract_base/mod.rs
+++ elrond-wasm/src/contract_base/mod.rs
@@ -1,13 +1,13 @@
+mod callable_contract;
 mod contract_abi_provider;
 mod contract_base_trait;
-mod contract_traits;
 mod proxy_obj_base;
 mod proxy_obj_callback_base;
 mod wrappers;
 
-pub use contract_abi_provider::*;
+pub use callable_contract::CallableContract;
+pub use contract_abi_provider::ContractAbiProvider;
 pub use contract_base_trait::ContractBase;
-pub use contract_traits::*;
 pub use proxy_obj_base::ProxyObjApi;
 pub use proxy_obj_callback_base::CallbackProxyObjApi;
 pub use wrappers::*;
