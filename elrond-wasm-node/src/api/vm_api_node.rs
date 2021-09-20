8575c5db19aa69cd45ecc5eab56d22912a22a1cb
Andrei Marinica <andrei.marinica@elrond.com>
2021-09-20 10:08:18 +0300

    ManagedVec - nested decode

--- elrond-wasm-node/src/api/vm_api_node.rs
+++ elrond-wasm-node/src/api/vm_api_node.rs
@@ -1,4 +1,6 @@
 use crate::ArwenApiImpl;
-use elrond_wasm::api::VMApi;
+use elrond_wasm::{api::VMApi, elrond_codec::TryStaticCast};
+
+impl TryStaticCast for ArwenApiImpl {}
 
 impl VMApi for ArwenApiImpl {}
