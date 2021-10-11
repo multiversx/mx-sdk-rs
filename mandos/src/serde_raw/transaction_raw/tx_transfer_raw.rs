e4b002b3245cf261e9f6ea6e5db7e79efceafa05
Alin Cruceat <alyn_509@yahoo.com>
2021-10-11 16:11:44 +0300

    fix set_check_esdt wrong uri verification

--- mandos/src/serde_raw/transaction_raw/tx_transfer_raw.rs
+++ mandos/src/serde_raw/transaction_raw/tx_transfer_raw.rs
@@ -7,7 +7,15 @@ use crate::serde_raw::{TxESDTRaw, ValueSubTree};
 pub struct TxTransferRaw {
     pub from: ValueSubTree,
     pub to: ValueSubTree,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub value: Option<ValueSubTree>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub egld: Option<ValueSubTree>,
+
+    #[serde(default)]
     pub esdt: Vec<TxESDTRaw>,
 }
