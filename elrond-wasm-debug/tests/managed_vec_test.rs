a2071bd039c92834af4bd687665707c4bfa245e3
Catalin Neagu <neagu.catalin.f@gmail.com>
2021-10-14 14:14:47 +0300

    managed vec tests: added more tests

--- elrond-wasm-debug/tests/managed_vec_test.rs
+++ elrond-wasm-debug/tests/managed_vec_test.rs
@@ -26,7 +26,7 @@ fn test_managed_vec_iter_rev() {
 }
 
 #[test]
-fn test_managed_into_vec() {
+fn test_into_vec() {
     let context = DebugApi::dummy();
 
     let mut vec = Vec::new();
@@ -36,6 +36,74 @@ fn test_managed_into_vec() {
         managed_vec.push(biguint.clone());
         vec.push(biguint);
     }
+
+    assert_eq!(vec, managed_vec.into_vec());
+}
+
+#[test]
+fn test_managed_from() {
+    let context = DebugApi::dummy();
+
+    let mut vec = Vec::new();
+    for i in 20u64..=30u64 {
+        let biguint = BigUint::managed_from(context.clone(), i);
+        vec.push(biguint);
+    }
+
+    let managed_vec =
+        ManagedVec::<DebugApi, BigUint<DebugApi>>::managed_from(context.clone(), vec.clone());
+
+    assert_eq!(vec, managed_vec.into_vec());
+}
+
+#[test]
+fn test_append_vec() {
+    let context = DebugApi::dummy();
+
+    let mut vec1 = Vec::new();
+    let mut vec2 = Vec::new();
+    let mut vec = Vec::new();
+
+    for i in 20u64..=30u64 {
+        let biguint = BigUint::managed_from(context.clone(), i);
+        vec.push(biguint.clone());
+        vec1.push(biguint);
+    }
+
+    for i in 20u64..=30u64 {
+        let biguint = BigUint::managed_from(context.clone(), i);
+        vec.push(biguint.clone());
+        vec2.push(biguint);
+    }
+
+    let managed_vec =
+        ManagedVec::<DebugApi, BigUint<DebugApi>>::managed_from(context.clone(), vec.clone());
+    let mut managed_vec1 =
+        ManagedVec::<DebugApi, BigUint<DebugApi>>::managed_from(context.clone(), vec1.clone());
+    let managed_vec2 =
+        ManagedVec::<DebugApi, BigUint<DebugApi>>::managed_from(context.clone(), vec2.clone());
+
+    managed_vec1.append_vec(managed_vec2);
+    assert_eq!(managed_vec, managed_vec1);
+}
+
+#[test]
+fn test_overwrite_with_single_item() {
+    let context = DebugApi::dummy();
+
+    let mut vec = Vec::new();
+    for i in 20u64..=30u64 {
+        let biguint = BigUint::managed_from(context.clone(), i);
+        vec.push(biguint);
+    }
+
+    let mut managed_vec =
+        ManagedVec::<DebugApi, BigUint<DebugApi>>::managed_from(context.clone(), vec.clone());
+    assert_eq!(vec, managed_vec.clone().into_vec());
+
+    let single_elem = BigUint::managed_from(context.clone(), 100u64);
+    managed_vec.overwrite_with_single_item(single_elem.clone());
+    vec = vec![single_elem];
 
     assert_eq!(vec, managed_vec.into_vec());
 }
