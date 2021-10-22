46e17eda1f91e1b468f4bec3bd9181ae2ce1832d
Andrei Marinica <andrei.marinica@elrond.com>
2021-10-22 17:07:23 +0300

    degub refactor - local mint/burn, nft burn

--- elrond-wasm-debug/src/tx_execution/builtin_function_mocks/builtin_func_exec.rs
+++ elrond-wasm-debug/src/tx_execution/builtin_function_mocks/builtin_func_exec.rs
@@ -4,15 +4,17 @@ use crate::{
 };
 
 use super::{
-    change_owner_mock::execute_change_owner, esdt_multi_transfer_mock::execute_esdt_multi_transfer,
-    esdt_nft_add_quantity_mock::execute_nft_add_quantity,
+    change_owner_mock::execute_change_owner, esdt_local_burn::execute_local_burn,
+    esdt_local_mint::execute_local_mint, esdt_multi_transfer_mock::execute_esdt_multi_transfer,
+    esdt_nft_add_quantity_mock::execute_nft_add_quantity, esdt_nft_burn_mock::execute_nft_burn,
     esdt_nft_create_mock::execute_esdt_nft_create,
     esdt_nft_transfer_mock::execute_esdt_nft_transfer, esdt_transfer_mock::execute_esdt_transfer,
     set_username_mock::execute_set_username, upgrade_contract::execute_upgrade_contract,
 };
 
 use elrond_wasm::api::{
-    CHANGE_OWNER_BUILTIN_FUNC_NAME, ESDT_MULTI_TRANSFER_FUNC_NAME, ESDT_NFT_ADD_QUANTITY_FUNC_NAME,
+    CHANGE_OWNER_BUILTIN_FUNC_NAME, ESDT_LOCAL_BURN_FUNC_NAME, ESDT_LOCAL_MINT_FUNC_NAME,
+    ESDT_MULTI_TRANSFER_FUNC_NAME, ESDT_NFT_ADD_QUANTITY_FUNC_NAME, ESDT_NFT_BURN_FUNC_NAME,
     ESDT_NFT_CREATE_FUNC_NAME, ESDT_NFT_TRANSFER_FUNC_NAME, ESDT_TRANSFER_FUNC_NAME,
     SET_USERNAME_FUNC_NAME, UPGRADE_CONTRACT_FUNC_NAME,
 };
@@ -22,11 +24,14 @@ pub fn execute_builtin_function_or_default(
     tx_cache: TxCache,
 ) -> (TxResult, BlockchainUpdate) {
     match tx_input.func_name.as_slice() {
-        ESDT_TRANSFER_FUNC_NAME => execute_esdt_transfer(tx_input, tx_cache),
-        ESDT_NFT_TRANSFER_FUNC_NAME => execute_esdt_nft_transfer(tx_input, tx_cache),
+        ESDT_LOCAL_MINT_FUNC_NAME => execute_local_mint(tx_input, tx_cache),
+        ESDT_LOCAL_BURN_FUNC_NAME => execute_local_burn(tx_input, tx_cache),
         ESDT_MULTI_TRANSFER_FUNC_NAME => execute_esdt_multi_transfer(tx_input, tx_cache),
+        ESDT_NFT_TRANSFER_FUNC_NAME => execute_esdt_nft_transfer(tx_input, tx_cache),
         ESDT_NFT_CREATE_FUNC_NAME => execute_esdt_nft_create(tx_input, tx_cache),
         ESDT_NFT_ADD_QUANTITY_FUNC_NAME => execute_nft_add_quantity(tx_input, tx_cache),
+        ESDT_NFT_BURN_FUNC_NAME => execute_nft_burn(tx_input, tx_cache),
+        ESDT_TRANSFER_FUNC_NAME => execute_esdt_transfer(tx_input, tx_cache),
         CHANGE_OWNER_BUILTIN_FUNC_NAME => execute_change_owner(tx_input, tx_cache),
         SET_USERNAME_FUNC_NAME => execute_set_username(tx_input, tx_cache),
         UPGRADE_CONTRACT_FUNC_NAME => execute_upgrade_contract(tx_input, tx_cache),
