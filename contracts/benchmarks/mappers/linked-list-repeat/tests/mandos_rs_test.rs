ed2a68cec6b4d6d964c93ece260c395a4390c7af
Claudiu-Marcel Bruda <claudiu725@yahoo.com>
2021-10-22 14:12:42 +0300

    update tests to the latest version

--- contracts/benchmarks/mappers/linked-list-repeat/tests/mandos_rs_test.rs
+++ contracts/benchmarks/mappers/linked-list-repeat/tests/mandos_rs_test.rs
@@ -1,6 +1,6 @@
 use elrond_wasm_debug::*;
 
-fn contract_map() -> ContractMap<TxContext> {
+fn contract_map() -> ContractMap<DebugApi> {
     let mut contract_map = ContractMap::new();
     contract_map.register_contract(
         "file:../output/linked-list-repeat.wasm",
@@ -11,5 +11,5 @@ fn contract_map() -> ContractMap<TxContext> {
 
 #[test]
 fn linked_list_repeat_mandos_rs() {
-    elrond_wasm_debug::mandos_rs("mandos/linked_list_repeat.scen.json", &contract_map());
+    elrond_wasm_debug::mandos_rs("mandos/linked_list_repeat.scen.json", contract_map());
 }
