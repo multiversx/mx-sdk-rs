79ac35dace00d7434a65f74213e0399858bde383
Claudiu-Marcel Bruda <claudiu725@yahoo.com>
2021-10-26 14:36:08 +0300

    update contract_map

--- contracts/benchmarks/mappers/linked-list-repeat/tests/mandos_rs_test.rs
+++ contracts/benchmarks/mappers/linked-list-repeat/tests/mandos_rs_test.rs
@@ -1,12 +1,14 @@
 use elrond_wasm_debug::*;
 
-fn contract_map() -> ContractMap<DebugApi> {
-    let mut contract_map = ContractMap::new();
-    contract_map.register_contract(
-        "file:../output/linked-list-repeat.wasm",
+fn contract_map() -> BlockchainMock {
+    let mut blockchain = BlockchainMock::new();
+    blockchain.set_current_dir_from_workspace("contracts/benchmarks/mappers/linked-list-repeat");
+
+    blockchain.register_contract(
+        "file:output/linked-list-repeat.wasm",
         Box::new(|context| Box::new(linked_list_repeat::contract_obj(context))),
     );
-    contract_map
+    blockchain
 }
 
 #[test]
--- contracts/benchmarks/mappers/queue-repeat/tests/mandos_rs_test.rs
+++ contracts/benchmarks/mappers/queue-repeat/tests/mandos_rs_test.rs
@@ -1,12 +1,14 @@
 use elrond_wasm_debug::*;
 
-fn contract_map() -> ContractMap<DebugApi> {
-    let mut contract_map = ContractMap::new();
-    contract_map.register_contract(
-        "file:../output/queue-repeat.wasm",
+fn contract_map() -> BlockchainMock {
+    let mut blockchain = BlockchainMock::new();
+    blockchain.set_current_dir_from_workspace("contracts/benchmarks/mappers/queue-repeat");
+
+    blockchain.register_contract(
+        "file:output/queue-repeat.wasm",
         Box::new(|context| Box::new(queue_repeat::contract_obj(context))),
     );
-    contract_map
+    blockchain
 }
 
 #[test]
