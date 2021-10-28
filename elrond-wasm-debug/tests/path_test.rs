251597b0ff4f05644b8c14a29c05aa290e601a1d
Alin Cruceat <alyn_509@yahoo.com>
2021-10-28 13:53:59 +0300

    debud path text

--- elrond-wasm-debug/tests/path_test.rs
+++ elrond-wasm-debug/tests/path_test.rs
@@ -1,18 +1,18 @@
 use elrond_wasm_debug::*;
 
-fn contract_map_local() -> BlockchainMock {
+fn contract_map() -> BlockchainMock {
     BlockchainMock::new()
 }
 
-fn contract_map_relative() -> BlockchainMock {
-    let mut blockchain = contract_map_local();
-    blockchain.set_current_dir_from_workspace("elrond-wasm-debug/tests/mandos/external_steps");
-
-    blockchain
+#[test]
+fn local_path_test() {
+    elrond_wasm_debug::mandos_rs("tests/mandos/path_test.scen.json", contract_map());
 }
 
-/// Checks that externalSteps work fine.
 #[test]
-fn relative_path_test() {
-    elrond_wasm_debug::mandos_rs("external_steps.scen.json", contract_map_relative());
+fn nested_path_test() {
+    elrond_wasm_debug::mandos_rs(
+        "tests/mandos/external_steps/external_path_test.scen.json",
+        contract_map(),
+    );
 }
