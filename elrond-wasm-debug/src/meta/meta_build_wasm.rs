37ba37472519a0398c084876818750a5f3dfc1fb
Andrei Marinica <andrei.marinica@elrond.com>
2021-10-31 19:01:57 +0200

    contract build from meta crate

---
+++ elrond-wasm-debug/src/meta/meta_build_wasm.rs
@@ -0,0 +1,27 @@
+use std::{
+    fs,
+    io::{self, Write},
+    process::Command,
+};
+
+use elrond_wasm::abi::ContractAbi;
+
+pub fn build_wasm(abi: &ContractAbi) {
+    let output = Command::new("cargo")
+        .args(["build", "--target=wasm32-unknown-unknown", "--release"])
+        .current_dir("../wasm")
+        .env("RUSTFLAGS", "-C link-arg=-s")
+        .output()
+        .expect("failed to execute process");
+
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
+
+    let source_wasm = format!(
+        "../wasm/target/wasm32-unknown-unknown/release/{}_wasm.wasm",
+        abi.get_module_name()
+    );
+    let dest_wasm = format!("../output/{}.wasm", abi.build_info.contract_crate.name);
+
+    fs::copy(source_wasm, dest_wasm).unwrap();
+}
