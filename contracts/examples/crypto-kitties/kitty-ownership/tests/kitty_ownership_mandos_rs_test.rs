94b251bec035e151db7fad59f47663d7f7532f16
Andrei Marinica <andrei.marinica@elrond.com>
2021-11-01 18:32:32 +0200

    rename contract_map() -> world()

--- contracts/examples/crypto-kitties/kitty-ownership/tests/kitty_ownership_mandos_rs_test.rs
+++ contracts/examples/crypto-kitties/kitty-ownership/tests/kitty_ownership_mandos_rs_test.rs
@@ -1,7 +1,7 @@
 use elrond_wasm::*;
 use elrond_wasm_debug::*;
 
-fn contract_map() -> BlockchainMock {
+fn world() -> BlockchainMock {
     let mut blockchain = BlockchainMock::new();
 
     blockchain.register_contract(
@@ -18,30 +18,30 @@ fn contract_map() -> BlockchainMock {
 
 #[test]
 fn approve_siring_rs() {
-    elrond_wasm_debug::mandos_rs("mandos/approve_siring.scen.json", contract_map());
+    elrond_wasm_debug::mandos_rs("mandos/approve_siring.scen.json", world());
 }
 
 #[test]
 fn breed_ok_rs() {
-    elrond_wasm_debug::mandos_rs("mandos/breed_ok.scen.json", contract_map());
+    elrond_wasm_debug::mandos_rs("mandos/breed_ok.scen.json", world());
 }
 
 #[test]
 fn give_birth_rs() {
-    elrond_wasm_debug::mandos_rs("mandos/give_birth.scen.json", contract_map());
+    elrond_wasm_debug::mandos_rs("mandos/give_birth.scen.json", world());
 }
 
 #[test]
 fn init_rs() {
-    elrond_wasm_debug::mandos_rs("mandos/init.scen.json", contract_map());
+    elrond_wasm_debug::mandos_rs("mandos/init.scen.json", world());
 }
 
 #[test]
 fn query_rs() {
-    elrond_wasm_debug::mandos_rs("mandos/query.scen.json", contract_map());
+    elrond_wasm_debug::mandos_rs("mandos/query.scen.json", world());
 }
 
 #[test]
 fn setup_accounts_rs() {
-    elrond_wasm_debug::mandos_rs("mandos/setup_accounts.scen.json", contract_map());
+    elrond_wasm_debug::mandos_rs("mandos/setup_accounts.scen.json", world());
 }
