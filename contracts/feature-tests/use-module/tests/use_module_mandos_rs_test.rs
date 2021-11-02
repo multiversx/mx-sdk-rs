b3956093e70c9e1e2dfc4629a6845e71d6eebce4
Andrei Marinica <andrei.marinica@elrond.com>
2021-11-02 13:32:13 +0200

    use module - enabled governance module rs tests

--- contracts/feature-tests/use-module/tests/use_module_mandos_rs_test.rs
+++ contracts/feature-tests/use-module/tests/use_module_mandos_rs_test.rs
@@ -44,16 +44,6 @@ fn world() -> BlockchainMock {
     blockchain
 }
 
-fn _gov_world() -> BlockchainMock {
-    let mut blockchain = BlockchainMock::new();
-    blockchain.register_contract(
-        "file:../output/use-module.wasm",
-        Box::new(|context| Box::new(use_module::contract_obj(context))),
-    );
-
-    blockchain
-}
-
 #[test]
 fn use_module_dns_register_rs() {
     elrond_wasm_debug::mandos_rs("mandos/use_module_dns_register.scen.json", world());
@@ -81,37 +71,32 @@ fn use_module_pause_rs() {
 
 // Governance module tests
 
-/*
-
 #[test]
 fn cancel_defeated_proposal_rs() {
     elrond_wasm_debug::mandos_rs(
         "mandos/use_module_governance/cancel_defeated_proposal.scen.json",
-        &gov_world(),
+        world(),
     );
 }
 
 #[test]
 fn change_configuration_rs() {
     elrond_wasm_debug::mandos_rs(
-        "/home/elrond/elrond-wasm-rs/contracts/feature-tests/use-module/mandos/use_module_governance/change_configuration.scen.json",
-        &gov_world(),
+        "mandos/use_module_governance/change_configuration.scen.json",
+        world(),
     );
 }
 
 #[test]
 fn init_rs() {
-    elrond_wasm_debug::mandos_rs(
-        "mandos/use_module_governance/init.scen.json",
-        &gov_world(),
-    );
+    elrond_wasm_debug::mandos_rs("mandos/use_module_governance/init.scen.json", world());
 }
 
 #[test]
 fn invalid_proposals_rs() {
     elrond_wasm_debug::mandos_rs(
         "mandos/use_module_governance/invalid_proposals.scen.json",
-        &gov_world(),
+        world(),
     );
 }
 
@@ -119,8 +104,6 @@ fn invalid_proposals_rs() {
 fn withdraw_governance_tokens_rs() {
     elrond_wasm_debug::mandos_rs(
         "mandos/use_module_governance/withdraw_governance_tokens.scen.json",
-        &gov_world(),
+        world(),
     );
 }
-
-*/
