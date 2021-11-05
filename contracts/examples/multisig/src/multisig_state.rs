1578e1345b6cccc991dee7a4c7ea74db89f7679e
Andrei Marinica <andrei.marinica@elrond.com>
2021-11-05 09:25:53 +0200

    multisig - fixed upgrade checks

--- contracts/examples/multisig/src/multisig_state.rs
+++ contracts/examples/multisig/src/multisig_state.rs
@@ -35,6 +35,28 @@ pub trait MultisigStateModule {
     #[storage_mapper("num_proposers")]
     fn num_proposers(&self) -> SingleValueMapper<usize>;
 
+    fn add_multiple_board_members(
+        &self,
+        new_board_members: ManagedVec<ManagedAddress>,
+    ) -> SCResult<usize> {
+        let mut duplicates = false;
+        self.user_mapper().get_or_create_users(
+            new_board_members.into_iter(),
+            |user_id, new_user| {
+                if !new_user {
+                    duplicates = true;
+                }
+                self.user_id_to_role(user_id).set(&UserRole::BoardMember);
+            },
+        );
+        require!(!duplicates, "duplicate board member");
+
+        let num_board_members_mapper = self.num_board_members();
+        let new_num_board_members = num_board_members_mapper.get() + new_board_members.len();
+        num_board_members_mapper.set(&new_num_board_members);
+        Ok(new_num_board_members)
+    }
+
     #[storage_mapper("action_data")]
     fn action_mapper(&self) -> VecMapper<Action<Self::Api>>;
 
