a880ee4c66c456558bf2a6551cac4a4c04e088a5
Claudiu-Marcel Bruda <claudiu725@yahoo.com>
2021-11-08 19:31:23 +0200

    specify crate

--- contracts/benchmarks/mappers/benchmark-common/src/lib.rs
+++ contracts/benchmarks/mappers/benchmark-common/src/lib.rs
@@ -3,5 +3,5 @@
 mod benchmark_common;
 mod example_struct;
 
-pub use benchmark_common::*;
-pub use example_struct::ExampleStruct;
+pub use crate::benchmark_common::*;
+pub use crate::example_struct::ExampleStruct;
