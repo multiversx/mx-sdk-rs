5941f58d29817f4610620f7c003abd3a1e20af86
Catalin Neagu <neagu.catalin.f@gmail.com>
2021-11-09 17:26:11 +0200

    print-biguint: added test

--- contracts/examples/adder/tests/adder_test.rs
+++ contracts/examples/adder/tests/adder_test.rs
@@ -1,9 +1,5 @@
 use adder::*;
-use elrond_wasm::{
-    api::PrintApi,
-    contract_base::ContractBase,
-    types::{BigInt, BigUint, ManagedFrom},
-};
+use elrond_wasm::{contract_base::ContractBase, types::BigInt};
 use elrond_wasm_debug::DebugApi;
 
 #[test]
@@ -18,9 +14,6 @@ fn test_add() {
         BigInt::from_i64(adder.type_manager(), 12),
         adder.sum().get()
     );
-
-    let biguint = BigUint::managed_from(adder.type_manager(), 0u64);
-    DebugApi::dummy().print_biguint(&biguint);
 
     let _ = adder.add(BigInt::from_i64(adder.type_manager(), 1));
     assert_eq!(
