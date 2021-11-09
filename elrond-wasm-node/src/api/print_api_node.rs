1f5df821fcc4da3fbf40044e8a64de9ff50d54d9
Catalin Neagu <neagu.catalin.f@gmail.com>
2021-11-09 18:39:54 +0200

    print-debug: added inline

--- elrond-wasm-node/src/api/print_api_node.rs
+++ elrond-wasm-node/src/api/print_api_node.rs
@@ -2,5 +2,6 @@ use crate::VmApiImpl;
 use elrond_wasm::{api::PrintApi, types::BigUint};
 
 impl PrintApi for VmApiImpl {
+    #[inline(always)]
     fn print_biguint(&self, _biguint: &BigUint<Self>) {}
 }
