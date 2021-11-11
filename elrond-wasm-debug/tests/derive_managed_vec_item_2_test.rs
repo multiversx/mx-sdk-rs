bad7f1a353ff18acce054dc4b7adc26e373b1d82
Alin Cruceat <alyn_509@yahoo.com>
2021-11-11 15:23:26 +0200

    managed vec test with generic & fixes

--- elrond-wasm-debug/tests/derive_managed_vec_item_2_test.rs
+++ elrond-wasm-debug/tests/derive_managed_vec_item_2_test.rs
@@ -1,45 +1,78 @@
-use elrond_wasm::{api::ManagedTypeApi, derive::ManagedVecItem, types::BigUint};
+use elrond_wasm::{
+    api::ManagedTypeApi,
+    derive::ManagedVecItem,
+    elrond_codec,
+    elrond_codec::elrond_codec_derive::{NestedDecode, NestedEncode, TopDecode, TopEncode},
+    types::{BigUint, ManagedFrom, ManagedType},
+};
+use elrond_wasm_debug::DebugApi;
 
 // to test, run the following command in elrond-wasm-debug folder:
 // cargo expand --test derive_managed_vec_item_2_test > expanded.rs
 
-#[derive(ManagedVecItem)]
+#[derive(
+    ManagedVecItem, NestedEncode, NestedDecode, TopEncode, TopDecode, PartialEq, Clone, Debug,
+)]
 pub struct ManagedStruct<M: ManagedTypeApi> {
     pub big_uint: elrond_wasm::types::BigUint<M>,
     pub num: u32,
 }
 
-// #[test]
-// fn struct_with_numbers_static() {
-//     assert_eq!(
-//         <StructWithNumbers as elrond_wasm::types::ManagedVecItem<DebugApi>>::PAYLOAD_SIZE,
-//         8
-//     );
-//     assert!(
-//         <StructWithNumbers as elrond_wasm::types::ManagedVecItem<DebugApi>>::SKIPS_RESERIALIZATION
-//     );
-// }
+#[test]
+fn struct_with_numbers_static() {
+    assert_eq!(
+        <ManagedStruct<DebugApi> as elrond_wasm::types::ManagedVecItem<DebugApi>>::PAYLOAD_SIZE,
+        8
+    );
+    assert!(
+        !<ManagedStruct<DebugApi> as elrond_wasm::types::ManagedVecItem<DebugApi>>::SKIPS_RESERIALIZATION
+    );
+}
 
-// #[test]
-// fn struct_named_fields_test() {
-//     let s = Struct {
-//         int: 0x42,
-//         seq: vec![0x1, 0x2, 0x3, 0x4, 0x5],
-//         another_byte: 0x6,
-//         uint_32: 0x12345,
-//         uint_64: 0x123456789,
-//     };
+#[test]
+fn struct_to_bytes_writer() {
+    let s = ManagedStruct::<DebugApi> {
+        big_uint: BigUint::managed_from(DebugApi::dummy(), 42u64),
+        num: 0x12345,
+    };
+    let mut arr: [u8; 8] = [0u8; <ManagedStruct<DebugApi> as elrond_wasm::types::ManagedVecItem<
+        DebugApi,
+    >>::PAYLOAD_SIZE];
 
-//     #[rustfmt::skip]
-// 	let bytes_1 = &[
-// 		/* int */ 0, 0x42,
-// 		/* seq length */ 0, 0, 0, 5,
-// 		/* seq contents */ 1, 2, 3, 4, 5,
-// 		/* another_byte */ 6,
-// 		/* uint_32 */ 0x00, 0x01, 0x23, 0x45,
-// 		/* uint_64 */ 0x00, 0x00, 0x00, 0x01, 0x23, 0x45, 0x67, 0x89,
-// 	];
+    <ManagedStruct<DebugApi> as elrond_wasm::types::ManagedVecItem<DebugApi>>::to_byte_writer(
+        &s,
+        |bytes| {
+            arr[0..<ManagedStruct::<DebugApi> as elrond_wasm::types::ManagedVecItem<DebugApi>>::PAYLOAD_SIZE].copy_from_slice(bytes);
 
-//     check_top_encode_decode(s.clone(), bytes_1);
-//     check_dep_encode_decode(s.clone(), bytes_1);
-// }
+            assert_eq!(arr, [0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x23, 0x45]);
+        },
+    );
+
+    assert_eq!(
+        BigUint::<DebugApi>::from_raw_handle(DebugApi::dummy(), 0x0),
+        42u64
+    );
+}
+
+#[test]
+fn struct_from_bytes_reader() {
+    let s = ManagedStruct::<DebugApi> {
+        big_uint: BigUint::managed_from(DebugApi::dummy(), 42u64),
+        num: 0x12345,
+    };
+    let arr: [u8; 8] = [0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x23, 0x45];
+
+    let struct_from_bytes = <ManagedStruct<DebugApi> as elrond_wasm::types::ManagedVecItem<
+        DebugApi,
+    >>::from_byte_reader(DebugApi::dummy(), |bytes| {
+        bytes.copy_from_slice(
+                    &arr
+                        [0
+                            ..<ManagedStruct::<DebugApi> as elrond_wasm::types::ManagedVecItem<
+                                DebugApi,
+                            >>::PAYLOAD_SIZE],
+                );
+    });
+    assert_eq!(s.num, struct_from_bytes.num);
+    assert_eq!(s.big_uint, struct_from_bytes.big_uint);
+}
