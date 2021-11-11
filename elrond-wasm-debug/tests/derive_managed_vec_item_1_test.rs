21487b6bbe60d6e9b1c680143153ba075ed91e3a
Alin Cruceat <alyn_509@yahoo.com>
2021-11-11 13:08:01 +0200

    test managed vec item

--- elrond-wasm-debug/tests/derive_managed_vec_item_1_test.rs
+++ elrond-wasm-debug/tests/derive_managed_vec_item_1_test.rs
@@ -1,10 +1,19 @@
-use elrond_wasm::derive::ManagedVecItem;
+use elrond_wasm::{
+    derive::ManagedVecItem,
+    elrond_codec,
+    elrond_codec::{
+        elrond_codec_derive::{NestedDecode, NestedEncode, TopDecode, TopEncode},
+        test_util::{check_dep_encode_decode, check_top_encode_decode},
+    },
+};
 use elrond_wasm_debug::DebugApi;
 
 // to test, run the following command in elrond-wasm-debug folder:
-// cargo expand --test derive_managed_vec_item_test > expanded.rs
+// cargo expand --test derive_managed_vec_item_1_test > expanded.rs
 
-#[derive(ManagedVecItem)]
+#[derive(
+    ManagedVecItem, NestedEncode, NestedDecode, TopEncode, TopDecode, PartialEq, Clone, Debug,
+)]
 pub struct StructWithNumbers {
     pub int1: u32,
     pub int2: u32,
@@ -21,26 +30,61 @@ fn struct_with_numbers_static() {
     );
 }
 
-// #[test]
-// fn struct_named_fields_test() {
-//     let s = Struct {
-//         int: 0x42,
-//         seq: vec![0x1, 0x2, 0x3, 0x4, 0x5],
-//         another_byte: 0x6,
-//         uint_32: 0x12345,
-//         uint_64: 0x123456789,
-//     };
+#[test]
+fn struct_named_fields_test() {
+    let s = StructWithNumbers {
+        int1: 0x42,
+        int2: 0x42,
+    };
 
-//     #[rustfmt::skip]
-// 	let bytes_1 = &[
-// 		/* int */ 0, 0x42,
-// 		/* seq length */ 0, 0, 0, 5,
-// 		/* seq contents */ 1, 2, 3, 4, 5,
-// 		/* another_byte */ 6,
-// 		/* uint_32 */ 0x00, 0x01, 0x23, 0x45,
-// 		/* uint_64 */ 0x00, 0x00, 0x00, 0x01, 0x23, 0x45, 0x67, 0x89,
-// 	];
+    #[rustfmt::skip]
+	let bytes_1 = &[
+		/* int1 */ 0, 0, 0, 0x42,
+		/* int2 */ 0, 0, 0, 0x42,
+	];
 
-//     check_top_encode_decode(s.clone(), bytes_1);
-//     check_dep_encode_decode(s.clone(), bytes_1);
-// }
+    check_top_encode_decode(s.clone(), bytes_1);
+    check_dep_encode_decode(s.clone(), bytes_1);
+}
+
+#[test]
+fn struct_to_bytes_writer() {
+    let s = StructWithNumbers {
+        int1: 0x42,
+        int2: 0x42,
+    };
+    let mut arr: [u8; 8] =
+        [0u8; <StructWithNumbers as elrond_wasm::types::ManagedVecItem<DebugApi>>::PAYLOAD_SIZE];
+
+    <StructWithNumbers as elrond_wasm::types::ManagedVecItem<DebugApi>>::to_byte_writer(
+        &s,
+        |bytes| {
+            arr[0..<StructWithNumbers as elrond_wasm::types::ManagedVecItem<DebugApi>>::PAYLOAD_SIZE].copy_from_slice(bytes);
+            assert_eq!(arr, [0, 0, 0, 0x42, 0, 0, 0, 0x42]);
+        },
+    );
+}
+
+#[test]
+fn struct_from_bytes_reader() {
+    let s = StructWithNumbers {
+        int1: 0x42,
+        int2: 0x42,
+    };
+    let arr: [u8; 8] = [0, 0, 0, 0x42, 0, 0, 0, 0x42];
+
+    let struct_from_bytes =
+        <StructWithNumbers as elrond_wasm::types::ManagedVecItem<DebugApi>>::from_byte_reader(
+            DebugApi::dummy(),
+            |bytes| {
+                bytes.copy_from_slice(
+                    &arr
+                        [0
+                            ..<StructWithNumbers as elrond_wasm::types::ManagedVecItem<
+                                DebugApi,
+                            >>::PAYLOAD_SIZE],
+                );
+            },
+        );
+    assert_eq!(s, struct_from_bytes);
+}
