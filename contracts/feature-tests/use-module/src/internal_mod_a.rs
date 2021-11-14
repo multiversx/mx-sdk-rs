db0801ffde3c7924418f94f8a7b1e706d89209c5
Andrei Marinica <andrei.marinica@elrond.com>
2021-11-14 15:39:24 +0200

    constructor can reside in a module

--- contracts/feature-tests/use-module/src/internal_mod_a.rs
+++ contracts/feature-tests/use-module/src/internal_mod_a.rs
@@ -3,7 +3,9 @@ elrond_wasm::imports!();
 /// Example of a module that lies in the same crate.
 /// It also includes another module, also from the same crate.
 #[elrond_wasm::module]
-pub trait InternalModuleA: super::internal_mod_b::InternalModuleB {
+pub trait InternalModuleA:
+    super::internal_mod_b::InternalModuleB + super::internal_mod_init::InternalModuleInit
+{
     #[view]
     fn call_mod_a(&self) {}
 }
