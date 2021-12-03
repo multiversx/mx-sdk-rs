2b19302569f961dcab8775c96ecdea968dae4711
Alin Cruceat <alyn_509@yahoo.com>
2021-12-03 13:19:44 +0200

    remove misleading comments

--- contracts/feature-tests/use-module/src/only_owner_derived_mod.rs
+++ contracts/feature-tests/use-module/src/only_owner_derived_mod.rs
@@ -1,6 +1,5 @@
 elrond_wasm::imports!();
 
-/// Example of a module that lies in the same crate.
 #[elrond_wasm::module]
 pub trait OnlyOwnerDerivedModule {
     #[view]
--- contracts/feature-tests/use-module/src/only_owner_mod.rs
+++ contracts/feature-tests/use-module/src/only_owner_mod.rs
@@ -1,7 +1,5 @@
 elrond_wasm::imports!();
 
-/// This module is in the crate, but it is not included.
-/// Its endpoints should not appear in the contract binary.
 #[elrond_wasm::only_owner_module]
 pub trait OnlyOwnerModule: super::only_owner_derived_mod::OnlyOwnerDerivedModule {
     #[endpoint]
