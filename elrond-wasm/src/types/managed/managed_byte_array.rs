4220f7f0f99dbdbad6c824d79f5f446e16930def
Andrei Marinica <andrei.marinica@elrond.com>
2021-12-04 17:41:56 +0200

    better Debug for managed types

--- elrond-wasm/src/types/managed/managed_byte_array.rs
+++ elrond-wasm/src/types/managed/managed_byte_array.rs
@@ -16,7 +16,7 @@ const DECODE_ERROR_BAD_LENGTH: &[u8] = b"bad array length";
 /// A list of items that lives inside a managed buffer.
 /// Items can be either stored there in full (e.g. `u32`),
 /// or just via handle (e.g. `BigUint<M>`).
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub struct ManagedByteArray<M, const N: usize>
 where
     M: ManagedTypeApi,
@@ -89,7 +89,7 @@ where
     }
 
     #[inline]
-    pub fn into_byte_array(&self) -> [u8; N] {
+    pub fn to_byte_array(&self) -> [u8; N] {
         let mut result = [0u8; N];
         let _ = self.buffer.load_slice(0, &mut result[..]);
         result
@@ -180,5 +180,18 @@ where
     /// It is semantically equivalent to `[u8; N]`.
     fn type_name() -> String {
         <&[u8; N] as TypeAbi>::type_name()
+    }
+}
+
+impl<M, const N: usize> core::fmt::Debug for ManagedByteArray<M, N>
+where
+    M: ManagedTypeApi,
+{
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("ManagedByteArray")
+            .field("handle", &self.buffer.handle)
+            .field("size", &N)
+            .field("hex-value", &hex::encode(&self.to_byte_array()))
+            .finish()
     }
 }
