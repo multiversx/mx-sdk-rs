024c6938176c7fb980cf09f6c0e9dab9efec8224
dorin.iancu <dorin.iancu@elrond.com>
2021-12-21 16:28:31 +0200

    esdt transfer, nft in progress

--- tools/erdpy-snippet-generator/src/constants.rs
+++ tools/erdpy-snippet-generator/src/constants.rs
@@ -1,9 +1,11 @@
 pub const ERDPY_PROGRAM_NAME: &str = "erdpy";
 pub const CONTRACT_COMMAND_NAME: &str = "contract";
+pub const MAX_GAS_LIMIT: u64 = 500_000_000;
 
 // deploy/upgrade arg names
 pub const DEPLOY_COMMAND_NAME: &str = "deploy";
 pub const UPGRADE_COMMAND_NAME: &str = "upgrade";
+pub const CALL_COMMAND_NAME: &str = "call";
 pub const PROJECT_ARG_NAME: &str = "project";
 pub const WASM_PATH_ARG_NAME: &str = "bytecode";
 pub const OUT_FILE_PATH_ARG_NAME: &str = "outfile";
@@ -17,6 +19,7 @@ pub const CHAIN_ID_ARG_NAME: &str = "chain";
 pub const NONCE_ARG_NAME: &str = "nonce";
 pub const GAS_LIMIT_ARG_NAME: &str = "gas-limit";
 pub const EGLD_VALUE_ARG_NAME: &str = "value";
+pub const FUNCTION_ARG_NAME: &str = "function";
 pub const ARGUMENTS_ARG_NAME: &str = "arguments";
 
 // general flags
@@ -24,7 +27,10 @@ pub const VERBOSE_FLAG: &str = "verbose";
 pub const RECALL_NONCE_FLAG: &str = "recall-nonce";
 pub const SEND_FLAG: &str = "send";
 
-pub const MAX_GAS_LIMIT: u64 = 500_000_000;
+// built-in function names
+pub const ESDT_TRANSFER_FUNC_NAME: &str = "ESDTTransfer";
+pub const NFT_TRANSFER_FUNC_NAME: &str = "ESDTNFTTransfer";
+pub const MULTI_TRANSFER_FUNC_NAME: &str = "MultiESDTNFTTransfer";
 
 // default proxies
 pub const TESTNET_PROXY: &str = "https://testnet-gateway.elrond.com";
