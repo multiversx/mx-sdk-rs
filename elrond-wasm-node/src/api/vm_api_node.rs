17cb12ef1f956707b6af5550e8988c70b1dd8dac
Andrei Marinica <andrei.marinica@elrond.com>
2021-12-30 03:13:29 +0200

    api refactor - StaticBufferApi

--- elrond-wasm-node/src/api/vm_api_node.rs
+++ elrond-wasm-node/src/api/vm_api_node.rs
@@ -1,14 +1,11 @@
 use elrond_wasm::{
     api::{CallTypeApi, StorageMapperApi, VMApi},
-    elrond_codec::TryStaticCast,
 };
 
 /// The reference to the API implementation based on Arwen hooks.
 /// It continas no data, can be embedded at no cost.
 /// Cloning it is a no-op.
 pub struct VmApiImpl {}
-
-impl TryStaticCast for VmApiImpl {}
 
 impl CallTypeApi for VmApiImpl {}
 
