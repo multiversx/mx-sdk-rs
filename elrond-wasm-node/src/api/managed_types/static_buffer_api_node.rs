1a5948fa6061c4fd013957b8c62b4225438f4dc2
Andrei Marinica <andrei.marinica@elrond.com>
2021-12-31 11:36:41 +0200

    external view - target address var

--- elrond-wasm-node/src/api/managed_types/static_buffer_api_node.rs
+++ elrond-wasm-node/src/api/managed_types/static_buffer_api_node.rs
@@ -1,11 +1,13 @@
 use elrond_wasm::{
-    api::{StaticVarApi, StaticVarApiImpl},
+    api::{Handle, StaticVarApi, StaticVarApiImpl},
     types::LockableStaticBuffer,
 };
 
 use crate::VmApiImpl;
 
 static mut STATIC_BUFFER: LockableStaticBuffer = LockableStaticBuffer::new();
+
+static mut EXTERNAL_VIEW_TARGET_ADDRESS_HANDLE: i32 = 0;
 
 impl StaticVarApi for VmApiImpl {
     type StaticVarApiImpl = VmApiImpl;
@@ -18,5 +20,15 @@ impl StaticVarApi for VmApiImpl {
 impl StaticVarApiImpl for VmApiImpl {
     fn with_lockable_static_buffer<R, F: FnOnce(&mut LockableStaticBuffer) -> R>(&self, f: F) -> R {
         unsafe { f(&mut STATIC_BUFFER) }
+    }
+
+    fn set_external_view_target_address_handle(&self, handle: Handle) {
+        unsafe {
+            EXTERNAL_VIEW_TARGET_ADDRESS_HANDLE = handle;
+        }
+    }
+
+    fn get_external_view_target_address_handle(&self) -> Handle {
+        unsafe { EXTERNAL_VIEW_TARGET_ADDRESS_HANDLE }
     }
 }
