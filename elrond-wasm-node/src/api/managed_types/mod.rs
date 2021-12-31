8e94af40b8a05f255da9570cd16e59176b37a261
Andrei Marinica <andrei.marinica@elrond.com>
2021-12-31 11:38:04 +0200

    api refactor - file renames

--- elrond-wasm-node/src/api/managed_types/mod.rs
+++ elrond-wasm-node/src/api/managed_types/mod.rs
@@ -2,4 +2,4 @@ pub(crate) mod big_int_api_node;
 mod elliptic_curve_api_node;
 pub(crate) mod managed_buffer_api_node;
 mod managed_type_api_node;
-mod static_buffer_api_node;
+mod static_var_api_node;
