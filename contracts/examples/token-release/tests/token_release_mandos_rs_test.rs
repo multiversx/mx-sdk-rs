fbfa5c3c45b2a99097a2960116d28a36f3a6b91b
Sorin Petreasca <petreasca.sorin@gmail.com>
2022-01-02 23:07:14 +0200

    Updated Rust tests

--- contracts/examples/token-release/tests/token_release_mandos_rs_test.rs
+++ contracts/examples/token-release/tests/token_release_mandos_rs_test.rs
@@ -15,3 +15,28 @@ fn world() -> BlockchainMock {
 fn token_release_init_rs() {
     elrond_wasm_debug::mandos_rs("mandos/test-init.scen.json", world());
 }
+
+#[test]
+fn token_release_add_group_rs() {
+    elrond_wasm_debug::mandos_rs("mandos/test-add-group.scen.json", world());
+}
+
+#[test]
+fn token_release_add_user_rs() {
+    elrond_wasm_debug::mandos_rs("mandos/test-add-user.scen.json", world());
+}
+
+#[test]
+fn token_release_end_setup_rs() {
+    elrond_wasm_debug::mandos_rs("mandos/test-end-setup.scen.json", world());
+}
+
+#[test]
+fn token_release_claim_rs() {
+    elrond_wasm_debug::mandos_rs("mandos/test-claim.scen.json", world());
+}
+
+#[test]
+fn token_release_change_user_rs() {
+    elrond_wasm_debug::mandos_rs("mandos/test-change-user.scen.json", world());
+}
