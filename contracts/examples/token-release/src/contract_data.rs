e4cac568ad0b65ca27e826efbad020299e392079
Andrei Marinica <andrei.marinica@elrond.com>
2022-01-17 15:05:48 +0200

    cargo fmt

--- contracts/examples/token-release/src/contract_data.rs
+++ contracts/examples/token-release/src/contract_data.rs
@@ -1,8 +1,4 @@
-
-use elrond_wasm::{
-    api::ManagedTypeApi,
-    types::BigUint
-};
+use elrond_wasm::{api::ManagedTypeApi, types::BigUint};
 
 elrond_wasm::derive_imports!();
 
@@ -11,17 +7,17 @@ pub enum UnlockType<M: ManagedTypeApi> {
     FixedAmount {
         period_unlock_amount: BigUint<M>,
         release_period: u64,
-        release_ticks: u64
+        release_ticks: u64,
     },
     Percentage {
         period_unlock_percentage: u8,
         release_period: u64,
-        release_ticks: u64
-    }
+        release_ticks: u64,
+    },
 }
 
 #[derive(TopEncode, TopDecode, NestedEncode, NestedDecode, PartialEq, TypeAbi, Clone)]
 pub struct Schedule<M: ManagedTypeApi> {
     pub group_total_amount: BigUint<M>,
-    pub unlock_type: UnlockType<M>
+    pub unlock_type: UnlockType<M>,
 }
