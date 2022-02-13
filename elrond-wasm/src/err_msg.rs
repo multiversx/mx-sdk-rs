a735a85474199402902b14ff8066d7572a9f315a
Andrei Marinica <andrei.marinica@elrond.com>
2022-02-13 08:55:55 +0200

    payments fix

--- elrond-wasm/src/err_msg.rs
+++ elrond-wasm/src/err_msg.rs
@@ -1,7 +1,9 @@
 pub const NON_PAYABLE_FUNC_EGLD: &[u8] = b"function does not accept EGLD payment";
 pub const NON_PAYABLE_FUNC_ESDT: &[u8] = b"function does not accept ESDT payment";
 pub const BAD_TOKEN_PROVIDED: &[u8] = b"bad call value token provided";
+pub const SINGLE_ESDT_EXPECTED: &[u8] = b"function expects single ESDT payment";
 pub const TOO_MANY_ESDT_TRANSFERS: &[u8] = b"too many ESDT transfers";
+pub const ESDT_INVALID_TOKEN_INDEX: &[u8] = b"invalid token index";
 
 pub const ARG_WRONG_NUMBER: &[u8] = b"wrong number of arguments";
 pub const ARG_ASYNC_WRONG_NUMBER: &[u8] = b"wrong number of arguments provided to async call";
