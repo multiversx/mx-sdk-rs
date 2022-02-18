d351e24cbea041fb1a6368c1a9012c9a800700cc
dorin.iancu <dorin.iancu@elrond.com>
2022-02-18 15:56:37 +0200

    whitelist + fungible token mapper

--- elrond-wasm/src/storage/mappers/mod.rs
+++ elrond-wasm/src/storage/mappers/mod.rs
@@ -1,3 +1,4 @@
+mod fungible_token_mapper;
 mod linked_list_mapper;
 mod map_mapper;
 mod map_storage_mapper;
@@ -9,7 +10,9 @@ mod token_attributes_mapper;
 mod unordered_set_mapper;
 mod user_mapper;
 mod vec_mapper;
+mod whitelist_mapper;
 
+pub use fungible_token_mapper::FungibleTokenMapper;
 pub use linked_list_mapper::{LinkedListMapper, LinkedListNode};
 pub use map_mapper::MapMapper;
 pub use map_storage_mapper::MapStorageMapper;
@@ -21,3 +24,4 @@ pub use token_attributes_mapper::TokenAttributesMapper;
 pub use unordered_set_mapper::UnorderedSetMapper;
 pub use user_mapper::UserMapper;
 pub use vec_mapper::VecMapper;
+pub use whitelist_mapper::WhitelistMapper;
