8d4c6e3980c5d6bfffd27395776afbcdfa09019c
Andrei Marinica <andrei.marinica@elrond.com>
2022-02-24 18:57:02 +0200

    managed type reorg

--- elrond-wasm/src/types/managed/multi_value/mod.rs
+++ elrond-wasm/src/types/managed/multi_value/mod.rs
@@ -1,11 +1,9 @@
+mod managed_multi_value;
+mod managed_multi_value_counted;
+mod managed_multi_value_eager;
+mod managed_multi_value_iter;
 
-mod managed_multi_value_vec;
-mod managed_multi_value_vec_counted;
-mod managed_multi_value_vec_eager;
-mod managed_multi_value_vec_iter;
-
-
-pub use managed_multi_value_vec::{ManagedMultiResultVec, ManagedVarArgs};
-pub use managed_multi_value_vec_counted::{ManagedCountedMultiResultVec, ManagedCountedVarArgs};
-pub use managed_multi_value_vec_eager::{ManagedMultiResultVecEager, ManagedVarArgsEager};
-pub use managed_multi_value_vec_iter::ManagedMultiResultVecIterator;
+pub use managed_multi_value::{ManagedMultiResultVec, ManagedVarArgs};
+pub use managed_multi_value_counted::{ManagedCountedMultiResultVec, ManagedCountedVarArgs};
+pub use managed_multi_value_eager::{ManagedMultiResultVecEager, ManagedVarArgsEager};
+pub use managed_multi_value_iter::ManagedMultiResultVecIterator;
