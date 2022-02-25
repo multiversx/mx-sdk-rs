a50d89be9d1b329278a9e1793d45ddfe58a5070d
Andrei Marinica <andrei.marinica@elrond.com>
2022-02-25 11:40:15 +0200

    managed type reorg

--- elrond-wasm/src/types/mod.rs
+++ elrond-wasm/src/types/mod.rs
@@ -4,7 +4,6 @@ mod heap;
 mod interaction;
 mod io;
 mod managed;
-mod managed_codec;
 mod static_buffer;
 
 pub use super::{Box, Vec};
@@ -14,5 +13,4 @@ pub use heap::*;
 pub use interaction::*;
 pub use io::*;
 pub use managed::*;
-pub use managed_codec::*;
 pub use static_buffer::*;
