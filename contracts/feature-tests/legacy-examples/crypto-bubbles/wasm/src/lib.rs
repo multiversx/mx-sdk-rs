455fdefb0e1776cee021d3e0dbeb76ad87c42d0e
Alin Cruceat <alyn_509@yahoo.com>
2022-02-26 14:11:46 +0200

    save old crypto-bubbles as legacy contract

---
+++ contracts/feature-tests/legacy-examples/crypto-bubbles/wasm/src/lib.rs
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    crypto_bubbles
+    (
+        balanceOf
+        joinGame
+        rewardAndSendToWallet
+        rewardWinner
+        topUp
+        withdraw
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
