3290e8a3ac97aeeab76c0506c228e974dad9fa37
Sorin Petreasca <petreasca.sorin@gmail.com>
2022-02-28 01:53:37 +0200

    Updated name & paths for Crypto Bubbles legacy SC

--- contracts/feature-tests/legacy-examples/crypto-bubbles-legacy/tests/crypto_bubbles_mandos_rs_test.rs
+++ contracts/feature-tests/legacy-examples/crypto-bubbles-legacy/tests/crypto_bubbles_mandos_rs_test.rs
@@ -2,11 +2,11 @@ use elrond_wasm_debug::*;
 
 fn world() -> BlockchainMock {
     let mut blockchain = BlockchainMock::new();
-    blockchain.set_current_dir_from_workspace("contracts/examples/crypto-bubbles");
+    blockchain.set_current_dir_from_workspace("contracts/feature-tests/legacy-examples/crypto-bubbles-legacy");
 
     blockchain.register_contract_builder(
-        "file:output/crypto-bubbles.wasm",
-        crypto_bubbles::ContractBuilder,
+        "file:output/crypto-bubbles-legacy.wasm",
+        crypto_bubbles_legacy::ContractBuilder,
     );
     blockchain
 }
