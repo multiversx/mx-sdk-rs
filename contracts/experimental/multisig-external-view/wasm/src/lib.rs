ae194cb30370955c4728b02136212426817f1124
Andrei Marinica <andrei.marinica@elrond.com>
2022-03-08 18:30:12 +0200

    multisig external_view backup

---
+++ contracts/experimental/multisig-external-view/wasm/src/lib.rs
@@ -0,0 +1,32 @@
+////////////////////////////////////////////////////
+////////////////// AUTO-GENERATED //////////////////
+////////////////////////////////////////////////////
+
+#![no_std]
+
+elrond_wasm_node::wasm_endpoints! {
+    multisig_external_view
+    (
+        deposit
+        discardAction
+        getActionLastIndex
+        getNumBoardMembers
+        getNumProposers
+        getQuorum
+        performAction
+        proposeAddBoardMember
+        proposeAddProposer
+        proposeAsyncCall
+        proposeChangeQuorum
+        proposeRemoveUser
+        proposeSCDeployFromSource
+        proposeSCUpgradeFromSource
+        proposeTransferExecute
+        quorumReached
+        sign
+        signed
+        unsign
+    )
+}
+
+elrond_wasm_node::wasm_empty_callback! {}
