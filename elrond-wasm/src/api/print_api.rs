475db85958cefdf2333722b5665cbb904ae28a0f
Alin Cruceat <alyn_509@yahoo.com>
2022-03-17 13:21:53 +0200

    initial commit formatter

--- elrond-wasm/src/api/print_api.rs
+++ elrond-wasm/src/api/print_api.rs
@@ -7,9 +7,15 @@ pub trait PrintApi: ManagedTypeApi {
 }
 
 pub trait PrintApiImpl {
+    type Buffer;
+
     #[inline]
     fn print_biguint(&self, _bu_handle: Handle) {}
 
     #[inline]
     fn print_managed_buffer(&self, _mb_handle: Handle) {}
+
+    fn new_buffer() -> Self::Buffer;
+
+    fn print_buffer(buffer: Self::Buffer);
 }
