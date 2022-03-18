0bdb8b08074144abdccfe6b60ed634ce3eab32b9
Andrei Marinica <andrei.marinica@elrond.com>
2022-03-19 00:11:46 +0200

    formatter - number optimization + test

--- elrond-wasm/src/formatter/formatter_impl_num.rs
+++ elrond-wasm/src/formatter/formatter_impl_num.rs
@@ -2,21 +2,30 @@ use super::SCDisplay;
 
 const MINUS_SYMBOL: &[u8] = b"-";
 
-fn format_unsigned_rec<F: super::FormatByteReceiver>(num: u64, f: &mut F) {
-    if num > 0 {
-        format_unsigned_rec(num / 10, f);
-        let last_digit = (num % 10) as u8;
-        let ascii_last_digit = b'0' + last_digit;
-        f.append_bytes(&[ascii_last_digit][..]);
+/// u64::MAX is 18446744073709551615 in base 10, which is 20 digits. so 20 digits is enough
+const MAX_BASE_10_LEN: usize = 20;
+
+fn format_unsigned_to_buffer(mut num: u64, buffer: &mut [u8; MAX_BASE_10_LEN]) -> &[u8] {
+    let mut buf_index = MAX_BASE_10_LEN;
+    if num == 0 {
+        buf_index -= 1;
+        buffer[buf_index] = b'0';
+    } else {
+        while num > 0 {
+            buf_index -= 1;
+            let last_digit = (num % 10) as u8;
+            let ascii_last_digit = b'0' + last_digit;
+            buffer[buf_index] = ascii_last_digit;
+            num /= 10;
+        }
     }
+    &buffer[buf_index..]
 }
 
 fn format_unsigned<F: super::FormatByteReceiver>(num: u64, f: &mut F) {
-    if num == 0 {
-        f.append_bytes(&b"0"[..]);
-    } else {
-        format_unsigned_rec(num, f);
-    }
+    let mut buffer = [0u8; MAX_BASE_10_LEN];
+    let formatted = format_unsigned_to_buffer(num, &mut buffer);
+    f.append_bytes(formatted);
 }
 
 macro_rules! formatter_unsigned {
@@ -37,12 +46,18 @@ formatter_unsigned! {u16}
 formatter_unsigned! {u8}
 
 fn format_signed<F: super::FormatByteReceiver>(num: i64, f: &mut F) {
-    if num < 0 {
-        f.append_bytes(MINUS_SYMBOL);
-        format_unsigned_rec((-num) as u64, f);
+    let abs = if num >= 0 {
+        num as u64
     } else {
-        format_unsigned(num as u64, f);
-    }
+        f.append_bytes(MINUS_SYMBOL);
+        if num == i64::MIN {
+            // overflow egde case
+            (i64::MAX as u64) + 1
+        } else {
+            (-num) as u64
+        }
+    };
+    format_unsigned(abs, f);
 }
 
 macro_rules! formatter_signed {
