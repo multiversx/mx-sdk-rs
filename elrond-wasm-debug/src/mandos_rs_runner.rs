4126d56a12e9dcb5ab8757eabebaa884158fa89c
Andrei Marinica <andrei.marinica@elrond.com>
2022-03-30 17:42:22 +0300

    mandos step execution refactor

--- elrond-wasm-debug/src/mandos_rs_runner.rs
+++ elrond-wasm-debug/src/mandos_rs_runner.rs
@@ -1,6 +1,6 @@
 #![allow(unused_variables)] // for now
 
-use crate::{mandos_step, world_mock::BlockchainMock};
+use crate::world_mock::BlockchainMock;
 
 use mandos::model::Step;
 use std::path::Path;
@@ -17,40 +17,23 @@ pub fn mandos_rs<P: AsRef<Path>>(relative_path: P, world: BlockchainMock) {
 fn parse_execute_mandos_steps(steps_path: &Path, mut state: BlockchainMock) -> BlockchainMock {
     let scenario = mandos::parse_scenario(steps_path);
 
-    for step in scenario.steps.iter() {
+    for step in scenario.steps.into_iter() {
         state = match step {
             Step::ExternalSteps(external_steps_step) => {
                 let parent_path = steps_path.parent().unwrap();
                 let new_path = parent_path.join(&external_steps_step.path);
                 parse_execute_mandos_steps(new_path.as_path(), state)
             },
-            Step::SetState(set_state_step) => {
-                mandos_step::set_state::execute(&mut state, set_state_step);
-                state
+            Step::SetState(set_state_step) => state.mandos_set_state(set_state_step),
+            Step::ScCall(sc_call_step) => state.mandos_sc_call(sc_call_step),
+            Step::ScQuery(sc_query_step) => state.mandos_sc_query(sc_query_step),
+            Step::ScDeploy(sc_deploy_step) => state.mandos_sc_deploy(sc_deploy_step),
+            Step::Transfer(transfer_step) => state.mandos_transfer(transfer_step),
+            Step::ValidatorReward(validator_reward_step) => {
+                state.mandos_validator_reward(validator_reward_step)
             },
-            Step::ScCall(sc_call_step) => mandos_step::sc_call::execute(state, sc_call_step),
-            Step::ScQuery(sc_query_step) => mandos_step::sc_query::execute(state, sc_query_step),
-            Step::ScDeploy(sc_deploy_step) => {
-                mandos_step::sc_deploy::execute(state, sc_deploy_step)
-            },
-            Step::Transfer(transfer_step) => {
-                mandos_step::transfer::execute(state, &transfer_step.tx)
-            },
-            Step::ValidatorReward(validator_reward) => {
-                state.increase_validator_reward(
-                    &validator_reward.tx.to.value.into(),
-                    &validator_reward.tx.egld_value.value,
-                );
-                state
-            },
-            Step::CheckState(check_state_step) => {
-                mandos_step::check_state::execute(&check_state_step.accounts, &mut state);
-                state
-            },
-            Step::DumpState(_) => {
-                state.print_accounts();
-                state
-            },
+            Step::CheckState(check_state_step) => state.mandos_check_state(check_state_step),
+            Step::DumpState(_) => state.mandos_dump_state(),
         };
     }
 
--- elrond-wasm-debug/src/mandos_step/transfer.rs
+++ elrond-wasm-debug/src/mandos_step/transfer.rs
@@ -1,17 +1,30 @@
 use std::rc::Rc;
 
 use elrond_wasm::types::heap::H256;
-use mandos::model::TxTransfer;
+use mandos::model::{TransferStep, TxTransfer, ValidatorRewardStep};
 
 use crate::{
     sc_call::tx_esdt_transfers_from_mandos, tx_execution::sc_call, tx_mock::TxInput,
     world_mock::BlockchainMock,
 };
 
-pub fn execute(state: BlockchainMock, tx_transfer: &TxTransfer) -> BlockchainMock {
-    let mut state_rc = Rc::new(state);
-    execute_rc(&mut state_rc, tx_transfer);
-    Rc::try_unwrap(state_rc).unwrap()
+impl BlockchainMock {
+    pub fn mandos_transfer(self, transfer_step: TransferStep) -> BlockchainMock {
+        let mut state_rc = Rc::new(self);
+        execute_rc(&mut state_rc, &transfer_step.tx);
+        Rc::try_unwrap(state_rc).unwrap()
+    }
+
+    pub fn mandos_validator_reward(
+        mut self,
+        validator_rewards_step: ValidatorRewardStep,
+    ) -> BlockchainMock {
+        self.increase_validator_reward(
+            &validator_rewards_step.tx.to.value.into(),
+            &validator_rewards_step.tx.egld_value.value,
+        );
+        self
+    }
 }
 
 pub fn execute_rc(state: &mut Rc<BlockchainMock>, tx_transfer: &TxTransfer) {
