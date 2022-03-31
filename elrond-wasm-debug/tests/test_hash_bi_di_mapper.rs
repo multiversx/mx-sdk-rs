15c028c5314677a74cd1186b25bbb65ec9c7939a
Alin Cruceat <alyn_509@yahoo.com>
2022-03-31 10:32:59 +0300

    complete tests set insert & check

--- elrond-wasm-debug/tests/test_hash_bi_di_mapper.rs
+++ elrond-wasm-debug/tests/test_hash_bi_di_mapper.rs
@@ -4,13 +4,13 @@ use elrond_wasm::storage::{
 };
 use elrond_wasm_debug::DebugApi;
 
-fn create_set() -> BiDiMapper<DebugApi, u64, u32> {
+fn create_set_1() -> BiDiMapper<DebugApi, u64, u32> {
     let _ = DebugApi::dummy();
     let base_key = StorageKey::new(&b"my_bidi_set"[..]);
     BiDiMapper::new(base_key)
 }
 
-fn check_set(set: &BiDiMapper<DebugApi, u64, u32>, ids: Vec<u64>, values: Vec<u32>) {
+fn check_set_1(set: &BiDiMapper<DebugApi, u64, u32>, ids: Vec<u64>, values: Vec<u32>) {
     assert_eq!(values.len(), ids.len());
     assert_eq!(set.len(), ids.len());
 
@@ -22,12 +22,12 @@ fn check_set(set: &BiDiMapper<DebugApi, u64, u32>, ids: Vec<u64>, values: Vec<u3
 }
 
 #[test]
-fn test_hash_set_simple() {
-    let mut set = create_set();
-    check_set(&set, vec![], vec![]);
+fn test_hash_set_simple_1() {
+    let mut set = create_set_1();
+    check_set_1(&set, vec![], vec![]);
 
     assert_eq!(set.insert(42, 43), true);
-    check_set(&set, vec![42], vec![43]);
+    check_set_1(&set, vec![42], vec![43]);
     assert_eq!(set.insert(42, 44), false);
     assert_eq!(set.insert(44, 43), false);
 
@@ -35,55 +35,8 @@ fn test_hash_set_simple() {
     assert_eq!(set.insert(2, 102), true);
     assert_eq!(set.insert(3, 103), true);
     assert_eq!(set.insert(4, 104), true);
-    check_set(&set, vec![42, 1, 2, 3, 4], vec![43, 101, 102, 103, 104]);
+    check_set_1(&set, vec![42, 1, 2, 3, 4], vec![43, 101, 102, 103, 104]);
 
     assert_eq!(set.get_id(&101), 1);
     assert_eq!(set.get_value(&4), 104);
 }
-
-// #[test]
-// fn test_set_removal() {
-//     let mut set = create_set();
-//     check_set(&set, vec![]);
-//     set.insert(42);
-//     check_set(&set, vec![42]);
-//     set.insert(43);
-//     check_set(&set, vec![42, 43]);
-//     assert_eq!(set.swap_remove(&50), false);
-//     check_set(&set, vec![42, 43]);
-//     assert_eq!(set.swap_remove(&42), true);
-//     check_set(&set, vec![43]);
-//     assert_eq!(set.contains(&42), false);
-//     assert_eq!(set.swap_remove(&42), false);
-//     check_set(&set, vec![43]);
-// }
-
-// #[test]
-// fn test_set_removal_from_middle() {
-//     let mut set = create_set();
-//     set.insert(42);
-//     set.insert(43);
-//     set.insert(44);
-//     set.insert(45);
-//     check_set(&set, vec![42, 43, 44, 45]);
-//     assert_eq!(set.swap_remove(&43), true);
-//     check_set(&set, vec![42, 45, 44]);
-//     assert_eq!(set.swap_remove(&45), true);
-//     check_set(&set, vec![42, 44]);
-//     assert_eq!(set.swap_remove(&44), true);
-//     check_set(&set, vec![42]);
-//     assert_eq!(set.swap_remove(&42), true);
-//     check_set(&set, vec![]);
-// }
-
-// #[test]
-// fn test_set_clear() {
-//     let mut set = create_set();
-//     set.insert(42);
-//     set.insert(43);
-//     set.insert(44);
-//     set.insert(45);
-//     set.clear();
-//     assert_eq!(set.len(), 0);
-//     assert!(set.is_empty());
-// }
