c0234677f3949c1883f1fa2d9af530670a512b52
Andrei Marinica <andrei.marinica@elrond.com>
2022-04-12 22:37:10 +0300

    promises contract - simplified promise_multi_transfer

--- contracts/feature-tests/composability/promises-features/src/promises_features.rs
+++ contracts/feature-tests/composability/promises-features/src/promises_features.rs
@@ -1,8 +1,6 @@
 #![no_std]
 #![allow(clippy::type_complexity)]
 
-use elrond_wasm::api::ESDT_MULTI_TRANSFER_FUNC_NAME;
-
 elrond_wasm::imports!();
 
 /// Test contract for investigating the new async call framework.
@@ -41,43 +39,28 @@ pub trait PromisesFeatures {
     }
 
     #[endpoint]
-    fn forwarder_multi_transfer_via_promise(
+    fn promise_multi_transfer(
         &self,
         to: ManagedAddress,
         endpoint_name: ManagedBuffer,
         extra_gas_for_callback: u64,
-        #[var_args] token_payments: MultiValueEncoded<MultiValue3<TokenIdentifier, u64, BigUint>>,
+        #[var_args] token_payment_args: MultiValueEncoded<EsdtTokenPaymentMultiValue>,
     ) {
-        let mut arg_buffer = ManagedArgBuffer::new_empty();
-        arg_buffer.push_arg(to);
-        arg_buffer.push_arg(token_payments.raw_len() / 3);
-
-        for multi_arg in token_payments.into_iter() {
-            let (token_identifier, token_nonce, amount) = multi_arg.into_tuple();
-
-            arg_buffer.push_arg(token_identifier);
-            arg_buffer.push_arg(token_nonce);
-            arg_buffer.push_arg(amount);
-        }
-
-        if !endpoint_name.is_empty() {
-            arg_buffer.push_arg_raw(endpoint_name);
+        let mut token_payments_vec = ManagedVec::new();
+        for token_payment_arg in token_payment_args {
+            token_payments_vec.push(token_payment_arg.into_esdt_token_payment());
         }
 
         let gas_limit = (self.blockchain().get_gas_left() - extra_gas_for_callback) * 9 / 10;
 
-        Self::Api::send_api_impl().create_async_call_raw(
-            &ManagedAddress::from_raw_handle(
-                Self::Api::blockchain_api_impl().get_sc_address_handle(),
-            ),
-            &BigUint::zero(),
-            &ManagedBuffer::new_from_bytes(ESDT_MULTI_TRANSFER_FUNC_NAME),
-            b"success_callback",
-            b"error_callback",
-            gas_limit,
-            extra_gas_for_callback,
-            &arg_buffer,
-        );
+        self.send()
+            .contract_call::<()>(to, endpoint_name)
+            .with_multi_token_transfer(token_payments_vec)
+            .with_gas_limit(gas_limit)
+            .with_extra_gas_for_callback(extra_gas_for_callback)
+            .with_success_callback(b"success_callback")
+            .with_error_callback(b"error_callback")
+            .register_promise();
     }
 
     #[endpoint]
