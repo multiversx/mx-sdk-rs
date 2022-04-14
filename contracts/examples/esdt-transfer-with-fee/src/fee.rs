4902d7e3a821acb2c5f742b1b59c2fcb70576e66
Alin Cruceat <alyn_509@yahoo.com>
2022-04-14 15:08:06 +0300

    Update contracts/examples/esdt-transfer-with-fee/src/fee.rs
    Co-authored-by: dorin-iancu <72252689+dorin-iancu@users.noreply.github.com>

--- contracts/examples/esdt-transfer-with-fee/src/fee.rs
+++ contracts/examples/esdt-transfer-with-fee/src/fee.rs
@@ -1,7 +1,7 @@
 elrond_wasm::imports!();
 elrond_wasm::derive_imports!();
 
-pub(crate) const PERCENTAGE_DIVISOR: u32 = 10000; // dividing the percentage fee by this number will result in a 4 decimal percentage
+pub(crate) const PERCENTAGE_DIVISOR: u32 = 10_000; // dividing the percentage fee by this number will result in a 4 decimal percentage
 
 #[derive(TopEncode, TopDecode, TypeAbi, PartialEq, Clone)]
 pub enum Fee<M>
