38de50ad0764fa0d5761bf931f9d9c074484150a
Alin Cruceat <alyn_509@yahoo.com>
2022-04-18 10:08:00 +0300

    ESDT transfer with fee contract (#673)

---
+++ contracts/examples/esdt-transfer-with-fee/src/fee.rs
@@ -0,0 +1,14 @@
+elrond_wasm::imports!();
+elrond_wasm::derive_imports!();
+
+pub(crate) const PERCENTAGE_DIVISOR: u32 = 10_000; // dividing the percentage fee by this number will result in a 2 decimal percentage
+
+#[derive(TopEncode, TopDecode, TypeAbi, PartialEq, Clone)]
+pub enum Fee<M>
+where
+    M: ManagedTypeApi,
+{
+    Unset,
+    ExactValue(EsdtTokenPayment<M>),
+    Percentage(u32),
+}
