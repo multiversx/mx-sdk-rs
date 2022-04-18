f7a1de7a54cfd1933e1a8e1cce463846eb7606bb
Andrei Marinica <andrei.marinica@elrond.com>
2022-04-18 12:10:49 +0300

    api handle refactor - uninitialized mb fix

--- elrond-wasm/src/api/managed_types/const_handles.rs
+++ elrond-wasm/src/api/managed_types/const_handles.rs
@@ -1,5 +1,6 @@
 use super::Handle;
 
+/// WARNING! With the current VM this still needs to be initialized before use.
 pub const BIG_INT_CONST_ZERO: Handle = -10;
 
 pub const CALL_VALUE_EGLD: Handle = -11;
@@ -8,6 +9,7 @@ pub const CALL_VALUE_SINGLE_ESDT: Handle = -13;
 pub const BIG_INT_TEMPORARY_1: Handle = -14;
 pub const BIG_INT_TEMPORARY_2: Handle = -15;
 
+/// WARNING! With the current VM this still needs to be initialized before use.
 pub const MBUF_CONST_EMPTY: Handle = -20;
 pub const CALL_VALUE_MULTI_ESDT: Handle = -21;
 pub const CALL_VALUE_SINGLE_ESDT_TOKEN_NAME: Handle = -22;
