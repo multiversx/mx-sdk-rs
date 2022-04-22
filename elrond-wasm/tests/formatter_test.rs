aa305d1738f6d882629ba34e4f7a4699983e2100
Andrei Marinica <andrei.marinica@elrond.com>
2022-04-22 13:02:55 +0300

    formatter - CodeMetadata test & fix

--- elrond-wasm/tests/formatter_test.rs
+++ elrond-wasm/tests/formatter_test.rs
@@ -39,66 +39,8 @@ fn check_display<T: SCDisplay + Display>(item: T) {
     assert_eq!(receiver.0, expected);
 }
 
-/// Expects that the output from SCLowerHex is the same as the standard Rust display.
-fn check_lower_hex<T: SCLowerHex + Display + std::fmt::LowerHex>(item: T) {
-    let mut receiver = SimpleReceiver::default();
-    SCLowerHex::fmt(&item, &mut receiver);
-    let expected = format!("{:x}", item);
-    assert_eq!(receiver.0, expected);
-}
-
-fn check_code_metadata_display<T: SCDisplay, D: Display>(item: T, expected: D) {
-    let mut receiver = SimpleReceiver::default();
-    SCDisplay::fmt(&item, &mut receiver);
-    assert_eq!(receiver.0, expected.to_string());
-}
-
 #[test]
-fn test_lower_hex_usigned() {
-    check_lower_hex(0x0u8);
-    check_lower_hex(0x50u8);
-    check_lower_hex(0xFFu8);
-    check_lower_hex(0x0u16);
-    check_lower_hex(0x50u16);
-    check_lower_hex(0xFFFFu16);
-    check_lower_hex(0x0u32);
-    check_lower_hex(0x50u32);
-    check_lower_hex(0xFFFFFFFFu32);
-    check_lower_hex(0x50u32);
-    check_lower_hex(0x0u64);
-    check_lower_hex(0x5u64);
-    check_lower_hex(0x50u64);
-    check_lower_hex(u64::MAX);
-}
-
-#[test]
-fn test_lower_hex_signed() {
-    check_lower_hex(0xai32);
-    check_lower_hex(5i32);
-    check_lower_hex(50i32);
-    check_lower_hex(-2i32);
-    check_lower_hex(-1i16);
-    check_lower_hex(-50i32);
-    check_lower_hex(-100i32);
-    check_lower_hex(-0xFFi32);
-    check_lower_hex(-0xFFFFi32);
-    check_lower_hex(-0x01i64);
-    check_lower_hex(-0x50i64);
-    check_lower_hex(-0xFFi64);
-    check_lower_hex(-0xFFFFi64);
-    check_lower_hex(-0xFFFFFFFFi64);
-    check_lower_hex(i8::MAX);
-    check_lower_hex(i8::MIN);
-    check_lower_hex(i16::MAX);
-    check_lower_hex(i16::MIN);
-    check_lower_hex(i32::MAX);
-    check_lower_hex(i32::MIN);
-    check_lower_hex(i64::MAX);
-    check_lower_hex(i64::MIN);
-}
-
-#[test]
-fn test_display_usigned() {
+fn test_display_unsigned() {
     check_display(0u8);
     check_display(50u8);
     check_display(0u16);
@@ -128,6 +70,12 @@ fn test_format_signed() {
     check_display(i32::MIN);
     check_display(i64::MAX);
     check_display(i64::MIN);
+}
+
+fn check_code_metadata_display<T: SCDisplay>(item: T, expected: &str) {
+    let mut receiver = SimpleReceiver::default();
+    SCDisplay::fmt(&item, &mut receiver);
+    assert_eq!(receiver.0.as_str(), expected);
 }
 
 #[test]
@@ -184,4 +132,97 @@ fn test_display_code_metadata() {
         "Upgradeable|Readable|Payable|PayableBySC",
     );
     check_code_metadata_display(CodeMetadata::DEFAULT, "Default");
+}
+
+fn check_lower_hex_eq<T: SCLowerHex>(item: T, expected: &str) {
+    let mut receiver = SimpleReceiver::default();
+    SCLowerHex::fmt(&item, &mut receiver);
+    assert_eq!(receiver.0.as_str(), expected);
+}
+
+/// Expects that the output from SCLowerHex is the same as the standard Rust display.
+fn check_lower_hex<T: SCLowerHex + std::fmt::LowerHex>(item: T) {
+    let expected = format!("{:x}", &item);
+    println!("{:x}", &item);
+    check_lower_hex_eq(item, expected.as_str());
+}
+
+#[test]
+fn test_lower_hex_usigned() {
+    check_lower_hex(0x0u8);
+    check_lower_hex(0x50u8);
+    check_lower_hex(0xFFu8);
+    check_lower_hex(0x0u16);
+    check_lower_hex(0x50u16);
+    check_lower_hex(0xFFFFu16);
+    check_lower_hex(0x0u32);
+    check_lower_hex(0x50u32);
+    check_lower_hex(0xFFFFFFFFu32);
+    check_lower_hex(0x50u32);
+    check_lower_hex(0x0u64);
+    check_lower_hex(0x5u64);
+    check_lower_hex(0x50u64);
+    check_lower_hex(u64::MAX);
+}
+
+#[test]
+fn test_lower_hex_signed() {
+    check_lower_hex(0xai32);
+    check_lower_hex(5i32);
+    check_lower_hex(50i32);
+    check_lower_hex(-2i32);
+    check_lower_hex(-1i16);
+    check_lower_hex(-50i32);
+    check_lower_hex(-100i32);
+    check_lower_hex(-0xFFi32);
+    check_lower_hex(-0xFFFFi32);
+    check_lower_hex(-0x01i64);
+    check_lower_hex(-0x50i64);
+    check_lower_hex(-0xFFi64);
+    check_lower_hex(-0xFFFFi64);
+    check_lower_hex(-0xFFFFFFFFi64);
+    check_lower_hex(i8::MAX);
+    check_lower_hex(i8::MIN);
+    check_lower_hex(i16::MAX);
+    check_lower_hex(i16::MIN);
+    check_lower_hex(i32::MAX);
+    check_lower_hex(i32::MIN);
+    check_lower_hex(i64::MAX);
+    check_lower_hex(i64::MIN);
+}
+
+#[test]
+fn test_lower_hex_code_metadata() {
+    check_lower_hex_eq(CodeMetadata::UPGRADEABLE, "0100");
+    check_lower_hex_eq(CodeMetadata::READABLE, "0400");
+    check_lower_hex_eq(CodeMetadata::PAYABLE, "0002");
+    check_lower_hex_eq(CodeMetadata::PAYABLE_BY_SC, "0004");
+    check_lower_hex_eq(
+        CodeMetadata::UPGRADEABLE
+            | CodeMetadata::PAYABLE
+            | CodeMetadata::READABLE
+            | CodeMetadata::PAYABLE_BY_SC,
+        "0506",
+    );
+}
+
+fn check_binary_eq<T: SCBinary>(item: T, expected: &str) {
+    let mut receiver = SimpleReceiver::default();
+    SCBinary::fmt(&item, &mut receiver);
+    assert_eq!(receiver.0.as_str(), expected);
+}
+
+#[test]
+fn test_binary_code_metadata() {
+    check_binary_eq(CodeMetadata::UPGRADEABLE, "0000000100000000");
+    check_binary_eq(CodeMetadata::READABLE, "0000010000000000");
+    check_binary_eq(CodeMetadata::PAYABLE, "0000000000000010");
+    check_binary_eq(CodeMetadata::PAYABLE_BY_SC, "0000000000000100");
+    check_binary_eq(
+        CodeMetadata::UPGRADEABLE
+            | CodeMetadata::PAYABLE
+            | CodeMetadata::READABLE
+            | CodeMetadata::PAYABLE_BY_SC,
+        "0000010100000110",
+    );
 }
