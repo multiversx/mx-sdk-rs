8da568068343743ed80603d90ea36738d4125142
Andrei Marinica <andrei.marinica@elrond.com>
2022-04-26 17:15:18 +0300

    callback arg generator refactor (with nested tuples)

--- elrond-wasm/src/io/arg_nested_tuple.rs
+++ elrond-wasm/src/io/arg_nested_tuple.rs
@@ -14,7 +14,7 @@ where
 
     fn check_num_single_args(index: i32);
     fn load_single_arg(index: i32, arg_names: Self::ArgNames) -> Self;
-    fn load_multi_arg(loader: EndpointDynArgLoader<AA>, arg_names: Self::ArgNames) -> Self;
+    fn load_multi_arg<L: TopDecodeMultiInput>(loader: L, arg_names: Self::ArgNames) -> Self;
 }
 
 impl<AA, Head, Tail> ArgNestedTuple<AA> for (Head, Tail)
@@ -44,11 +44,12 @@ where
             let Ok(value) = Head::multi_decode_or_handle_err(&mut arg_loader, h);
             (value, Tail::load_single_arg(index + 1, tail_names))
         } else {
-            let loader = EndpointDynArgLoader::new_at_index(index);
+            let loader = EndpointDynArgLoader::<AA>::new_at_index(index);
             Self::load_multi_arg(loader, arg_names)
         }
     }
-    fn load_multi_arg(mut loader: EndpointDynArgLoader<AA>, arg_names: Self::ArgNames) -> Self {
+
+    fn load_multi_arg<L: TopDecodeMultiInput>(mut loader: L, arg_names: Self::ArgNames) -> Self {
         let (arg_name, tail_names) = arg_names;
         let h = ArgErrorHandler::<AA>::from(ArgId::from(arg_name));
         let result = Head::multi_decode_or_handle_err(&mut loader, h);
@@ -67,12 +68,13 @@ where
         AA::argument_api_impl().check_num_arguments(index);
     }
     fn load_single_arg(_index: i32, _arg_names: Self::ArgNames) -> Self {}
-    fn load_multi_arg(loader: EndpointDynArgLoader<AA>, _arg_names: Self::ArgNames) -> Self {
+    fn load_multi_arg<L: TopDecodeMultiInput>(loader: L, _arg_names: Self::ArgNames) -> Self {
         let h = ExitCodecErrorHandler::<AA>::from(&[][..]);
         let Ok(()) = loader.assert_no_more_args(h);
     }
 }
 
+/// Used for loading all regular endpoint arguments. Call to it gets generated for all endpoints and callbacks.
 pub fn load_endpoint_args<AA, N>(arg_names: N::ArgNames) -> N
 where
     AA: EndpointArgumentApi + ManagedTypeApi + ErrorApi,
@@ -80,4 +82,14 @@ where
 {
     N::check_num_single_args(0);
     N::load_single_arg(0, arg_names)
+}
+
+/// Currently used for the callback closure.
+pub fn load_multi_args_custom_loader<AA, L, N>(loader: L, arg_names: N::ArgNames) -> N
+where
+    AA: EndpointArgumentApi + ManagedTypeApi + ErrorApi,
+    L: TopDecodeMultiInput,
+    N: ArgNestedTuple<AA>,
+{
+    N::load_multi_arg(loader, arg_names)
 }
