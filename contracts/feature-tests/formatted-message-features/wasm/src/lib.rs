c5aebe441088bc6494e5cda1dd341b41a3cabdf4
Alin Cruceat <alyn_509@yahoo.com>
2022-05-11 12:55:31 +0300

    fix typeabi impl for bidimapper (#708)
    * fix typeabi impl for bidimapper
    
    * remove cargo expand file
    
    Co-authored-by: Claudiu-Marcel Bruda <claudiu725@yahoo.com>

--- contracts/feature-tests/formatted-message-features/wasm/src/lib.rs
+++ contracts/feature-tests/formatted-message-features/wasm/src/lib.rs
@@ -10,10 +10,14 @@ elrond_wasm_node::wasm_endpoints! {
         decode_error_message
         dynamic_message
         dynamic_message_ascii
+        dynamic_message_hex
         dynamic_message_multiple
         format_message_multiple_arguments
         format_message_one_argument
         print_message
+        print_message_binary
+        print_message_codec
+        print_message_hex
         static_message
     )
 }
