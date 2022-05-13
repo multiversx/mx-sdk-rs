5aa42313e402999d8908a24dd6b8ace0556243d6
dorin.iancu <dorin.iancu@elrond.com>
2022-05-13 14:19:56 +0300

    move function to another file

--- elrond-wasm-debug/src/world_mock/blockchain_mock_account_util.rs
+++ elrond-wasm-debug/src/world_mock/blockchain_mock_account_util.rs
@@ -8,10 +8,6 @@ use crate::address_hex;
 use super::AccountData;
 
 const SC_ADDRESS_NUM_LEADING_ZEROS: u8 = 8;
-const UNDERSCORE: u8 = b'_';
-static ADDR_PREFIX: &str = "address:";
-static SC_ADDR_PREFIX: &str = "sc:";
-static HEX_PREFIX: &str = "0x";
 
 use super::BlockchainMock;
 
@@ -27,7 +23,7 @@ impl BlockchainMock {
             return;
         }
 
-        let addr_pretty = address_as_mandos_string(&address);
+        let addr_pretty = super::address_as_mandos_string(&address);
         self.addr_to_mandos_string_map.insert(address, addr_pretty);
     }
 
@@ -98,34 +94,4 @@ pub fn is_smart_contract_address(address: &Address) -> bool {
         .iter()
         .take(SC_ADDRESS_NUM_LEADING_ZEROS.into())
         .all(|item| item == &0u8)
-}
-
-fn address_as_mandos_string(address: &Address) -> String {
-    let addr_bytes = address.as_array();
-    let (string_start_index, prefix) = if is_smart_contract_address(address) {
-        (SC_ADDRESS_NUM_LEADING_ZEROS as usize, SC_ADDR_PREFIX)
-    } else {
-        (0, ADDR_PREFIX)
-    };
-
-    let mut string_end_index = Address::len_bytes() - 1;
-    while addr_bytes[string_end_index] == UNDERSCORE {
-        string_end_index -= 1;
-    }
-
-    let addr_readable_part = &addr_bytes[string_start_index..=string_end_index];
-    match String::from_utf8(addr_readable_part.to_vec()) {
-        Ok(readable_string) => {
-            let mut result = prefix.to_string();
-            result.push_str(&readable_string);
-
-            result
-        },
-        Err(_) => {
-            let mut result = HEX_PREFIX.to_string();
-            result.push_str(&hex::encode(&addr_bytes[..]));
-
-            result
-        },
-    }
 }
