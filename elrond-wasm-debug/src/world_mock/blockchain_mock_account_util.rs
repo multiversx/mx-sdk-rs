4336d8232b81123b3a7f2690f8f149d2e8cf305c
dorin.iancu <dorin.iancu@elrond.com>
2022-05-13 14:12:52 +0300

    pretty address format for generated mandos

--- elrond-wasm-debug/src/world_mock/blockchain_mock_account_util.rs
+++ elrond-wasm-debug/src/world_mock/blockchain_mock_account_util.rs
@@ -1,24 +1,47 @@
 use alloc::vec::Vec;
 use elrond_wasm::types::heap::Address;
 
-use std::fmt::Write;
+use std::{collections::HashMap, fmt::Write};
 
 use crate::address_hex;
 
 use super::AccountData;
 
 const SC_ADDRESS_NUM_LEADING_ZEROS: u8 = 8;
+const UNDERSCORE: u8 = b'_';
+static ADDR_PREFIX: &str = "address:";
+static SC_ADDR_PREFIX: &str = "sc:";
+static HEX_PREFIX: &str = "0x";
 
 use super::BlockchainMock;
 
 impl BlockchainMock {
     pub fn add_account(&mut self, acct: AccountData) {
-        self.accounts.insert(acct.address.clone(), acct);
+        let address = acct.address.clone();
+        self.accounts.insert(address.clone(), acct);
+        self.add_addr_mandos_string(address);
+    }
+
+    pub fn add_addr_mandos_string(&mut self, address: Address) {
+        if self.addr_to_mandos_string_map.contains_key(&address) {
+            return;
+        }
+
+        let addr_pretty = address_as_mandos_string(&address);
+        self.addr_to_mandos_string_map.insert(address, addr_pretty);
     }
 
     pub fn validate_and_add_account(&mut self, acct: AccountData) {
         self.validate_account(&acct);
         self.add_account(acct);
+    }
+
+    pub fn update_accounts(&mut self, accounts: HashMap<Address, AccountData>) {
+        for addr in accounts.keys() {
+            self.add_addr_mandos_string(addr.clone());
+        }
+
+        self.accounts.extend(accounts.into_iter());
     }
 
     pub fn print_accounts(&self) {
@@ -75,4 +98,34 @@ pub fn is_smart_contract_address(address: &Address) -> bool {
         .iter()
         .take(SC_ADDRESS_NUM_LEADING_ZEROS.into())
         .all(|item| item == &0u8)
+}
+
+fn address_as_mandos_string(address: &Address) -> String {
+    let addr_bytes = address.as_array();
+    let (string_start_index, prefix) = if is_smart_contract_address(address) {
+        (SC_ADDRESS_NUM_LEADING_ZEROS as usize, SC_ADDR_PREFIX)
+    } else {
+        (0, ADDR_PREFIX)
+    };
+
+    let mut string_end_index = Address::len_bytes() - 1;
+    while addr_bytes[string_end_index] == UNDERSCORE {
+        string_end_index -= 1;
+    }
+
+    let addr_readable_part = &addr_bytes[string_start_index..=string_end_index];
+    match String::from_utf8(addr_readable_part.to_vec()) {
+        Ok(readable_string) => {
+            let mut result = prefix.to_string();
+            result.push_str(&readable_string);
+
+            result
+        },
+        Err(_) => {
+            let mut result = HEX_PREFIX.to_string();
+            result.push_str(&hex::encode(&addr_bytes[..]));
+
+            result
+        },
+    }
 }
