702894487e32e1db82173953992225e503e56955
Andrei Marinica <andrei.marinica@elrond.com>
2022-05-28 02:47:10 +0300

    moved ManagedRef

--- elrond-wasm/src/types/managed/mod.rs
+++ elrond-wasm/src/types/managed/mod.rs
@@ -1,13 +1,11 @@
 mod basic;
 mod codec_util;
-mod managed_ref;
 mod managed_type_trait;
 mod multi_value;
 mod wrapped;
 
 pub use basic::*;
 pub use codec_util::*;
-pub use managed_ref::ManagedRef;
 pub use managed_type_trait::ManagedType;
 pub use multi_value::*;
 pub use wrapped::*;
--- elrond-wasm/src/types/managed/wrapped/mod.rs
+++ elrond-wasm/src/types/managed/wrapped/mod.rs
@@ -4,6 +4,7 @@ mod managed_address;
 mod managed_buffer_cached_builder;
 mod managed_byte_array;
 mod managed_option;
+mod managed_ref;
 mod managed_vec;
 mod managed_vec_item;
 mod managed_vec_owned_iter;
@@ -20,6 +21,7 @@ pub use managed_buffer_cached_builder::ManagedBufferCachedBuilder;
 pub(crate) use managed_byte_array::ManagedBufferSizeContext;
 pub use managed_byte_array::ManagedByteArray;
 pub use managed_option::ManagedOption;
+pub use managed_ref::ManagedRef;
 pub use managed_vec::{
     managed_vec_from_slice_of_boxed_bytes, managed_vec_of_buffers_to_arg_buffer, ManagedVec,
 };
