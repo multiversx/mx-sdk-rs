0890691f0bfd67560d8d379414381ca1fae29809
dorin.iancu <dorin.iancu@elrond.com>
2022-06-03 14:04:56 +0300

    fix proxy refund logic

--- elrond-wasm-modules/src/transfer_role/transfer_proxy.rs
+++ elrond-wasm-modules/src/transfer_role/transfer_proxy.rs
@@ -3,6 +3,9 @@ elrond_wasm::imports!();
 use elrond_wasm::elrond_codec::Empty;
 
 const CALLBACK_RESERVED_GAS_PER_TOKEN: u64 = 1_000_000;
+static ERR_CALLBACK_MSG: &[u8] = b"Error received in callback:";
+
+pub type PaymentsVec<M> = ManagedVec<M, EsdtTokenPayment<M>>;
 
 #[elrond_wasm::module]
 pub trait TransferProxyModule {
@@ -10,13 +13,13 @@ pub trait TransferProxyModule {
         &self,
         original_caller: ManagedAddress,
         dest: ManagedAddress,
-        payments: ManagedVec<EsdtTokenPayment<Self::Api>>,
+        payments: PaymentsVec<Self::Api>,
         data: ManagedBuffer,
     ) -> ! {
         let contract_call =
-            ContractCall::<Self::Api, Empty>::new_with_esdt_payment(dest, data, payments);
+            ContractCall::<Self::Api, Empty>::new_with_esdt_payment(dest, data, payments.clone());
 
-        self.execute_async_call(original_caller, contract_call);
+        self.execute_async_call(original_caller, payments, contract_call);
     }
 
     fn transfer_to_contract_typed_call(
@@ -28,28 +31,36 @@ pub trait TransferProxyModule {
         original_caller_arg.push_arg(original_caller.clone());
         contract_call.arg_buffer = original_caller_arg.concat(contract_call.arg_buffer);
 
-        self.execute_async_call(original_caller, contract_call);
+        self.execute_async_call(
+            original_caller,
+            contract_call.payments.clone(),
+            contract_call,
+        );
     }
 
     fn transfer_to_contract_raw(
         &self,
         original_caller: ManagedAddress,
         dest: ManagedAddress,
-        payments: ManagedVec<EsdtTokenPayment<Self::Api>>,
+        payments: PaymentsVec<Self::Api>,
         endpoint_name: ManagedBuffer,
         args: ManagedArgBuffer<Self::Api>,
     ) -> ! {
-        let mut contract_call =
-            ContractCall::<Self::Api, Empty>::new_with_esdt_payment(dest, endpoint_name, payments);
+        let mut contract_call = ContractCall::<Self::Api, Empty>::new_with_esdt_payment(
+            dest,
+            endpoint_name,
+            payments.clone(),
+        );
         contract_call.arg_buffer.push_arg(original_caller.clone());
         contract_call.arg_buffer = contract_call.arg_buffer.concat(args);
 
-        self.execute_async_call(original_caller, contract_call);
+        self.execute_async_call(original_caller, payments, contract_call);
     }
 
     fn execute_async_call(
         &self,
         original_caller: ManagedAddress,
+        initial_payments: PaymentsVec<Self::Api>,
         contract_call: ContractCall<Self::Api, Empty>,
     ) -> ! {
         let remaining_gas = self.blockchain().get_gas_left();
@@ -60,7 +71,8 @@ pub trait TransferProxyModule {
         );
 
         let async_call_gas = remaining_gas - cb_gas_needed;
-        let cb = TransferProxyModule::callbacks(self).transfer_callback(original_caller);
+        let cb = TransferProxyModule::callbacks(self)
+            .transfer_callback(original_caller, initial_payments);
         contract_call
             .with_gas_limit(async_call_gas)
             .async_call()
@@ -72,18 +84,19 @@ pub trait TransferProxyModule {
     fn transfer_callback(
         &self,
         original_caller: ManagedAddress,
+        initial_payments: ManagedVec<EsdtTokenPayment<Self::Api>>,
         #[call_result] result: ManagedAsyncCallResult<MultiValueEncoded<ManagedBuffer>>,
     ) -> MultiValueEncoded<ManagedBuffer> {
         match result {
             ManagedAsyncCallResult::Ok(return_values) => return_values,
             ManagedAsyncCallResult::Err(err) => {
-                let returned_tokens = self.call_value().all_esdt_transfers();
-                if returned_tokens.len() > 0 {
+                if initial_payments.len() > 0 {
                     self.send()
-                        .direct_multi(&original_caller, &returned_tokens, &[]);
+                        .direct_multi(&original_caller, &initial_payments, &[]);
                 }
 
                 let mut err_result = MultiValueEncoded::new();
+                err_result.push(ManagedBuffer::new_from_bytes(ERR_CALLBACK_MSG));
                 err_result.push(err.err_msg);
 
                 err_result
