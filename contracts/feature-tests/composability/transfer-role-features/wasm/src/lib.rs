7df8f6a4dc895eb2d662e9550514409071935bd4
dorin.iancu <dorin.iancu@elrond.com>
2022-06-17 13:10:21 +0300

    build

--- contracts/feature-tests/composability/transfer-role-features/wasm/src/lib.rs
+++ contracts/feature-tests/composability/transfer-role-features/wasm/src/lib.rs
@@ -5,21 +5,9 @@
 #![no_std]
 
 elrond_wasm_node::wasm_endpoints! {
-    vault
+    transfer_role_features
     (
-        accept_funds
-        accept_funds_echo_payment
-        accept_funds_single_esdt_transfer
-        burn_and_create_retrive_async
-        call_counts
-        echo_arguments
-        echo_caller
-        get_owner_address
-        reject_funds
-        retrieve_funds
-        retrieve_funds_with_transfer_exec
-        retrieve_multi_funds_async
+        callBack
+        forwardPayments
     )
 }
-
-elrond_wasm_node::wasm_empty_callback! {}
