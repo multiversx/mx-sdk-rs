5185f8f154b2852eed83e8c5631b10fc4f70c74e
Andrei Marinica <andrei.marinica@elrond.com>
2022-06-20 14:30:38 +0300

    mandos better print

--- elrond-wasm-debug/src/mandos_step/tx_output_check.rs
+++ elrond-wasm-debug/src/mandos_step/tx_output_check.rs
@@ -6,7 +6,7 @@ pub fn check_tx_output(tx_id: &str, tx_expect: &TxExpect, tx_result: &TxResult) 
     let have_str = tx_result.result_message.as_str();
     assert!(
         tx_expect.status.check(tx_result.result_status),
-        "result code mismatch. Tx id: {}. Want: {}. Have: {}. Message: {}",
+        "result code mismatch. Tx id: '{}'. Want: {}. Have: {}. Message: {}",
         tx_id,
         tx_expect.status,
         tx_result.result_status,
@@ -15,7 +15,7 @@ pub fn check_tx_output(tx_id: &str, tx_expect: &TxExpect, tx_result: &TxResult) 
 
     assert!(
         tx_expect.out.check(tx_result.result_values.as_slice()),
-        "bad out value. Tx id: {}. Want: [{}]. Have: [{}]",
+        "bad out value. Tx id: '{}'. Want: [{}]. Have: [{}]",
         tx_id,
         tx_expect.out_to_string(),
         tx_result.result_values_to_string()
@@ -23,7 +23,7 @@ pub fn check_tx_output(tx_id: &str, tx_expect: &TxExpect, tx_result: &TxResult) 
 
     assert!(
         tx_expect.message.check(tx_result.result_message.as_bytes()),
-        "result message mismatch. Tx id: {}. Want: {}. Have: {}.",
+        "result message mismatch. Tx id: '{}'. Want: {}. Have: {}.",
         tx_id,
         &tx_expect.message,
         have_str,
@@ -34,7 +34,7 @@ pub fn check_tx_output(tx_id: &str, tx_expect: &TxExpect, tx_result: &TxResult) 
         CheckLogs::List(expected_logs) => {
             assert!(
                 tx_result.result_logs.len() >= expected_logs.list.len(),
-                "Too few logs. Tx id: {}. Want: {}. Have: {}",
+                "Too few logs. Tx id: '{}'. Want: {}. Have: {}",
                 tx_id,
                 expected_logs.list.len(),
                 tx_result.result_logs.len()
@@ -45,7 +45,7 @@ pub fn check_tx_output(tx_id: &str, tx_expect: &TxExpect, tx_result: &TxResult) 
                     let expected_log = &expected_logs.list[i];
                     assert!(
                         actual_log.mandos_check(expected_log),
-                        "Logs do not match. Tx id: {}. Index: {}.\nWant: Address: {}, Endpoint: {}, Topics: {:?}, Data: {}\nHave: Address: {}, Endpoint: {}, Topics: {:?}, Data: {}",
+                        "Logs do not match. Tx id: '{}'. Index: {}.\nWant: Address: {}, Endpoint: {}, Topics: {:?}, Data: {}\nHave: Address: {}, Endpoint: {}, Topics: {:?}, Data: {}",
                         tx_id,
                         i,
                         &expected_log.address,
@@ -59,7 +59,7 @@ pub fn check_tx_output(tx_id: &str, tx_expect: &TxExpect, tx_result: &TxResult) 
                     );
                 } else if !expected_logs.more_allowed_at_end {
                     panic!(
-                        "Unexpected log. Tx id: {}. Index: {}.\nAddress: {}, Endpoint: {}, Topics: {:?}, Data: {}",
+                        "Unexpected log. Tx id: '{}'. Index: {}.\nAddress: {}, Endpoint: {}, Topics: {:?}, Data: {}",
                         tx_id,
                         i,
                         address_hex(&actual_log.address),
