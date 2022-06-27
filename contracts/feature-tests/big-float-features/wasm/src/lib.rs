5a0cbc8b7a58556217e0acfc878dddb26b786f79
Alin Cruceat <alyn_509@yahoo.com>
2022-06-27 17:26:42 +0300

    rebuild

--- contracts/feature-tests/big-float-features/wasm/src/lib.rs
+++ contracts/feature-tests/big-float-features/wasm/src/lib.rs
@@ -9,8 +9,12 @@ elrond_wasm_node::wasm_endpoints! {
     (
         add_assign_big_float
         add_assign_big_float_ref
+        add_assign_big_float_ref_wrapped
+        add_assign_big_float_wrapped
         add_big_float
         add_big_float_ref
+        add_big_float_ref_wrapped
+        add_big_float_wrapped
         big_float_from_big_int_1
         big_float_from_big_int_1_wrapped
         big_float_from_big_int_2
@@ -31,18 +35,26 @@ elrond_wasm_node::wasm_endpoints! {
         big_float_from_isize_wrapped
         big_float_from_man_buf
         big_float_from_man_buf_ref
-        big_float_from_man_buf_ref_wrapped
-        big_float_from_man_buf_wrapped
         big_float_neg
+        big_float_neg_wrapped
         big_float_zero
+        big_float_zero_wrapped
         div_assign_big_float
         div_assign_big_float_ref
+        div_assign_big_float_ref_wrapped
+        div_assign_big_float_wrapped
         div_big_float
         div_big_float_ref
+        div_big_float_ref_wrapped
+        div_big_float_wrapped
         mul_assign_big_float
         mul_assign_big_float_ref
+        mul_assign_big_float_ref_wrapped
+        mul_assign_big_float_wrapped
         mul_big_float
         mul_big_float_ref
+        mul_big_float_ref_wrapped
+        mul_big_float_wrapped
         new_from_frac_big_float
         new_from_frac_big_float_wrapped
         new_from_parts_big_float
@@ -51,12 +63,20 @@ elrond_wasm_node::wasm_endpoints! {
         new_from_sci_big_float_wrapped
         pow_big_float
         pow_big_float_ref
+        pow_big_float_ref_wrapped
+        pow_big_float_wrapped
         sqrt_big_float
         sqrt_big_float_ref
+        sqrt_big_float_ref_wrapped
+        sqrt_big_float_wrapped
         sub_assign_big_float
         sub_assign_big_float_ref
+        sub_assign_big_float_ref_wrapped
+        sub_assign_big_float_wrapped
         sub_big_float
         sub_big_float_ref
+        sub_big_float_ref_wrapped
+        sub_big_float_wrapped
     )
 }
 
