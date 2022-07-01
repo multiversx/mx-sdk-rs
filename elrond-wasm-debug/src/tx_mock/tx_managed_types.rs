a4bcdbc5e18f04ddb318682842cfb77299614875
Claudiu-Marcel Bruda <claudiu725@yahoo.com>
2022-07-01 16:48:20 +0300

    simplify HandleMap

--- elrond-wasm-debug/src/tx_mock/tx_managed_types.rs
+++ elrond-wasm-debug/src/tx_mock/tx_managed_types.rs
@@ -1,77 +1,62 @@
 use crate::num_bigint::BigInt;
-use elrond_wasm::api::{
-    const_handles, use_raw_handle, HandleConstraints, HandleTypeInfo, RawHandle,
-};
-use std::{collections::HashMap, marker::PhantomData};
+use elrond_wasm::api::{const_handles, use_raw_handle, HandleConstraints, RawHandle};
+use std::collections::HashMap;
 
 type ManagedBufferImpl = Vec<u8>;
 
 #[derive(Debug)]
-pub struct HandleMap<H: HandleConstraints, V> {
+pub struct HandleMap<V> {
     next_handle: RawHandle,
     pub map: HashMap<RawHandle, V>,
-    _phantom: PhantomData<H>,
 }
 
-impl<H: HandleConstraints, V> HandleMap<H, V> {
+impl<V> HandleMap<V> {
     pub fn new() -> Self {
         HandleMap {
             next_handle: 0,
             map: HashMap::new(),
-            _phantom: PhantomData,
         }
     }
 }
 
-impl<H: HandleConstraints, V> Default for HandleMap<H, V> {
+impl<V> Default for HandleMap<V> {
     fn default() -> Self {
         HandleMap::new()
     }
 }
 
-impl<H: HandleConstraints, V> HandleMap<H, V> {
-    pub fn insert_new_handle(&mut self, value: V) -> H {
+impl<V> HandleMap<V> {
+    pub fn insert_new_handle<H: HandleConstraints>(&mut self, value: V) -> H {
         let new_handle = self.next_handle;
         self.map.insert(new_handle, value);
         self.next_handle += 1;
         use_raw_handle(new_handle)
     }
 
-    pub fn get(&self, handle: H) -> &V {
+    pub fn get<H: HandleConstraints>(&self, handle: H) -> &V {
         // TODO: consider simulating the actual error from the VM
         self.map
             .get(&handle.get_raw_handle())
             .unwrap_or_else(|| panic!("handle not found"))
     }
 
-    pub fn get_mut(&mut self, handle: H) -> &mut V {
+    pub fn get_mut<H: HandleConstraints>(&mut self, handle: H) -> &mut V {
         // TODO: consider simulating the actual error from the VM
         self.map
             .get_mut(&handle.get_raw_handle())
             .unwrap_or_else(|| panic!("handle not found"))
     }
 
-    pub fn insert(&mut self, handle: H, value: V) {
+    pub fn insert<H: HandleConstraints>(&mut self, handle: H, value: V) {
         let _ = self.map.insert(handle.get_raw_handle(), value);
     }
 }
 
 #[derive(Debug)]
 pub struct TxManagedTypes {
-    pub(crate) big_int_map: HandleMap<<Self as HandleTypeInfo>::BigIntHandle, BigInt>,
-    pub(crate) big_float_map: HandleMap<<Self as HandleTypeInfo>::BigFloatHandle, f64>,
-    pub(crate) managed_buffer_map:
-        HandleMap<<Self as HandleTypeInfo>::ManagedBufferHandle, ManagedBufferImpl>,
-}
-
-impl HandleTypeInfo for TxManagedTypes {
-    type ManagedBufferHandle = i32;
-
-    type BigIntHandle = i32;
-
-    type BigFloatHandle = i32;
-
-    type EllipticCurveHandle = i32;
+    pub(crate) big_int_map: HandleMap<BigInt>,
+    pub(crate) big_float_map: HandleMap<f64>,
+    pub(crate) managed_buffer_map: HandleMap<ManagedBufferImpl>,
 }
 
 impl TxManagedTypes {
