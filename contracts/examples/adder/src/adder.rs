b50d1a2aee812e898208fb00a1d3b766281dd43d
dorin.iancu <dorin.iancu@elrond.com>
2022-08-31 14:52:16 +0300

    generate generic parts

--- contracts/examples/adder/src/adder.rs
+++ contracts/examples/adder/src/adder.rs
@@ -2,6 +2,8 @@
 
 elrond_wasm::imports!();
 
+// mod example_interactor_gen;
+
 /// One of the simplest smart contracts possible,
 /// it holds a single variable in storage, which anyone can increment.
 #[elrond_wasm::contract]
