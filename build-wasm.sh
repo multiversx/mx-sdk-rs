6a7d3a4c91a9021ab017e0b6e7a684821e8971b0
Andrei Marinica <andrei.marinica@elrond.com>
2022-09-20 15:45:51 +0300

    Cargo.lock update

--- build-wasm.sh
+++ build-wasm.sh
@@ -3,7 +3,7 @@
 # builds all wasm targets
 
 set -e
-SMART_CONTRACT_JSONS=$(find . -name "elrond.json")
+SMART_CONTRACT_JSONS=$(find ./contracts -name "elrond.json")
 for smart_contract_json in $SMART_CONTRACT_JSONS
 do
     smart_contract_folder=$(dirname $smart_contract_json)
