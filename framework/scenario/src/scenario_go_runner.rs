c77d91c30e53aa9e4d98f48a41a71c4bd8ba03b3
Claudiu-Marcel Bruda <claudiu725@yahoo.com>
2023-01-26 16:28:48 +0200

    add fallback for run-scenarios

--- framework/scenario/src/scenario_go_runner.rs
+++ framework/scenario/src/scenario_go_runner.rs
@@ -1,4 +1,4 @@
-use std::{path::Path, process::Command};
+use std::{io::ErrorKind, path::Path, process::Command};
 
 /// Runs the Arwen executable,
 /// which reads parses and executes one or more mandos tests.
@@ -10,10 +10,25 @@ pub fn run_go<P: AsRef<Path>>(relative_path: P) {
     let mut absolute_path = std::env::current_dir().unwrap();
     absolute_path.push(relative_path);
 
-    let output = Command::new("run-scenarios")
-        .arg(absolute_path)
-        .output()
-        .expect("failed to execute process");
+    let run_scenarios_result = Command::new("run-scenarios")
+        .arg(absolute_path.clone())
+        .output();
+
+    let not_found = if let Err(error) = &run_scenarios_result {
+        error.kind() == ErrorKind::NotFound
+    } else {
+        false
+    };
+
+    let result = if not_found {
+        // fallback - use the old binary
+        println!("Warning: `run-scenarios` not found. Using `mandos-test` as fallback.");
+        Command::new("mandos-test").arg(absolute_path).output()
+    } else {
+        run_scenarios_result
+    };
+
+    let output = result.expect("failed to execute process");
 
     if output.status.success() {
         println!("{}", String::from_utf8_lossy(output.stdout.as_slice()));
