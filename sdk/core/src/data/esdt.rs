d6d9a28cff7fff9213dff8b7f42245cbc5305bc4
Costin Carabaș <costin.carabas@gmail.com>
2023-01-30 09:56:06 +0200

    Add attributes for SFT esdts

--- sdk/core/src/data/esdt.rs
+++ sdk/core/src/data/esdt.rs
@@ -1,12 +1,26 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+use super::address::Address;
+
 // Account holds an Account's information
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct EsdtBalance {
     pub token_identifier: String,
     pub balance: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub nonce: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub attributes: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub creator: Option<Address>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub hash: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub royalties: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub uris: Option<Vec<String>>,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
