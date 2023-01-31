8ba1d958cddb8c00010e0f37b0c386675e22a62b
dorin.iancu <dorin.iancu@elrond.com>
2023-01-31 16:42:16 +0200

    compile test

--- contracts/feature-tests/rust-snippets-generator-test/tests/test_of_test.rs
+++ contracts/feature-tests/rust-snippets-generator-test/tests/test_of_test.rs
@@ -1,6 +1,19 @@
+use std::{cell::RefCell, rc::Rc};
+
+use multiversx_sc_scenario::{rust_biguint, testing_framework::BlockchainStateWrapper, DebugApi};
+
 mod rust_snippets_generator_test_setup;
 
-// use rust_snippets_generator_test_setup::*;
+use rust_snippets_generator_test_setup::*;
 
 #[test]
-fn test_mod_compile() {}
+fn test_mod_compile() {
+    let _ = DebugApi::dummy();
+    let b_mock = Rc::new(RefCell::new(BlockchainStateWrapper::new()));
+    let owner = b_mock.borrow_mut().create_user_account(&rust_biguint!(0));
+    let _ = RustSnippetsGeneratorTestSetup::new(
+        b_mock,
+        rust_snippets_generator_test::contract_obj,
+        &owner,
+    );
+}
