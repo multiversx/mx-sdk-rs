9b5342905c7914df4825f1c1b747e9622d94cbe1
dorin.iancu <dorin.iancu@elrond.com>
2023-02-01 11:39:17 +0200

    managed types failing test

--- contracts/feature-tests/rust-snippets-generator-test/tests/test_of_test.rs
+++ contracts/feature-tests/rust-snippets-generator-test/tests/test_of_test.rs
@@ -17,3 +17,20 @@ fn test_mod_compile() {
         &owner,
     );
 }
+
+#[test]
+fn test_managed_types() {
+    let _ = DebugApi::dummy();
+    let b_mock = Rc::new(RefCell::new(BlockchainStateWrapper::new()));
+    let owner = b_mock.borrow_mut().create_user_account(&rust_biguint!(0));
+    let setup = RustSnippetsGeneratorTestSetup::new(
+        b_mock,
+        rust_snippets_generator_test::contract_obj,
+        &owner,
+    );
+
+    let result = setup.one_arg_one_result_endpoint(&owner, 50);
+
+    // try access wrong api stack index
+    println!("{:?}", result.unwrap());
+}
