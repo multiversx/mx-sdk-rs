c18900ae7b5de4e295c8314656f47e164534d99b
Andrei Marinica <andrei.marinica@elrond.com>
2023-02-03 09:53:40 +0200

    meta local deps path management

--- framework/meta/src/local_deps.rs
+++ framework/meta/src/local_deps.rs
@@ -5,6 +5,7 @@ use crate::{
     },
     CargoTomlContents,
 };
+use common_path::common_path_all;
 use serde::{Deserialize, Serialize};
 use std::{
     collections::{HashSet, LinkedList},
@@ -16,6 +17,9 @@ use std::{
 #[derive(Serialize, Deserialize, Default)]
 #[serde(rename_all = "camelCase")]
 pub struct LocalDeps {
+    pub root: String,
+    pub contract_path: String,
+    pub common_dependency_path: Option<String>,
     pub dependencies: Vec<String>,
 }
 
@@ -35,8 +39,8 @@ pub fn local_deps(args: &LocalDepsArgs) {
 }
 
 fn perform_local_deps(path: impl AsRef<Path>, ignore: &[String]) {
-    let path_ref = path.as_ref();
-    let dirs = RelevantDirectories::find_all(path_ref, ignore);
+    let root_path = path.as_ref();
+    let dirs = RelevantDirectories::find_all(root_path, ignore);
     dir_pretty_print(dirs.iter_contract_crates(), "", &|_| {});
 
     let mut unique_paths = HashSet::new();
@@ -57,11 +61,22 @@ fn perform_local_deps(path: impl AsRef<Path>, ignore: &[String]) {
 
         expand_deps(&mut unique_paths);
 
-        let mut deps_contents = LocalDeps::default();
-        for path in &unique_paths {
+        let common_dependency_path =
+            common_path_all(unique_paths.iter().map(|pbuf| pbuf.as_path()));
+
+        let mut deps_contents = LocalDeps {
+            root: root_path.to_string_lossy().to_string(),
+            contract_path: contract_dir.path.to_string_lossy().to_string(),
+            common_dependency_path: common_dependency_path
+                .map(|pbuf| pbuf.to_string_lossy().to_string()),
+            dependencies: Vec::new(),
+        };
+
+        for dep_path in &unique_paths {
+            let path_diff = pathdiff::diff_paths(dep_path, root_path).unwrap();
             deps_contents
                 .dependencies
-                .push(path.to_string_lossy().to_string());
+                .push(path_diff.to_string_lossy().to_string());
         }
         let mut deps_file = File::create(output_dir_path.join("local_deps.txt")).unwrap();
         writeln!(deps_file, "{}", serialize_local_deps_json(&deps_contents)).unwrap();
