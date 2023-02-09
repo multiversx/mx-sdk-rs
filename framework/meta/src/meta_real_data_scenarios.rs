50988b8ba4fb5765e5ee66171515160f6a7aabfa
Costin Carabaș <costin.carabas@gmail.com>
2023-02-09 18:55:46 +0200

    WIP: sc-meta real data integration

--- framework/meta/src/meta_real_data_scenarios.rs
+++ framework/meta/src/meta_real_data_scenarios.rs
@@ -22,35 +22,35 @@ pub fn call_real_data_scenario_builder(args: &RealDataScenarioArgs) {
     println!("account storage keys {:?}\n account = {:?}", account_storage.unwrap(), account);
 
     
-    let esdt = BTreeMap<String, EsdtRaw>::from(account_esdt);
-    let accounts = BTreeMap::new();
+    // let esdt = BTreeMap<String, EsdtRaw>::from(account_esdt);
+    // let accounts = BTreeMap::new();
 
-    let account_raw = AccountRaw {
-        nonce: Some(ValueSubTree::Str(account.nonce.to_string())),
-        balance: Some(ValueSubTree::Str(account.balance.to_string())),
-        esdt,
-        username: Some(ValueSubTree::Str(account.address.to_string())),
-        storage: account_storage,
-        comment: None,
-        code: None,
-        owner: None,
-        developer_rewards: None,
-    };
-    accounts.insert("address:owner".to_string(), account_raw);
+    // let account_raw = AccountRaw {
+    //     nonce: Some(ValueSubTree::Str(account.nonce.to_string())),
+    //     balance: Some(ValueSubTree::Str(account.balance.to_string())),
+    //     esdt,
+    //     username: Some(ValueSubTree::Str(account.address.to_string())),
+    //     storage: account_storage,
+    //     comment: None,
+    //     code: None,
+    //     owner: None,
+    //     developer_rewards: None,
+    // };
+    // accounts.insert("address:owner".to_string(), account_raw);
 
-    let _real_data_scenario = ScenarioRaw {
-        check_gas: None,
-        comment: None,
-        gas_schedule: None,
-        name: None,
-        steps: vec![StepRaw::SetState {
-            accounts: accounts,
-            block_hashes: Vec::new(),
-            new_addresses: Vec::new(),
-            comment: None,
-            current_block_info: None,
-            previous_block_info: None,
-        }],
-    };
+    // let _real_data_scenario = ScenarioRaw {
+    //     check_gas: None,
+    //     comment: None,
+    //     gas_schedule: None,
+    //     name: None,
+    //     steps: vec![StepRaw::SetState {
+    //         accounts: accounts,
+    //         block_hashes: Vec::new(),
+    //         new_addresses: Vec::new(),
+    //         comment: None,
+    //         current_block_info: None,
+    //         previous_block_info: None,
+    //     }],
+    // };
 
 }
