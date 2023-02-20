a0c8bbb459e70fd505d9545f60242726624d194d
Alin Cruceat <alyn_509@yahoo.com>
2023-02-20 12:56:11 +0200

    undo about

--- framework/meta/src/cli_args/cli_args_standalone.rs
+++ framework/meta/src/cli_args/cli_args_standalone.rs
@@ -8,7 +8,7 @@ use super::{CliArgsToRaw, ContractCliAction};
     version,
     about,
     after_help = "
-The MultiversX smart contract Meta crate can be used in three ways:
+The MultiversX smart contract Meta crate can be used in two ways:
     A. Import it into a contract's specific meta-crate. 
         There it will receive access to the contract ABI generator. 
         Based on that it is able to build the contract and apply various tools.
@@ -16,8 +16,6 @@ The MultiversX smart contract Meta crate can be used in three ways:
 
     B. Use it as a standalone tool.
         It can be used to automatically upgrade contracts from one version to the next.
-
-    C. Create a new contract from a preexisting template.
 
 You are currently using the standalone tool (B).
 "
