669207bf3099e18941e5a622b492c5cfa3a06b97
Ovidiu Stinga <ovstinga@gmail.com>
2023-02-22 18:34:43 +0200

    add some docs

--- contracts/examples/multisig/interact-rs/src/multisig_interact_config.rs
+++ contracts/examples/multisig/interact-rs/src/multisig_interact_config.rs
@@ -2,8 +2,10 @@ use std::io::Read;
 
 use serde::Deserialize;
 
-const CONFIG_FILE_NAME: &str = "config.toml";
+/// Configuration file
+const CONFIG_FILE: &str = "config.toml";
 
+/// Multisig Interact configuration
 #[derive(Debug, Deserialize)]
 pub struct Config {
     gateway: String,
@@ -11,17 +13,20 @@ pub struct Config {
 }
 
 impl Config {
+    // Loads configuration from file and deserializes it
     pub fn load_config() -> Self {
-        let mut file = std::fs::File::open(CONFIG_FILE_NAME).unwrap();
+        let mut file = std::fs::File::open(CONFIG_FILE).unwrap();
         let mut content = String::new();
         file.read_to_string(&mut content).unwrap();
         toml::from_str(&content).unwrap()
     }
 
+    // Returns the gateway
     pub fn gateway(&self) -> &str {
         &self.gateway
     }
 
+    // Returns the pem
     pub fn pem(&self) -> &str {
         &self.pem
     }
--- contracts/examples/multisig/interact-rs/src/multisig_interact_state.rs
+++ contracts/examples/multisig/interact-rs/src/multisig_interact_state.rs
@@ -1,33 +1,40 @@
-use crate::DebugApi;
-use crate::ContractInfo;
+use crate::{ContractInfo, DebugApi};
 use std::io::{Read, Write};
 
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
+/// Default multisig address expr if None is set
 const DEFAULT_MULTISIG_ADDRESS_EXPR: &str =
     "0x0000000000000000000000000000000000000000000000000000000000000000";
-const STATE_FILE_NAME: &str = "state.toml";
+
+/// State file
+const STATE_FILE: &str = "state.toml";
 
 type MultisigContract = ContractInfo<multisig::Proxy<DebugApi>>;
 
+/// Multisig Interact state
 #[derive(Debug, Serialize, Deserialize)]
 pub struct State {
     multisig_address: Option<String>,
 }
 
 impl State {
+    // Loads state from file and deserializes it
+    // Creates file if it doesn't exist
     pub fn load_state() -> Self {
-        let mut file = std::fs::File::open(STATE_FILE_NAME)
-            .unwrap_or(std::fs::File::create(STATE_FILE_NAME).unwrap());
+        let mut file =
+            std::fs::File::open(STATE_FILE).unwrap_or(std::fs::File::create(STATE_FILE).unwrap());
         let mut content = String::new();
         file.read_to_string(&mut content).unwrap();
         toml::from_str(&content).unwrap()
     }
 
+    /// Sets the multisig address
     pub fn set_multisig_address(&mut self, address: &str) {
         self.multisig_address = Some(String::from(address));
     }
 
+    /// Returns the multisig contract
     pub fn multisig(&self) -> MultisigContract {
         match &self.multisig_address {
             Some(address) => MultisigContract::new(address.clone()),
@@ -37,8 +44,9 @@ impl State {
 }
 
 impl Drop for State {
+    // Serializes state to file on drop
     fn drop(&mut self) {
-        let mut file = std::fs::File::create(STATE_FILE_NAME).unwrap();
+        let mut file = std::fs::File::create(STATE_FILE).unwrap();
         file.write_all(toml::to_string(self).unwrap().as_bytes())
             .unwrap();
     }
