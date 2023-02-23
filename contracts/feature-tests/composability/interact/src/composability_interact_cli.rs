cdc67c1ce1bedecfdfd46a09942c503fb9033df0
Costin Carabaș <costin.carabas@gmail.com>
2023-02-23 08:49:13 +0200

    Interactor for composability
    Only one interactor will be used for all feature tests.

---
+++ contracts/feature-tests/composability/interact/src/composability_interact_cli.rs
@@ -0,0 +1,19 @@
+use clap::{Parser, Subcommand};
+
+/// Composability Interact CLI
+#[derive(Default, PartialEq, Eq, Debug, Parser)]
+#[command(version, about)]
+#[command(propagate_version = true)]
+pub struct InteractCli {
+    #[command(subcommand)]
+    pub command: Option<InteractCliCommand>,
+}
+
+/// Composability Interact CLI Commands
+#[derive(Clone, PartialEq, Eq, Debug, Subcommand)]
+pub enum InteractCliCommand {
+    #[command(name = "deploy-vault", about = "Deploy Vault contract")]
+    DeployVault,
+    #[command(name = "deploy-forwarder-raw", about = "Deploy ForwarderRaw contract")]
+    DeployForwarderRaw,
+}
