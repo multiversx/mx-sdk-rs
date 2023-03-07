586b0e909b10c8cd035d463518068878ab8daa59
Alin Cruceat <alyn_509@yahoo.com>
2023-03-07 17:42:34 +0200

    apply for wasm and meta crate

--- framework/meta/src/template.rs
+++ framework/meta/src/template.rs
@@ -1,6 +1,11 @@
 use crate::{cargo_toml_contents::CargoTomlContents, cli_args::TemplateArgs};
 use copy_dir::*;
-use std::{env, fs::File, io::Write, path::Path};
+use std::{
+    env,
+    fs::File,
+    io::Write,
+    path::{Path, PathBuf},
+};
 
 const REPOSITORY: &str = "https://github.com/multiversx/mx-sdk-rs/archive/refs/heads/master.zip";
 const TEMPLATES_SUBDIRECTORY: &str = "mx-sdk-rs-master/contracts/examples/";
@@ -54,21 +59,58 @@ pub fn copy_template_to_location(template: &str, location: &Path) {
 }
 
 pub fn update_dependencies(template: &str) {
-    let path_buf = Path::new(template)
-        .join(ROOT_CARGO_TOML)
-        .canonicalize()
-        .unwrap_or_else(|err| {
-            panic!("error canonicalizing input path: {err}",);
-        });
+    update_dependencies_root(template);
+    update_dependencies_wasm(template);
+    update_dependencies_meta(template);
+}
+
+pub fn update_dependencies_root(template: &str) {
+    let path_buf = get_canonicalized_path(template, ROOT_CARGO_TOML);
     let root_cargo_toml_path = Path::new(&path_buf);
     let mut toml = CargoTomlContents::load_from_file(root_cargo_toml_path);
 
     toml.dev_dependencies_mut().clear();
 
-    let deps_map = toml.dependencies_mut();
-    for (_key, value) in deps_map {
+    remove_paths_from_dependencies(&mut toml, root_cargo_toml_path, &[]);
+}
+
+pub fn update_dependencies_meta(template: &str) {
+    let path_buf = get_canonicalized_path(template, META_CARGO_TOML);
+    let root_cargo_toml_path = Path::new(&path_buf);
+    let mut toml = CargoTomlContents::load_from_file(root_cargo_toml_path);
+
+    remove_paths_from_dependencies(&mut toml, root_cargo_toml_path, &[template]);
+}
+
+pub fn update_dependencies_wasm(template: &str) {
+    let path_buf = get_canonicalized_path(template, WASM_CARGO_TOML);
+    let root_cargo_toml_path = Path::new(&path_buf);
+    let mut toml = CargoTomlContents::load_from_file(root_cargo_toml_path);
+
+    remove_paths_from_dependencies(&mut toml, root_cargo_toml_path, &[template]);
+}
+
+pub fn get_canonicalized_path(template: &str, toml: &str) -> PathBuf {
+    Path::new(template)
+        .join(toml)
+        .canonicalize()
+        .unwrap_or_else(|err| {
+            panic!("error canonicalizing input path: {err}",);
+        })
+}
+
+pub fn remove_paths_from_dependencies(
+    cargo_toml_contents: &mut CargoTomlContents,
+    path: &Path,
+    ignore_deps: &[&str],
+) {
+    let deps_map = cargo_toml_contents.dependencies_mut();
+    for (key, value) in deps_map {
+        if ignore_deps.contains(&key.as_str()) {
+            continue;
+        }
         value.as_table_mut().unwrap().remove("path");
     }
 
-    toml.save_to_file(root_cargo_toml_path);
+    cargo_toml_contents.save_to_file(path);
 }
