06ccfbe7c742bf711e4febb680ddcd1ef7ea1063
Costin Carabaș <costin.carabas@gmail.com>
2023-03-20 09:52:52 +0200

    comp/interact: Move logic to config

--- contracts/feature-tests/composability/interact/src/comp_interact_cli.rs
+++ contracts/feature-tests/composability/interact/src/comp_interact_cli.rs
@@ -12,35 +12,17 @@ pub struct InteractCli {
 /// Composability Interact CLI Commands
 #[derive(Clone, PartialEq, Eq, Debug, Subcommand)]
 pub enum InteractCliCommand {
-    // #[command(name = "deploy-vault", about = "Deploy Vault contract")]
-    // DeployVault,
-    // #[command(name = "deploy-forwarder-raw", about = "Deploy ForwarderRaw contract")]
-    // DeployForwarderRaw,
-    // #[command(name = "deploy-promises", about = "Deploy Promises contract")]
-    // DeployPromises,
     #[command(name = "full", about = "Full scenario, whatever that means")]
     Full(FullArgs),
 }
 
 #[derive(Default, Clone, PartialEq, Eq, Debug, Args)]
 pub struct FullArgs {
-    /// Call type: Sync, LegacyAsync, TransferExecute.
-    #[arg(long = "call-type", verbatim_doc_comment)]
-    pub call_type: String,
-
     /// Endpoint name for Vault.
     #[arg(long = "endpoint", verbatim_doc_comment)]
     pub endpoint_name: String,
 
-    /// Payment token.
-    #[arg(long = "payment-token", verbatim_doc_comment)]
-    pub payment_token: String,
-
-    /// Payment nonce.
-    #[arg(long = "payment-nonce", verbatim_doc_comment)]
-    pub payment_nonce: u64,
-
-    /// Payment amount.
-    #[arg(long = "payment-amount", verbatim_doc_comment)]
-    pub payment_amount: u64,
+    /// Endpoint args.
+    #[arg(long = "endpoint-args", verbatim_doc_comment)]
+    pub endpoint_args: Option<Vec<String>>,
 }
