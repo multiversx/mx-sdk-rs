d0fcc7a7efaada562fb2e8c36ada51092bfdd704
Andrei Marinica <andrei.marinica@elrond.com>
2023-04-05 01:31:02 +0300

    forwarder-queue - promise callback included conditionally

--- contracts/feature-tests/composability/vault/wasm/src/lib.rs
+++ contracts/feature-tests/composability/vault/wasm/src/lib.rs
@@ -18,21 +18,22 @@ multiversx_sc_wasm_adapter::panic_handler!();
 multiversx_sc_wasm_adapter::endpoints! {
     vault
     (
-        echo_arguments
-        echo_arguments_without_storage
-        echo_caller
-        accept_funds
-        accept_funds_echo_payment
-        accept_funds_single_esdt_transfer
-        reject_funds
-        retrieve_funds_with_transfer_exec
-        retrieve_funds_promises
-        retrieve_funds
-        retrieve_multi_funds_async
-        burn_and_create_retrive_async
-        get_owner_address
-        call_counts
-        num_called_retrieve_funds_promises
+        init => init
+        echo_arguments => echo_arguments
+        echo_arguments_without_storage => echo_arguments_without_storage
+        echo_caller => echo_caller
+        accept_funds => accept_funds
+        accept_funds_echo_payment => accept_funds_echo_payment
+        accept_funds_single_esdt_transfer => accept_funds_single_esdt_transfer
+        reject_funds => reject_funds
+        retrieve_funds_with_transfer_exec => retrieve_funds_with_transfer_exec
+        retrieve_funds_promises => retrieve_funds_promises
+        retrieve_funds => retrieve_funds
+        retrieve_multi_funds_async => retrieve_multi_funds_async
+        burn_and_create_retrive_async => burn_and_create_retrive_async
+        get_owner_address => get_owner_address
+        call_counts => call_counts
+        num_called_retrieve_funds_promises => num_called_retrieve_funds_promises
     )
 }
 
