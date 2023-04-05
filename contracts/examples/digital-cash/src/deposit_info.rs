17d747b290d2f6fd6b2659259c72bda180cc863f
Alin Cruceat <alyn_509@yahoo.com>
2023-04-05 12:58:56 +0300

    digital cash changes 1

--- contracts/examples/digital-cash/src/deposit_info.rs
+++ contracts/examples/digital-cash/src/deposit_info.rs
@@ -1,6 +1,9 @@
 use multiversx_sc::{
     api::ManagedTypeApi,
-    types::{EgldOrEsdtTokenIdentifier, EgldOrEsdtTokenPayment, ManagedAddress},
+    types::{
+        BigUint, EgldOrEsdtTokenIdentifier, EgldOrEsdtTokenPayment, EsdtTokenPayment,
+        ManagedAddress, ManagedVec,
+    },
 };
 
 multiversx_sc::derive_imports!();
@@ -9,6 +12,7 @@ multiversx_sc::derive_imports!();
 pub struct DepositInfo<M: ManagedTypeApi> {
     pub depositor_address: ManagedAddress<M>,
     pub payment: EgldOrEsdtTokenPayment<M>,
+    pub valability: u64,
     pub expiration_round: u64,
 }
 
@@ -16,4 +20,16 @@ pub struct DepositInfo<M: ManagedTypeApi> {
 pub struct FundType<M: ManagedTypeApi> {
     pub token: EgldOrEsdtTokenIdentifier<M>,
     pub nonce: u64,
+}
+
+#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, TypeAbi)]
+pub struct PaymentFunds<M: ManagedTypeApi> {
+    pub num_token_transfer: u64,
+    pub value: BigUint<M>,
+}
+
+#[derive()]
+pub struct Funds<M: ManagedTypeApi> {
+    pub esdt_funds: ManagedVec<M, EsdtTokenPayment<M>>,
+    pub egld_funds: BigUint<M>,
 }
--- contracts/examples/digital-cash/src/digital_cash.rs
+++ contracts/examples/digital-cash/src/digital_cash.rs
@@ -6,7 +6,7 @@ multiversx_sc::derive_imports!();
 
 mod deposit_info;
 
-use deposit_info::{DepositInfo, FundType};
+use deposit_info::{DepositInfo, FundType, PaymentFunds};
 
 pub const SECONDS_PER_ROUND: u64 = 6;
 pub use multiversx_sc::api::{ED25519_KEY_BYTE_LEN, ED25519_SIGNATURE_BYTE_LEN};
@@ -14,27 +14,44 @@ pub use multiversx_sc::api::{ED25519_KEY_BYTE_LEN, ED25519_SIGNATURE_BYTE_LEN};
 #[multiversx_sc::contract]
 pub trait DigitalCash {
     #[init]
-    fn init(&self) {}
+    fn init(&self, fee: BigUint) {
+        self.fee().set(fee);
+    }
 
     //endpoints
 
     #[endpoint]
     #[payable("*")]
     fn fund(&self, address: ManagedAddress, valability: u64) {
+        // egld or single esdt
         let payment = self.call_value().egld_or_single_esdt();
+
+        let depositor_address = self.blockchain().get_caller();
+        let fee = self.fee().get();
+
         require!(
             payment.amount > BigUint::zero(),
             "amount must be greater than 0"
         );
+
+        self.payment(&depositor_address).update(|caller_fees| {
+            require!(
+                fee * caller_fees.num_token_transfer <= caller_fees.value,
+                "cannot deposit funds without covering the fee cost first"
+            );
+
+            caller_fees.num_token_transfer += 1;
+        });
+
         let fund_type = FundType {
             token: payment.token_identifier.clone(),
             nonce: payment.token_nonce,
         };
 
-        let depositor_address = self.blockchain().get_caller();
         let mut deposit = DepositInfo {
             depositor_address,
             payment,
+            valability,
             expiration_round: self.get_expiration_round(valability),
         };
 
@@ -55,8 +72,15 @@ pub trait DigitalCash {
         let mut withdrawed_tokens = ManagedVec::<Self::Api, FundType<Self::Api>>::new();
         let block_round = self.blockchain().get_block_round();
         let mut transfer_occured = false;
+        let mut esdt_funds = ManagedVec::<Self::Api, EsdtTokenPayment<Self::Api>>::new();
+        let mut egld_funds = BigUint::zero();
         for (key, deposit) in self.deposit(&address).iter() {
             if deposit.expiration_round < block_round {
+                if deposit.payment.token_identifier.is_esdt() {
+                    esdt_funds.push(deposit.payment.unwrap_esdt());
+                } else {
+                    egld_funds += deposit.payment.amount;
+                }
                 self.send().direct(
                     &deposit.depositor_address,
                     &deposit.payment.token_identifier,
@@ -68,6 +92,8 @@ pub trait DigitalCash {
             }
         }
 
+        self.send()
+            .direct_multi(&deposit.depositor_address, &esdt_funds);
         require!(transfer_occured, "withdrawal has not been available yet");
 
         for token in withdrawed_tokens.iter() {
@@ -84,18 +110,12 @@ pub trait DigitalCash {
         require!(!self.deposit(&address).is_empty(), "non-existent key");
 
         let caller_address = self.blockchain().get_caller();
-
-        let addr = address.as_managed_byte_array();
-        let message = caller_address.as_managed_buffer();
+        let fee = self.fee().get();
+        self.require_signature(&address, &caller_address, signature);
 
         let mut withdrawed_tokens = ManagedVec::<Self::Api, FundType<Self::Api>>::new();
         let mut transfer_occured = false;
         let block_round = self.blockchain().get_block_round();
-        require!(
-            self.crypto()
-                .verify_ed25519_legacy_managed::<32>(addr, message, &signature),
-            "invalid signature"
-        );
 
         for (key, deposit) in self.deposit(&address).iter() {
             if deposit.expiration_round >= block_round {
@@ -111,28 +131,84 @@ pub trait DigitalCash {
         }
         require!(transfer_occured, "deposit expired");
 
+        self.payment(&caller_address).update(|caller_fees| {
+            let num_tokens_transfered = withdrawed_tokens.len() as u64;
+            let fee_cost = fee * num_tokens_transfered;
+
+            caller_fees.num_token_transfer -= num_tokens_transfered;
+            caller_fees.value -= fee_cost.clone();
+
+            self.collected_fees()
+                .update(|collected_fees| *collected_fees += fee_cost);
+        });
+
         for token in withdrawed_tokens.iter() {
             self.deposit(&address).remove(&token);
         }
     }
 
-    #[endpoint]
-    fn forward(&self, address: ManagedAddress, forward_address: ManagedAddress) {
-        let caller = self.blockchain().get_caller();
+    fn require_signature(
+        &self,
+        address: &ManagedAddress,
+        caller_address: &ManagedAddress,
+        signature: ManagedByteArray<Self::Api, ED25519_SIGNATURE_BYTE_LEN>,
+    ) {
+        let addr = address.as_managed_byte_array();
+        let message = caller_address.as_managed_buffer();
+        require!(
+            self.crypto()
+                .verify_ed25519_legacy_managed::<32>(addr, message, &signature),
+            "invalid signature"
+        );
+    }
 
+    #[endpoint]
+    #[payable("EGLD")]
+    fn payment_funds(&self) {
+        let payment = self.call_value().egld_value();
+        let caller_address = self.blockchain().get_caller();
+        self.payment(&caller_address)
+            .update(|payment_funds| payment_funds.value += payment);
+    }
+
+    #[endpoint]
+    fn forward(
+        &self,
+        address: ManagedAddress,
+        forward_address: ManagedAddress,
+        signature: ManagedByteArray<Self::Api, ED25519_SIGNATURE_BYTE_LEN>,
+    ) {
+        let caller_address = self.blockchain().get_caller();
+        let fee = self.fee().get();
+        self.require_signature(&address, &caller_address, signature);
+
+        let mut forwarded_tokens_number = 0u64;
         for (key, fund) in self.deposit(&address).iter() {
-            require!(
-                fund.depositor_address == caller,
-                "only depositor can forward"
-            );
             let forwarded_fund = DepositInfo {
-                depositor_address: forward_address.clone(),
+                depositor_address: fund.depositor_address,
                 payment: fund.payment,
-                expiration_round: fund.expiration_round,
+                valability: fund.valability,
+                expiration_round: self.get_expiration_round(fund.valability),
             };
-            self.deposit(&address).insert(key, forwarded_fund);
+            self.deposit(&forward_address).insert(key, forwarded_fund);
+            forwarded_tokens_number += 1;
         }
-        self.deposit(&caller).clear();
+
+        self.payment(&caller_address).update(|caller_fees| {
+            let fee_cost = &fee * forwarded_tokens_number;
+
+            require!(
+                &fee * caller_fees.num_token_transfer + &fee_cost <= caller_fees.value,
+                "forward not permited due to uncovered fee costs by depositor"
+            );
+
+            caller_fees.value -= &fee_cost;
+
+            self.collected_fees()
+                .update(|collected_fees| *collected_fees += fee_cost);
+        });
+
+        self.deposit(&address).clear();
     }
 
     //views
@@ -171,4 +247,13 @@ pub trait DigitalCash {
         &self,
         donor: &ManagedAddress,
     ) -> MapMapper<FundType<Self::Api>, DepositInfo<Self::Api>>;
+
+    #[storage_mapper("payment")]
+    fn payment(&self, donor: &ManagedAddress) -> SingleValueMapper<PaymentFunds<Self::Api>>;
+
+    #[storage_mapper("fee")]
+    fn fee(&self) -> SingleValueMapper<BigUint>;
+
+    #[storage_mapper("collected_fees")]
+    fn collected_fees(&self) -> SingleValueMapper<BigUint>;
 }
