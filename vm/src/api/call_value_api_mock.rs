392bbcfba8c38df1f6b4d93006eda794bd5686ea
Andrei Marinica <andrei.marinica@elrond.com>
2023-04-11 18:13:08 +0300

    removed api single esdt methods

--- vm/src/api/call_value_api_mock.rs
+++ vm/src/api/call_value_api_mock.rs
@@ -1,21 +1,9 @@
 use crate::{num_bigint, tx_mock::TxPanic, DebugApi};
 use multiversx_sc::{
-    api::{CallValueApi, CallValueApiImpl},
+    api::{CallValueApi, CallValueApiImpl, ManagedBufferApi, handle_to_be_bytes},
     err_msg,
-    types::EsdtTokenType,
 };
 use num_traits::Zero;
-
-impl DebugApi {
-    fn fail_if_more_than_one_esdt_transfer(&self) {
-        if self.esdt_num_transfers() > 1 {
-            std::panic::panic_any(TxPanic {
-                status: 10,
-                message: err_msg::TOO_MANY_ESDT_TRANSFERS.to_string(),
-            });
-        }
-    }
-}
 
 impl CallValueApi for DebugApi {
     type CallValueApiImpl = DebugApi;
@@ -41,94 +29,28 @@ impl CallValueApiImpl for DebugApi {
         }
     }
 
-    #[inline]
     fn load_egld_value(&self, dest: Self::BigIntHandle) {
         self.set_big_uint(dest, self.input_ref().received_egld().clone())
     }
 
-    #[inline]
-    fn load_single_esdt_value(&self, dest: Self::BigIntHandle) {
-        self.fail_if_more_than_one_esdt_transfer();
-        if let Some(esdt_value) = self.input_ref().received_esdt().get(0) {
-            self.set_big_uint(dest, esdt_value.value.clone());
-        } else {
-            std::panic::panic_any(TxPanic {
-                status: 10,
-                message: err_msg::ESDT_INVALID_TOKEN_INDEX.to_string(),
-            });
+    fn load_all_esdt_transfers(&self, dest_handle: Self::ManagedBufferHandle) {
+        let transfers = self.input_ref().received_esdt();
+        self.mb_overwrite(dest_handle.clone(), &[]);
+
+        for transfer in transfers {
+            let token_identifier_handle = self.mb_new_from_bytes(&transfer.token_identifier);
+            let amount_handle = self.bi_new_from_big_int(transfer.value.clone().into());
+
+            self.mb_append_bytes(
+                dest_handle.clone(),
+                &handle_to_be_bytes(token_identifier_handle)[..],
+            );
+            self.mb_append_bytes(dest_handle.clone(), &transfer.nonce.to_be_bytes()[..]);
+            self.mb_append_bytes(dest_handle.clone(), &handle_to_be_bytes(amount_handle)[..]);
         }
     }
 
-    #[inline]
-    fn token(&self) -> Option<Self::ManagedBufferHandle> {
-        self.fail_if_more_than_one_esdt_transfer();
-
-        if self.esdt_num_transfers() > 0 {
-            Some(self.token_by_index(0))
-        } else {
-            None
-        }
-    }
-
-    #[inline]
-    fn esdt_token_nonce(&self) -> u64 {
-        self.fail_if_more_than_one_esdt_transfer();
-        self.esdt_token_nonce_by_index(0)
-    }
-
-    #[inline]
-    fn esdt_token_type(&self) -> EsdtTokenType {
-        self.fail_if_more_than_one_esdt_transfer();
-        self.esdt_token_type_by_index(0)
-    }
-
-    #[inline]
     fn esdt_num_transfers(&self) -> usize {
         self.input_ref().received_esdt().len()
-    }
-
-    #[inline]
-    fn esdt_value_by_index(&self, index: usize) -> Self::BigIntHandle {
-        if let Some(esdt_value) = self.input_ref().received_esdt().get(index) {
-            self.insert_new_big_uint(esdt_value.value.clone())
-        } else {
-            std::panic::panic_any(TxPanic {
-                status: 10,
-                message: err_msg::ESDT_INVALID_TOKEN_INDEX.to_string(),
-            });
-        }
-    }
-
-    #[inline]
-    fn token_by_index(&self, index: usize) -> Self::ManagedBufferHandle {
-        if let Some(esdt_value) = self.input_ref().received_esdt().get(index) {
-            self.insert_new_managed_buffer(esdt_value.token_identifier.clone())
-        } else {
-            std::panic::panic_any(TxPanic {
-                status: 10,
-                message: err_msg::ESDT_INVALID_TOKEN_INDEX.to_string(),
-            });
-        }
-    }
-
-    #[inline]
-    fn esdt_token_nonce_by_index(&self, index: usize) -> u64 {
-        if let Some(esdt_value) = self.input_ref().received_esdt().get(index) {
-            esdt_value.nonce
-        } else {
-            std::panic::panic_any(TxPanic {
-                status: 10,
-                message: err_msg::ESDT_INVALID_TOKEN_INDEX.to_string(),
-            });
-        }
-    }
-
-    #[inline]
-    fn esdt_token_type_by_index(&self, index: usize) -> EsdtTokenType {
-        if self.esdt_token_nonce_by_index(index) == 0 {
-            EsdtTokenType::Fungible
-        } else {
-            EsdtTokenType::NonFungible
-        }
     }
 }
