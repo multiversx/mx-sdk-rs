fedc746f1850814cd434281792217ca0799cd150
Costin Carabaș <costin.carabas@gmail.com>
2023-04-25 10:58:38 +0300

    fwd-q: Add payable to forward_queued_calls

--- contracts/feature-tests/composability/forwarder-queue/src/forwarder_queue.rs
+++ contracts/feature-tests/composability/forwarder-queue/src/forwarder_queue.rs
@@ -148,6 +148,7 @@ pub trait ForwarderQueue {
     }
 
     #[endpoint]
+    #[payable("*")]
     fn forward_queued_calls(&self) {
         while let Some(node) = self.queued_calls().pop_front() {
             let call = node.clone().into_value();
