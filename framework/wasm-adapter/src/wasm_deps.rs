0d22b60431fa90827a16a2f7b831c622ea65396d
Andrei Marinica <andrei.marinica@elrond.com>
2023-04-25 14:33:51 +0300

    allocator syntax changes for rustc 1.71.0

--- framework/wasm-adapter/src/wasm_deps.rs
+++ framework/wasm-adapter/src/wasm_deps.rs
@@ -2,10 +2,6 @@ pub use alloc::alloc::Layout;
 pub use core::panic::PanicInfo;
 pub use wee_alloc::WeeAlloc;
 
-pub fn alloc_error_handler(_layout: Layout) -> ! {
-    crate::error_hook::signal_error(&b"allocation error"[..])
-}
-
 pub fn panic_fmt(_: &PanicInfo) -> ! {
     crate::error_hook::signal_error(multiversx_sc::err_msg::PANIC_OCCURRED.as_bytes())
 }
