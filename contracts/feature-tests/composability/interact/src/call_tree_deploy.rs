23d7ea1050c3fe13534652595579a0cd30c8d0a6
Costin Carabaș <costin.carabas@gmail.com>
2023-05-02 13:37:54 +0300

    composability/interactor: Refactor

--- contracts/feature-tests/composability/interact/src/call_tree_deploy.rs
+++ contracts/feature-tests/composability/interact/src/call_tree_deploy.rs
@@ -4,12 +4,13 @@ use forwarder_queue::ProxyTrait as _;
 use multiversx_sc_snippets::{
     multiversx_sc::{
         codec::multi_types::OptionalValue,
-        types::{BoxedBytes, CodeMetadata},
+        types::{BoxedBytes, CodeMetadata, ManagedBuffer},
     },
     multiversx_sc_scenario::{
         bech32,
+        multiversx_chain_vm::tx_mock::TxContextRef,
         scenario_format::interpret_trait::InterpreterContext,
-        scenario_model::{IntoBlockchainCall, TxExpect},
+        scenario_model::{IntoBlockchainCall, TxExpect, TypedScDeploy},
     },
     StepBuffer,
 };
@@ -17,41 +18,8 @@ use vault::ProxyTrait as _;
 
 impl ComposabilityInteract {
     pub async fn deploy_call_tree_contracts(&mut self, call_state: &CallState) {
-        let mut typed_vault_deploys = Vec::new();
-        for _ in call_state.vaults.iter() {
-            let typed_sc_deploy = self
-                .state
-                .default_vault_address()
-                .init(OptionalValue::<BoxedBytes>::None)
-                .into_blockchain_call()
-                .from(&self.wallet_address)
-                .code_metadata(CodeMetadata::all())
-                .contract_code(
-                    "file:../vault/output/vault.wasm",
-                    &InterpreterContext::default(),
-                )
-                .gas_limit("70,000,000")
-                .expect(TxExpect::ok());
-            typed_vault_deploys.push(typed_sc_deploy);
-        }
-
-        let mut typed_forwarder_deploys = Vec::new();
-        for _ in call_state.forwarders.iter() {
-            let typed_sc_deploy = self
-                .state
-                .default_forwarder_queue_address()
-                .init()
-                .into_blockchain_call()
-                .from(&self.wallet_address)
-                .code_metadata(CodeMetadata::all())
-                .contract_code(
-                    "file:../forwarder-queue/output/forwarder-queue.wasm",
-                    &InterpreterContext::default(),
-                )
-                .gas_limit("70,000,000")
-                .expect(TxExpect::ok());
-            typed_forwarder_deploys.push(typed_sc_deploy);
-        }
+        let mut typed_vault_deploys = self.typed_sc_deploy_vault(call_state).await;
+        let mut typed_forwarder_deploys = self.typed_sc_deploy_forwarder_queue(call_state).await;
 
         let mut steps = Vec::new();
         for typed_sc_deploy in &mut typed_vault_deploys {
@@ -102,5 +70,56 @@ impl ComposabilityInteract {
 
             fwd.address = Some(result.unwrap());
         }
+    }
+
+    pub async fn typed_sc_deploy_vault(
+        &mut self,
+        call_state: &CallState,
+    ) -> Vec<TypedScDeploy<OptionalValue<ManagedBuffer<TxContextRef>>>> {
+        let mut typed_vault_deploys = Vec::new();
+        for _ in call_state.vaults.iter() {
+            let typed_sc_deploy = self
+                .state
+                .default_vault_address()
+                .init(OptionalValue::<BoxedBytes>::None)
+                .into_blockchain_call()
+                .from(&self.wallet_address)
+                .code_metadata(CodeMetadata::all())
+                .contract_code(
+                    "file:../vault/output/vault.wasm",
+                    &InterpreterContext::default(),
+                )
+                .gas_limit("70,000,000")
+                .expect(TxExpect::ok());
+
+            typed_vault_deploys.push(typed_sc_deploy);
+        }
+        typed_vault_deploys
+    }
+
+    pub async fn typed_sc_deploy_forwarder_queue(
+        &mut self,
+        call_state: &CallState,
+    ) -> Vec<TypedScDeploy<()>> {
+        let mut typed_forwarder_deploys = Vec::new();
+
+        for _ in call_state.forwarders.iter() {
+            let typed_sc_deploy = self
+                .state
+                .default_forwarder_queue_address()
+                .init()
+                .into_blockchain_call()
+                .from(&self.wallet_address)
+                .code_metadata(CodeMetadata::all())
+                .contract_code(
+                    "file:../forwarder-queue/output/forwarder-queue.wasm",
+                    &InterpreterContext::default(),
+                )
+                .gas_limit("70,000,000")
+                .expect(TxExpect::ok());
+
+            typed_forwarder_deploys.push(typed_sc_deploy);
+        }
+        typed_forwarder_deploys
     }
 }
