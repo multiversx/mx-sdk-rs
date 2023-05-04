0cc570aa7be7672eaa807daf662099fa17dfac19
Costin Carabaș <costin.carabas@gmail.com>
2023-05-04 17:38:14 +0300

    Fix `assert_positive` function

--- contracts/feature-tests/basic-features/tests/basic_features_big_num_test.rs
+++ contracts/feature-tests/basic-features/tests/basic_features_big_num_test.rs
@@ -1,4 +1,7 @@
-use multiversx_sc::types::{BigInt, BigUint, ManagedBuffer};
+use multiversx_sc::{
+    api::BigIntApi,
+    types::{BigInt, BigUint, ManagedBuffer, ManagedType},
+};
 use multiversx_sc_scenario::*;
 
 use basic_features::big_num_methods::BigIntMethods;
@@ -41,4 +44,15 @@ fn test_big_int_from() {
     assert_eq!(BigInt::from(5), result);
     let result = bf.big_int_from_i64_2(6);
     assert_eq!(BigInt::from(6), result);
+}
+
+#[test]
+fn test_big_int_shr() {
+    let api = DebugApi::dummy();
+    let bf = basic_features::contract_obj::<DebugApi>();
+    // let debug_handle = DebugHandle::new(8i32);
+    let big_int = bf.big_int_from_i64_1(128);
+
+    api.bi_shr(big_int.get_handle(), big_int.get_handle(), 3);
+    assert_eq!(BigInt::from(16i32), big_int);
 }
