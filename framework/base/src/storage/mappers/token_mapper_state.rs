2dc9b3c080f2d50b86b7fea2700a683f4544cc60
Alin Cruceat <alyn_509@yahoo.com>
2023-05-10 09:04:16 +0300

    top encode backwards compatibility

--- framework/base/src/storage/mappers/token_mapper_state.rs
+++ framework/base/src/storage/mappers/token_mapper_state.rs
@@ -1,13 +1,10 @@
 use crate::{
     api::ManagedTypeApi,
-    codec::{
-        self,
-        derive::{NestedDecode, NestedEncode, TopDecode, TopEncode},
-    },
+    codec::{self, derive::TopDecode},
     types::TokenIdentifier,
 };
 
-#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, Default)]
+#[derive(TopDecode, Default)]
 pub enum TokenMapperState<M: ManagedTypeApi> {
     #[default]
     NotSet,
@@ -24,5 +21,27 @@ impl<M: ManagedTypeApi> TokenMapperState<M> {
     }
     pub fn is_not_available(&self) -> bool {
         matches!(self, TokenMapperState::Pending | TokenMapperState::NotSet)
+    }
+}
+
+impl<M: ManagedTypeApi> codec::TopEncode for TokenMapperState<M> {
+    fn top_encode_or_handle_err<O, H>(
+        &self,
+        output: O,
+        h: H,
+    ) -> core::result::Result<(), H::HandledErr>
+    where
+        O: codec::TopEncodeOutput,
+        H: codec::EncodeErrorHandler,
+    {
+        match self {
+            TokenMapperState::NotSet => codec::TopEncode::top_encode_or_handle_err(&"", output, h),
+            TokenMapperState::Pending => {
+                codec::TopEncode::top_encode_or_handle_err(&"pending", output, h)
+            },
+            TokenMapperState::Token(token) => {
+                codec::TopEncode::top_encode_or_handle_err(&token, output, h)
+            },
+        }
     }
 }
