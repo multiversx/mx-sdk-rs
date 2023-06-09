6f66a2df52564efb424248195274a7ffb84b939e
Andrei Marinica <andrei.marinica@multiversx.com>
2023-06-09 11:07:23 +0300

    sc 0.41.2, codec 0.17.2, vm 0.3.2

--- framework/meta/src/cmd/standalone/upgrade/upgrade_versions.rs
+++ framework/meta/src/cmd/standalone/upgrade/upgrade_versions.rs
@@ -1,7 +1,7 @@
 /// Not necessarily the last entry in `VERSIONS`.
 ///
 /// Indicates where to stop with the upgrades.
-pub const DEFAULT_LAST_VERSION: &str = "0.41.0";
+pub const DEFAULT_LAST_VERSION: &str = "0.41.2";
 
 /// Known version for the upgrader.
 #[rustfmt::skip]
@@ -35,6 +35,8 @@ pub const VERSIONS: &[&str] = &[
     "0.40.0",
     "0.40.1",
     "0.41.0",
+    "0.41.1",
+    "0.41.2",
 ];
 
 pub struct VersionIterator {
