b21fd0015bbec5c2105ee882e56aee102a369252
Andrei Marinica <andrei.marinica@multiversx.com>
2023-07-14 22:35:19 +0300

    renamed scenario step files

---
+++ framework/scenario/src/scenario/model/step.rs
@@ -0,0 +1,25 @@
+mod into_blockchain_call;
+mod check_state_step;
+mod dump_state_step;
+mod step_enum;
+mod sc_call_step;
+mod sc_deploy_step;
+mod sc_query_step;
+mod set_state_step;
+mod transfer_step;
+mod typed_sc_call;
+mod typed_sc_deploy;
+mod typed_sc_query;
+
+pub use into_blockchain_call::*;
+pub use check_state_step::*;
+pub use dump_state_step::*;
+pub use step_enum::*;
+pub use sc_call_step::*;
+pub use sc_deploy_step::*;
+pub use sc_query_step::*;
+pub use set_state_step::*;
+pub use transfer_step::*;
+pub use typed_sc_call::*;
+pub use typed_sc_deploy::*;
+pub use typed_sc_query::*;
