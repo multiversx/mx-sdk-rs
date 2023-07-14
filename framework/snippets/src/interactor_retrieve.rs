b796b4f348322311f18d1be0d17d6d8d204a5c4f
Ovidiu Stinga <ovstinga@gmail.com>
2023-07-14 14:47:41 +0300

    add extra time for tx result retrieval for safety

--- framework/snippets/src/interactor_retrieve.rs
+++ framework/snippets/src/interactor_retrieve.rs
@@ -4,32 +4,45 @@ use multiversx_sdk::data::transaction::TransactionOnNetwork;
 use std::time::Duration;
 
 const TX_GET_RESULTS_NUM_RETRIES: usize = 8;
+const EXTRA_WAITING_TIME_MS: Duration = Duration::from_millis(8000);
 
 impl Interactor {
     pub(crate) async fn retrieve_tx_on_network(&self, tx_hash: String) -> TransactionOnNetwork {
         let mut waiting_time_ms = 0;
+        let mut break_outer = false;
         sleep(&mut waiting_time_ms, Duration::from_secs(25)).await;
 
-        let mut retries = TX_GET_RESULTS_NUM_RETRIES;
-        let mut wait = 1000u64;
-        let tx = loop {
-            let tx_info_result = self.proxy.get_transaction_info_with_results(&tx_hash).await;
-            match tx_info_result {
-                Ok(tx) => break tx,
-                Err(err) => {
-                    assert!(
-                        retries > 0,
-                        "still no answer after {TX_GET_RESULTS_NUM_RETRIES} retries"
-                    );
+        let tx = 'outer: loop {
+            let mut retries = TX_GET_RESULTS_NUM_RETRIES;
+            let mut wait = 1000u64;
+            loop {
+                let tx_info_result = self.proxy.get_transaction_info_with_results(&tx_hash).await;
+                match tx_info_result {
+                    Ok(tx) => {
+                        if break_outer {
+                            break 'outer tx;
+                        }
+                        waiting_time_ms = Duration::from_secs(25).as_millis() as u64;
+                        tokio::time::sleep(EXTRA_WAITING_TIME_MS).await;
+                        break_outer = true;
 
-                    info!(
-                        "tx result fetch error after {} ms: {}",
-                        self.waiting_time_ms, err
-                    );
-                    retries -= 1;
-                    sleep(&mut waiting_time_ms, Duration::from_millis(wait)).await;
-                    wait *= 2;
-                },
+                        break;
+                    },
+                    Err(err) => {
+                        assert!(
+                            retries > 0,
+                            "still no answer after {TX_GET_RESULTS_NUM_RETRIES} retries"
+                        );
+
+                        info!(
+                            "tx result fetch error after {} ms: {}",
+                            self.waiting_time_ms, err
+                        );
+                        retries -= 1;
+                        sleep(&mut waiting_time_ms, Duration::from_millis(wait)).await;
+                        wait *= 2;
+                    },
+                }
             }
         };
 
