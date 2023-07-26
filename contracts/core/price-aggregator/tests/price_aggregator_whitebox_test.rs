ed02c5999b72760ec0449113f0f6217e13cd3343
Ovidiu Stinga <ovstinga@gmail.com>
2023-07-26 19:30:44 +0300

    migrate another test

--- contracts/core/price-aggregator/tests/price_aggregator_whitebox_test.rs
+++ contracts/core/price-aggregator/tests/price_aggregator_whitebox_test.rs
@@ -1,5 +1,5 @@
 use multiversx_price_aggregator_sc::{
-    price_aggregator_data::{OracleStatus, TokenPair},
+    price_aggregator_data::{OracleStatus, TimestampedPrice, TokenPair},
     staking::EndpointWrappers as StakingEndpointWrappers,
     PriceAggregator,
 };
@@ -36,6 +36,7 @@ fn world() -> ScenarioWorld {
 
 #[test]
 fn test_price_aggregator_submit() {
+    // setup
     let mut world = world();
     let current_timestamp = 100;
     let price_aggregator_whitebox = WhiteboxContract::new(
@@ -61,6 +62,7 @@ fn test_price_aggregator_submit() {
         oracles.push(oracle_address);
     }
 
+    // init price aggregator
     world.set_state_step(set_state_step).whitebox_deploy(
         &price_aggregator_whitebox,
         ScDeployStep::new()
@@ -93,111 +95,64 @@ fn test_price_aggregator_submit() {
         );
     }
 
-    world
-        .whitebox_call(
-            &price_aggregator_whitebox,
-            ScCallStep::new().from(OWNER_ADDRESS_EXPR),
-            |sc| {
-                sc.set_pair_decimals(
-                    managed_buffer!(EGLD_TICKER),
-                    managed_buffer!(USD_TICKER),
-                    DECIMALS,
-                )
-            },
-        )
-        .whitebox_call_check(
-            &price_aggregator_whitebox,
-            ScCallStep::new()
-                .from(&oracles[0])
-                .expect(TxExpect::user_error("str:Contract is paused")),
-            |sc| {
-                sc.submit(
-                    managed_buffer!(EGLD_TICKER),
-                    managed_buffer!(USD_TICKER),
-                    99,
-                    managed_biguint!(100),
-                    DECIMALS,
-                )
-            },
-            |r| r.assert_user_error("Contract is paused"),
-        )
-        .whitebox_call(
-            &price_aggregator_whitebox,
-            ScCallStep::new().from(OWNER_ADDRESS_EXPR),
-            |sc| sc.call_unpause_endpoint(),
-        )
-        .whitebox_call_check(
-            &price_aggregator_whitebox,
-            ScCallStep::new().from(&oracles[0]).no_expect(),
-            |sc| {
-                sc.submit(
-                    managed_buffer!(EGLD_TICKER),
-                    managed_buffer!(USD_TICKER),
-                    10,
-                    managed_biguint!(100),
-                    DECIMALS,
-                )
-            },
-            |r| {
-                r.assert_user_error("First submission too old");
-            },
-        )
-        .whitebox_call_check(
-            &price_aggregator_whitebox,
-            ScCallStep::new().from(&oracles[0]),
-            |sc| {
-                sc.submit(
-                    managed_buffer!(EGLD_TICKER),
-                    managed_buffer!(USD_TICKER),
-                    95,
-                    managed_biguint!(100),
-                    DECIMALS,
-                )
-            },
-            |r| {
-                r.assert_ok();
-            },
-        );
-
-    world.whitebox_query_check(
+    // configure the number of decimals
+    world.whitebox_call(
         &price_aggregator_whitebox,
+        ScCallStep::new().from(OWNER_ADDRESS_EXPR),
         |sc| {
-            let token_pair = TokenPair {
-                from: managed_buffer!(EGLD_TICKER),
-                to: managed_buffer!(USD_TICKER),
-            };
-            assert_eq!(
-                sc.first_submission_timestamp(&token_pair).get(),
-                current_timestamp
-            );
-            assert_eq!(
-                sc.last_submission_timestamp(&token_pair).get(),
-                current_timestamp
-            );
-
-            let submissions = sc.submissions().get(&token_pair).unwrap();
-            assert_eq!(submissions.len(), 1);
-            assert_eq!(
-                submissions
-                    .get(&managed_address!(&oracles[0].to_address()))
-                    .unwrap(),
-                managed_biguint!(100)
-            );
-
-            assert_eq!(
-                sc.oracle_status()
-                    .get(&managed_address!(&oracles[0].to_address()))
-                    .unwrap(),
-                OracleStatus {
-                    total_submissions: 1,
-                    accepted_submissions: 1
-                }
-            );
+            sc.set_pair_decimals(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                DECIMALS,
+            )
         },
-        |r| r.assert_ok(),
     );
 
+    // try submit while paused
     world.whitebox_call_check(
+        &price_aggregator_whitebox,
+        ScCallStep::new()
+            .from(&oracles[0])
+            .expect(TxExpect::user_error("str:Contract is paused")),
+        |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                99,
+                managed_biguint!(100),
+                DECIMALS,
+            )
+        },
+        |r| r.assert_user_error("Contract is paused"),
+    );
+
+    // unpause
+    world.whitebox_call(
+        &price_aggregator_whitebox,
+        ScCallStep::new().from(OWNER_ADDRESS_EXPR),
+        |sc| sc.call_unpause_endpoint(),
+    );
+
+    // submit first timestamp too old
+    world.whitebox_call_check(
+        &price_aggregator_whitebox,
+        ScCallStep::new().from(&oracles[0]).no_expect(),
+        |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                10,
+                managed_biguint!(100),
+                DECIMALS,
+            )
+        },
+        |r| {
+            r.assert_user_error("First submission too old");
+        },
+    );
+
+    // submit ok
+    world.whitebox_call(
         &price_aggregator_whitebox,
         ScCallStep::new().from(&oracles[0]),
         |sc| {
@@ -209,22 +164,225 @@ fn test_price_aggregator_submit() {
                 DECIMALS,
             )
         },
-        |r| r.assert_ok(),
     );
 
-    world.whitebox_query_check(
+    world.whitebox_query(&price_aggregator_whitebox, |sc| {
+        let token_pair = TokenPair {
+            from: managed_buffer!(EGLD_TICKER),
+            to: managed_buffer!(USD_TICKER),
+        };
+        assert_eq!(
+            sc.first_submission_timestamp(&token_pair).get(),
+            current_timestamp
+        );
+        assert_eq!(
+            sc.last_submission_timestamp(&token_pair).get(),
+            current_timestamp
+        );
+
+        let submissions = sc.submissions().get(&token_pair).unwrap();
+        assert_eq!(submissions.len(), 1);
+        assert_eq!(
+            submissions
+                .get(&managed_address!(&oracles[0].to_address()))
+                .unwrap(),
+            managed_biguint!(100)
+        );
+
+        assert_eq!(
+            sc.oracle_status()
+                .get(&managed_address!(&oracles[0].to_address()))
+                .unwrap(),
+            OracleStatus {
+                total_submissions: 1,
+                accepted_submissions: 1
+            }
+        );
+    });
+
+    // first oracle submit again - submission not accepted
+    world.whitebox_call(
         &price_aggregator_whitebox,
+        ScCallStep::new().from(&oracles[0]),
         |sc| {
-            assert_eq!(
-                sc.oracle_status()
-                    .get(&managed_address!(&oracles[0].to_address()))
-                    .unwrap(),
-                OracleStatus {
-                    total_submissions: 2,
-                    accepted_submissions: 1
-                }
-            );
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                95,
+                managed_biguint!(100),
+                DECIMALS,
+            )
         },
-        |r| r.assert_ok(),
     );
+
+    world.whitebox_query(&price_aggregator_whitebox, |sc| {
+        assert_eq!(
+            sc.oracle_status()
+                .get(&managed_address!(&oracles[0].to_address()))
+                .unwrap(),
+            OracleStatus {
+                total_submissions: 2,
+                accepted_submissions: 1
+            }
+        );
+    });
+}
+
+#[test]
+fn test_price_aggregator_submit_round_ok() {
+    // setup
+    let mut world = world();
+    let price_aggregator_whitebox = WhiteboxContract::new(
+        PRICE_AGGREGATOR_ADDRESS_EXPR,
+        multiversx_price_aggregator_sc::contract_obj,
+    );
+    let price_aggregator_code = world.code_expression(PRICE_AGGREGATOR_PATH_EXPR);
+
+    let mut set_state_step = SetStateStep::new()
+        .put_account(OWNER_ADDRESS_EXPR, Account::new().nonce(1))
+        .new_address(OWNER_ADDRESS_EXPR, 1, PRICE_AGGREGATOR_ADDRESS_EXPR)
+        .block_timestamp(100);
+
+    let mut oracles = Vec::new();
+    for i in 1..=NR_ORACLES {
+        let oracle_address_expr = format!("address:oracle{i}");
+        let oracle_address = AddressValue::from(oracle_address_expr.as_str());
+
+        set_state_step = set_state_step.put_account(
+            oracle_address_expr.as_str(),
+            Account::new().nonce(1).balance(STAKE_AMOUNT),
+        );
+        oracles.push(oracle_address);
+    }
+
+    // init price aggregator
+    world.set_state_step(set_state_step).whitebox_deploy(
+        &price_aggregator_whitebox,
+        ScDeployStep::new()
+            .from(OWNER_ADDRESS_EXPR)
+            .code(price_aggregator_code),
+        |sc| {
+            let mut oracle_args = MultiValueEncoded::new();
+            for oracle_address in &oracles {
+                oracle_args.push(managed_address!(&oracle_address.to_address()));
+            }
+
+            sc.init(
+                EgldOrEsdtTokenIdentifier::egld(),
+                managed_biguint!(STAKE_AMOUNT),
+                managed_biguint!(SLASH_AMOUNT),
+                SLASH_QUORUM,
+                SUBMISSION_COUNT,
+                oracle_args,
+            )
+        },
+    );
+
+    for oracle_address in &oracles {
+        world.whitebox_call(
+            &price_aggregator_whitebox,
+            ScCallStep::new()
+                .from(oracle_address)
+                .egld_value(STAKE_AMOUNT),
+            |sc| sc.call_stake(),
+        );
+    }
+
+    // configure the number of decimals
+    world.whitebox_call(
+        &price_aggregator_whitebox,
+        ScCallStep::new().from(OWNER_ADDRESS_EXPR),
+        |sc| {
+            sc.set_pair_decimals(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                DECIMALS,
+            )
+        },
+    );
+
+    // unpause
+    world.whitebox_call(
+        &price_aggregator_whitebox,
+        ScCallStep::new().from(OWNER_ADDRESS_EXPR),
+        |sc| sc.call_unpause_endpoint(),
+    );
+
+    // submit first
+    world.whitebox_call(
+        &price_aggregator_whitebox,
+        ScCallStep::new().from(&oracles[0]),
+        |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                95,
+                managed_biguint!(10_000),
+                DECIMALS,
+            )
+        },
+    );
+
+    let current_timestamp = 110;
+    world.set_state_step(SetStateStep::new().block_timestamp(current_timestamp));
+
+    // submit second
+    world.whitebox_call(
+        &price_aggregator_whitebox,
+        ScCallStep::new().from(&oracles[1]),
+        |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                101,
+                managed_biguint!(11_000),
+                DECIMALS,
+            )
+        },
+    );
+
+    // submit third
+    world.whitebox_call(
+        &price_aggregator_whitebox,
+        ScCallStep::new().from(&oracles[2]),
+        |sc| {
+            sc.submit(
+                managed_buffer!(EGLD_TICKER),
+                managed_buffer!(USD_TICKER),
+                105,
+                managed_biguint!(12_000),
+                DECIMALS,
+            )
+        },
+    );
+
+    world.whitebox_query(&price_aggregator_whitebox, |sc| {
+        let result = sc
+            .latest_price_feed(managed_buffer!(EGLD_TICKER), managed_buffer!(USD_TICKER))
+            .unwrap();
+
+        let (round_id, from, to, timestamp, price, decimals) = result.into_tuple();
+        assert_eq!(round_id, 1);
+        assert_eq!(from, managed_buffer!(EGLD_TICKER));
+        assert_eq!(to, managed_buffer!(USD_TICKER));
+        assert_eq!(timestamp, current_timestamp);
+        assert_eq!(price, managed_biguint!(11_000));
+        assert_eq!(decimals, DECIMALS);
+
+        // submissions are deleted after round is created
+        let token_pair = TokenPair { from, to };
+        let submissions = sc.submissions().get(&token_pair).unwrap();
+        assert_eq!(submissions.len(), 0);
+
+        let rounds = sc.rounds().get(&token_pair).unwrap();
+        assert_eq!(rounds.len(), 1);
+        assert_eq!(
+            rounds.get(1),
+            TimestampedPrice {
+                timestamp,
+                price,
+                decimals
+            }
+        );
+    });
 }
