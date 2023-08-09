951af92a771ed080b96fc68ea17ab6591a3a87a8
Fusee <quentus123@orange.fr>
2023-08-09 15:34:48 +0200

    made ScenarioWorld and related stuff thread safe

--- vm/src/tx_execution/blockchain_vm.rs
+++ vm/src/tx_execution/blockchain_vm.rs
@@ -7,14 +7,14 @@ use super::BuiltinFunctionContainer;
 
 pub struct BlockchainVM {
     pub builtin_functions: BuiltinFunctionContainer,
-    pub executor: Box<dyn Executor>,
+    pub executor: Box<dyn Executor + Send + Sync>,
 }
 
 #[derive(Clone)]
 pub struct BlockchainVMRef(Arc<BlockchainVM>);
 
 impl BlockchainVM {
-    pub fn new(executor: Box<dyn Executor>) -> Self {
+    pub fn new(executor: Box<dyn Executor + Send + Sync>) -> Self {
         BlockchainVM {
             builtin_functions: BuiltinFunctionContainer,
             executor,
@@ -23,7 +23,7 @@ impl BlockchainVM {
 }
 
 impl BlockchainVMRef {
-    pub fn new(executor: Box<dyn Executor>) -> Self {
+    pub fn new(executor: Box<dyn Executor + Send + Sync>) -> Self {
         BlockchainVMRef(Arc::new(BlockchainVM::new(executor)))
     }
 }
