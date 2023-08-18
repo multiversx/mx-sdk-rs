21303433389f8d4bac1f60591c0a287eb433a8af
Andrei Marinica <andrei.marinica@multiversx.com>
2023-08-18 23:31:49 +0300

    release upload action fix

--- zip-example-wasm.sh
+++ zip-example-wasm.sh
@@ -4,19 +4,18 @@
 ## Used in generating an output artefact for each elrond-wasm release.
 
 ZIP_OUTPUT="examples-wasm.zip"
+TARGET_DIR=$PWD/target
+cargo install multiversx-sc-meta
 
 # start fresh
 rm -f $ZIP_OUTPUT
 
-set -e
+sc-meta all build --target-dir-wasm $TARGET_DIR --path ./contracts/examples || return 1
+
 SMART_CONTRACT_JSONS=$(find contracts/examples -name "multiversx.json")
 for smart_contract_json in $SMART_CONTRACT_JSONS
 do
     smart_contract_folder=$(dirname $smart_contract_json)
-    echo ""
-    # build example wasm + ABI
-    rm -rf $smart_contract_folder/output
-    (set -x; mxpy --verbose contract build $smart_contract_folder)
 
     # add to zip
     zip -ur --junk-paths $ZIP_OUTPUT $smart_contract_folder/output
