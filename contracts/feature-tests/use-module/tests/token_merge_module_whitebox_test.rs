f12b02d30a97cd58d313e3b0454db9cc0009a94e
Ovidiu Stinga <ovstinga@gmail.com>
2023-08-22 18:31:42 +0300

    first test attempt

--- contracts/feature-tests/use-module/tests/token_merge_module_whitebox_test.rs
+++ contracts/feature-tests/use-module/tests/token_merge_module_whitebox_test.rs
@@ -1,9 +1,16 @@
 #![allow(unused)]
 
-use multiversx_sc::types::Address;
-use multiversx_sc_scenario::{scenario_model::AddressValue, ScenarioWorld};
+use multiversx_sc::{storage::mappers::StorageTokenWrapper, types::Address};
+use multiversx_sc_modules::token_merge::merged_token_setup::MergedTokenSetupModule;
+use multiversx_sc_scenario::{
+    managed_token_id, rust_biguint,
+    scenario_model::{Account, AddressValue, ScCallStep, SetStateStep},
+    testing_framework::TxTokenTransfer,
+    ScenarioWorld, WhiteboxContract,
+};
 
 const OWNER_ADDRESS_EXPR: &str = "address:owner";
+const USER_ADDRESS_EXPR: &str = "address:user";
 
 const USE_MODULE_ADDRESS_EXPR: &str = "sc:use-module";
 const USE_MODULE_PATH_EXPR: &str = "file:output/use-module.wasm";
@@ -37,7 +44,92 @@ fn world() -> ScenarioWorld {
 }
 
 #[test]
-fn test_token_merge() {}
+fn test_token_merge() {
+    let mut world = world();
+
+    let use_module_whitebox =
+        WhiteboxContract::new(USE_MODULE_ADDRESS_EXPR, use_module::contract_obj);
+    let use_modue_code = world.code_expression(USE_MODULE_PATH_EXPR);
+
+    let roles = vec![
+        "ESDTRoleNFTCreate".to_string(),
+        "ESDTRoleNFTBurn".to_string(),
+    ];
+
+    world.set_state_step(
+        SetStateStep::new()
+            .put_account(OWNER_ADDRESS_EXPR, Account::new().nonce(1))
+            .put_account(
+                USER_ADDRESS_EXPR,
+                Account::new()
+                    .nonce(1)
+                    .esdt_balance(FUNGIBLE_TOKEN_ID_EXPR, rust_biguint!(FUNGIBLE_AMOUNT)),
+            )
+            .put_account(
+                USE_MODULE_ADDRESS_EXPR,
+                Account::new()
+                    .nonce(1)
+                    .code(use_modue_code)
+                    .owner(OWNER_ADDRESS_EXPR)
+                    .esdt_roles(MERGED_TOKEN_ID_EXPR, roles),
+            ),
+    );
+
+    world.whitebox_call(
+        &use_module_whitebox,
+        ScCallStep::new().from(OWNER_ADDRESS_EXPR),
+        |sc| {
+            sc.merged_token()
+                .set_token_id(managed_token_id!(MERGED_TOKEN_ID));
+            let _ = sc
+                .mergeable_tokens_whitelist()
+                .insert(managed_token_id!(NFT_TOKEN_ID));
+            let _ = sc
+                .mergeable_tokens_whitelist()
+                .insert(managed_token_id!(FUNGIBLE_TOKEN_ID));
+        },
+    );
+
+    // TODO: implement esdt_nft_balance_all_properties
+    //     b_mock.set_nft_balance_all_properties(
+    //     &user,
+    //     NFT_TOKEN_ID,
+    //     FIRST_NFT_NONCE,
+    //     &rust_biguint!(NFT_AMOUNT),
+    //     &FIRST_ATTRIBUTES.to_vec(),
+    //     FIRST_ROYALTIES,
+    //     None,
+    //     None,
+    //     None,
+    //     &uris_to_vec(FIRST_URIS),
+    // );
+    // b_mock.set_nft_balance_all_properties(
+    //     &user,
+    //     NFT_TOKEN_ID,
+    //     SECOND_NFT_NONCE,
+    //     &rust_biguint!(NFT_AMOUNT),
+    //     &SECOND_ATTRIBUTES.to_vec(),
+    //     SECOND_ROYALTIES,
+    //     None,
+    //     None,
+    //     None,
+    //     &uris_to_vec(SECOND_URIS),
+    // );
+
+    // merge two NFTs
+    let nft_transfers = vec![
+        TxTokenTransfer {
+            token_identifier: NFT_TOKEN_ID.to_vec(),
+            nonce: FIRST_NFT_NONCE,
+            value: rust_biguint!(NFT_AMOUNT),
+        },
+        TxTokenTransfer {
+            token_identifier: NFT_TOKEN_ID.to_vec(),
+            nonce: SECOND_NFT_NONCE,
+            value: rust_biguint!(NFT_AMOUNT),
+        },
+    ];
+}
 
 #[test]
 fn test_partial_split() {}
