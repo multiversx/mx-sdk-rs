0204c32f0d4f689f4a300ed02489fbc6e1801877
Andrei Marinica <andrei.marinica@multiversx.com>
2023-08-31 12:20:14 +0300

    clippy fix

--- framework/base/src/contract_base/wrappers/callback_args_wrapper.rs
+++ framework/base/src/contract_base/wrappers/callback_args_wrapper.rs
@@ -10,7 +10,7 @@ use crate::{
 
 /// Replaces the EndpointArgumentApi inside a promises callback,
 /// and causes it to read arguments from the callback data instead of the regular tx input.
-#[derive(Clone)]
+#[derive(Clone, Default)]
 pub struct CallbackArgApiWrapper<A: VMApi> {
     _phantom: PhantomData<A>,
 }
