8a320aab5f62c8286f9ef6fd4c21c174ef496d22
Alin Cruceat <alyn_509@yahoo.com>
2023-09-19 13:32:13 +0300

    pay fee and fund egld 1 transfer

--- contracts/examples/digital-cash/src/pay_fee_and_fund.rs
+++ contracts/examples/digital-cash/src/pay_fee_and_fund.rs
@@ -7,7 +7,7 @@ use crate::{constants::*, helpers, storage};
 pub trait PayFeeAndFund: storage::StorageModule + helpers::HelpersModule {
     #[endpoint]
     #[payable("*")]
-    fn pay_fee_and_fund(&self, address: ManagedAddress, valability: u64) {
+    fn pay_fee_and_fund_esdt(&self, address: ManagedAddress, valability: u64) {
         let mut payments = self.call_value().all_esdt_transfers().clone_value();
         let fee = EgldOrEsdtTokenPayment::from(payments.get(0));
         let caller_address = self.blockchain().get_caller();
@@ -16,6 +16,20 @@ pub trait PayFeeAndFund: storage::StorageModule + helpers::HelpersModule {
         payments.remove(0);
 
         self.make_fund(0u64.into(), payments, address, valability)
+    }
+    #[endpoint]
+    #[payable("EGLD")]
+    fn pay_fee_and_fund_egld(&self, address: ManagedAddress, valability: u64) {
+        let mut fund = self.call_value().egld_value().clone_value();
+        let fee_value = self.fee().get();
+        require!(fund > fee_value, "payment not covering fees");
+
+        fund -= fee_value.clone();
+        let fee = EgldOrEsdtTokenPayment::new(EgldOrEsdtTokenIdentifier::egld(), 0, fee_value);
+        let caller_address = self.blockchain().get_caller();
+        self.update_fees(caller_address, &address, fee);
+
+        self.make_fund(fund, ManagedVec::new(), address, valability);
     }
 
     #[endpoint]
