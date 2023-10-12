3bbf856e5fc7200d5e6962981c2e763f3daeaec6
mihaicalinluca <mihaicalin.luca10@gmail.com>
2023-10-13 01:26:23 +0200

    added allow_multiple_var_args annotation and tests for the validate function

--- contracts/examples/adder/tests/adder_scenario_rs_test.rs
+++ contracts/examples/adder/tests/adder_scenario_rs_test.rs
@@ -1,11 +1,7 @@
 use multiversx_sc_scenario::*;
 
 fn world() -> ScenarioWorld {
-    let mut blockchain = ScenarioWorld::new();
-    blockchain.set_current_dir_from_workspace("contracts/examples/adder");
-
-    blockchain.register_contract("file:output/adder.wasm", adder::ContractBuilder);
-    blockchain
+    todo!()
 }
 
 #[test]
