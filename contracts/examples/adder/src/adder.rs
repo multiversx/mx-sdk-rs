007f5e230b1f3559d5de2fe0633c2665d7e3e007
mihaicalinluca <mihaicalin.luca10@gmail.com>
2023-10-27 19:33:19 +0200

    started implementation

--- contracts/examples/adder/src/adder.rs
+++ contracts/examples/adder/src/adder.rs
@@ -5,6 +5,7 @@ multiversx_sc::imports!();
 /// One of the simplest smart contracts possible,
 /// it holds a single variable in storage, which anyone can increment.
 #[multiversx_sc::contract]
+#[esdt_attribute("dummy", BigUint)]
 pub trait Adder {
     #[view(getSum)]
     #[storage_mapper("sum")]
