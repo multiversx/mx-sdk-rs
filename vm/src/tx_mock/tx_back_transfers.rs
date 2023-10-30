6c69a55471914763b422d91242d30fa88684f59d
Andrei Marinica <andrei.marinica@multiversx.com>
2023-10-30 18:51:17 +0200

    vm - back transfer getter disabled

--- vm/src/tx_mock/tx_back_transfers.rs
+++ vm/src/tx_mock/tx_back_transfers.rs
@@ -1,5 +1,4 @@
 use num_bigint::BigUint;
-use num_traits::Zero;
 
 use super::{TxResult, TxTokenTransfer};
 
@@ -14,49 +13,7 @@ impl BackTransfers {
         BackTransfers::default()
     }
 
-    pub fn append_from_result(&mut self, result: &TxResult) {
-        if result.result_status != 0 {
-            return;
-        }
-
-        for call in &result.all_calls {
-            if call.call_value > BigUint::zero() {
-                self.call_value += call.call_value;
-            }
-            let tx_data = 
-        }
+    pub fn append_from_result(&mut self, _result: &TxResult) {
+        todo!()
     }
 }
-
-// func (host *vmHost) addNewBackTransfersFromVMOutput(vmOutput *vmcommon.VMOutput, parent, child []byte) {
-// 	if vmOutput == nil || vmOutput.ReturnCode != vmcommon.Ok {
-// 		return
-// 	}
-// 	callerOutAcc, ok := vmOutput.OutputAccounts[string(parent)]
-// 	if !ok {
-// 		return
-// 	}
-
-// 	for _, transfer := range callerOutAcc.OutputTransfers {
-// 		if !bytes.Equal(transfer.SenderAddress, child) {
-// 			continue
-// 		}
-// 		if transfer.CallType == vm.AsynchronousCallBack {
-// 			continue
-// 		}
-
-// 		if transfer.Value.Cmp(vmhost.Zero) > 0 {
-// 			if len(transfer.Data) == 0 {
-// 				host.managedTypesContext.AddValueOnlyBackTransfer(transfer.Value)
-// 			}
-// 			continue
-// 		}
-
-// 		esdtTransfers, isWithoutExec := host.isESDTTransferWithoutExecution(transfer.Data, parent, child)
-// 		if !isWithoutExec {
-// 			continue
-// 		}
-
-// 		host.managedTypesContext.AddBackTransfers(esdtTransfers.ESDTTransfers)
-// 	}
-// }
