b7d64afa24dd08ffc8d6376d465bf8f65165e835
Costin Carabaș <costin.carabas@gmail.com>
2023-11-03 10:24:41 +0200

    Promises feature refactor

--- contracts/feature-tests/composability/promises-features/src/call_promises_bt.rs
+++ contracts/feature-tests/composability/promises-features/src/call_promises_bt.rs
@@ -1,17 +1,10 @@
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
 
-#[derive(TopEncode, TopDecode, TypeAbi)]
-pub struct CallbackData<M: ManagedTypeApi> {
-    callback_name: ManagedBuffer<M>,
-    token_identifier: EgldOrEsdtTokenIdentifier<M>,
-    token_nonce: u64,
-    token_amount: BigUint<M>,
-    args: ManagedVec<M, ManagedBuffer<M>>,
-}
+use crate::common::{CallbackData, self};
 
 #[multiversx_sc::module]
-pub trait CallPromisesBackTransfersModule {
+pub trait CallPromisesBackTransfersModule: common::CommonModule {
     #[proxy]
     fn vault_proxy(&self) -> vault::Proxy<Self::Api>;
 
@@ -66,42 +59,4 @@ pub trait CallPromisesBackTransfersModule {
         }
     }
 
-    #[event("retrieve_funds_callback")]
-    fn retrieve_funds_callback_event(
-        &self,
-        #[indexed] token: &EgldOrEsdtTokenIdentifier,
-        #[indexed] nonce: u64,
-        #[indexed] payment: &BigUint,
-    );
-
-    #[view]
-    #[storage_mapper("callback_data")]
-    fn callback_data(&self) -> VecMapper<CallbackData<Self::Api>>;
-
-    #[view]
-    fn callback_data_at_index(
-        &self,
-        index: usize,
-    ) -> MultiValue5<
-        ManagedBuffer,
-        EgldOrEsdtTokenIdentifier,
-        u64,
-        BigUint,
-        MultiValueManagedVec<Self::Api, ManagedBuffer>,
-    > {
-        let cb_data = self.callback_data().get(index);
-        (
-            cb_data.callback_name,
-            cb_data.token_identifier,
-            cb_data.token_nonce,
-            cb_data.token_amount,
-            cb_data.args.into(),
-        )
-            .into()
-    }
-
-    #[endpoint]
-    fn clear_callback_data(&self) {
-        self.callback_data().clear();
-    }
 }
--- contracts/feature-tests/composability/promises-features/src/promises_main.rs
+++ contracts/feature-tests/composability/promises-features/src/promises_main.rs
@@ -5,13 +5,15 @@ mod call_promise_direct;
 mod call_promises;
 pub mod call_sync_bt;
 mod call_promises_bt;
+mod common;
 
 multiversx_sc::imports!();
 
 /// Test contract for investigating the new async call framework.
 #[multiversx_sc::contract]
 pub trait PromisesFeatures:
-    call_promises::CallPromisesModule
+    common::CommonModule
+    + call_promises::CallPromisesModule
     + call_promise_direct::CallPromisesDirectModule
     + call_sync_bt::BackTransfersFeatureModule
     + call_promises_bt::CallPromisesBackTransfersModule
