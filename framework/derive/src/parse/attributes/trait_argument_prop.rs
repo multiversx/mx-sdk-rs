9296f88505ed02b20dd088445ecf3969efc6497d
mihaicalinluca <mihaicalin.luca10@gmail.com>
2023-11-03 04:04:00 +0100

    removed allow multiple var args from trait attributes

--- framework/derive/src/parse/attributes/trait_argument_prop.rs
+++ framework/derive/src/parse/attributes/trait_argument_prop.rs
@@ -10,10 +10,6 @@ pub fn is_only_admin_prop(attr: &syn::Attribute) -> bool {
     is_attribute_with_no_args(attr, PROP_ADMIN_OWNER)
 }
 
-pub fn is_allow_multiple_var_args_prop(attr: &syn::Attribute) -> bool {
-    is_attribute_with_no_args(attr, PROP_ALLOW_MULTIPLE_VAR_ARGS)
-}
-
 pub fn get_esdt_attribute_prop(attr: &syn::Attribute) -> Option<EsdtAttribute> {
     get_attribute_with_one_type_arg(attr, PROP_ESDT_ATTRIBUTE)
 }
