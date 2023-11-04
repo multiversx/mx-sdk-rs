ce4c03efc263de9ad09965f6bf17d638528ec707
Andrei Marinica <andrei.marinica@multiversx.com>
2023-11-04 05:24:58 +0200

    dependency cleanup - meta - removed tokio

--- framework/meta/src/cmd/standalone.rs
+++ framework/meta/src/cmd/standalone.rs
@@ -18,7 +18,7 @@ use test::test;
 use upgrade::upgrade_sc;
 
 /// Entry point in the program when calling it as a standalone tool.
-pub async fn cli_main_standalone() {
+pub fn cli_main_standalone() {
     let cli_args = StandaloneCliArgs::parse();
     match &cli_args.command {
         Some(StandaloneCliAction::Info(args)) => call_info(args),
@@ -30,10 +30,10 @@ pub async fn cli_main_standalone() {
             local_deps(args);
         },
         Some(StandaloneCliAction::Template(args)) => {
-            create_contract(args).await;
+            create_contract(args);
         },
         Some(StandaloneCliAction::TemplateList(args)) => {
-            print_template_names(args).await;
+            print_template_names(args);
         },
         Some(StandaloneCliAction::TestGen(args)) => {
             test_gen_tool(args);
