7a77f3dbd1a0b9171a64dff53547ace6d8963874
Alin Cruceat <alyn_509@yahoo.com>
2023-11-15 18:35:12 +0200

    fix after review

--- contracts/feature-tests/basic-features/src/small_num_overflow_test_ops.rs
+++ contracts/feature-tests/basic-features/src/small_num_overflow_test_ops.rs
@@ -6,25 +6,25 @@ multiversx_sc::imports!();
 pub trait SmallIntOverflow {
     #[endpoint]
     #[allow(arithmetic_overflow)]
-    fn usize(&self) -> usize {
+    fn no_overflow_usize(&self) -> usize {
         usize::MAX + 1
     }
 
     #[endpoint]
     #[allow(arithmetic_overflow)]
-    fn u8(&self) -> u8 {
+    fn no_overflow_u8(&self) -> u8 {
         u8::MAX + 1
     }
 
     #[endpoint]
     #[allow(arithmetic_overflow)]
-    fn u16(&self) -> u16 {
+    fn no_overflow_u16(&self) -> u16 {
         u16::MAX + 1
     }
 
     #[endpoint]
     #[allow(arithmetic_overflow)]
-    fn u32(&self) -> u32 {
+    fn no_overflow_u32(&self) -> u32 {
         u32::MAX + 1
     }
 
@@ -34,82 +34,82 @@ pub trait SmallIntOverflow {
         u64::MAX + 1
     }
     #[endpoint]
-    fn usize_overflow(&self) -> usize {
+    fn overflow_usize(&self) -> usize {
         usize::MAX.wrapping_add(1)
     }
 
     #[endpoint]
-    fn u8_overflow(&self) -> u8 {
+    fn overflow_u8(&self) -> u8 {
         u8::MAX.wrapping_add(1)
     }
 
     #[endpoint]
-    fn u16_overflow(&self) -> u16 {
+    fn overflow_u16(&self) -> u16 {
         u16::MAX.wrapping_add(1)
     }
 
     #[endpoint]
-    fn u32_overflow(&self) -> u32 {
+    fn overflow_u32(&self) -> u32 {
         u32::MAX.wrapping_add(1)
     }
 
     #[endpoint]
-    fn u64_overflow(&self) -> u64 {
+    fn overflow_u64(&self) -> u64 {
         u64::MAX.wrapping_add(1)
     }
 
     #[endpoint]
     #[allow(arithmetic_overflow)]
-    fn isize(&self) -> isize {
+    fn no_overflow_isize(&self) -> isize {
         isize::MAX + isize::MAX
     }
 
     #[endpoint]
     #[allow(arithmetic_overflow)]
-    fn i8(&self) -> i8 {
+    fn no_overflow_i8(&self) -> i8 {
         i8::MAX + i8::MAX
     }
 
     #[endpoint]
     #[allow(arithmetic_overflow)]
-    fn i16(&self) -> i16 {
+    fn no_overflow_i16(&self) -> i16 {
         i16::MAX + i16::MAX
     }
 
     #[endpoint]
     #[allow(arithmetic_overflow)]
-    fn i32(&self) -> i32 {
+    fn no_overflow_i32(&self) -> i32 {
         i32::MAX + i32::MAX
     }
 
     #[endpoint]
     #[allow(arithmetic_overflow)]
-    fn i64(&self) -> i64 {
+    fn no_overflow_i64(&self) -> i64 {
         i64::MAX + i64::MAX
     }
 
     #[endpoint]
-    fn isize_overflow(&self) -> isize {
+    fn overflow_isize(&self) -> isize {
         isize::MAX.wrapping_add(isize::MAX)
     }
 
     #[endpoint]
-    fn i8_overflow(&self) -> i8 {
+    fn overflow_i8(&self) -> i8 {
         i8::MAX.wrapping_add(i8::MAX)
     }
 
     #[endpoint]
-    fn i16_overflow(&self) -> i16 {
+    fn overflow_i16(&self) -> i16 {
         i16::MAX.wrapping_add(i16::MAX)
     }
 
     #[endpoint]
-    fn i32_overflow(&self) -> i32 {
+    fn overflow_i32(&self) -> i32 {
         i32::MAX.wrapping_add(i32::MAX)
     }
 
     #[endpoint]
-    fn i64_overflow(&self) -> i64 {
+    fn overflow_i64(&self) -> i64 {
         i64::MAX.wrapping_add(i64::MAX)
     }
 }
