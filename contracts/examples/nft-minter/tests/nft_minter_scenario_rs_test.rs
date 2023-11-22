f3516679f3d2ee08459c2f5cf631a763577be3cd
Andrei Marinica <andrei.marinica@multiversx.com>
2023-11-23 00:00:42 +0200

    un-ignored more tests

--- contracts/examples/nft-minter/tests/nft_minter_scenario_rs_test.rs
+++ contracts/examples/nft-minter/tests/nft_minter_scenario_rs_test.rs
@@ -1,23 +1,23 @@
 use multiversx_sc_scenario::*;
 
 fn world() -> ScenarioWorld {
-    todo!()
-}
+    let mut blockchain = ScenarioWorld::new();
+    blockchain.set_current_dir_from_workspace("contracts/examples/nft-minter");
 
+    blockchain.register_contract("file:output/nft-minter.wasm", nft_minter::ContractBuilder);
+    blockchain
+}
 #[test]
-#[ignore = "not supported"]
 fn buy_nft_rs() {
     world().run("scenarios/buy_nft.scen.json");
 }
 
 #[test]
-#[ignore = "not supported"]
 fn create_nft_rs() {
     world().run("scenarios/create_nft.scen.json");
 }
 
 #[test]
-#[ignore = "not supported"]
 fn init_rs() {
     world().run("scenarios/init.scen.json");
 }
