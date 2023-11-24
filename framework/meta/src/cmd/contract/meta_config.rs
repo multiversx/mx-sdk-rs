37865700dd258260c6cb0001d5d61e053230f2a6
Andrei Marinica <andrei.marinica@multiversx.com>
2023-11-24 12:17:51 +0200

    sc 0.45.0, vm 0.7.0, scenario-format 0.21.0, sdk 0.3.0

--- framework/meta/src/cmd/contract/meta_config.rs
+++ framework/meta/src/cmd/contract/meta_config.rs
@@ -204,7 +204,7 @@ panic = \"abort\"
 path = \"..\"
 
 [dependencies.multiversx-sc-wasm-adapter]
-version = \"0.44.0\"
+version = \"0.45.0\"
 path = \"../../../../framework/wasm-adapter\"
 
 [workspace]
@@ -217,7 +217,7 @@ members = [\".\"]
             name: "test".to_string(),
             edition: "2021".to_string(),
             profile: ContractVariantProfile::default(),
-            framework_version: "0.44.0".to_string(),
+            framework_version: "0.45.0".to_string(),
             framework_path: Option::Some("../../../framework/base".to_string()),
             contract_features: Vec::<String>::new(),
         };
