9639b7fa6028f04dcc1ce0e72eaa3ef2432771cb
Andrei Marinica <andrei.marinica@multiversx.com>
2023-11-27 18:45:05 +0200

    cleanup

--- framework/base/src/types/interaction/tx_env.rs
+++ framework/base/src/types/interaction/tx_env.rs
@@ -22,31 +22,3 @@ pub trait TxEnv: Sized {
 
     fn default_gas(&self) -> u64;
 }
-
-// pub struct TxTestingEnvironemnt<Api>
-// where
-//     Api: CallTypeApi,
-// {
-//     pub(super) _phantom: PhantomData<Api>,
-//     pub from_annotation: ManagedBuffer<Api>,
-//     pub to_annotation: ManagedBuffer<Api>,
-// }
-
-// impl<Api> TxEnv<Api> for TxTestingEnvironemnt<Api>
-// where
-//     Api: CallTypeApi,
-// {
-//     fn annotate_from<From>(&mut self, from: &From)
-//     where
-//         From: AnnotatedValue<Api, ManagedAddress<Api>>,
-//     {
-//         self.from_annotation = from.annotation();
-//     }
-
-//     fn annotate_to<To>(&mut self, to: &To)
-//     where
-//         To: AnnotatedValue<Api, ManagedAddress<Api>>,
-//     {
-//         self.to_annotation = to.annotation();
-//     }
-// }
