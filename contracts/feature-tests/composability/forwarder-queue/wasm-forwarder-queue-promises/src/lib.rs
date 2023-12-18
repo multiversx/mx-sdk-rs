ac89cbf0fcfa1007225fc02270e56450d0f15375
Andrei Marinica <andrei.marinica@multiversx.com>
2023-12-18 18:13:29 +0200

    wasm crate update

--- contracts/feature-tests/composability/forwarder-queue/wasm-forwarder-queue-promises/src/lib.rs
+++ contracts/feature-tests/composability/forwarder-queue/wasm-forwarder-queue-promises/src/lib.rs
@@ -11,9 +11,7 @@
 // Total number of exported functions:  13
 
 #![no_std]
-
-// Configuration that works with rustc < 1.73.0.
-// TODO: Recommended rustc version: 1.73.0 or newer.
+#![allow(internal_features)]
 #![feature(lang_items)]
 
 multiversx_sc_wasm_adapter::allocator!();
