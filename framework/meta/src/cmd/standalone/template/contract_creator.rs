16071048b7ef9b352d1f6469cca7638694726990
BiancaIalangi <bianca.ialangi@multiversx.com>
2023-12-18 17:15:11 +0200

    Refactor version functions

--- framework/meta/src/cmd/standalone/template/contract_creator.rs
+++ framework/meta/src/cmd/standalone/template/contract_creator.rs
@@ -1,6 +1,6 @@
 use crate::{
     cli_args::TemplateArgs,
-    version_history::{validate_template_tag, LAST_TEMPLATE_VERSION, TEMPLATE_VERSIONS},
+    version_history::{validate_template_tag, LAST_TEMPLATE_VERSION},
 };
 
 use super::{
@@ -31,10 +31,7 @@ fn target_from_args(args: &TemplateArgs) -> ContractCreatorTarget {
 
 pub(crate) fn get_repo_version(args_tag: &Option<String>) -> RepoVersion {
     if let Some(tag) = args_tag {
-        assert!(
-            validate_template_tag(tag, TEMPLATE_VERSIONS),
-            "invalid template tag"
-        );
+        assert!(validate_template_tag(tag), "invalid template tag");
         RepoVersion::Tag(tag.clone())
     } else {
         RepoVersion::Tag(LAST_TEMPLATE_VERSION.to_string())
