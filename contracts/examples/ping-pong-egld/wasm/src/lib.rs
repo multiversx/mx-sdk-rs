8aa543bbdc1a9511ce686b8603f8945bf4570d6b
Andrei Marinica <andrei.marinica@multiversx.com>
2024-03-08 14:21:09 +0200

    Cargo.lock update

--- contracts/examples/ping-pong-egld/wasm/src/lib.rs
+++ contracts/examples/ping-pong-egld/wasm/src/lib.rs
@@ -5,9 +5,9 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
-// Endpoints:                           10
+// Endpoints:                           11
 // Async Callback (empty):               1
-// Total number of exported functions:  12
+// Total number of exported functions:  13
 
 #![no_std]
 #![allow(internal_features)]
@@ -20,6 +20,7 @@ multiversx_sc_wasm_adapter::endpoints! {
     ping_pong_egld
     (
         init => init
+        upgrade => upgrade
         ping => ping
         pong => pong
         pongAll => pong_all
