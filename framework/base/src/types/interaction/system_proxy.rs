6d92e45114855a64b3ec58bb852052508f3f20da
Andrei Marinica <andrei.marinica@multiversx.com>
2024-04-01 13:07:43 +0300

    moved UserBuiltinProxy to framework

--- framework/base/src/types/interaction/system_proxy.rs
+++ framework/base/src/types/interaction/system_proxy.rs
@@ -1,6 +1,8 @@
+mod builtin_func_proxy;
 mod system_sc_proxy;
 mod system_sc_proxy_legacy;
 pub(crate) mod token_properties;
 
+pub use builtin_func_proxy::*;
 pub use system_sc_proxy::*;
 pub use token_properties::*;
