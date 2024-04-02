0bd3e86fb8a6fc26c5e45339d995e8857b2c166e
Mihai Calin Luca <mihaicalin.luca10@gmail.com>
2024-04-02 16:12:46 +0200

    impl EgldOrEsdtTokenPaymentRefs

--- framework/base/src/types/interaction/tx_payment.rs
+++ framework/base/src/types/interaction/tx_payment.rs
@@ -1,5 +1,6 @@
 mod tx_payment_egld;
 mod tx_payment_egld_or_esdt;
+mod tx_payment_egld_or_esdt_ref;
 mod tx_payment_egld_or_multi_esdt;
 mod tx_payment_egld_or_multi_esdt_ref;
 mod tx_payment_egld_value;
@@ -9,6 +10,7 @@ mod tx_payment_single_esdt;
 mod tx_payment_single_esdt_ref;
 
 pub use tx_payment_egld::{Egld, EgldPayment};
+pub use tx_payment_egld_or_esdt_ref::*;
 pub use tx_payment_egld_value::TxEgldValue;
 pub use tx_payment_multi_esdt::TxPaymentMultiEsdt;
 
