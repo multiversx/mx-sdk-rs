a358568c3b141796ac92bb27a66db2ea4ac559e6
Andrei Marinica <andrei.marinica@multiversx.com>
2024-04-07 22:06:49 +0300

    fixed retrieving new token identifier

--- sdk/core/src/data/transaction.rs
+++ sdk/core/src/data/transaction.rs
@@ -132,6 +132,7 @@ pub struct TransactionInfoData {
 // TransactionInfo holds a transaction info response from the network
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct TransactionInfo {
+    #[serde(default)]
     pub error: String,
     pub code: String,
     pub data: Option<TransactionInfoData>,
