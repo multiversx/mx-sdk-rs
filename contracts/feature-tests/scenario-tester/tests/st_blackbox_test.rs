c30c1afc239548d1d6e49e23faf681ffcc473fe9
Mihai Calin Luca <mihaicalin.luca10@gmail.com>
2024-04-12 09:56:48 +0200

    added tests

--- contracts/feature-tests/scenario-tester/tests/st_blackbox_test.rs
+++ contracts/feature-tests/scenario-tester/tests/st_blackbox_test.rs
@@ -1,19 +1,23 @@
-use multiversx_sc_scenario::imports::*;
+use multiversx_sc_scenario::{imports::*, scenario_model::U64Value};
 use num_bigint::BigUint;
 
 use scenario_tester::*;
 
-const ADDER_PATH_EXPR: &str = "mxsc:output/scenario-tester.mxsc.json";
+const SC_SCENARIO_TESTER_PATH_EXPR: &str = "mxsc:output/scenario-tester.mxsc.json";
 
 const OWNER: AddressExpr = AddressExpr("owner");
-const SC_ADDER: ScExpr = ScExpr("scenario-tester");
+const OTHER: AddressExpr = AddressExpr("other");
+const SC_SCENARIO_TESTER: ScExpr = ScExpr("scenario-tester");
 const CODE_EXPR: MxscExpr = MxscExpr("output/scenario-tester.mxsc.json");
 
 fn world() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
     blockchain.set_current_dir_from_workspace("contracts/examples/scenario-tester");
 
-    blockchain.register_contract(ADDER_PATH_EXPR, scenario_tester::ContractBuilder);
+    blockchain.register_contract(
+        SC_SCENARIO_TESTER_PATH_EXPR,
+        scenario_tester::ContractBuilder,
+    );
     blockchain
 }
 
@@ -21,15 +25,40 @@ fn world() -> ScenarioWorld {
 fn st_blackbox() {
     let mut world = world();
     let owner_address = "address:owner";
+    let other_address = "address:other";
+
     let st_contract = ContractInfo::<scenario_tester::Proxy<StaticApi>>::new("sc:scenario-tester");
 
     world.start_trace();
 
-    world.set_state_step(
-        SetStateStep::new()
-            .put_account(owner_address, Account::new().nonce(1))
-            .new_address(owner_address, 1, "sc:scenario-tester"),
+    world
+        .account(owner_address)
+        .nonce(1)
+        .balance("100")
+        .account(other_address)
+        .nonce(2)
+        .balance("300")
+        .esdt_balance("str:TOKEN-123456", "500")
+        .commit();
+
+    world.check_state_step(
+        CheckStateStep::new()
+            .put_account(
+                owner_address,
+                CheckAccount::new()
+                    .nonce(U64Value::from(1u64))
+                    .balance("100"),
+            )
+            .put_account(
+                other_address,
+                CheckAccount::new()
+                    .nonce(U64Value::from(2u64))
+                    .balance("300")
+                    .esdt_balance("str:TOKEN-123456", "500"),
+            ),
     );
+
+    world.set_state_step(SetStateStep::new().new_address(owner_address, 1, "sc:scenario-tester"));
 
     let new_address = world
         .tx()
@@ -44,7 +73,7 @@ fn st_blackbox() {
 
     let value = world
         .query()
-        .to(SC_ADDER)
+        .to(SC_SCENARIO_TESTER)
         .typed(scenario_tester_proxy::ScenarioTesterProxy)
         .sum()
         .returns(ReturnsResultConv::<BigUint>::new())
@@ -54,7 +83,7 @@ fn st_blackbox() {
     world
         .tx()
         .from(OWNER)
-        .to(SC_ADDER)
+        .to(SC_SCENARIO_TESTER)
         .typed(scenario_tester_proxy::ScenarioTesterProxy)
         .add(1u32)
         .run();
@@ -67,6 +96,14 @@ fn st_blackbox() {
                 CheckAccount::new().check_storage("str:sum", "6"),
             ),
     );
+
+    world
+        .tx()
+        .from(OTHER)
+        .to(SC_SCENARIO_TESTER)
+        .typed(scenario_tester_proxy::ScenarioTesterProxy)
+        .add(1u32)
+        .run();
 
     world.write_scenario_trace("trace1.scen.json");
 }
