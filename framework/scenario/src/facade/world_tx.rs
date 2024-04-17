43edef0cb8a36f1d59d408b155b5c9f666c70bb5
Andrei Marinica <andrei.marinica@multiversx.com>
2024-04-17 17:44:12 +0300

    scenario tx file rename

--- framework/scenario/src/facade/world_tx.rs
+++ framework/scenario/src/facade/world_tx.rs
@@ -1,11 +1,11 @@
 #![allow(unused)] // TEMP
 
-mod scenario_env;
-mod scenario_env_deploy;
-mod scenario_env_exec;
-mod scenario_env_query;
+mod scenario_exec_call;
+mod scenario_exec_deploy;
+mod scenario_query_call;
 mod scenario_rh_impl;
+mod scenario_tx_env;
 
-pub use scenario_env::{ScenarioTxEnv, ScenarioTxEnvData, ScenarioTxRun};
-pub use scenario_env_exec::ScenarioEnvExec;
-pub use scenario_env_query::ScenarioEnvQuery;
+pub use scenario_exec_call::ScenarioEnvExec;
+pub use scenario_query_call::ScenarioEnvQuery;
+pub use scenario_tx_env::{ScenarioTxEnv, ScenarioTxEnvData, ScenarioTxRun};
