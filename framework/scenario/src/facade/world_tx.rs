a372df542b3922ae98d6a00599571b35b1417981
Andrei Marinica <andrei.marinica@multiversx.com>
2024-04-18 12:03:40 +0300

    set state - refactor

--- framework/scenario/src/facade/world_tx.rs
+++ framework/scenario/src/facade/world_tx.rs
@@ -6,6 +6,7 @@ mod scenario_query_call;
 mod scenario_rh_impl;
 mod scenario_set_state;
 mod scenario_tx_env;
+mod scenario_set_state_account;
 
 pub use scenario_exec_call::ScenarioEnvExec;
 pub use scenario_query_call::ScenarioEnvQuery;
