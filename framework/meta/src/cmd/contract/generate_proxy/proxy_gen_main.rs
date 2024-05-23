8f3d791f0c67816b82ecea2741d37e5ef908b926
Mihai Calin Luca <mihaicalin.luca10@gmail.com>
2024-05-23 14:00:06 +0300

    cleanup and replaced file from proxy gen to Write trait

--- framework/meta/src/cmd/contract/generate_proxy/proxy_gen_main.rs
+++ framework/meta/src/cmd/contract/generate_proxy/proxy_gen_main.rs
@@ -9,6 +9,8 @@ use super::{
     super::meta_config::MetaConfig, proxy_crate_gen::create_file, proxy_generator::ProxyGenerator,
 };
 
+const PROXY_COMPARE_ERR_MSG: &str = "Contract has been modified and proxies have not been updated. Regenerate proxies to avoid inconsistencies.";
+
 impl MetaConfig {
     pub fn generate_proxy(&mut self) {
         let default_proxy = ProxyConfigSerde::new();
@@ -25,27 +27,20 @@ impl MetaConfig {
     }
 }
 
-fn compare_proxy_explicit_path(proxy_config: &ProxyConfigSerde, meta_config: &MetaConfig) { 
-    let err_msg = "Contract has been modified and proxies have not been updated. Regenerate proxies to avoid inconsistencies.".to_string();
-    
+fn compare_proxy_explicit_path(proxy_config: &ProxyConfigSerde, meta_config: &MetaConfig) {
     let contract_abi = extract_contract_abi(proxy_config, meta_config);
-    let mut temp = create_file("temp");
+    let mut temp = Vec::<u8>::new();
     let mut proxy_generator =
-    ProxyGenerator::new(meta_config, &mut temp, proxy_config, contract_abi);
+        ProxyGenerator::new(meta_config, &mut temp, proxy_config, contract_abi);
     proxy_generator.write_proxy_to_file();
-    
+
     let existent_proxy_path = format!("../{}", proxy_config.path);
-    let temp_path = "../temp";
-
     let existent_proxy = fs::read_to_string(existent_proxy_path).unwrap();
-    let temp = fs::read_to_string(temp_path).unwrap();
+    let newly_gen_proxy = String::from_utf8(temp).unwrap();
 
-    if existent_proxy != temp {
-        fs::remove_file(temp_path).unwrap();
-        panic!("{}", err_msg.red());
+    if existent_proxy != newly_gen_proxy {
+        panic!("{}", PROXY_COMPARE_ERR_MSG.to_string().red());
     }
-
-    fs::remove_file(temp_path).unwrap();
 }
 
 fn write_proxy_with_explicit_path(proxy_config: &ProxyConfigSerde, meta_config: &MetaConfig) {
