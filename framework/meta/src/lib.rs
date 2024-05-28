cc059599eb6c8d30d6d4b2a07fb7300598171655
Andrei Marinica <andrei.marinica@multiversx.com>
2024-05-28 13:39:29 +0300

    deprecated meta cli_main and multi_contract_config

--- framework/meta/src/lib.rs
+++ framework/meta/src/lib.rs
@@ -9,11 +9,21 @@ pub use multiversx_sc_meta_lib::version;
 pub use multiversx_sc_meta_lib::version_history;
 
 /// Backwards compatibility, please use `multiversx_sc_meta_lib::cli_main::<AbiObj>()`.
+///
+/// Failure to do so will result in slower build time.
+#[deprecated(
+    since = "0.41.0",
+    note = "Backwards compatibility only, please use `cli_main` from crate `multiversx-sc-meta-lib` instead."
+)]
 pub fn cli_main<AbiObj: multiversx_sc::contract_base::ContractAbiProvider>() {
     multiversx_sc_meta_lib::cli_main::<AbiObj>()
 }
 
 /// Backwards compatibility, please use `multiversx_sc_meta_lib::multi_contract_config::<AbiObj>(contract_crate_path)`.
+#[deprecated(
+    since = "0.41.0",
+    note = "Backwards compatibility only, please use `multi_contract_config` from crate `multiversx-sc-meta-lib` instead."
+)]
 pub fn multi_contract_config<AbiObj: multiversx_sc::contract_base::ContractAbiProvider>(
     contract_crate_path: &std::path::Path,
 ) -> multiversx_sc_meta_lib::contract::sc_config::ScConfig {
