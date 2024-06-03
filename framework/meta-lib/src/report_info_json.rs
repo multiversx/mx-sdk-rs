9a1119c6f5388ea1d3b9d345075643238d8109fa
BiancaIalangi <bianca.ialangi@multiversx.com>
2024-06-03 14:11:39 +0300

    print code report in mxsc.json

--- framework/meta-lib/src/report_info_json.rs
+++ framework/meta-lib/src/report_info_json.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-use crate::{ei_check_json::EiCheckJson, tools::WasmInfo};
+use crate::{code_report_json::CodeReportJson, ei_check_json::EiCheckJson, tools::WasmInfo};
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -10,18 +10,18 @@ pub struct ReportInfoJson {
     pub imports: Vec<String>,
 
     #[serde(default)]
-    pub memory_allocation_error: bool,
-
-    #[serde(default)]
     pub is_mem_grow: bool,
 
     #[serde(default)]
     #[serde(skip_serializing_if = "Option::is_none")]
     pub ei_check: Option<EiCheckJson>,
+
+    #[serde(default)]
+    pub code_report: CodeReportJson,
 }
 
 impl ReportInfoJson {
-    pub fn new(wasm_info: &WasmInfo, ei_check_info: Option<EiCheckJson>) -> Self {
+    pub fn new(wasm_info: &WasmInfo, ei_check_info: Option<EiCheckJson>, size: usize) -> Self {
         let ei_check = if wasm_info.imports.is_empty() {
             None
         } else {
@@ -30,9 +30,9 @@ impl ReportInfoJson {
 
         ReportInfoJson {
             imports: wasm_info.imports.iter().map(|i| i.to_string()).collect(),
-            memory_allocation_error: wasm_info.allocator_trigger,
             is_mem_grow: wasm_info.memory_grow_flag,
             ei_check,
+            code_report: CodeReportJson::new(&wasm_info.report, size),
         }
     }
 }
