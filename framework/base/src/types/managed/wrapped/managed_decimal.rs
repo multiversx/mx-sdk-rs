3e422427a7e9d32eaa1c24bef14d5345730def8a
Andrei Marinica <andrei.marinica@multiversx.com>
2024-06-23 02:29:24 +0300

    managed decimal reorg

--- framework/base/src/types/managed/wrapped/managed_decimal.rs
+++ framework/base/src/types/managed/wrapped/managed_decimal.rs
@@ -1,14 +1,16 @@
+mod decimals;
 mod managed_decimal_cmp;
 mod managed_decimal_macros;
 mod managed_decimal_operators;
 
+pub use decimals::{ConstDecimals, Decimals, NumDecimals};
 pub use managed_decimal_operators::MulToPrecision;
 
 use crate::{
     abi::{TypeAbi, TypeAbiFrom, TypeName},
     api::{
         const_handles, use_raw_handle, BigFloatApiImpl, BigIntApiImpl, HandleConstraints,
-        ManagedBufferApiImpl, ManagedTypeApi, StaticVarApiImpl,
+        ManagedBufferApiImpl, ManagedTypeApi,
     },
     formatter::{FormatBuffer, FormatByteReceiver, SCDisplay},
     proxy_imports::{ManagedBuffer, ManagedType},
@@ -24,58 +26,6 @@ use multiversx_sc_codec::{
 use core::{cmp::Ordering, ops::Deref};
 
 use super::{ManagedBufferCachedBuilder, ManagedRef};
-
-fn scaling_factor<M: ManagedTypeApi>(
-    num_decimals: NumDecimals,
-) -> ManagedRef<'static, M, BigUint<M>> {
-    let handle: M::BigIntHandle =
-        use_raw_handle(const_handles::get_scaling_factor_handle(num_decimals));
-
-    if !M::static_var_api_impl().is_scaling_factor_cached(num_decimals) {
-        cache_scaling_factor::<M>(handle.clone(), num_decimals);
-        M::static_var_api_impl().set_scaling_factor_cached(num_decimals);
-    }
-
-    unsafe { ManagedRef::<'static, M, BigUint<M>>::wrap_handle(handle) }
-}
-
-fn cache_scaling_factor<M: ManagedTypeApi>(handle: M::BigIntHandle, num_decimals: NumDecimals) {
-    let temp1: M::BigIntHandle = use_raw_handle(const_handles::BIG_INT_TEMPORARY_1);
-    let temp2: M::BigIntHandle = use_raw_handle(const_handles::BIG_INT_TEMPORARY_2);
-    let api = M::managed_type_impl();
-    api.bi_set_int64(temp1.clone(), 10);
-    api.bi_set_int64(temp2.clone(), num_decimals as i64);
-    api.bi_pow(handle, temp1, temp2);
-}
-
-pub trait Decimals {
-    fn num_decimals(&self) -> NumDecimals;
-
-    fn scaling_factor<M: ManagedTypeApi>(&self) -> ManagedRef<'static, M, BigUint<M>> {
-        scaling_factor(self.num_decimals())
-    }
-}
-
-impl Decimals for NumDecimals {
-    fn num_decimals(&self) -> NumDecimals {
-        *self
-    }
-}
-
-pub type NumDecimals = usize;
-
-#[derive(Clone, Debug)]
-pub struct ConstDecimals<const DECIMALS: NumDecimals>;
-
-impl<const DECIMALS: NumDecimals> Decimals for ConstDecimals<DECIMALS> {
-    fn num_decimals(&self) -> NumDecimals {
-        DECIMALS
-    }
-
-    fn scaling_factor<M: ManagedTypeApi>(&self) -> ManagedRef<'static, M, BigUint<M>> {
-        scaling_factor(self.num_decimals())
-    }
-}
 
 #[derive(Clone)]
 pub struct ManagedDecimal<M: ManagedTypeApi, D: Decimals> {
