43a99d8c92286b2e3cfd04fafecf8131b2297005
Andrei Vasilescu <vasilescuandrei24@gmail.com>
2024-08-14 20:19:29 +0300

    keystore signing done

---
+++ sdk/core/src/data/keystore.rs
@@ -0,0 +1,35 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CryptoParams {
+    pub iv: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct KdfParams {
+    pub dklen: u32,
+    pub salt: String,
+    pub n: u32,
+    pub r: u32,
+    pub p: u32,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Crypto {
+    pub ciphertext: String,
+    pub cipherparams: CryptoParams,
+    pub cipher: String,
+    pub kdf: String,
+    pub kdfparams: KdfParams,
+    pub mac: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Keystore {
+    pub version: u32,
+    pub kind: String,
+    pub id: String,
+    pub address: String,
+    pub bech32: String,
+    pub crypto: Crypto,
+}
