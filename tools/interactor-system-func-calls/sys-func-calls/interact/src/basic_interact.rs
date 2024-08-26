91763211a17e083a3c1e06359f83b32c83eb30b6
Andrei Vasilescu <vasilescuandrei24@gmail.com>
2024-08-26 15:34:55 +0300

    added more sys calls (SFTs/MetaESDTs)

--- tools/interactor-system-func-calls/sys-func-calls/interact/src/basic_interact.rs
+++ tools/interactor-system-func-calls/sys-func-calls/interact/src/basic_interact.rs
@@ -2,6 +2,7 @@ mod basic_interact_cli;
 mod basic_interact_config;
 mod basic_interact_state;
 
+use basic_interact_cli::NftDummyAttributes;
 use basic_interact_config::Config;
 use basic_interact_state::State;
 use clap::Parser;
@@ -28,14 +29,62 @@ async fn main() {
                 .await;
         },
         Some(basic_interact_cli::InteractCliCommand::Mint(args)) => {
-            basic_interact.mint_token(args.amount.clone()).await;
+            basic_interact
+                .mint_token(&args.token_id, args.amount.clone())
+                .await;
         },
         Some(basic_interact_cli::InteractCliCommand::SetRoles(args)) => {
             // let parsed_args = SetRolesArgs::parse();
-            basic_interact.set_role(args.roles.clone()).await;
+            basic_interact
+                .set_role(&args.token_id, args.roles.clone())
+                .await;
         },
         Some(basic_interact_cli::InteractCliCommand::Burn(args)) => {
-            basic_interact.burn_token(args.amount.clone()).await;
+            basic_interact
+                .burn_token(&args.token_id, args.amount.clone())
+                .await;
+        },
+        Some(basic_interact_cli::InteractCliCommand::IssueFungible(args)) => {
+            basic_interact
+                ._issue_fungible_token(
+                    args.cost.clone(),
+                    &args.display_name,
+                    &args.ticker,
+                    args.supply.clone(),
+                    args.num_decimals,
+                )
+                .await;
+        },
+        Some(basic_interact_cli::InteractCliCommand::IssueSft(args)) => {
+            basic_interact
+                .issue_semi_fungible_token(args.cost.clone(), &args.display_name, &args.ticker)
+                .await;
+        },
+        Some(basic_interact_cli::InteractCliCommand::MintSft(args)) => {
+            basic_interact
+                .mint_sft(
+                    &args.token_id,
+                    args.amount.clone(),
+                    &args.name,
+                    args.royalties.clone(),
+                    &args.hash,
+                )
+                .await;
+        },
+        Some(basic_interact_cli::InteractCliCommand::RegisterMetaEsdt(args)) => {
+            basic_interact
+                .register_meta_esdt(
+                    args.cost.clone(),
+                    &args.display_name,
+                    &args.ticker,
+                    args.num_decimals,
+                )
+                .await;
+        },
+        Some(basic_interact_cli::InteractCliCommand::ChangeSftMetaEsdt(args)) => {
+            basic_interact
+                .change_sft_meta_esdt(&args.token_id, args.num_decimals)
+                .await;
         },
 
         None => {},
@@ -47,7 +96,6 @@ struct SysFuncCallsInteract {
     interactor: Interactor,
     wallet_address: Bech32Address,
     state: State,
-    token_id: String,
 }
 
 impl SysFuncCallsInteract {
@@ -62,7 +110,6 @@ impl SysFuncCallsInteract {
             interactor,
             wallet_address: wallet_address.into(),
             state: State::load_state(),
-            token_id: String::new(),
         }
     }
 
@@ -71,6 +118,7 @@ impl SysFuncCallsInteract {
         issue_cost: RustBigUint,
         token_display_name: &str,
         token_ticker: &str,
+        initial_supply: RustBigUint,
         num_decimals: usize,
     ) {
         self.interactor
@@ -83,7 +131,7 @@ impl SysFuncCallsInteract {
                 issue_cost.into(),
                 &token_display_name.into(),
                 &token_ticker.into(),
-                &BigUint::from(100000000000000000000u128),
+                &initial_supply.into(),
                 FungibleTokenProperties {
                     num_decimals: num_decimals,
                     can_freeze: true,
@@ -91,6 +139,37 @@ impl SysFuncCallsInteract {
                     can_pause: true,
                     can_mint: true,
                     can_burn: true,
+                    can_change_owner: true,
+                    can_upgrade: true,
+                    can_add_special_roles: true,
+                },
+            )
+            .prepare_async()
+            .run()
+            .await;
+    }
+
+    async fn issue_semi_fungible_token(
+        &mut self,
+        issue_cost: RustBigUint,
+        token_display_name: &str,
+        token_ticker: &str,
+    ) {
+        self.interactor
+            .tx()
+            .from(&self.wallet_address)
+            .to(ESDTSystemSCAddress.to_managed_address())
+            .gas(100_000_000u64)
+            .typed(ESDTSystemSCProxy)
+            .issue_semi_fungible(
+                issue_cost.into(),
+                &token_display_name.into(),
+                &token_ticker.into(),
+                SemiFungibleTokenProperties {
+                    can_freeze: true,
+                    can_wipe: true,
+                    can_pause: true,
+                    can_transfer_create_role: true,
                     can_change_owner: true,
                     can_upgrade: true,
                     can_add_special_roles: true,
@@ -129,10 +208,9 @@ impl SysFuncCallsInteract {
             .await;
 
         println!("TOKEN ID: {:?}", token_id.to_string());
-        self.token_id = token_id.to_string();
     }
 
-    async fn set_role(&mut self, roles: Vec<u16>) {
+    async fn set_role(&mut self, token_id: &str, roles: Vec<u16>) {
         let wallet_address = &self.wallet_address.clone().into_address();
         let converted_roles: Vec<EsdtLocalRole> =
             roles.into_iter().map(|r| EsdtLocalRole::from(r)).collect();
@@ -147,7 +225,7 @@ impl SysFuncCallsInteract {
             .typed(ESDTSystemSCProxy)
             .set_special_roles::<std::vec::IntoIter<EsdtLocalRole>>(
                 &ManagedAddress::from_address(wallet_address),
-                &TokenIdentifier::from(&self.token_id),
+                &TokenIdentifier::from(token_id),
                 converted_roles.into_iter(),
             )
             .prepare_async()
@@ -155,35 +233,108 @@ impl SysFuncCallsInteract {
             .await;
     }
 
-    async fn mint_token(&mut self, amount: RustBigUint) {
+    async fn mint_sft(
+        &mut self,
+        token_id: &str,
+        amount: RustBigUint,
+        name: &str,
+        royalties: RustBigUint,
+        hash: &str,
+    ) {
         self.interactor
             .tx()
             .from(&self.wallet_address)
             .to(&self.wallet_address)
             .gas(100_000_000u64)
             .typed(UserBuiltinProxy)
-            .esdt_local_mint(
-                &TokenIdentifier::from(&self.token_id),
-                0,
+            .esdt_nft_create(
+                &TokenIdentifier::from(token_id),
                 &BigUint::from(amount),
+                &ManagedBuffer::from(name),
+                &royalties.into(),
+                &ManagedBuffer::from(hash),
+                &NftDummyAttributes {
+                    creation_epoch: 2104,
+                    cool_factor: 5,
+                },
+                &ManagedVec::new(),
             )
             .prepare_async()
             .run()
             .await;
     }
 
-    async fn burn_token(&mut self, amount: RustBigUint) {
+    async fn register_meta_esdt(
+        &mut self,
+        issue_cost: RustBigUint,
+        token_display_name: &str,
+        token_ticker: &str,
+        num_decimals: usize,
+    ) {
+        let meta_esdt = self
+            .interactor
+            .tx()
+            .from(&self.wallet_address)
+            .to(ESDTSystemSCAddress.to_managed_address())
+            .gas(100_000_000u64)
+            .typed(ESDTSystemSCProxy)
+            .register_meta_esdt(
+                issue_cost.into(),
+                &token_display_name.into(),
+                &token_ticker.into(),
+                MetaTokenProperties {
+                    num_decimals: num_decimals,
+                    can_freeze: true,
+                    can_wipe: true,
+                    can_transfer_create_role: true,
+                    can_change_owner: true,
+                    can_upgrade: true,
+                    can_pause: true,
+                    can_add_special_roles: true,
+                },
+            )
+            .returns(ReturnsResultUnmanaged)
+            .prepare_async()
+            .run()
+            .await;
+
+        println!("META ESDT: {:?}", meta_esdt.to_string());
+    }
+
+    async fn change_sft_meta_esdt(&mut self, token_id: &str, num_decimals: usize) {
+        self.interactor
+            .tx()
+            .from(&self.wallet_address)
+            .to(ESDTSystemSCAddress.to_managed_address())
+            .gas(100_000_000u64)
+            .typed(ESDTSystemSCProxy)
+            .change_sft_to_meta_esdt(&TokenIdentifier::from(token_id), num_decimals)
+            .prepare_async()
+            .run()
+            .await;
+    }
+
+    async fn mint_token(&mut self, token_id: &str, amount: RustBigUint) {
         self.interactor
             .tx()
             .from(&self.wallet_address)
             .to(&self.wallet_address)
             .gas(100_000_000u64)
             .typed(UserBuiltinProxy)
-            .esdt_local_burn(
-                &TokenIdentifier::from(&self.token_id),
-                0,
-                &BigUint::from(amount),
-            )
+            .esdt_local_mint(&TokenIdentifier::from(token_id), 0, &BigUint::from(amount))
+            .prepare_async()
+            .run()
+            .await;
+    }
+
+    async fn burn_token(&mut self, token_id: &str, amount: RustBigUint) {
+        self.interactor
+            .tx()
+            .from(&self.wallet_address)
+            .to(&self.wallet_address)
+            .gas(100_000_000u64)
+            .typed(UserBuiltinProxy)
+            .esdt_local_burn(&TokenIdentifier::from(token_id), 0, &BigUint::from(amount))
             .prepare_async()
             .run()
             .await;
--- tools/interactor-system-func-calls/sys-func-calls/interact/src/basic_interact_cli.rs
+++ tools/interactor-system-func-calls/sys-func-calls/interact/src/basic_interact_cli.rs
@@ -1,5 +1,5 @@
 use clap::{Args, Parser, Subcommand};
-use multiversx_sc_snippets::imports::RustBigUint;
+use multiversx_sc_snippets::{imports::RustBigUint, multiversx_sc::proxy_imports::*};
 
 /// SysFuncCalls Interact CLI
 #[derive(Default, PartialEq, Eq, Debug, Parser)]
@@ -13,14 +13,27 @@ pub struct InteractCli {
 /// SysFuncCalls Interact CLI Commands
 #[derive(Clone, PartialEq, Eq, Debug, Subcommand)]
 pub enum InteractCliCommand {
-    #[command(name = "issue-token", about = "Issue a token")]
+    #[command(name = "issue-token", about = "Issues a token")]
     IssueToken(IssueTokenArgs),
     #[command(name = "mint", about = "Mints fungible tokens")]
     Mint(MintArgs),
-    #[command(name = "set-roles", about = "Set roles")]
+    #[command(name = "set-roles", about = "Sets roles")]
     SetRoles(SetRolesArgs),
     #[command(name = "burn", about = "Burns fungible tokens")]
     Burn(BurnArgs),
+    #[command(
+        name = "issue-fungible",
+        about = "Issues fungible tokens and sends them to your wallet"
+    )]
+    IssueFungible(IssueFungibleArgs),
+    #[command(name = "issue-sft", about = "Issues a SFT")]
+    IssueSft(IssueSftArgs),
+    #[command(name = "mint-sft", about = "Mints a SFT")]
+    MintSft(MintSFTArgs),
+    #[command(name = "register-meta-esdt", about = "Registers a meta ESDT")]
+    RegisterMetaEsdt(RegisterMetaEsdtArgs),
+    #[command(name = "change-sft-meta-esdt", about = "Changes a SFT to a Meta ESDT")]
+    ChangeSftMetaEsdt(ChangeSftMetaEsdtArgs),
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Args)]
@@ -39,18 +52,88 @@ pub struct IssueTokenArgs {
 
 #[derive(Clone, Debug, PartialEq, Eq, Args)]
 pub struct MintArgs {
+    #[arg(long = "token-id")]
+    pub token_id: String,
     #[arg(long = "amount")]
     pub amount: RustBigUint,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Parser)]
 pub struct SetRolesArgs {
+    #[arg(long = "token-id")]
+    pub token_id: String,
     #[arg(long = "roles", value_delimiter = ',')]
     pub roles: Vec<u16>,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq, Args)]
 pub struct BurnArgs {
+    #[arg(long = "token-id")]
+    pub token_id: String,
     #[arg(long = "amount")]
     pub amount: RustBigUint,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Args)]
+pub struct IssueFungibleArgs {
+    #[arg(short = 'c', long = "cost", default_value = "50000000000000000")]
+    pub cost: RustBigUint,
+    #[arg(long = "display-name")]
+    pub display_name: String,
+    #[arg(long = "token-ticker")]
+    pub ticker: String,
+    #[arg(long = "num-decimals")]
+    pub num_decimals: usize,
+    #[arg(short = 's', long = "supply")]
+    pub supply: RustBigUint,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Args)]
+pub struct IssueSftArgs {
+    #[arg(short = 'c', long = "cost", default_value = "50000000000000000")]
+    pub cost: RustBigUint,
+    #[arg(long = "display-name")]
+    pub display_name: String,
+    #[arg(long = "token-ticker")]
+    pub ticker: String,
+}
+
+#[derive(TopEncode, TopDecode, Clone, Debug, PartialEq, Eq)]
+pub struct NftDummyAttributes {
+    pub creation_epoch: u64,
+    pub cool_factor: u8,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Args)]
+pub struct MintSFTArgs {
+    #[arg(long = "token-id")]
+    pub token_id: String,
+    #[arg(short = 'a', long = "amount")]
+    pub amount: RustBigUint,
+    #[arg(short = 'n', long = "name")]
+    pub name: String,
+    #[arg(short = 'r', long = "royalties")]
+    pub royalties: RustBigUint,
+    #[arg(long = "hash")]
+    pub hash: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Args)]
+pub struct RegisterMetaEsdtArgs {
+    #[arg(short = 'c', long = "cost", default_value = "50000000000000000")]
+    pub cost: RustBigUint,
+    #[arg(long = "display-name")]
+    pub display_name: String,
+    #[arg(long = "token-ticker")]
+    pub ticker: String,
+    #[arg(long = "num-decimals")]
+    pub num_decimals: usize,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Args)]
+pub struct ChangeSftMetaEsdtArgs {
+    #[arg(long = "token-id")]
+    pub token_id: String,
+    #[arg(long = "num-decimals")]
+    pub num_decimals: usize,
 }
