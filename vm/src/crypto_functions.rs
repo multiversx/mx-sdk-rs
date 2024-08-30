d6003f3706eb2011297720adc034eed6496e5a74
JustEatAnApple <mblacioti@gmail.com>
2024-08-30 16:49:58 +0300

    BLS Signature PR with clippy fixed
    Co-authored-by: Andrei Vasilescu <vasilescuandrei24@gmail.com>

--- vm/src/crypto_functions.rs
+++ vm/src/crypto_functions.rs
@@ -3,6 +3,7 @@ use sha3::{Digest, Keccak256};
 
 pub const SHA256_RESULT_LEN: usize = 32;
 pub const KECCAK256_RESULT_LEN: usize = 32;
+pub const BLS_DST_VALUE: &[u8] = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_";
 
 pub fn sha256(data: &[u8]) -> [u8; SHA256_RESULT_LEN] {
     let mut hasher = Sha256::new();
@@ -16,29 +17,77 @@ pub fn keccak256(data: &[u8]) -> [u8; KECCAK256_RESULT_LEN] {
     hasher.finalize().into()
 }
 
+#[cfg(feature = "wasm-incompatible")]
 pub fn verify_ed25519(key: &[u8], message: &[u8], signature: &[u8]) -> bool {
-    use ed25519_dalek::{Signature, Verifier, VerifyingKey};
+    use ed25519_dalek::*;
 
-    let key_32: [u8; 32] = if let Ok(key_32) = key.try_into() {
-        key_32
-    } else {
+    let public = PublicKey::from_bytes(key);
+    if public.is_err() {
         return false;
-    };
-    let signature_64: [u8; 64] = if let Ok(signature_64) = signature.try_into() {
-        signature_64
-    } else {
+    }
+
+    let sig = Signature::from_bytes(signature);
+    if sig.is_err() {
         return false;
+    }
+
+    public.unwrap().verify(message, &sig.unwrap()).is_ok()
+}
+
+#[cfg(not(feature = "wasm-incompatible"))]
+pub fn verify_ed25519(_key: &[u8], _message: &[u8], _signature: &[u8]) -> bool {
+    panic!("verify_ed25519 not supported for wasm builds, feature `wasm-incopatible` needs to be enabled")
+}
+
+pub fn verify_bls_signature(key: &[u8], message: &[u8], signature: &[u8]) -> bool {
+    use blst::{
+        min_pk::{PublicKey, Signature},
+        BLST_ERROR,
     };
 
-    let verifying_key_result = VerifyingKey::from_bytes(&key_32);
-    let verifying_key = if let Ok(verifying_key) = verifying_key_result {
-        verifying_key
-    } else {
-        return false;
+    let public_key = PublicKey::from_bytes(key).unwrap();
+
+    let sig = Signature::from_bytes(signature).unwrap();
+
+    let verify_response = sig.verify(true, message, BLS_DST_VALUE, &[], &public_key, true);
+
+    matches!(verify_response, BLST_ERROR::BLST_SUCCESS)
+}
+
+pub fn verify_multi_bls_signature(
+    public_keys: Vec<&[u8]>,
+    message: &[u8],
+    aggregated_signature: &[u8],
+) -> bool {
+    use blst::{
+        min_pk::{AggregatePublicKey, AggregateSignature, PublicKey, Signature},
+        BLST_ERROR,
     };
 
-    let sig = Signature::from_bytes(&signature_64);
+    let mut aggregate_pk: AggregatePublicKey =
+        AggregatePublicKey::from_public_key(&PublicKey::default());
 
-    let result = verifying_key.verify(message, &sig);
-    result.is_ok()
+    let public_keys_converted: Vec<PublicKey> = public_keys
+        .iter()
+        .filter_map(|key| PublicKey::from_bytes(key).ok())
+        .collect();
+
+    public_keys_converted
+        .iter()
+        .for_each(|pk| aggregate_pk.add_public_key(pk, true).unwrap());
+
+    let aggregate_sig: Signature =
+        AggregateSignature::from_signature(&Signature::from_bytes(aggregated_signature).unwrap())
+            .to_signature();
+
+    let verify_response = aggregate_sig.verify(
+        true,
+        message,
+        BLS_DST_VALUE,
+        &[],
+        &aggregate_pk.to_public_key(),
+        true,
+    );
+
+    matches!(verify_response, BLST_ERROR::BLST_SUCCESS)
 }
