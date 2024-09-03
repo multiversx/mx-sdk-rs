2659d239976b42ec540d261665bec4fb56f89d09
Mihai Calin Luca <mihaicalin.luca10@gmail.com>
2024-09-03 16:47:56 +0200

    added more tests for managed decimal var

--- contracts/feature-tests/basic-features/wasm/src/lib.rs
+++ contracts/feature-tests/basic-features/wasm/src/lib.rs
@@ -5,9 +5,9 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
-// Endpoints:                          403
+// Endpoints:                          408
 // Async Callback:                       1
-// Total number of exported functions: 405
+// Total number of exported functions: 410
 
 #![no_std]
 
@@ -421,6 +421,11 @@ multiversx_sc_wasm_adapter::endpoints! {
         managed_decimal_into_raw_units => managed_decimal_into_raw_units
         managed_decimal_ln => managed_decimal_ln
         managed_decimal_log2 => managed_decimal_log2
+        managed_decimal_addition_var => managed_decimal_addition_var
+        managed_decimal_subtraction_var => managed_decimal_subtraction_var
+        managed_decimal_eq_var => managed_decimal_eq_var
+        managed_decimal_ln_var => managed_decimal_ln_var
+        managed_decimal_log2_var => managed_decimal_log2_var
     )
 }
 
