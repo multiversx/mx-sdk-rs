af928ec2d7cd2f4820a9ad29030a77a78de809f3
BiancaIalangi <bianca.ialangi@multiversx.com>
2024-10-01 23:15:11 +0300

    chain sim - fix tx to ESDTSystemSCAddress

--- sdk/core/src/gateway/gateway_chain_simulator.rs
+++ sdk/core/src/gateway/gateway_chain_simulator.rs
@@ -5,9 +5,11 @@ use anyhow::{anyhow, Error};
 use serde::{Deserialize, Serialize};
 
 const SEND_USER_FUNDS_ENDPOINT: &str = "transaction/send-user-funds";
-const GENERATE_BLOCKS_FUNDS_ENDPOINT: &str = "simulator/generate-blocks";
+const GENERATE_BLOCKS_ENDPOINT: &str = "simulator/generate-blocks";
 const GENERATE_BLOCKS_UNTIL_TX_PROCESSED_ENDPOINT: &str =
     "simulator/generate-blocks-until-transaction-processed";
+const GENERATE_BLOCKS_UNTIL_EPOCH_REACHED_ENDPOINT: &str =
+    "simulator/generate-blocks-until-epoch-reached";
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct GenerateBlocksResponse {
@@ -45,9 +47,32 @@ impl GatewayProxy {
             return Ok(String::from("no-simulator"));
         }
 
-        let url_gen_blocks: String =
-            format!("{}/{}", GENERATE_BLOCKS_FUNDS_ENDPOINT, number_blocks);
-        let endpoint_blocks = self.get_endpoint(&url_gen_blocks);
+        let uri_gen_blocks: String = format!("{}/{}", GENERATE_BLOCKS_ENDPOINT, number_blocks);
+        let endpoint_blocks = self.get_endpoint(&uri_gen_blocks);
+        let resp = self
+            .client
+            .post(endpoint_blocks)
+            .send()
+            .await?
+            .json::<GenerateBlocksResponse>()
+            .await?;
+
+        match resp.code.as_str() {
+            "successful" => Ok(resp.code),
+            _ => Err(anyhow!("{}", resp.error)),
+        }
+    }
+
+    pub async fn generate_blocks_until_epoch(&self, epoch_number: u64) -> Result<String, Error> {
+        if !self.chain_simulator {
+            return Ok(String::from("no-simulator"));
+        }
+
+        let uri_gen_blocks_until_reached_epoch: String = format!(
+            "{}/{}",
+            GENERATE_BLOCKS_UNTIL_EPOCH_REACHED_ENDPOINT, epoch_number
+        );
+        let endpoint_blocks = self.get_endpoint(&uri_gen_blocks_until_reached_epoch);
         let resp = self
             .client
             .post(endpoint_blocks)
