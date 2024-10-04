af7764588df0476a3f164a58ddce5c864e4d45a1
Alin Cruceat <alyn_509@yahoo.com>
2024-10-04 13:21:06 +0300

    changes from mx-contracts-rs

--- contracts/core/price-aggregator/src/lib.rs
+++ contracts/core/price-aggregator/src/lib.rs
@@ -390,9 +390,11 @@ pub trait PriceAggregator:
     #[only_owner]
     #[endpoint(setPairDecimals)]
     fn set_pair_decimals(&self, from: ManagedBuffer, to: ManagedBuffer, decimals: u8) {
-        self.require_paused();
-
-        self.pair_decimals(&from, &to).set(Some(decimals));
+        let pair_decimals_mapper = self.pair_decimals(&from, &to);
+        if !pair_decimals_mapper.is_empty() {
+            self.require_paused();
+        }
+        pair_decimals_mapper.set(Some(decimals));
         let pair = TokenPair { from, to };
         self.clear_submissions(&pair);
     }
