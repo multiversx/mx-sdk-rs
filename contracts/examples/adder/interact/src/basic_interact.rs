4370ca7ef50d76b822dbc87792c0315985286566
BiancaIalangi <bianca.ialangi@multiversx.com>
2024-10-11 19:57:21 +0300

    intearctor - fix ExpectError

--- contracts/examples/adder/interact/src/basic_interact.rs
+++ contracts/examples/adder/interact/src/basic_interact.rs
@@ -276,13 +276,12 @@ impl AdderInteract {
     }
 }
 
-#[cfg(feature = "chain_simulator")]
 #[tokio::test]
 async fn simulator_upgrade_test() {
     let mut basic_interact = AdderInteract::init().await;
-    // let wallet_address = basic_interact.wallet_address.clone();
+    let wallet_address = basic_interact.wallet_address.clone();
     let adder_owner_address = basic_interact.adder_owner_address.clone();
-    // let error_not_owner = (4, "upgrade is allowed only for owner");
+    let error_not_owner = (4, "upgrade is allowed only for owner");
 
     basic_interact.deploy().await;
     basic_interact.add(1u32).await;
@@ -297,10 +296,10 @@ async fn simulator_upgrade_test() {
     // Sum will be the updated value of 7
     basic_interact.print_sum().await;
 
-    // basic_interact
-    //     .upgrade(10u32, &wallet_address, Some(error_not_owner))
-    //     .await;
+    basic_interact
+        .upgrade(10u32, &wallet_address, Some(error_not_owner))
+        .await;
 
-    // // Sum will remain 7
-    // basic_interact.print_sum().await;
+    // Sum will remain 7
+    basic_interact.print_sum().await;
 }
