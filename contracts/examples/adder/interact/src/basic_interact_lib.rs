a35096d284e7c4d2ba823050153dcacd66e99fa8
Andrei Marinica <andrei.marinica@multiversx.com>
2024-10-14 23:25:41 +0300

    chain sim - adder interactor integration test

---
+++ contracts/examples/adder/interact/src/basic_interact_lib.rs
@@ -0,0 +1,7 @@
+pub mod basic_interact_cli;
+pub mod basic_interact_config;
+pub mod basic_interact_state;
+pub mod basic_interact;
+
+pub use basic_interact::{AdderInteract, adder_cli};
+pub use basic_interact_config::Config;
