858d88605bacb35221d42ae6ad9803fc36a499bf
Andrei Marinica <andrei.marinica@multiversx.com>
2024-10-21 02:32:10 +0300

    chain sim - get_network_config_test

--- sdk/http/src/lib.rs
+++ sdk/http/src/lib.rs
@@ -7,6 +7,7 @@ pub use multiversx_sdk as core;
 pub const MAINNET_GATEWAY: &str = "https://gateway.multiversx.com";
 pub const TESTNET_GATEWAY: &str = "https://testnet-gateway.multiversx.com";
 pub const DEVNET_GATEWAY: &str = "https://devnet-gateway.multiversx.com";
+pub const CHAIN_SIMULATOR_GATEWAY: &str = "http://localhost:8085";
 
 // MetachainShardId will be used to identify a shard ID as metachain
 pub const METACHAIN_SHARD_ID: u32 = 0xFFFFFFFF;
