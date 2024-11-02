f7e908818474ec1ca3500a468797a9ad27bd5e86
Alin Cruceat <alyn_509@yahoo.com>
2024-11-02 20:10:49 +0200

    cleanup unused paths

--- tools/rust-debugger/format-tests/src/init_internal_dict.py
+++ tools/rust-debugger/format-tests/src/init_internal_dict.py
@@ -2,7 +2,3 @@ import sys
 
 def __lldb_init_module(debugger, internal_dict):
     internal_dict['adapter_settings'] = {'sourceLanguages': ['rust'], 'evaluateForHovers': True, 'commandCompletions': True, 'displayFormat': 'auto', 'showDisassembly': 'auto', 'dereferencePointers': True, 'evaluationTimeout': 5.0, 'suppressMissingSourceFiles': True, 'consoleMode': 'commands', 'scriptConfig': {}, 'reproducer': False}
-    #sys.path.append('/home/alin/.vscode/extensions/vadimcn.vscode-lldb-1.11.0-linux-x64/adapter/scripts')
-    sys.path.append('/home/alin/.vscode/extensions/vadimcn.vscode-lldb-1.11.0-linux-x64/lldb/lib/python3.9/site-packages')
-    sys.path.append('/home/alin/.vscode/extensions/vadimcn.vscode-lldb-1.11.0-linux-x64/lldb/lib/python3.9/site-packages/setuptools-56.0.0-py3.9.egg')
-    sys.path.append('/home/alin/.vscode/extensions/vadimcn.vscode-lldb-1.11.0-linux-x64/lldb/lib/python3.9/site-packages/pip-21.0.1-py3.9.egg')
