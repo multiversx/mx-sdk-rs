65aa54992662ba42407e7f87a866e2b0a8682edd
Mihai Calin Luca <mihaicalin.luca10@gmail.com>
2024-11-21 08:43:35 +0100

    further cleanup of rustVM impl

--- chain/vm/src/tx_mock.rs
+++ chain/vm/src/tx_mock.rs
@@ -18,7 +18,6 @@ mod tx_managed_types;
 mod tx_panic;
 mod tx_result;
 mod tx_result_calls;
-mod tx_transfer_by_user_data;
 
 pub use blockchain_update::BlockchainUpdate;
 pub use tx_async_call_data::*;
@@ -37,7 +36,6 @@ pub use tx_managed_types::*;
 pub use tx_panic::*;
 pub use tx_result::*;
 pub use tx_result_calls::*;
-pub use tx_transfer_by_user_data::*;
 
 #[cfg(feature = "wasm-incompatible")]
 mod blockchain_rng;
