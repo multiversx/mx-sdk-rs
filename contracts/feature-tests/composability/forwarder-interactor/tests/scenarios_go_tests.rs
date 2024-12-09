bc71228b7946df0b4c7a4aca80230aecd8cdf491
Mihai Calin Luca <mihaicalin.luca10@gmail.com>
2024-12-09 18:04:23 +0100

    ignore tests (missing syntax), accounts fix

--- contracts/feature-tests/composability/forwarder-interactor/tests/scenarios_go_tests.rs
+++ contracts/feature-tests/composability/forwarder-interactor/tests/scenarios_go_tests.rs
@@ -10,21 +10,25 @@ fn deploy_go() {
 }
 
 #[test]
+#[ignore = "missing 'newTokenIdentifiers' syntax"]
 fn builtin_func_go() {
     world().run("scenarios/forwarder_builtin_scenario.scen.json");
 }
 
 #[test]
+#[ignore = "missing 'newTokenIdentifiers' syntax"]
 fn change_to_dynamic_go() {
     world().run("scenarios/forwarder_change_to_dynamic_scenario.scen.json");
 }
 
 #[test]
+#[ignore = "missing 'newTokenIdentifiers' syntax"]
 fn update_token_go() {
     world().run("scenarios/forwarder_update_token_scenario.scen.json");
 }
 
 #[test]
+#[ignore = "missing 'newTokenIdentifiers' syntax"]
 fn modify_creator_go() {
     world().run("scenarios/forwarder_modify_creator_scenario.scen.json");
 }
