cbb75bf1415c59438ee374db5137fa1aa4bf5abb
Andrei Marinica <andrei.marinica@multiversx.com>
2025-01-08 01:35:50 +0200

    call value - simplified egld handling

--- framework/base/src/api/managed_types/static_var_api_flags.rs
+++ framework/base/src/api/managed_types/static_var_api_flags.rs
@@ -4,12 +4,10 @@ bitflags! {
     #[derive(Clone, Copy, Debug, PartialEq, Eq)]
     pub struct StaticVarApiFlags: u8 {
         const NONE                                  = 0b00000000;
-        const CALL_VALUE_EGLD_SINGLE_INITIALIZED    = 0b00000001;
+        const CALL_VALUE_EGLD_DIRECT_INITIALIZED    = 0b00000001;
         const CALL_VALUE_ESDT_UNCHECKED_INITIALIZED = 0b00000010;
         const CALL_VALUE_ESDT_INITIALIZED           = 0b00000100;
-        const CALL_VALUE_EGLD_MULTI_INITIALIZED     = 0b00001000;
-        const CALL_VALUE_EGLD_FROM_ESDT_INITIALIZED = 0b00010000;
-        const CALL_VALUE_ALL_INITIALIZED            = 0b00100000;
+        const CALL_VALUE_ALL_INITIALIZED            = 0b00001000;
     }
 }
 
@@ -34,27 +32,27 @@ pub mod tests {
         assert!(current.check_and_set(StaticVarApiFlags::NONE));
         assert_eq!(current, StaticVarApiFlags::NONE);
 
-        assert!(!current.check_and_set(StaticVarApiFlags::CALL_VALUE_EGLD_SINGLE_INITIALIZED));
+        assert!(!current.check_and_set(StaticVarApiFlags::CALL_VALUE_EGLD_DIRECT_INITIALIZED));
         assert_eq!(
             current,
-            StaticVarApiFlags::CALL_VALUE_EGLD_SINGLE_INITIALIZED
+            StaticVarApiFlags::CALL_VALUE_EGLD_DIRECT_INITIALIZED
         );
-        assert!(current.check_and_set(StaticVarApiFlags::CALL_VALUE_EGLD_SINGLE_INITIALIZED));
+        assert!(current.check_and_set(StaticVarApiFlags::CALL_VALUE_EGLD_DIRECT_INITIALIZED));
         assert_eq!(
             current,
-            StaticVarApiFlags::CALL_VALUE_EGLD_SINGLE_INITIALIZED
+            StaticVarApiFlags::CALL_VALUE_EGLD_DIRECT_INITIALIZED
         );
 
         assert!(!current.check_and_set(StaticVarApiFlags::CALL_VALUE_ALL_INITIALIZED));
         assert_eq!(
             current,
-            StaticVarApiFlags::CALL_VALUE_EGLD_SINGLE_INITIALIZED
+            StaticVarApiFlags::CALL_VALUE_EGLD_DIRECT_INITIALIZED
                 | StaticVarApiFlags::CALL_VALUE_ALL_INITIALIZED
         );
         assert!(current.check_and_set(StaticVarApiFlags::CALL_VALUE_ALL_INITIALIZED));
         assert_eq!(
             current,
-            StaticVarApiFlags::CALL_VALUE_EGLD_SINGLE_INITIALIZED
+            StaticVarApiFlags::CALL_VALUE_EGLD_DIRECT_INITIALIZED
                 | StaticVarApiFlags::CALL_VALUE_ALL_INITIALIZED
         );
     }
--- framework/base/src/contract_base/wrappers/call_value_wrapper.rs
+++ framework/base/src/contract_base/wrappers/call_value_wrapper.rs
@@ -9,7 +9,7 @@ use crate::{
     },
     err_msg,
     types::{
-        big_num_cmp::bi_gt_zero, BigInt, BigUint, ConstDecimals, EgldOrEsdtTokenIdentifier,
+        big_num_cmp::bi_gt_zero, BigUint, ConstDecimals, EgldOrEsdtTokenIdentifier,
         EgldOrEsdtTokenPayment, EgldOrMultiEsdtPayment, EsdtTokenPayment, ManagedDecimal,
         ManagedRef, ManagedType, ManagedVec, ManagedVecItem, ManagedVecItemPayload,
         ManagedVecPayloadIterator, ManagedVecRef, TokenIdentifier,
@@ -47,38 +47,21 @@ where
         all_transfers_unchecked_handle
     }
 
-    /// Cached egld transfer searched for in the ESDT transfers from the VM.
-    fn egld_from_multi_esdt(&self) -> A::BigIntHandle {
-        let egld_from_multi_esdt_handle: A::BigIntHandle =
-            use_raw_handle(const_handles::CALL_VALUE_EGLD_FROM_ESDT);
+    fn egld_value_unchecked(&self) -> ManagedRef<'static, A, BigUint<A>> {
+        let call_value_handle: A::BigIntHandle = use_raw_handle(const_handles::CALL_VALUE_EGLD);
         if !A::static_var_api_impl()
-            .flag_is_set_or_update(StaticVarApiFlags::CALL_VALUE_EGLD_FROM_ESDT_INITIALIZED)
+            .flag_is_set_or_update(StaticVarApiFlags::CALL_VALUE_EGLD_DIRECT_INITIALIZED)
         {
-            let all_transfers_unchecked_handle = self.all_esdt_transfers_unchecked();
-            let egld_handle_result = find_egld_000000_transfer::<A>(all_transfers_unchecked_handle);
-            if let Some(found_egld_handle) = egld_handle_result {
-                BigInt::<A>::clone_to_handle(
-                    use_raw_handle(found_egld_handle),
-                    egld_from_multi_esdt_handle.clone(),
-                );
-            } else {
-                BigInt::<A>::set_value(egld_from_multi_esdt_handle.clone(), 0);
-            }
+            A::call_value_api_impl().load_egld_value(call_value_handle.clone());
         }
-        egld_from_multi_esdt_handle
+        unsafe { ManagedRef::wrap_handle(call_value_handle) }
     }
 
     /// Retrieves the EGLD call value from the VM.
     ///
     /// Will return 0 in case of an ESDT transfer, even though EGLD and ESDT transfers are now posible.
     pub fn egld_value(&self) -> ManagedRef<'static, A, BigUint<A>> {
-        let call_value_handle: A::BigIntHandle = use_raw_handle(const_handles::CALL_VALUE_EGLD);
-        if !A::static_var_api_impl()
-            .flag_is_set_or_update(StaticVarApiFlags::CALL_VALUE_EGLD_SINGLE_INITIALIZED)
-        {
-            A::call_value_api_impl().load_egld_value(call_value_handle.clone());
-        }
-        unsafe { ManagedRef::wrap_handle(call_value_handle) }
+        self.egld_value_unchecked()
     }
 
     /// Returns the EGLD call value from the VM as ManagedDecimal
@@ -88,43 +71,16 @@ where
         )
     }
 
-    /// The quantity of EGLD transfered, either via simple EGLD transfer, or via ESDT multi-transfer.
-    pub fn egld_value_multi(&self) -> ManagedRef<'static, A, BigUint<A>> {
-        let egld_value_multi_handle: A::BigIntHandle =
-            use_raw_handle(const_handles::CALL_VALUE_EGLD_MULTI);
-        if !A::static_var_api_impl()
-            .flag_is_set_or_update(StaticVarApiFlags::CALL_VALUE_EGLD_MULTI_INITIALIZED)
-        {
-            let egld_single = self.egld_value();
-            if bi_gt_zero::<A>(egld_single.get_handle()) {
-                BigInt::<A>::clone_to_handle(
-                    egld_single.get_handle(),
-                    egld_value_multi_handle.clone(),
-                );
-            } else {
-                let egld_from_multi_esdt_handle = self.egld_from_multi_esdt();
-                BigInt::<A>::clone_to_handle(
-                    egld_from_multi_esdt_handle,
-                    egld_value_multi_handle.clone(),
-                );
-            }
-        }
-        unsafe { ManagedRef::wrap_handle(egld_value_multi_handle) }
-    }
-
     /// Returns all ESDT transfers that accompany this SC call.
     /// Will return 0 results if nothing was transfered, or just EGLD.
     ///
     /// Will crash for EGLD + ESDT multi transfers.
     pub fn all_esdt_transfers(&self) -> ManagedRef<'static, A, ManagedVec<A, EsdtTokenPayment<A>>> {
         let multi_esdt_handle: A::ManagedBufferHandle = self.all_esdt_transfers_unchecked();
-        if !A::static_var_api_impl()
-            .flag_is_set_or_update(StaticVarApiFlags::CALL_VALUE_ESDT_INITIALIZED)
-        {
-            let egld_value_multi_handle = self.egld_from_multi_esdt();
-            if bi_gt_zero::<A>(egld_value_multi_handle) {
-                A::error_api_impl().signal_error(err_msg::ESDT_UNEXPECTED_EGLD.as_bytes())
-            }
+        let checked = A::static_var_api_impl()
+            .flag_is_set_or_update(StaticVarApiFlags::CALL_VALUE_ESDT_INITIALIZED);
+        if !checked && egld_000000_transfer_exists::<A>(multi_esdt_handle.clone()) {
+            A::error_api_impl().signal_error(err_msg::ESDT_UNEXPECTED_EGLD.as_bytes())
         }
 
         unsafe { ManagedRef::wrap_handle(multi_esdt_handle) }
@@ -144,7 +100,7 @@ where
         if !A::static_var_api_impl()
             .flag_is_set_or_update(StaticVarApiFlags::CALL_VALUE_ALL_INITIALIZED)
         {
-            let egld_single = self.egld_value();
+            let egld_single = self.egld_value_unchecked();
             if bi_gt_zero::<A>(egld_single.get_handle()) {
                 A::managed_type_impl().mb_overwrite(
                     use_raw_handle(const_handles::MBUF_EGLD_000000),
@@ -246,7 +202,7 @@ where
             0 => EgldOrEsdtTokenPayment {
                 token_identifier: EgldOrEsdtTokenIdentifier::egld(),
                 token_nonce: 0,
-                amount: self.egld_value().clone_value(),
+                amount: self.egld_value_unchecked().clone_value(),
             },
             1 => esdt_transfers.get(0).clone(),
             _ => A::error_api_impl().signal_error(err_msg::INCORRECT_NUM_ESDT_TRANSFERS.as_bytes()),
@@ -276,14 +232,14 @@ where
     pub fn any_payment(&self) -> EgldOrMultiEsdtPayment<A> {
         let esdt_transfers = self.all_esdt_transfers();
         if esdt_transfers.is_empty() {
-            EgldOrMultiEsdtPayment::Egld(self.egld_value().clone_value())
+            EgldOrMultiEsdtPayment::Egld(self.egld_value_unchecked().clone_value())
         } else {
             EgldOrMultiEsdtPayment::MultiEsdt(esdt_transfers.clone_value())
         }
     }
 }
 
-fn find_egld_000000_transfer<A>(transfers_vec_handle: A::ManagedBufferHandle) -> Option<RawHandle>
+fn egld_000000_transfer_exists<A>(transfers_vec_handle: A::ManagedBufferHandle) -> bool
 where
     A: CallValueApi + ErrorApi + ManagedTypeApi,
 {
@@ -297,19 +253,12 @@ where
             <EsdtTokenPayment<A> as ManagedVecItem>::PAYLOAD,
         > = ManagedVecPayloadIterator::new(transfers_vec_handle);
 
-        if iter.remaining_count() <= 1 {
-            // EGLD is not allowed in single transfers
-            return None;
-        }
-
-        let egld_payload = iter.find(|payload| {
+        iter.any(|payload| {
             let token_identifier_handle = RawHandle::read_from_payload(payload.slice_unchecked(0));
             A::managed_type_impl().mb_eq(
                 use_raw_handle(const_handles::MBUF_EGLD_000000),
                 use_raw_handle(token_identifier_handle),
             )
-        });
-
-        egld_payload.map(|payload| RawHandle::read_from_payload(payload.slice_unchecked(12)))
+        })
     }
 }
