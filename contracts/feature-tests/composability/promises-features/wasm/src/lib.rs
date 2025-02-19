6cbac234e9ee7bd9c05364acce8f253cac0b89f1
Mihai Calin Luca <mihaicalin.luca10@gmail.com>
2025-02-19 02:55:01 +0100

    blackbox tests

--- contracts/feature-tests/composability/promises-features/wasm/src/lib.rs
+++ contracts/feature-tests/composability/promises-features/wasm/src/lib.rs
@@ -5,10 +5,10 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
-// Endpoints:                           12
+// Endpoints:                           13
 // Async Callback (empty):               1
 // Promise callbacks:                    4
-// Total number of exported functions:  18
+// Total number of exported functions:  19
 
 #![no_std]
 
@@ -29,6 +29,7 @@ multiversx_sc_wasm_adapter::endpoints! {
         promise_raw_single_token => promise_raw_single_token
         promise_raw_multi_transfer => promise_raw_multi_transfer
         forward_sync_retrieve_funds_bt => forward_sync_retrieve_funds_bt
+        forward_sync_retrieve_funds_bt_reset_twice => forward_sync_retrieve_funds_bt_reset_twice
         forward_sync_retrieve_funds_bt_twice => forward_sync_retrieve_funds_bt_twice
         forward_promise_retrieve_funds_back_transfers => forward_promise_retrieve_funds_back_transfers
         retrieve_funds_callback => retrieve_funds_callback
