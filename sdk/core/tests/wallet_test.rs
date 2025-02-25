dc7f901d3448342424a7f8fcd815121ae02f88e3
Alin Cruceat <alyn_509@yahoo.com>
2025-02-25 14:29:51 +0200

    add address as byte array in test for confirmation

--- sdk/core/tests/wallet_test.rs
+++ sdk/core/tests/wallet_test.rs
@@ -62,19 +62,19 @@ fn test_load_from_pem() {
 }
 #[test]
 fn test_get_shard() {
-    let alice = test_wallets::alice();
+    let alice = test_wallets::alice(); // [1, 57, 71, 46, 255, 104, 134, 119, 26, 152, 47, 48, 131, 218, 93, 66, 31, 36, 194, 145, 129, 230, 56, 136, 34, 141, 200, 28, 166, 13, 105, 225]
     assert_eq!(0, alice.get_shard());
 
-    let bob = test_wallets::bob();
+    let bob = test_wallets::bob(); // [128, 73, 214, 57, 229, 166, 152, 13, 28, 210, 57, 42, 188, 206, 65, 2, 156, 218, 116, 161, 86, 53, 35, 162, 2, 240, 150, 65, 204, 38, 24, 248]
     assert_eq!(2, bob.get_shard());
 
-    let carol = test_wallets::carol();
+    let carol = test_wallets::carol(); // [178, 161, 21, 85, 206, 82, 30, 73, 68, 224, 154, 177, 117, 73, 216, 91, 72, 125, 205, 38, 200, 75, 80, 23, 163, 158, 49, 163, 103, 8, 137, 186]
     assert_eq!(0, carol.get_shard());
 
-    let heidi = test_wallets::heidi();
+    let heidi = test_wallets::heidi(); // [110, 34, 65, 24, 217, 6, 138, 230, 38, 135, 138, 28, 251, 235, 203, 106, 149, 164, 113, 93, 184, 109, 27, 81, 224, 106, 4, 34, 108, 243, 15, 214]
     assert_eq!(1, heidi.get_shard());
 
-    let mike = test_wallets::mike();
+    let mike = test_wallets::mike(); // [227, 42, 254, 220, 144, 79, 225, 147, 151, 70, 173, 151, 59, 235, 56, 53, 99, 207, 99, 100, 43, 166, 105, 179, 4, 15, 155, 148, 40, 165, 237, 96]
     assert_eq!(0, mike.get_shard());
 }
 
