62fbc366ed0815d7aeabe3318ad2a376cfd07d05
BiancaIalangi <bianca.ialangi@multiversx.com>
2025-02-25 10:14:46 +0200

    typeabi bitflag - test

--- contracts/examples/empty/src/empty.rs
+++ contracts/examples/empty/src/empty.rs
@@ -1,7 +1,20 @@
 #![no_std]
 
-#[allow(unused_imports)]
-use multiversx_sc::imports::*;
+mod file;
+
+use bitflags::bitflags;
+use multiversx_sc::derive::type_abi;
+
+bitflags! {
+    #[type_abi]
+    #[derive(Clone)]
+    pub struct Permission: u32 {
+        const NONE = 0;
+        const OWNER = 1;
+        const ADMIN = 2;
+        const PAUSE = 4;
+    }
+}
 
 /// An empty contract. To be used as a template when starting a new contract from scratch.
 #[multiversx_sc::contract]
