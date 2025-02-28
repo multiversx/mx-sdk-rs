786c17e8c20da5b82638ce4a6f0174cc05655393
Mihai Calin Luca <mihaicalin.luca10@gmail.com>
2025-02-28 14:50:45 +0100

    read from address

--- contracts/feature-tests/basic-features/src/storage_mapper_single_locked.rs
+++ contracts/feature-tests/basic-features/src/storage_mapper_single_locked.rs
@@ -10,6 +10,12 @@ pub trait SingleValueMapperLockedFeatures {
     #[storage_mapper("timelock_mapper_with_key")]
     fn timelock_mapper_with_key(&self, extra_key: usize) -> TimelockMapper<ManagedBuffer>;
 
+    #[storage_mapper_from_address("timelock_mapper_from_address")]
+    fn timelock_mapper_from_address(
+        &self,
+        address: ManagedAddress,
+    ) -> TimelockMapper<BigUint, ManagedAddress>;
+
     #[endpoint]
     fn timelock_set_initial_value(&self, initial_value: BigUint) {
         self.timelock_mapper().set(initial_value);
@@ -36,6 +42,24 @@ pub trait SingleValueMapperLockedFeatures {
     #[endpoint]
     fn timelock_get_future_value(&self) -> BigUint {
         let timelock = self.timelock_mapper();
+        timelock.get_future_value()
+    }
+
+    #[endpoint]
+    fn timelock_get_current_value_at_address(&self, address: ManagedAddress) -> BigUint {
+        let timelock = self.timelock_mapper_from_address(address);
+        timelock.get()
+    }
+
+    #[endpoint]
+    fn timelock_get_unlock_timestamp_at_address(&self, address: ManagedAddress) -> u64 {
+        let timelock = self.timelock_mapper_from_address(address);
+        timelock.get_unlock_timestamp()
+    }
+
+    #[endpoint]
+    fn timelock_get_future_value_at_address(&self, address: ManagedAddress) -> BigUint {
+        let timelock = self.timelock_mapper_from_address(address);
         timelock.get_future_value()
     }
 }
