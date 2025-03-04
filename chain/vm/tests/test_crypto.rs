0fcb3614e8fe585621790b8f414d35cfbc775e66
Mihai Calin Luca <mihaicalin.luca10@gmail.com>
2025-03-05 00:45:05 +0100

    multi bls tests

--- chain/vm/tests/test_crypto.rs
+++ chain/vm/tests/test_crypto.rs
@@ -1,3 +1,4 @@
+use blst::min_pk::{AggregateSignature, Signature};
 use hex::FromHex;
 use multiversx_chain_vm::crypto_functions;
 
@@ -41,4 +42,80 @@ fn test_verify_ed25519_invalid_args() {
 
     let success = crypto_functions::verify_ed25519(&pub_bytes, &msg_bytes, &sig_bytes);
     assert!(!success);
+}
+
+#[test]
+fn test_verify_bls_aggregated_signature() {
+    let message = b"hello";
+
+    let pk1 = b"82eb2ddfa71f1673fbfbd17952838cbca3816d5e60bf5cdb220d8cad6cb800e2ed18bb747ef45b17c9b8cbc971c6b980";
+    let pk2 = b"a81795a7afa09274717a170d6ba42ab06b65b25c7887eca7be46dfddae4e5b1a249f104b15551a7a445cccac9b403926";
+    let pk3 = b"8bf9e68f8fc54d8cb808ba43f0ada562cafa3c07448ab038eff6f579f1e4c1d497a957f50f6eca2608f36c39d874cbea";
+
+    let pk1_bytes: Vec<u8> = FromHex::from_hex(pk1).unwrap();
+    let pk2_bytes: Vec<u8> = FromHex::from_hex(pk2).unwrap();
+    let pk3_bytes: Vec<u8> = FromHex::from_hex(pk3).unwrap();
+
+    let sig1 = b"979a97882bd59dd97d860c99f9c4295e7d63e3fede1823b942d31d71ea3707d8c179ab733d38f7497b53bfa1535fe5e202f2a1c6e4df1dbc97dbe315dccd51676dbef31af1fe60d4b11c304db61913dc1d39e929f80f2cd10b72cbc661235048";
+    let sig2 = b"a5a465bb34264faa5d695ad844cfbacc5514ef8cb50abdc874bbbe065e74fc2dcd69f22e6e3a25260c8a9b6c55d7ca6d0d42227a58385f7af440b85d447675bee433c88726c582ef66fba5357b60e6d980ce43e7c6a6929ed605dbe51d47c453";
+    let sig3 = b"9936262ea50315d00c2b082cae3a97315f8852a021de195cb256bf532d439f1965a65c9fa536693ecb7a238611cb9ac30926d4958a7852955ff56aba9f60d9bda7ba10a939bd8b370bc42b667ee2f4fe4bb671482abc93bcac11ec10c0230229";
+
+    let sig1_bytes: Vec<u8> = FromHex::from_hex(sig1).unwrap();
+    let sig2_bytes: Vec<u8> = FromHex::from_hex(sig2).unwrap();
+    let sig3_bytes: Vec<u8> = FromHex::from_hex(sig3).unwrap();
+
+    let sig1_converted = Signature::from_bytes(&sig1_bytes).unwrap();
+    let sig2_converted = Signature::from_bytes(&sig2_bytes).unwrap();
+    let sig3_converted = Signature::from_bytes(&sig3_bytes).unwrap();
+
+    let mut agg_sig = AggregateSignature::from_signature(&sig1_converted);
+    agg_sig.add_signature(&sig2_converted, true).unwrap();
+    agg_sig.add_signature(&sig3_converted, true).unwrap();
+    let final_agg_sig = agg_sig.to_signature();
+
+    let success = crypto_functions::verify_bls_aggregated_signature(
+        vec![pk1_bytes, pk2_bytes, pk3_bytes],
+        message,
+        &final_agg_sig.to_bytes(),
+    );
+
+    assert!(success)
+}
+
+#[test]
+fn test_verify_bls_aggregated_signature_bad_sig() {
+    let message = b"hello";
+
+    let pk1 = b"82eb2ddfa71f1673fbfbd17952838cbca3816d5e60bf5cdb220d8cad6cb800e2ed18bb747ef45b17c9b8cbc971c6b980";
+    let pk2 = b"a81795a7afa09274717a170d6ba42ab06b65b25c7887eca7be46dfddae4e5b1a249f104b15551a7a445cccac9b403926";
+    let pk3 = b"8bf9e68f8fc54d8cb808ba43f0ada562cafa3c07448ab038eff6f579f1e4c1d497a957f50f6eca2608f36c39d874cbea";
+
+    let pk1_bytes: Vec<u8> = FromHex::from_hex(pk1).unwrap();
+    let pk2_bytes: Vec<u8> = FromHex::from_hex(pk2).unwrap();
+    let pk3_bytes: Vec<u8> = FromHex::from_hex(pk3).unwrap();
+
+    let sig1 = b"979b87882bd59dd97d860c99f9c4295e7d63e3fede1823b942d31d71ea3707d8c179ab733d38f7497b53bfa1535fe5e202f2a1c6e4df1dbc97dbe315dccd51676dbef31af1fe60d4b11c304db61913dc1d39e929f80f2cd10b72cbc661235048";
+    let sig2 = b"a5a465bb34264faa5d695ad844cfbacc5514ef8cb50abdc874bbbe065e74fc2dcd69f22e6e3a25260c8a9b6c55d7ca6d0d42227a58385f7af440b85d447675bee433c88726c582ef66fba5357b60e6d980ce43e7c6a6929ed605dbe51d47c453";
+    let sig3 = b"9936262ea50315d00c2b082cae3a97315f8852a021de195cb256bf532d439f1965a65c9fa536693ecb7a238611cb9ac30926d4958a7852955ff56aba9f60d9bda7ba10a939bd8b370bc42b667ee2f4fe4bb671482abc93bcac11ec10c0230229";
+
+    let sig1_bytes: Vec<u8> = FromHex::from_hex(sig1).unwrap();
+    let sig2_bytes: Vec<u8> = FromHex::from_hex(sig2).unwrap();
+    let sig3_bytes: Vec<u8> = FromHex::from_hex(sig3).unwrap();
+
+    let sig1_converted = Signature::from_bytes(&sig1_bytes).unwrap();
+    let sig2_converted = Signature::from_bytes(&sig2_bytes).unwrap();
+    let sig3_converted = Signature::from_bytes(&sig3_bytes).unwrap();
+
+    let mut agg_sig = AggregateSignature::from_signature(&sig1_converted);
+    agg_sig.add_signature(&sig2_converted, true).unwrap();
+    agg_sig.add_signature(&sig3_converted, true).unwrap();
+    let final_agg_sig = agg_sig.to_signature();
+
+    let success = crypto_functions::verify_bls_aggregated_signature(
+        vec![pk1_bytes, pk2_bytes, pk3_bytes],
+        message,
+        &final_agg_sig.to_bytes(),
+    );
+
+    assert!(!success)
 }
