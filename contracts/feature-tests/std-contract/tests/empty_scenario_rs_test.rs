b24b22d0d2fdef8b949b47fb8a7ede5ca0313f23
Mihai Calin Luca <mihaicalin.luca10@gmail.com>
2025-04-08 00:55:29 +0200

    test fix

--- contracts/feature-tests/std-contract/tests/empty_scenario_rs_test.rs
+++ contracts/feature-tests/std-contract/tests/empty_scenario_rs_test.rs
@@ -3,7 +3,7 @@ use multiversx_sc_scenario::*;
 fn world() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
 
-    blockchain.set_current_dir_from_workspace("contracts/examples/feature-tests/std-contract");
+    blockchain.set_current_dir_from_workspace("contracts/feature-tests/std-contract");
     blockchain.register_contract(
         "mxsc:output/std-contract.mxsc.json",
         std_contract::ContractBuilder,
