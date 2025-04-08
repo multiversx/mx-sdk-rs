6a8f5387d667bf1b2241f36846c45543dd247371
Mihai Calin Luca <mihaicalin.luca10@gmail.com>
2025-04-08 23:39:42 +0200

    full schedule and sections

--- chain/vm/src/schedule/gas_schedule.rs
+++ chain/vm/src/schedule/gas_schedule.rs
@@ -1,13 +1,42 @@
 use multiversx_chain_vm_executor::OpcodeCost;
 use serde::{Deserialize, Serialize};
 
-use super::gas_schedule_version::GasScheduleVersion;
+use super::{
+    gas_schedule_version::GasScheduleVersion,
+    sections::{
+        BaseOperationCost, BaseOpsAPICost, BigFloatAPICost, BigIntAPICost, BuiltInCost,
+        CryptoAPICost, DynamicStorageLoad, EthAPICost, ManagedBufferAPICost, MaxPerTransaction,
+        MetaChainSystemSCsCost,
+    },
+};
 
 #[derive(Serialize, Deserialize, Clone, Debug, Default)]
 #[serde(default)]
 pub struct GasSchedule {
+    #[serde(rename = "BuiltInCost")]
+    pub builtin_cost: BuiltInCost,
+    #[serde(rename = "MetaChainSystemSCsCost")]
+    pub metachain_system_scs_cost: MetaChainSystemSCsCost,
+    #[serde(rename = "BaseOperationCost")]
+    pub base_operation_cost: BaseOperationCost,
+    #[serde(rename = "BaseOpsAPICost")]
+    pub base_ops_api_cost: BaseOpsAPICost,
+    #[serde(rename = "EthAPICost")]
+    pub eth_api_cost: EthAPICost,
+    #[serde(rename = "BigIntAPICost")]
+    pub big_int_api_cost: BigIntAPICost,
+    #[serde(rename = "CryptoAPICost")]
+    pub crypto_api_cost: CryptoAPICost,
+    #[serde(rename = "ManagedBufferAPICost")]
+    pub managed_buffer_api_cost: ManagedBufferAPICost,
+    #[serde(rename = "BigFloatAPICost")]
+    pub big_float_api_cost: BigFloatAPICost,
     #[serde(rename = "WASMOpcodeCost")]
     pub wasm_opcode_cost: OpcodeCost,
+    #[serde(rename = "MaxPerTransaction")]
+    pub max_per_transaction: MaxPerTransaction,
+    #[serde(rename = "DynamicStorageLoad")]
+    pub dynamic_storage_load: DynamicStorageLoad,
 }
 
 impl GasSchedule {
--- chain/vm/tests/gas_schedule_deserialization.rs
+++ chain/vm/tests/gas_schedule_deserialization.rs
@@ -2,9 +2,17 @@ use multiversx_chain_vm::schedule::{GasSchedule, GasScheduleVersion};
 
 #[test]
 fn new_struct_with_gas_schedule_test() {
-    let gas_schedule = GasSchedule::new(GasScheduleVersion::V8);
+    let gas_schedule_v8 = GasSchedule::new(GasScheduleVersion::V8);
 
-    assert_eq!(gas_schedule.wasm_opcode_cost.opcode_unreachable, 5);
-    assert_eq!(gas_schedule.wasm_opcode_cost.opcode_nop, 5);
-    assert_eq!(gas_schedule.wasm_opcode_cost.opcode_block, 5);
+    assert_eq!(gas_schedule_v8.wasm_opcode_cost.opcode_unreachable, 5);
+    assert_eq!(gas_schedule_v8.wasm_opcode_cost.opcode_nop, 5);
+    assert_eq!(gas_schedule_v8.wasm_opcode_cost.opcode_block, 5);
+
+    let _gas_schedule_v1 = GasSchedule::new(GasScheduleVersion::V1);
+    let _gas_schedule_v2 = GasSchedule::new(GasScheduleVersion::V2);
+    let _gas_schedule_v3 = GasSchedule::new(GasScheduleVersion::V3);
+    let _gas_schedule_v4 = GasSchedule::new(GasScheduleVersion::V4);
+    let _gas_schedule_v5 = GasSchedule::new(GasScheduleVersion::V5);
+    let _gas_schedule_v6 = GasSchedule::new(GasScheduleVersion::V6);
+    let _gas_schedule_v7 = GasSchedule::new(GasScheduleVersion::V7);
 }
