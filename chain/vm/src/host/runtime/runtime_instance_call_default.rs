360b2d009c1b630b34c95b3184419999148b829e
Andrei Marinica <andrei.marinica@multiversx.com>
2025-04-14 19:28:36 +0300

    vm - refactor

--- chain/vm/src/host/runtime/runtime_instance_call_default.rs
+++ chain/vm/src/host/runtime/runtime_instance_call_default.rs
@@ -15,37 +15,41 @@ pub struct RuntimeInstanceCallLambdaDefault;
 
 impl RuntimeInstanceCallLambda for RuntimeInstanceCallLambdaDefault {
     fn call(self, instance_call: RuntimeInstanceCall<'_>) {
-        if !instance_call.instance.has_function(instance_call.func_name) {
-            *instance_call.tx_context_ref.result_lock() = TxResult::from_function_not_found();
-            return;
-        }
-
-        let result = instance_call.instance.call(instance_call.func_name);
-        let mut tx_result_ref = instance_call.tx_context_ref.result_lock();
-        if let Err(err) = result {
-            let breakpoint = instance_call
-                .instance
-                .get_breakpoint_value()
-                .expect("error retrieving instance breakpoint value");
-            if let Some(error_tx_result) = breakpoint_error_result(breakpoint, err) {
-                *tx_result_ref = error_tx_result;
-            }
-        }
-
-        if tx_result_ref.result_status.is_success() {
-            let gas_used = instance_call
-                .instance
-                .get_points_used()
-                .expect("error retrieving gas used");
-            tx_result_ref.gas_used = GasUsed::SomeGas(gas_used);
-        } else {
-            tx_result_ref.gas_used =
-                GasUsed::AllGas(instance_call.tx_context_ref.tx_input_box.gas_limit);
-        }
+        default_instance_call(instance_call);
     }
 
     fn override_function_name(&self) -> Option<TxFunctionName> {
         None
+    }
+}
+
+fn default_instance_call(instance_call: RuntimeInstanceCall<'_>) {
+    if !instance_call.instance.has_function(instance_call.func_name) {
+        *instance_call.tx_context_ref.result_lock() = TxResult::from_function_not_found();
+        return;
+    }
+
+    let result = instance_call.instance.call(instance_call.func_name);
+    let mut tx_result_ref = instance_call.tx_context_ref.result_lock();
+    if let Err(err) = result {
+        let breakpoint = instance_call
+            .instance
+            .get_breakpoint_value()
+            .expect("error retrieving instance breakpoint value");
+        if let Some(error_tx_result) = breakpoint_error_result(breakpoint, err) {
+            *tx_result_ref = error_tx_result;
+        }
+    }
+
+    if tx_result_ref.result_status.is_success() {
+        let gas_used = instance_call
+            .instance
+            .get_points_used()
+            .expect("error retrieving gas used");
+        tx_result_ref.gas_used = GasUsed::SomeGas(gas_used);
+    } else {
+        tx_result_ref.gas_used =
+            GasUsed::AllGas(instance_call.tx_context_ref.tx_input_box.gas_limit);
     }
 }
 
