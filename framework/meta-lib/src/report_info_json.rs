158edba568110ba4aea2108cabee18bfbc8e142e
BiancaIalangi <bianca.ialangi@multiversx.com>
2025-04-23 14:43:57 +0300

    detect opcode - add forbidden_opcodes to report json

--- framework/meta-lib/src/report_info_json.rs
+++ framework/meta-lib/src/report_info_json.rs
@@ -17,6 +17,10 @@ pub struct ReportInfoJson {
     pub ei_check: Option<EiCheckJson>,
 
     #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    pub forbidden_opcodes: Vec<String>,
+
+    #[serde(default)]
     pub code_report: CodeReportJson,
 }
 
@@ -32,6 +36,11 @@ impl ReportInfoJson {
             imports: report.imports.iter().map(|i| i.to_string()).collect(),
             is_mem_grow: report.memory_grow_flag,
             ei_check,
+            forbidden_opcodes: report
+                .forbidden_opcodes
+                .iter()
+                .map(|i| i.to_string())
+                .collect(),
             code_report: CodeReportJson::new(&report.code, size),
         }
     }
