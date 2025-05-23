28abe80c9f8d93ec0e6f50c524648ecf430d0cc8
BiancaIalangi <bianca.ialangi@multiversx.com>
2025-05-23 15:05:52 +0300

    back-transfer - cleanup

--- contracts/feature-tests/basic-features/src/token_identifier_features.rs
+++ contracts/feature-tests/basic-features/src/token_identifier_features.rs
@@ -16,11 +16,4 @@ pub trait TokenIdentifierFeatures {
     fn token_identifier_is_valid_2(&self, bytes: ManagedBuffer) -> bool {
         TokenIdentifier::from(bytes).is_valid_esdt_identifier()
     }
-
-    fn handle_back_transfers_if_any(
-        &self,
-        opt_back_transfers: Option<BackTransfers<Self::Api>>,
-    ) -> Option<BackTransfers<Self::Api>> {
-        opt_back_transfers.map(|back_transfers| back_transfers.clone())
-    }
 }
