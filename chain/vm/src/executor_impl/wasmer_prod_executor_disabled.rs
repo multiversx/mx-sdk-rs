25da28b8e267324578b79de42fdbd280d7885bc2
Andrei Marinica <andrei.marinica@multiversx.com>
2025-05-27 01:14:32 +0300

    vm - wasmer prod dependency inversion, removed from vm

--- chain/vm/src/executor_impl/wasmer_prod_executor_disabled.rs
+++
@@ -1,7 +0,0 @@
-use multiversx_chain_vm_executor::Executor;
-
-use crate::host::runtime::RuntimeWeakRef;
-
-pub fn new_prod_executor(_runtime_ref: RuntimeWeakRef) -> Box<dyn Executor + Send + Sync> {
-    panic!("ExperimentalExecutor not available, need to activate features = [\"wasmer-experimental\"] in multiversx-sc-scenario or multiversx-chain-vm")
-}
