89521b736291a40e16e0707d40bb8c2bd024aa87
Andrei Marinica <andrei.marinica@multiversx.com>
2025-06-16 13:47:44 +0300

    barnard - more sync call fallible tests

--- contracts/feature-tests/composability/forwarder-barnard/src/forwarder_barnard.rs
+++ contracts/feature-tests/composability/forwarder-barnard/src/forwarder_barnard.rs
@@ -1,5 +1,7 @@
 #![no_std]
 
+pub mod vault_proxy;
+
 multiversx_sc::imports!();
 multiversx_sc::derive_imports!();
 
@@ -28,6 +30,60 @@ pub trait ForwarderBarnard {
         match result {
             Ok(success) => {
                 self.sync_call_fallible_success(success);
+            },
+            Err(error_code) => {
+                self.sync_call_fallible_error(error_code);
+            },
+        }
+    }
+
+    #[endpoint]
+    fn forward_sync_fallible_accept_funds_multi_transfer(
+        &self,
+        to: ManagedAddress,
+        payment_args: MultiValueEncoded<MultiValue3<EgldOrEsdtTokenIdentifier, u64, BigUint>>,
+    ) -> bool {
+        let result = self
+            .tx()
+            .to(&to)
+            .typed(vault_proxy::VaultProxy)
+            .accept_funds()
+            .payment(payment_args.convert_payment_multi_triples())
+            .returns(ReturnsHandledOrError::new())
+            .sync_call_fallible();
+
+        self.log_result(result);
+
+        result.is_ok()
+    }
+
+    #[endpoint]
+    fn forward_sync_reject_funds_multi_transfer(
+        &self,
+        to: ManagedAddress,
+        payment_args: MultiValueEncoded<MultiValue3<EgldOrEsdtTokenIdentifier, u64, BigUint>>,
+    ) -> bool {
+        let half_gas = self.blockchain().get_gas_left() / 2;
+
+        let result = self
+            .tx()
+            .to(&to)
+            .gas(half_gas)
+            .typed(vault_proxy::VaultProxy)
+            .reject_funds()
+            .payment(payment_args.convert_payment_multi_triples())
+            .returns(ReturnsHandledOrError::new())
+            .sync_call_fallible();
+
+        self.log_result(result);
+
+        result.is_ok()
+    }
+
+    fn log_result(&self, result: Result<(), u32>) {
+        match result {
+            Ok(()) => {
+                self.sync_call_fallible_success(ManagedVec::new());
             },
             Err(error_code) => {
                 self.sync_call_fallible_error(error_code);
