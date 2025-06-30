012e33fba310c2fb5451b48eb461ea9c32b78e04
BiancaIalangi <bianca.ialangi@multiversx.com>
2025-06-30 16:27:45 +0300

    cargo fmt

--- sdk/core/src/lib.rs
+++ sdk/core/src/lib.rs
@@ -4,8 +4,8 @@ pub mod gateway;
 pub mod retrieve_tx_on_network;
 pub mod test_wallets;
 pub mod utils;
-pub mod wallet;
 pub mod validator;
+pub mod wallet;
 
 pub use multiversx_chain_core as chain_core;
 pub use retrieve_tx_on_network::retrieve_tx_on_network;
