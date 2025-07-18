297d69d22b1d871ba5be2ca2747769b06191c18d
Andrei Marinica <andrei.marinica@multiversx.com>
2025-07-18 12:11:45 +0300

    sc-meta EI delta moved from tests to lib

--- framework/meta-lib/src/ei.rs
+++ framework/meta-lib/src/ei.rs
@@ -4,6 +4,7 @@ mod ei_1_2;
 mod ei_1_3;
 mod ei_1_4;
 mod ei_1_5;
+mod ei_delta;
 mod ei_version;
 
 pub use ei_1_0::EI_1_0_NAMES;
@@ -12,4 +13,5 @@ pub use ei_1_2::EI_1_2_NAMES;
 pub use ei_1_3::EI_1_3_NAMES;
 pub use ei_1_4::EI_1_4_NAMES;
 pub use ei_1_5::EI_1_5_NAMES;
+pub use ei_delta::*;
 pub use ei_version::{parse_check_ei, EIVersion};
--- tools/wat-gen/src/wat_gen_main.rs
+++ tools/wat-gen/src/wat_gen_main.rs
@@ -1,25 +1,7 @@
 mod wat_gen_single_import;
 
 fn main() {
-    for &hook_name in EI_1_5_ADDED_NAMES {
+    for &hook_name in multiversx_sc_meta_lib::ei::EI_1_5_ADDED_NAMES {
         wat_gen_single_import::write_sc_files(hook_name);
     }
 }
-
-pub const EI_1_5_ADDED_NAMES: &[&str] = &[
-    "getBlockTimestampMs",
-    "getPrevBlockTimestampMs",
-    "getBlockRoundTimeMs",
-    "epochStartBlockTimestampMs",
-    "epochStartBlockNonce",
-    "epochStartBlockRound",
-    "managedGetAllTransfersCallValue",
-    "managedGetESDTTokenType",
-    "managedExecuteOnDestContextWithErrorReturn",
-    "managedMultiTransferESDTNFTExecuteWithReturn",
-    "managedGetCodeHash",
-    "mBufferToSmallIntUnsigned",
-    "mBufferToSmallIntSigned",
-    "mBufferFromSmallIntUnsigned",
-    "mBufferFromSmallIntSigned",
-];
