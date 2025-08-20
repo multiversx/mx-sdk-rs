c91ff3317a7876ef83c837c3b1da403710e8f430
BiancaIalangi <bianca.ialangi@multiversx.com>
2025-08-20 12:04:20 +0300

    verify bls signature

--- chain/vm/src/crypto_functions.rs
+++ chain/vm/src/crypto_functions.rs
@@ -1,7 +1,10 @@
 use blst::{
-    min_pk::{AggregatePublicKey, PublicKey, Signature},
-    BLST_ERROR,
+    blst_fp, blst_fp2, blst_fp_from_lendian, blst_p1, blst_p1_affine, blst_p1_to_affine, blst_p2,
+    blst_p2_affine, blst_p2_to_affine,
+    min_sig::{AggregatePublicKey, PublicKey, Signature},
+    Pairing, BLST_ERROR,
 };
+use mcl_rust::{init, CurveType, G1, G2};
 use sha2::Sha256;
 use sha3::{Digest, Keccak256};
 
@@ -49,15 +52,77 @@ pub fn verify_ed25519(key: &[u8], message: &[u8], signature: &[u8]) -> bool {
 }
 
 pub fn verify_bls(key: &[u8], message: &[u8], signature: &[u8]) -> bool {
-    let public_key = PublicKey::from_bytes(key)
-        .unwrap_or_else(|e| panic!("Failed to deserialize public key: {key:?}. Error: {e:?}"));
+    init(CurveType::BLS12_381);
 
-    let sig = Signature::from_bytes(signature)
-        .unwrap_or_else(|e| panic!("Failed to deserialize signature: {signature:?}. Error: {e:?}"));
+    let public_key = create_bls_public_key(key);
+    let signature = create_bls_signature(signature);
+    let aug_msg = [BLS_DST_VALUE, message].concat();
 
-    let verify_response = sig.verify(true, message, BLS_DST_VALUE, &[], &public_key, true);
+    let mut pairing = Pairing::new(true, BLS_DST_VALUE);
+    let err = pairing.aggregate(&public_key, true, &signature, true, &aug_msg, &[]);
 
-    matches!(verify_response, BLST_ERROR::BLST_SUCCESS)
+    if err == blst::BLST_ERROR::BLST_SUCCESS {
+        return true;
+    }
+
+    false
+}
+
+fn create_bls_public_key(key: &[u8]) -> blst_p2_affine {
+    let mut point_g2 = G2::from_str("1 352701069587466618187139116011060144890029952792775240219908644239793785735715026873347600343865175952761926303160 3059144344244213709971259814753781636986470325476647558659373206291635324768958432433509563104347017837885763365758 1985150602287291935568054521177171638300868978215655730859378665066344726373823718423869104263333984641494340347905 927553665492332455747201965776037880757740193453592970025027978793976877002675564980949289727957565575433344219582", 10).expect("failed to create");
+    point_g2.deserialize(key);
+
+    let pk_x = mcl_g2_coord_to_blst(point_g2.x.serialize());
+    let pk_y = mcl_g2_coord_to_blst(point_g2.y.serialize());
+    let pk_z = mcl_g2_coord_to_blst(point_g2.z.serialize());
+
+    let pk_jacobian = blst_p2 {
+        x: pk_x,
+        y: pk_y,
+        z: pk_z,
+    };
+
+    let mut key = blst_p2_affine::default();
+    unsafe { blst_p2_to_affine(&mut key, &pk_jacobian) };
+
+    key
+}
+
+fn create_bls_signature(signature: &[u8]) -> blst_p1_affine {
+    let mut point_g1 = G1::from_str("1 3685416753713387016781088315183077757961620795782546409894578378688607592378376318836054947676345821548104185464507 1339506544944476473020471379941921221584933875938349620426543736416511423956333506472724655353366534992391756441569",10).expect("failed to create");
+    point_g1.deserialize(signature);
+
+    let sig_x = mcl_g1_coord_to_blst(point_g1.x.serialize());
+    let sig_y = mcl_g1_coord_to_blst(point_g1.y.serialize());
+    let sig_z = mcl_g1_coord_to_blst(point_g1.z.serialize());
+
+    let sig_jacobian = blst_p1 {
+        x: sig_x,
+        y: sig_y,
+        z: sig_z,
+    };
+
+    let mut signature = blst_p1_affine::default();
+    unsafe { blst_p1_to_affine(&mut signature, &sig_jacobian) };
+
+    signature
+}
+
+fn mcl_g2_coord_to_blst(mcl_g2_coord: Vec<u8>) -> blst_fp2 {
+    let mut out = blst_fp2::default();
+    unsafe {
+        blst_fp_from_lendian(&mut out.fp[0], mcl_g2_coord[0..48].as_ptr()); // real part
+        blst_fp_from_lendian(&mut out.fp[1], mcl_g2_coord[48..96].as_ptr()); // imag part
+    }
+    out
+}
+
+fn mcl_g1_coord_to_blst(mcl_g1_coord: Vec<u8>) -> blst_fp {
+    let mut out = blst_fp::default();
+    unsafe {
+        blst_fp_from_lendian(&mut out, mcl_g1_coord.as_ptr());
+    }
+    out
 }
 
 pub fn verify_bls_aggregated_signature(
