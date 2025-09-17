477bd5223d356ce2ec827999e540e4d468a33b66
Andrei Marinica <andrei.marinica@multiversx.com>
2025-09-17 15:53:55 +0300

    wat-gen crate vm hook signatures

--- tools/wat-gen/src/wat_gen_main.rs
+++ tools/wat-gen/src/wat_gen_main.rs
@@ -1,7 +1,8 @@
 mod wat_gen_single_import;
 
 fn main() {
+    let signature_map = multiversx_sc_meta_lib::ei::vm_hook_signature_map();
     for &hook_name in multiversx_sc_meta_lib::ei::EI_1_5_ADDED_NAMES {
-        wat_gen_single_import::write_sc_files(hook_name);
+        wat_gen_single_import::write_sc_files(hook_name, &signature_map);
     }
 }
--- tools/wat-gen/src/wat_gen_single_import.rs
+++ tools/wat-gen/src/wat_gen_single_import.rs
@@ -1,13 +1,15 @@
-use std::fs;
+use std::{collections::HashMap, fs};
+
+use multiversx_sc_meta_lib::ei::VmHookSignature;
 
 pub const OUTPUT_WAT: &str = "single-import/wat";
 pub const OUTPUT_WASM: &str = "single-import/wasm";
 
-pub fn write_sc_files(hook_name: &str) {
+pub fn write_sc_files(hook_name: &str, signature_map: &HashMap<&'static str, VmHookSignature>) {
     fs::create_dir_all(OUTPUT_WAT).unwrap();
     fs::create_dir_all(OUTPUT_WASM).unwrap();
 
-    let sc_wat = generate_wat(hook_name);
+    let sc_wat = generate_wat(hook_name, signature_map);
     fs::write(
         format!("{OUTPUT_WAT}/single_import_{hook_name}.wat"),
         &sc_wat,
@@ -21,12 +23,19 @@ pub fn write_sc_files(hook_name: &str) {
     .unwrap();
 }
 
-fn generate_wat(hook_name: &str) -> String {
+fn generate_wat(hook_name: &str, signature_map: &HashMap<&'static str, VmHookSignature>) -> String {
     let mut sc_wat = String::new();
     sc_wat.push_str(WAT_1);
-    let import_line = format!(r#"  (import "env" "{hook_name}" (func (type 1)))"#);
-    sc_wat.push_str(&import_line);
+
+    let hook_signature = signature_map
+        .get(hook_name)
+        .unwrap_or_else(|| panic!("unknown vm hook function: {hook_name}"));
+    sc_wat.push_str("  ");
+    sc_wat.push_str(&hook_signature.to_wat_func_type_declaration(" (;3;)"));
     sc_wat.push_str(WAT_2);
+    let import_line = format!(r#"  (import "env" "{hook_name}" (func (type 3)))"#);
+    sc_wat.push_str(&import_line);
+    sc_wat.push_str(WAT_3);
     sc_wat
 }
 
@@ -35,11 +44,13 @@ const WAT_1: &str = r#"
   (type (;0;) (func))
   (type (;1;) (func (result i32)))
   (type (;2;) (func (param i32 i32)))
+"#;
+const WAT_2: &str = r#"
   (import "env" "getNumArguments" (func (;0;) (type 1)))
   (import "env" "signalError" (func (;1;) (type 2)))
   (import "env" "checkNoPayment" (func (;2;) (type 0)))
 "#;
-const WAT_2: &str = r#"
+const WAT_3: &str = r#"
   (memory (;0;) 3)
   (global (;0;) i32 i32.const 131097)
   (global (;1;) i32 i32.const 131104)
