a36db0539dda1d778208e27d75a655433b076b93
Andrei Marinica <andrei.marinica@multiversx.com>
2025-10-10 19:28:18 +0300

    esdt_token_identifier.rs rename

--- framework/base/src/types/managed/wrapped/token.rs
+++ framework/base/src/types/managed/wrapped/token.rs
@@ -2,14 +2,14 @@ mod egld_or_esdt_token_identifier;
 mod egld_or_esdt_token_payment;
 mod egld_or_multi_esdt_payment;
 mod esdt_token_data;
+mod esdt_token_identifier;
 mod esdt_token_payment;
 mod multi_egld_or_esdt_token_payment;
-mod token_identifier;
 
 pub use egld_or_esdt_token_identifier::EgldOrEsdtTokenIdentifier;
 pub use egld_or_esdt_token_payment::{EgldOrEsdtTokenPayment, EgldOrEsdtTokenPaymentRefs};
 pub use egld_or_multi_esdt_payment::{EgldOrMultiEsdtPayment, EgldOrMultiEsdtPaymentRefs};
 pub use esdt_token_data::EsdtTokenData;
+pub use esdt_token_identifier::{EsdtTokenIdentifier, TokenIdentifier};
 pub use esdt_token_payment::{EsdtTokenPayment, EsdtTokenPaymentRefs, MultiEsdtPayment};
 pub use multi_egld_or_esdt_token_payment::MultiEgldOrEsdtPayment;
-pub use token_identifier::{EsdtTokenIdentifier, TokenIdentifier};
