a9d65ff912734af26d72c2f3a0f51151d5f12813
Alin Cruceat <alyn_509@yahoo.com>
2025-11-17 17:16:11 +0200

    scenario tests fix

--- contracts/examples/lottery-esdt/wasm/src/lib.rs
+++ contracts/examples/lottery-esdt/wasm/src/lib.rs
@@ -5,9 +5,9 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
-// Endpoints:                            6
+// Endpoints:                            7
 // Async Callback (empty):               1
-// Total number of exported functions:   8
+// Total number of exported functions:   9
 
 #![no_std]
 
@@ -24,6 +24,7 @@ multiversx_sc_wasm_adapter::endpoints! {
         getLotteryWhitelist => lottery_whitelist
         claim_rewards => claim_rewards
         buy_ticket => buy_ticket
+        start_lottery => start_lottery
     )
 }
 
