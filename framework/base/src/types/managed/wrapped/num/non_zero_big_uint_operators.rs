efdbe8cabf32313cc2696dfe234b032b949c0f41
Andrei Marinica <andrei.marinica@multiversx.com>
2025-11-25 16:45:04 +0200

    NonZeroBigUint - operator cleanup

--- framework/base/src/types/managed/wrapped/num/non_zero_big_uint_operators.rs
+++ framework/base/src/types/managed/wrapped/num/non_zero_big_uint_operators.rs
@@ -1,10 +1,13 @@
+
+
 use crate::{
     api::ManagedTypeApi,
     types::{BigUint, NonZeroBigUint},
 };
 use core::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
 
-macro_rules! nz_unchecked_binary_operator {
+
+macro_rules! nz_binary_operator {
     ($trait:ident, $method:ident, $wrap_method:ident) => {
         impl<M: ManagedTypeApi> $trait<NonZeroBigUint<M>> for NonZeroBigUint<M> {
             type Output = NonZeroBigUint<M>;
@@ -41,18 +44,122 @@ macro_rules! nz_unchecked_binary_operator {
     };
 }
 
-nz_unchecked_binary_operator! {Add, add, wrap_big_int_unchecked}
-nz_unchecked_binary_operator! {Sub, sub, wrap_big_int_assert_gt_zero}
-nz_unchecked_binary_operator! {Mul, mul, wrap_big_int_unchecked}
-nz_unchecked_binary_operator! {Div, div, wrap_big_int_assert_gt_zero}
+nz_binary_operator! {Add, add, wrap_big_int_unchecked}
+nz_binary_operator! {Sub, sub, wrap_big_int_assert_gt_zero}
+nz_binary_operator! {Mul, mul, wrap_big_int_unchecked}
+nz_binary_operator! {Div, div, wrap_big_int_assert_gt_zero}
 
-macro_rules! nz_unchecked_assign_operator {
-    ($trait:ident, $method:ident, $validate_method:ident) => {
+// assignment operators
+
+// AddAssign
+
+impl<M: ManagedTypeApi> AddAssign<NonZeroBigUint<M>> for NonZeroBigUint<M> {
+    #[inline]
+    fn add_assign(&mut self, other: NonZeroBigUint<M>) {
+        self.value.add_assign(other.into_big_int());
+        // no need to validate, NonZeroBigUint + NonZeroBigUint is always non-zero
+    }
+}
+
+impl<M: ManagedTypeApi> AddAssign<&NonZeroBigUint<M>> for NonZeroBigUint<M> {
+    #[inline]
+    fn add_assign(&mut self, other: &NonZeroBigUint<M>) {
+        self.value.add_assign(other.as_big_int());
+        // no need to validate, NonZeroBigUint + NonZeroBigUint is always non-zero
+    }
+}
+
+impl<M: ManagedTypeApi> AddAssign<BigUint<M>> for NonZeroBigUint<M> {
+    #[inline]
+    fn add_assign(&mut self, other: BigUint<M>) {
+        self.value.add_assign(other.into_big_int());
+        // no need to validate, NonZeroBigUint + BigUint is always non-zero
+    }
+}
+
+impl<M: ManagedTypeApi> AddAssign<&BigUint<M>> for NonZeroBigUint<M> {
+    #[inline]
+    fn add_assign(&mut self, other: &BigUint<M>) {
+        self.value.add_assign(other.as_big_int());
+        // no need to validate, NonZeroBigUint + BigUint is always non-zero
+    }
+}
+
+impl<M: ManagedTypeApi> AddAssign<u32> for NonZeroBigUint<M> {
+    fn add_assign(&mut self, other: u32) {
+        unsafe {
+            self.as_big_uint_mut().add_assign(other);
+        }
+        // no need to validate, NonZeroBigUint + u32 is always non-zero
+    }
+}
+
+impl<M: ManagedTypeApi> AddAssign<u64> for NonZeroBigUint<M> {
+    fn add_assign(&mut self, other: u64) {
+        unsafe {
+            self.as_big_uint_mut().add_assign(other);
+        }
+        // no need to validate, NonZeroBigUint + u64 is always non-zero
+    }
+}
+
+// MulAssign
+
+impl<M: ManagedTypeApi> MulAssign<NonZeroBigUint<M>> for NonZeroBigUint<M> {
+    #[inline]
+    fn mul_assign(&mut self, other: NonZeroBigUint<M>) {
+        self.value.mul_assign(other.into_big_int());
+        // no need to validate, NonZeroBigUint * NonZeroBigUint is always non-zero
+    }
+}
+impl<M: ManagedTypeApi> MulAssign<&NonZeroBigUint<M>> for NonZeroBigUint<M> {
+    #[inline]
+    fn mul_assign(&mut self, other: &NonZeroBigUint<M>) {
+        self.value.mul_assign(other.as_big_int());
+        // no need to validate, NonZeroBigUint * NonZeroBigUint is always non-zero
+    }
+}
+
+impl<M: ManagedTypeApi> MulAssign<BigUint<M>> for NonZeroBigUint<M> {
+    #[inline]
+    fn mul_assign(&mut self, other: BigUint<M>) {
+        self.value.mul_assign(other.into_big_int());
+        self.assume_valid_after_op(); // validation needed, as BigUint can be zero
+    }
+}
+
+impl<M: ManagedTypeApi> MulAssign<&BigUint<M>> for NonZeroBigUint<M> {
+    #[inline]
+    fn mul_assign(&mut self, other: &BigUint<M>) {
+        self.value.mul_assign(other.as_big_int());
+        self.assume_valid_after_op(); // validation needed, as BigUint can be zero
+    }
+}
+
+impl<M: ManagedTypeApi> MulAssign<u32> for NonZeroBigUint<M> {
+    fn mul_assign(&mut self, other: u32) {
+        unsafe {
+            self.as_big_uint_mut().mul_assign(other);
+        }
+        self.assume_valid_after_op(); // validation needed, as u32 can be zero
+    }
+}
+impl<M: ManagedTypeApi> MulAssign<u64> for NonZeroBigUint<M> {
+    fn mul_assign(&mut self, other: u64) {
+        unsafe {
+            self.as_big_uint_mut().mul_assign(other);
+        }
+        self.assume_valid_after_op(); // validation needed, as u64 can be zero
+    }
+}
+
+macro_rules! nz_checked_assign_operator {
+    ($trait:ident, $method:ident) => {
         impl<M: ManagedTypeApi> $trait<NonZeroBigUint<M>> for NonZeroBigUint<M> {
             #[inline]
             fn $method(&mut self, other: NonZeroBigUint<M>) {
                 self.value.$method(other.into_big_int());
-                self.$validate_method();
+                self.validate_after_op();
             }
         }
 
@@ -60,7 +167,7 @@ macro_rules! nz_unchecked_assign_operator {
             #[inline]
             fn $method(&mut self, other: &NonZeroBigUint<M>) {
                 self.value.$method(other.as_big_int());
-                self.$validate_method();
+                self.validate_after_op();
             }
         }
 
@@ -68,7 +175,7 @@ macro_rules! nz_unchecked_assign_operator {
             #[inline]
             fn $method(&mut self, other: BigUint<M>) {
                 self.value.$method(other.into_big_int());
-                self.$validate_method();
+                self.validate_after_op();
             }
         }
 
@@ -76,7 +183,7 @@ macro_rules! nz_unchecked_assign_operator {
             #[inline]
             fn $method(&mut self, other: &BigUint<M>) {
                 self.value.$method(other.as_big_int());
-                self.$validate_method();
+                self.validate_after_op();
             }
         }
 
@@ -85,7 +192,7 @@ macro_rules! nz_unchecked_assign_operator {
                 unsafe {
                     self.as_big_uint_mut().$method(other);
                 }
-                self.$validate_method();
+                self.validate_after_op();
             }
         }
 
@@ -94,13 +201,11 @@ macro_rules! nz_unchecked_assign_operator {
                 unsafe {
                     self.as_big_uint_mut().$method(other);
                 }
-                self.$validate_method();
+                self.validate_after_op();
             }
         }
     };
 }
 
-nz_unchecked_assign_operator! {AddAssign, add_assign, assume_valid_after_op}
-nz_unchecked_assign_operator! {SubAssign, sub_assign, validate_after_op}
-nz_unchecked_assign_operator! {MulAssign, mul_assign, assume_valid_after_op}
-nz_unchecked_assign_operator! {DivAssign, div_assign, validate_after_op}
+nz_checked_assign_operator! {SubAssign, sub_assign}
+nz_checked_assign_operator! {DivAssign, div_assign}
