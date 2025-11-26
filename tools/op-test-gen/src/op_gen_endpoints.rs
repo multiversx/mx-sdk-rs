66959335b6f750bce934296760326c5062bab875
Andrei Marinica <andrei.marinica@multiversx.com>
2025-11-26 11:57:42 +0200

    BigIntOperators generator (type enum)

--- tools/op-test-gen/src/op_gen_endpoints.rs
+++ tools/op-test-gen/src/op_gen_endpoints.rs
@@ -2,12 +2,45 @@ use std::fmt::Write;
 
 use crate::{OperatorGroup, OperatorInfo, OperatorList};
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum ValueType {
+    BigInt,
+    BigIntRef,
+    BigUint,
+    BigUintRef,
+    NonZeroBigUint,
+    NonZeroBigUintRef,
+    Usize,
+    U32,
+    U64,
+}
+
+impl ValueType {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            ValueType::BigInt => "BigInt",
+            ValueType::BigIntRef => "&BigInt",
+            ValueType::BigUint => "BigUint",
+            ValueType::BigUintRef => "&BigUint",
+            ValueType::NonZeroBigUint => "NonZeroBigUint",
+            ValueType::NonZeroBigUintRef => "&NonZeroBigUint",
+            ValueType::Usize => "usize",
+            ValueType::U32 => "u32",
+            ValueType::U64 => "u64",
+        }
+    }
+
+    pub fn is_signed(self) -> bool {
+        matches!(self, ValueType::BigInt | ValueType::BigIntRef)
+    }
+}
+
 pub struct BigNumOperatorTestEndpoint {
     pub fn_name: String,
     pub op_info: OperatorInfo,
-    pub a_type: String,
-    pub b_type: String,
-    pub return_type: String,
+    pub a_type: ValueType,
+    pub b_type: ValueType,
+    pub return_type: ValueType,
     pub body: String,
 }
 
@@ -15,9 +48,9 @@ impl BigNumOperatorTestEndpoint {
     pub fn new(
         fn_name: &str,
         op_info: &OperatorInfo,
-        a_type: &str,
-        b_type: &str,
-        return_type: &str,
+        a_type: ValueType,
+        b_type: ValueType,
+        return_type: ValueType,
     ) -> Self {
         let body = if op_info.assign {
             format!(
@@ -40,9 +73,9 @@ impl BigNumOperatorTestEndpoint {
         Self {
             fn_name: fn_name.to_string(),
             op_info: op_info.clone(),
-            a_type: a_type.to_string(),
-            b_type: b_type.to_string(),
-            return_type: return_type.to_string(),
+            a_type,
+            b_type,
+            return_type,
             body,
         }
     }
@@ -53,7 +86,11 @@ impl BigNumOperatorTestEndpoint {
             "
     #[endpoint]
     fn {}(&self, a: {}, b: {}) -> {} {{{}}}",
-            self.fn_name, self.a_type, self.b_type, self.return_type, self.body
+            self.fn_name,
+            self.a_type.as_str(),
+            self.b_type.as_str(),
+            self.return_type.as_str(),
+            self.body
         )
         .unwrap();
     }
@@ -67,16 +104,16 @@ pub fn create_endpoints_for_op(op: &OperatorInfo) -> Vec<BigNumOperatorTestEndpo
         endpoints.push(BigNumOperatorTestEndpoint::new(
             &format!("{}_big_int", op.name),
             op,
-            "BigInt",
-            "BigInt",
-            "BigInt",
+            ValueType::BigInt,
+            ValueType::BigInt,
+            ValueType::BigInt,
         ));
         endpoints.push(BigNumOperatorTestEndpoint::new(
             &format!("{}_big_int_ref", op.name),
             op,
-            "&BigInt",
-            "&BigInt",
-            "BigInt",
+            ValueType::BigIntRef,
+            ValueType::BigIntRef,
+            ValueType::BigInt,
         ));
     }
 
@@ -84,31 +121,31 @@ pub fn create_endpoints_for_op(op: &OperatorInfo) -> Vec<BigNumOperatorTestEndpo
         endpoints.push(BigNumOperatorTestEndpoint::new(
             &format!("{}_big_uint", op.name),
             op,
-            "BigUint",
-            "usize",
-            "BigUint",
+            ValueType::BigUint,
+            ValueType::Usize,
+            ValueType::BigUint,
         ));
         endpoints.push(BigNumOperatorTestEndpoint::new(
             &format!("{}_big_uint_ref", op.name),
             op,
-            "&BigUint",
-            "usize",
-            "BigUint",
+            ValueType::BigUintRef,
+            ValueType::Usize,
+            ValueType::BigUint,
         ));
     } else {
         endpoints.push(BigNumOperatorTestEndpoint::new(
             &format!("{}_big_uint", op.name),
             op,
-            "BigUint",
-            "BigUint",
-            "BigUint",
+            ValueType::BigUint,
+            ValueType::BigUint,
+            ValueType::BigUint,
         ));
         endpoints.push(BigNumOperatorTestEndpoint::new(
             &format!("{}_big_uint_ref", op.name),
             op,
-            "&BigUint",
-            "&BigUint",
-            "BigUint",
+            ValueType::BigUintRef,
+            ValueType::BigUintRef,
+            ValueType::BigUint,
         ));
     }
 
