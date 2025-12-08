f29ae33f6af63105a470086e2b64778ead11ade2
Andrei Marinica <andrei.marinica@multiversx.com>
2025-12-08 23:37:06 +0200

    TxPayment for Option<P> cleaned up

--- framework/base/src/types/interaction/tx_payment/tx_payment_payment_option.rs
+++ framework/base/src/types/interaction/tx_payment/tx_payment_payment_option.rs
@@ -1,13 +1,17 @@
 use crate::{
     contract_base::TransferExecuteFailed,
-    types::{BigUint, ManagedAddress, Payment, Ref, TxFrom, TxToSpecified},
+    types::{BigUint, ManagedAddress, TxFrom, TxToSpecified},
 };
 
 use super::{FullPaymentData, FunctionCall, TxEnv, TxPayment};
 
-impl<Env> TxPayment<Env> for Option<&Payment<Env::Api>>
+/// TxPayment should work for any Option, 
+/// where for Some(payment) it behaves like payment,
+/// and for None it behaves like no payment.
+impl<Env, P> TxPayment<Env> for Option<P>
 where
     Env: TxEnv,
+    P: TxPayment<Env>,
 {
     #[inline]
     fn is_no_payment(&self, _env: &Env) -> bool {
@@ -40,134 +44,6 @@ where
         } else {
             ().perform_transfer_execute_legacy(env, to, gas_limit, fc)
         }
-    }
-
-    fn with_normalized<From, To, F, R>(
-        self,
-        env: &Env,
-        from: &From,
-        to: To,
-        fc: FunctionCall<Env::Api>,
-        f: F,
-    ) -> R
-    where
-        From: TxFrom<Env>,
-        To: TxToSpecified<Env>,
-        F: FnOnce(&ManagedAddress<Env::Api>, &BigUint<Env::Api>, FunctionCall<Env::Api>) -> R,
-    {
-        if let Some(payment) = self {
-            payment.with_normalized(env, from, to, fc, f)
-        } else {
-            ().with_normalized(env, from, to, fc, f)
-        }
-    }
-
-    fn into_full_payment_data(self, env: &Env) -> FullPaymentData<Env::Api> {
-        if let Some(payment) = self {
-            payment.into_full_payment_data(env)
-        } else {
-            ().into_full_payment_data(env)
-        }
-    }
-}
-
-impl<Env> TxPayment<Env> for Option<Ref<'_, Payment<Env::Api>>>
-where
-    Env: TxEnv,
-{
-    #[inline]
-    fn is_no_payment(&self, _env: &Env) -> bool {
-        self.is_none()
-    }
-
-    fn perform_transfer_execute_fallible(
-        self,
-        env: &Env,
-        to: &ManagedAddress<Env::Api>,
-        gas_limit: u64,
-        fc: FunctionCall<Env::Api>,
-    ) -> Result<(), TransferExecuteFailed> {
-        if let Some(payment) = self {
-            payment.perform_transfer_execute_fallible(env, to, gas_limit, fc)
-        } else {
-            ().perform_transfer_execute_fallible(env, to, gas_limit, fc)
-        }
-    }
-
-    fn perform_transfer_execute_legacy(
-        self,
-        env: &Env,
-        to: &ManagedAddress<Env::Api>,
-        gas_limit: u64,
-        fc: FunctionCall<Env::Api>,
-    ) {
-        if let Some(payment) = self {
-            payment.perform_transfer_execute_legacy(env, to, gas_limit, fc)
-        } else {
-            ().perform_transfer_execute_legacy(env, to, gas_limit, fc)
-        }
-    }
-
-    fn with_normalized<From, To, F, R>(
-        self,
-        env: &Env,
-        from: &From,
-        to: To,
-        fc: FunctionCall<Env::Api>,
-        f: F,
-    ) -> R
-    where
-        From: TxFrom<Env>,
-        To: TxToSpecified<Env>,
-        F: FnOnce(&ManagedAddress<Env::Api>, &BigUint<Env::Api>, FunctionCall<Env::Api>) -> R,
-    {
-        if let Some(payment) = self {
-            payment.with_normalized(env, from, to, fc, f)
-        } else {
-            ().with_normalized(env, from, to, fc, f)
-        }
-    }
-
-    fn into_full_payment_data(self, env: &Env) -> FullPaymentData<Env::Api> {
-        if let Some(payment) = self {
-            payment.into_full_payment_data(env)
-        } else {
-            ().into_full_payment_data(env)
-        }
-    }
-}
-
-impl<Env> TxPayment<Env> for Option<Payment<Env::Api>>
-where
-    Env: TxEnv,
-{
-    #[inline]
-    fn is_no_payment(&self, env: &Env) -> bool {
-        self.as_ref().is_no_payment(env)
-    }
-
-    #[inline]
-    fn perform_transfer_execute_fallible(
-        self,
-        env: &Env,
-        to: &ManagedAddress<Env::Api>,
-        gas_limit: u64,
-        fc: FunctionCall<Env::Api>,
-    ) -> Result<(), TransferExecuteFailed> {
-        self.as_ref()
-            .perform_transfer_execute_fallible(env, to, gas_limit, fc)
-    }
-
-    #[inline]
-    fn perform_transfer_execute_legacy(
-        self,
-        env: &Env,
-        to: &ManagedAddress<Env::Api>,
-        gas_limit: u64,
-        fc: FunctionCall<Env::Api>,
-    ) {
-        self.as_ref()
-            .perform_transfer_execute_legacy(env, to, gas_limit, fc)
     }
 
     fn with_normalized<From, To, F, R>(
