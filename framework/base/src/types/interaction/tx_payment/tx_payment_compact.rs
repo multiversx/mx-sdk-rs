196c2d54bca49d6bda3db9391962c0dd7ab6b280
Andrei Marinica <andrei.marinica@multiversx.com>
2025-12-12 15:08:46 +0200

    TxPayment impl for Compact dedup

--- framework/base/src/types/interaction/tx_payment/tx_payment_compact.rs
+++ framework/base/src/types/interaction/tx_payment/tx_payment_compact.rs
@@ -1,18 +1,21 @@
-use core::ops::Deref;
-
 use crate::{
     contract_base::TransferExecuteFailed,
-    types::{BigUint, Compact, ManagedAddress, ManagedRef, PaymentVec, TxFrom, TxToSpecified},
+    types::{
+        BigUint, Compact, CompactPayment, ManagedAddress, PaymentVec, TxFrom,
+        TxToSpecified,
+    },
 };
 
 use super::{FullPaymentData, FunctionCall, TxEnv, TxPayment};
 
-impl<Env> TxPayment<Env> for Compact<&PaymentVec<Env::Api>>
+impl<Env, P> TxPayment<Env> for Compact<P>
 where
     Env: TxEnv,
+    P: CompactPayment + AsRef<PaymentVec<Env::Api>>,
 {
     fn is_no_payment(&self, _env: &Env) -> bool {
-        self.0.is_empty()
+        let pv = self.0.as_ref();
+        pv.is_empty()
     }
 
     fn perform_transfer_execute_fallible(
@@ -22,15 +25,13 @@ where
         gas_limit: u64,
         fc: FunctionCall<Env::Api>,
     ) -> Result<(), TransferExecuteFailed> {
-        match self.0.len() {
+        let pv = self.0.as_ref();
+        match pv.len() {
             0 => ().perform_transfer_execute_fallible(env, to, gas_limit, fc),
-            1 => self
-                .0
+            1 => pv
                 .get(0)
                 .perform_transfer_execute_fallible(env, to, gas_limit, fc),
-            _ => self
-                .0
-                .perform_transfer_execute_fallible(env, to, gas_limit, fc),
+            _ => pv.perform_transfer_execute_fallible(env, to, gas_limit, fc),
         }
     }
 
@@ -41,15 +42,13 @@ where
         gas_limit: u64,
         fc: FunctionCall<Env::Api>,
     ) {
-        match self.0.len() {
+        let pv = self.0.as_ref();
+        match pv.len() {
             0 => ().perform_transfer_execute_legacy(env, to, gas_limit, fc),
-            1 => self
-                .0
+            1 => pv
                 .get(0)
                 .perform_transfer_execute_legacy(env, to, gas_limit, fc),
-            _ => self
-                .0
-                .perform_transfer_execute_legacy(env, to, gas_limit, fc),
+            _ => pv.perform_transfer_execute_legacy(env, to, gas_limit, fc),
         }
     }
 
@@ -66,71 +65,20 @@ where
         To: TxToSpecified<Env>,
         F: FnOnce(&ManagedAddress<Env::Api>, &BigUint<Env::Api>, FunctionCall<Env::Api>) -> R,
     {
-        match self.0.len() {
+        let pv = self.0.as_ref();
+        match pv.len() {
             0 => ().with_normalized(env, from, to, fc, f),
-            1 => self.0.get(0).with_normalized(env, from, to, fc, f),
-            _ => self.0.with_normalized(env, from, to, fc, f),
+            1 => pv.get(0).with_normalized(env, from, to, fc, f),
+            _ => pv.with_normalized(env, from, to, fc, f),
         }
     }
 
     fn into_full_payment_data(self, env: &Env) -> FullPaymentData<Env::Api> {
-        match self.0.len() {
+        let pv = self.0.as_ref();
+        match pv.len() {
             0 => ().into_full_payment_data(env),
-            1 => self.0.get(0).into_full_payment_data(env),
-            _ => self.0.into_full_payment_data(env),
+            1 => pv.get(0).into_full_payment_data(env),
+            _ => pv.into_full_payment_data(env),
         }
-    }
-}
-
-impl<Env> TxPayment<Env> for Compact<ManagedRef<'_, Env::Api, PaymentVec<Env::Api>>>
-where
-    Env: TxEnv,
-{
-    #[inline]
-    fn is_no_payment(&self, _env: &Env) -> bool {
-        self.0.is_empty()
-    }
-
-    #[inline]
-    fn perform_transfer_execute_fallible(
-        self,
-        env: &Env,
-        to: &ManagedAddress<Env::Api>,
-        gas_limit: u64,
-        fc: FunctionCall<Env::Api>,
-    ) -> Result<(), TransferExecuteFailed> {
-        Compact(self.0.deref()).perform_transfer_execute_fallible(env, to, gas_limit, fc)
-    }
-
-    #[inline]
-    fn perform_transfer_execute_legacy(
-        self,
-        env: &Env,
-        to: &ManagedAddress<Env::Api>,
-        gas_limit: u64,
-        fc: FunctionCall<Env::Api>,
-    ) {
-        Compact(self.0.deref()).perform_transfer_execute_legacy(env, to, gas_limit, fc)
-    }
-
-    #[inline]
-    fn with_normalized<From, To, F, R>(
-        self,
-        env: &Env,
-        from: &From,
-        to: To,
-        fc: FunctionCall<Env::Api>,
-        f: F,
-    ) -> R
-    where
-        From: TxFrom<Env>,
-        To: TxToSpecified<Env>,
-        F: FnOnce(&ManagedAddress<Env::Api>, &BigUint<Env::Api>, FunctionCall<Env::Api>) -> R,
-    {
-        Compact(self.0.deref()).with_normalized(env, from, to, fc, f)
-    }
-
-    fn into_full_payment_data(self, env: &Env) -> FullPaymentData<Env::Api> {
-        Compact(self.0.deref()).into_full_payment_data(env)
     }
 }
