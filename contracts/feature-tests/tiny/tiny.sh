815d8f296b24d1233983d230a0d48eb0dd1ef247
Andrei Marinica <andrei.marinica@multiversx.com>
2026-01-15 21:24:03 +0200

    tiny.wat fix

--- contracts/feature-tests/tiny/tiny.sh
+++ contracts/feature-tests/tiny/tiny.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-wat2wasm tiny.wat                           
-stat -f%z tiny.wasm                         
-mx-scenario-go run scenarios/empty.scen.json
-wasm-opt -O3 tiny.wasm -o tiny.wasm                
+wat2wasm tiny.wat
 stat -f%z tiny.wasm
+mx-scenario-go run scenarios/tiny.scen.json
+wasm-opt -O3 tiny.wasm -o tiny.wasm
--- contracts/feature-tests/tiny/tiny.wat
+++ contracts/feature-tests/tiny/tiny.wat
@@ -1,5 +1,6 @@
 (module
   (type (;0;) (func))
+  (memory 1)
   (export "memory" (memory 0))
   (export "init" (func $nothing))
   (export "x" (func $nothing))
