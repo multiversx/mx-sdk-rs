4fc36f17b7064789cf78d1a6b44bb54d197c8471
Andrei Marinica <andrei.marinica@multiversx.com>
2026-03-17 14:56:08 +0200

    clippy fix

--- tools/managed-mem-bench/src/main.rs
+++ tools/managed-mem-bench/src/main.rs
@@ -137,15 +137,16 @@ fn main() {
 
     bench_type("BigFloat", || BigFloat::<StaticApi>::from_frac(1, 2));
 
-    bench_type("ManagedAddress", || ManagedAddress::<StaticApi>::zero());
+    bench_type("ManagedAddress", ManagedAddress::<StaticApi>::zero);
 
     bench_type("TokenIdentifier (EsdtTokenIdentifier)", || {
         TokenIdentifier::<StaticApi>::from("MYTOKEN-123456")
     });
 
-    bench_type("EgldOrEsdtTokenIdentifier (EGLD)", || {
-        EgldOrEsdtTokenIdentifier::<StaticApi>::egld()
-    });
+    bench_type(
+        "EgldOrEsdtTokenIdentifier (EGLD)",
+        EgldOrEsdtTokenIdentifier::<StaticApi>::egld,
+    );
 
     bench_type("EgldOrEsdtTokenIdentifier (ESDT)", || {
         EgldOrEsdtTokenIdentifier::<StaticApi>::esdt(TokenIdentifier::from("MYTOKEN-123456"))
