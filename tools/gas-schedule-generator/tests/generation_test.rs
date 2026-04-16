e38425d76af7d6147c251673408a42fce5bd6e58
Andrei Marinica <andrei.marinica@multiversx.com>
2026-04-16 08:11:48 +0300

    gas-schedule-generator comment update

--- tools/gas-schedule-generator/tests/generation_test.rs
+++ tools/gas-schedule-generator/tests/generation_test.rs
@@ -6,5 +6,8 @@ fn generation_test() {
     std::fs::write(GENERATED_SECTIONS_FILE, &generated).unwrap();
 
     let on_disk = std::fs::read_to_string(gas_schedule_generator::SECTIONS_FILE_PATH).unwrap();
-    assert_eq!(generated, on_disk);
+    assert_eq!(
+        generated, on_disk,
+        "Generated content does not match the content on disk. Please run `cargo run` in `tools/gas-schedule-generator` to regenerate the file."
+    );
 }
