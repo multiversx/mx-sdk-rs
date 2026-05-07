e12b892d87a66e52ffe369a56d6d2fdeac41ab88
Andrei Marinica <andrei.marinica@multiversx.com>
2026-05-07 19:13:12 +0300

    core crypto import fix

--- chain/core/src/std/crypto.rs
+++ chain/core/src/std/crypto.rs
@@ -1,9 +1,6 @@
 //! Standard crypto functions and constants used across many MultiversX crates.
 //!
-//! TODO: move here the rest of the function,s and the Wallet, with signing.
-
-use sha2::Sha256;
-use sha3::{Digest, Keccak256};
+//! TODO: move here the rest of the functions and the Wallet, with signing.
 
 pub const SHA256_RESULT_LEN: usize = 32;
 pub const KECCAK256_RESULT_LEN: usize = 32;
@@ -12,7 +9,8 @@ pub const KECCAK256_RESULT_LEN: usize = 32;
 ///
 /// Returns a 32-byte array containing the hash digest.
 pub fn sha256(data: &[u8]) -> [u8; SHA256_RESULT_LEN] {
-    let mut hasher = Sha256::new();
+    use sha2::Digest;
+    let mut hasher = sha2::Sha256::new();
     hasher.update(data);
     hasher.finalize().into()
 }
@@ -21,7 +19,8 @@ pub fn sha256(data: &[u8]) -> [u8; SHA256_RESULT_LEN] {
 ///
 /// Returns a 32-byte array containing the hash digest.
 pub fn keccak256(data: &[u8]) -> [u8; KECCAK256_RESULT_LEN] {
-    let mut hasher = Keccak256::new();
+    use sha3::Digest;
+    let mut hasher = sha3::Keccak256::new();
     hasher.update(data);
     hasher.finalize().into()
 }
