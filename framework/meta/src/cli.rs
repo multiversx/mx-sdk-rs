636fd42d865c66ac7f35ea35ba6c717e33a3ec8e
Andrei Marinica <andrei.marinica@multiversx.com>
2026-05-20 12:34:13 +0300

    sc-meta cli repro build args refactor

--- framework/meta/src/cli.rs
+++ framework/meta/src/cli.rs
@@ -1,5 +1,6 @@
-mod cli_args_standalone;
+pub mod cli_args_reproducible_builds;
 pub mod cli_args_sender;
+mod cli_args_standalone;
 pub mod cli_args_tx;
 mod cli_args_validate;
 mod cli_standalone_main;
