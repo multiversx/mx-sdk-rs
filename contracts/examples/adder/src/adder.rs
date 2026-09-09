c4b39bb74570fbb04e74e334ae0d785dbe6018f5
Andrei Marinica <andrei.marinica@multiversx.com>
2026-09-09 20:48:19 +0300

    adder abi cleanup

--- contracts/examples/adder/src/adder.rs
+++ contracts/examples/adder/src/adder.rs
@@ -2,9 +2,6 @@
 
 use multiversx_sc::imports::*;
 
-// pub mod adder_proxy;
-// pub mod adder_abi;
-
 /// One of the simplest smart contracts possible,
 /// it holds a single variable in storage, which anyone can increment.
 #[multiversx_sc::contract(call = AdderCall)]
