aa7c67edc4f712c2454e710e68cd803df6159d67
Andrei Marinica <andrei.marinica@multiversx.com>
2026-09-17 01:27:18 +0300

    ReturnsResultUnmanaged - revert SyncCallRawResult impl

--- framework/base/src/types/interaction/result_handlers/returns_result_unmanaged.rs
+++ framework/base/src/types/interaction/result_handlers/returns_result_unmanaged.rs
@@ -1,6 +1,9 @@
 use multiversx_sc_codec::TopDecodeMulti;
 
-use crate::types::{RHListItem, TxEnv};
+use crate::{
+    contract_base::SyncCallRawResult,
+    types::{RHListItem, RHListItemExec, TxEnv, interaction::decode_result},
+};
 
 use super::HasUnmanaged;
 
@@ -14,4 +17,16 @@ where
     Original::Unmanaged: TopDecodeMulti,
 {
     type Returns = Original::Unmanaged;
+}
+
+impl<Env, Original> RHListItemExec<SyncCallRawResult<Env::Api>, Env, Original>
+    for ReturnsResultUnmanaged
+where
+    Env: TxEnv,
+    Original: HasUnmanaged,
+    Original::Unmanaged: TopDecodeMulti,
+{
+    fn item_process_result(self, raw_result: &SyncCallRawResult<Env::Api>) -> Self::Returns {
+        decode_result::<Env::Api, Original::Unmanaged>(raw_result.0.clone())
+    }
 }
